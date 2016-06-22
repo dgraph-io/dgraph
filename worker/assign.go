a575f4cdfd2b2f7e8b093052c764506628caedcc
Pawan Rawal <pawan@dgraph.io>
2016-06-22 12:29:29 +0530

    Changing to DGraph Lab for copyright

--- worker/assign.go
+++ worker/assign.go
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Dgraph Labs, Inc.
+ * Copyright 2016 DGraph Labs, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
