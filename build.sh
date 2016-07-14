a03821b3f64f3f68488caf2741e89a8426a2bf12
Pawan Rawal <pawan@dgraph.io>
2016-07-14 09:42:20 +0530

    Incorporating feedback

--- build.sh
+++ build.sh
@@ -15,23 +15,29 @@ dgraph_cmd=$GOPATH/src/github.com/dgraph-io/dgraph/cmd;
 
 echo -e "\033[1;33mBuilding binaries\033[0m"
 echo "dgraph"
-cd $dgraph_cmd/dgraph && go build .;
+cd $dgraph_cmd/dgraph && go build -tags=embed .;
 echo "dgraphassigner"
-cd $dgraph_cmd/dgraphassigner && go build .;
+cd $dgraph_cmd/dgraphassigner && go build -tags=embed .;
 echo "dgraphloader"
-cd $dgraph_cmd/dgraphloader && go build .;
+cd $dgraph_cmd/dgraphloader && go build -tags=embed .;
 echo "dgraphlist"
-cd $dgraph_cmd/dgraphlist && go build .;
+cd $dgraph_cmd/dgraphlist && go build -tags=embed .;
 echo "dgraphmerge"
-cd $dgraph_cmd/dgraphmerge && go build .;
+cd $dgraph_cmd/dgraphmerge && go build -tags=embed .;
 
 echo -e "\n\033[1;33mCopying binaries to tmp folder\033[0m"
 cd $tmp_dir;
 cp $dgraph_cmd/dgraph/dgraph $dgraph_cmd/dgraphassigner/dgraphassigner $dgraph_cmd/dgraphlist/dgraphlist $dgraph_cmd/dgraphmerge/dgraphmerge $dgraph_cmd/dgraphloader/dgraphloader .;
+# Stripping the binaries if strip command is available.
+if type strip > /dev/null; then
+	strip dgraph dgraphassigner dgraphloader dgraphmerge dgraphlist
+fi
+echo -e "\n\033[1;34mSize of files after strip: $(du -sh)\033[0m"
 
 echo -e "\n\033[1;33mCreating tar file\033[0m"
 tar_file=dgraph-"$(uname | tr '[:upper:]' '[:lower:]')"-amd64-v$release_version
 tar -zcf $tar_file.tar.gz dgraph dgraphassigner dgraphlist dgraphmerge dgraphloader;
+echo -e "\n\033[1;34mSize of tar file: $(du -sh $tar_file.tar.gz)\033[0m"
 
 echo -e "\n\033[1;33mMoving tarfile to original directory\033[0m"
 mv $tar_file.tar.gz $cur_dir
