884b8e772c75f056187140f8bccd0a965f1a65df
Pawan Rawal <pawan@dgraph.io>
2016-07-14 09:51:24 +0530

    Adding a variable for build flags

--- build.sh
+++ build.sh
@@ -12,18 +12,19 @@ fi
 mkdir $tmp_dir;
 
 dgraph_cmd=$GOPATH/src/github.com/dgraph-io/dgraph/cmd;
+build_flags='-tags=embed'
 
 echo -e "\033[1;33mBuilding binaries\033[0m"
 echo "dgraph"
-cd $dgraph_cmd/dgraph && go build -tags=embed .;
+cd $dgraph_cmd/dgraph && go build $build_flags .;
 echo "dgraphassigner"
-cd $dgraph_cmd/dgraphassigner && go build -tags=embed .;
+cd $dgraph_cmd/dgraphassigner && go build $build_flags .;
 echo "dgraphloader"
-cd $dgraph_cmd/dgraphloader && go build -tags=embed .;
+cd $dgraph_cmd/dgraphloader && go build $build_flags .;
 echo "dgraphlist"
-cd $dgraph_cmd/dgraphlist && go build -tags=embed .;
+cd $dgraph_cmd/dgraphlist && go build $build_flags .;
 echo "dgraphmerge"
-cd $dgraph_cmd/dgraphmerge && go build -tags=embed .;
+cd $dgraph_cmd/dgraphmerge && go build $build_flags .;
 
 echo -e "\n\033[1;33mCopying binaries to tmp folder\033[0m"
 cd $tmp_dir;
