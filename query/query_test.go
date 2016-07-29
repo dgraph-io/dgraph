1882b0a11f543693e6df2e7e5de2c92b365bea6c
Ashwin Ramesh <ashwin@dgraph.io>
2016-07-29 09:10:37 +0000

    Add benchmark test for Unmarshal

--- query/query_test.go
+++ query/query_test.go
@@ -531,11 +531,15 @@ func benchmarkToPBMarshal(file string, b *testing.B) {
 	}
 }
 
-func BenchmarkToPBMarshal_10_Actor(b *testing.B) { benchmarkToPBMarshal("benchmark/actors10.bin", b) }
+func BenchmarkToPBMarshal_10_Actor(b *testing.B) {
+	benchmarkToPBMarshal("benchmark/actors10.bin", b)
+}
 func BenchmarkToPBMarshal_10_Director(b *testing.B) {
 	benchmarkToPBMarshal("benchmark/directors10.bin", b)
 }
-func BenchmarkToPBMarshal_100_Actor(b *testing.B) { benchmarkToPBMarshal("benchmark/actors100.bin", b) }
+func BenchmarkToPBMarshal_100_Actor(b *testing.B) {
+	benchmarkToPBMarshal("benchmark/actors100.bin", b)
+}
 func BenchmarkToPBMarshal_100_Director(b *testing.B) {
 	benchmarkToPBMarshal("benchmark/directors100.bin", b)
 }
@@ -544,4 +548,60 @@ func BenchmarkToPBMarshal_1000_Actor(b *testing.B) {
 }
 func BenchmarkToPBMarshal_1000_Director(b *testing.B) {
 	benchmarkToPBMarshal("benchmark/directors1000.bin", b)
+}
+
+func benchmarkToPBUnmarshal(file string, b *testing.B) {
+	b.ReportAllocs()
+	var sg SubGraph
+	var l Latency
+
+	f, err := ioutil.ReadFile(file)
+	if err != nil {
+		b.Error(err)
+	}
+
+	buf := bytes.NewBuffer(f)
+	dec := gob.NewDecoder(buf)
+	err = dec.Decode(&sg)
+	if err != nil {
+		b.Error(err)
+	}
+	p, err := sg.ToProtocolBuffer(&l)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	pbb, err := proto.Marshal(p)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	pdu := &graph.Node{}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		err = proto.Unmarshal(pbb, pdu)
+		if err != nil {
+			b.Fatal(err)
+		}
+
+	}
+}
+
+func BenchmarkToPBUnmarshal_10_Actor(b *testing.B) {
+	benchmarkToPBUnmarshal("benchmark/actors10.bin", b)
+}
+func BenchmarkToPBUnmarshal_10_Director(b *testing.B) {
+	benchmarkToPBUnmarshal("benchmark/directors10.bin", b)
+}
+func BenchmarkToPBUnmarshal_100_Actor(b *testing.B) {
+	benchmarkToPBUnmarshal("benchmark/actors100.bin", b)
+}
+func BenchmarkToPBUnmarshal_100_Director(b *testing.B) {
+	benchmarkToPBUnmarshal("benchmark/directors100.bin", b)
+}
+func BenchmarkToPBUnmarshal_1000_Actor(b *testing.B) {
+	benchmarkToPBUnmarshal("benchmark/actors1000.bin", b)
+}
+func BenchmarkToPBUnmarshal_1000_Director(b *testing.B) {
+	benchmarkToPBUnmarshal("benchmark/directors1000.bin", b)
 }
