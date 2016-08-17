15f4c2c94ff87486e6138c824a2ee64d3234bfe5
Pawan Rawal <pawan@dgraph.io>
2016-08-17 10:19:21 +0530

    some more echo statements

--- contrib/assign.sh
+++ contrib/assign.sh
@@ -17,14 +17,18 @@ export LD_LIBRARY_PATH="${ROCKSDBDIR}:${LD_LIBRARY_PATH}"
 
 set -e
 
+echo "before src" $(pwd)
 pushd $SRC &> /dev/null
+echo "after src" $SRC $(pwd)
 
 # Get git-lfs and benchmark data.
 wget https://github.com/github/git-lfs/releases/download/v1.3.1/git-lfs-linux-amd64-1.3.1.tar.gz
 tar -xzf git-lfs-linux-amd64-1.3.1.tar.gz
 pushd git-lfs-1.3.1 &> /dev/null
+echo "after git-lfs" $(pwd)
 sudo /bin/bash ./install.sh
 popd &> /dev/null
+echo "after pop git-lfs" $(pwd)
 
 git clone https://github.com/dgraph-io/benchmarks.git
 benchmark=$(pwd)/data
