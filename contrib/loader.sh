86cdf619d2a14c8b5eb9f1cdad607b73d863f158
Pawan Rawal <pawan@dgraph.io>
2016-08-17 12:15:22 +0530

    Add loader.sh

---
+++ contrib/loader.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SRC="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
+
+BUILD=$1
+# If build variable is empty then we set it.
+if [ -z "$1" ]; then
+  BUILD=$SRC/build
+fi
+
+ROCKSDBDIR=$BUILD/rocksdb-4.2
+
+set -e
+
+pushd $BUILD &> /dev/null
+benchmark=$(pwd)/benchmarks/data
+popd &> /dev/null
+
+# build flags needed for rocksdb
+export CGO_CFLAGS="-I${ROCKSDBDIR}/include"
+export CGO_LDFLAGS="-L${ROCKSDBDIR}"
+export LD_LIBRARY_PATH="${ROCKSDBDIR}:${LD_LIBRARY_PATH}"
+
+pushd cmd/dgraphloader &> /dev/null
+go build .
+mkdir -p ~/dgraph/p
+./dgraphloader --numInstances 1 --instanceIdx 0 --rdfgzips $benchmark/rdf-films.gz,$benchmark/names.gz --uids ~/dgraph/u --postings ~/dgraph/p
+popd &> /dev/null
