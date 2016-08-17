d6f363ebd21d373888ab342e12be2a3cf8721d35
Pawan Rawal <pawan@dgraph.io>
2016-08-17 11:05:46 +0530

    Checking if dirs exist for benchmark and git-lfs

--- contrib/assign.sh
+++ contrib/assign.sh
@@ -19,15 +19,20 @@ set -e
 
 pushd $BUILD &> /dev/null
 
-# Get git-lfs and benchmark data.
-wget https://github.com/github/git-lfs/releases/download/v1.3.1/git-lfs-linux-amd64-1.3.1.tar.gz
-tar -xzf git-lfs-linux-amd64-1.3.1.tar.gz
-pushd git-lfs-1.3.1 &> /dev/null
-sudo /bin/bash ./install.sh
-popd &> /dev/null
+gitlfsfile="git-lfs-1.3.1"
+if [ ! -d $gitlfsfile]; then
+  # Get git-lfs and benchmark data.
+  wget https://github.com/github/git-lfs/releases/download/v1.3.1/git-lfs-linux-amd64-1.3.1.tar.gz
+  tar -xzf git-lfs-linux-amd64-1.3.1.tar.gz
+  pushd git-lfs-1.3.1 &> /dev/null
+  sudo /bin/bash ./install.sh
+  popd &> /dev/null
+fi
 
-git clone https://github.com/dgraph-io/benchmarks.git
-benchmark=$(pwd)/benchmarks/data
+if [ ! -d "benchmarks"]; then
+  git clone https://github.com/dgraph-io/benchmarks.git
+  benchmark=$(pwd)/benchmarks/data
+fi
 popd &> /dev/null
 # We are back in the Dgraph repo.
 
