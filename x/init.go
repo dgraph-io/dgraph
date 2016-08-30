69ea976b59e1eda73efd050ea8d11988669a862d
Jay Chiu <jchiu2547@gmail.com>
2016-08-30 18:44:58 +0800

    Comment

--- x/init.go
+++ x/init.go
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// package x provides some common utilities used by the entire library.
+// Package x provides some common utilities used by the entire library.
 package x
 
 import (
