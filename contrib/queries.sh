786d72a181d3756654433627e70e26d4a8669728
Pawan Rawal <pawan@dgraph.io>
2016-09-13 07:54:33 +0530

    try and upload artifacts

--- contrib/queries.sh
+++ contrib/queries.sh
@@ -20,13 +20,13 @@ export LD_LIBRARY_PATH="${ROCKSDBDIR}:${LD_LIBRARY_PATH}"
 pushd cmd/dgraph &> /dev/null
 go build .
 # Start dgraph in the background.
-./dgraph --mutations ~/dgraph/m --postings ~/dgraph/p --uids ~/dgraph/u &
+./dgraph --mutations ~/dgraph/m --postings ~/dgraph/p --uids ~/dgraph/u --memprof dmem.prof --cpuprof dcpu.prof --shutdown true &
 
 pushd $BUILD/benchmarks/throughputtest &> /dev/null
 go build . && ./throughputtest --numsec 30 --ip "http://127.0.0.1:8080/query"  --numuser 1000
 popd &> /dev/null
-
-killall dgraph
+# shutdown Dgraph server.
+curl 127.0.0.1:8080/query -XPOST -d 'SHUTDOWN'
 # TODO - Have a way to upload cpu and memory profiles after adding the functionality
 # to stop the server from the client.
 
