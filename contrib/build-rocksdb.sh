9491eff1a65d8bd337775c50aa8b11d767ff1d2a
Jay Chiu <jchiu@dgraph.io>
2016-09-13 16:39:10 +0800

    Update

--- contrib/build-rocksdb.sh
+++ contrib/build-rocksdb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export ROCKSDBVER="4.6.1"
+export ROCKSDBVER="4.9.0"
 ROCKSDBURL="https://github.com/facebook/rocksdb/archive/v${ROCKSDBVER}.tar.gz"
 ROCKSDBFILE="rocksdb-${ROCKSDBVER}.tar.gz"
 ROCKSDBDIR=rocksdb-${ROCKSDBVER}
