2afac65c22c23bca6b4b7aba062c5b10c9ad445d
Kostub Deshmukh <kostub@dgraph.io>
2016-09-19 14:53:57 -0700

    Code review changes.

--- loader/loader.go
+++ loader/loader.go
@@ -21,7 +21,9 @@ import (
 	"bytes"
 	"context"
 	"flag"
+	"fmt"
 	"io"
+	"log"
 	"math/rand"
 	"runtime"
 	"strconv"
@@ -106,21 +108,25 @@ func (s *state) printCounters(ticker *time.Ticker) {
 	}
 }
 
-// Reads a single line from a buffered reader. This is the preferred
+// Reads a single line from a buffered reader. The line is read into the
+// passed in buffer to minimize allocations. This is the preferred
 // method for loading long lines which could be longer than the buffer
 // size of bufio.Scanner.
-func readLine(r *bufio.Reader) (string, error) {
+func readLine(r *bufio.Reader, buf *bytes.Buffer) error {
 	isPrefix := true
 	var err error
-	var buff bytes.Buffer
+	buf.Reset()
 	for isPrefix && err == nil {
 		var line []byte
+		// The returned line is an internal buffer in bufio and is only
+		// valid until the next call to ReadLine. It needs to be copied
+		// over to our own buffer.
 		line, isPrefix, err = r.ReadLine()
 		if err == nil {
-			buff.Write(line)
+			buf.Write(line)
 		}
 	}
-	return buff.String(), err
+	return err
 }
 
 // readLines reads the file and pushes the nquads onto a channel.
@@ -128,30 +134,35 @@ func readLine(r *bufio.Reader) (string, error) {
 func (s *state) readLines(r io.Reader) {
 	var buf []string
 	var err error
-	var str string
+	var strBuf bytes.Buffer
 	bufReader := bufio.NewReader(r)
 	// Randomize lines to avoid contention on same subject.
 	for i := 0; i < 1000; i++ {
-		str, err = readLine(bufReader)
-		if err == nil {
-			buf = append(buf, str)
-			atomic.AddUint64(&s.ctr.read, 1)
-		} else {
+		err = readLine(bufReader, &strBuf)
+		if err != nil {
 			break
 		}
+		buf = append(buf, strBuf.String())
+		atomic.AddUint64(&s.ctr.read, 1)
 	}
 	ln := len(buf)
-	for err == nil {
-		k := rand.Intn(ln)
-		s.input <- buf[k]
-		str, err = readLine(bufReader)
-		if err == nil {
-			buf[k] = str
+	if err == nil {
+		// If we haven't yet finished reading the file
+		// read the rest of the rows.
+		for {
+			err = readLine(bufReader, &strBuf)
+			if err != nil {
+				break
+			}
+			k := rand.Intn(ln)
+			s.input <- buf[k]
+			buf[k] = strBuf.String()
 			atomic.AddUint64(&s.ctr.read, 1)
 		}
 	}
 	if err != nil && err != io.EOF {
-		glog.WithError(err).Fatal("While reading file.")
+		err := fmt.Errorf("Error while reading file: %v", err)
+		log.Fatalf("%+v", err)
 	}
 	for i := 0; i < len(buf); i++ {
 		s.input <- buf[i]
