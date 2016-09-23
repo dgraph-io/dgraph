74aee25e5cbcf35bf353de0a1e2a537efbad24c4
Pawan Rawal <pawan@dgraph.io>
2016-09-23 08:15:05 +0530

    modify flags

--- contrib/queries.sh
+++ contrib/queries.sh
@@ -30,12 +30,12 @@ echo "done running throughput test"
 popd &> /dev/null
 
 # Write top x from memory and cpu profile to a file.
-go tool pprof --text dgraph dmem-"$TRAVIS_COMMIT".prof | head -25 > topmem.txt
-go tool pprof --svg dgraph dmem-"$TRAVIS_COMMIT".prof > topmem.svg
+go tool pprof -text dgraph dmem-"$TRAVIS_COMMIT".prof | head -25 > topmem.txt
+go tool pprof -svg dgraph dmem-"$TRAVIS_COMMIT".prof > topmem.svg
 
-go tool pprof --text --alloc_space dgraph dmem-"$TRAVIS_COMMIT".prof | head -25 > topmem-alloc.txt
-go tool pprof --svg --alloc_space dgraph dmem-"$TRAVIS_COMMIT".prof > topmem-alloc.svg
+go tool pprof -text --alloc_space dgraph dmem-"$TRAVIS_COMMIT".prof | head -25 > topmem-alloc.txt
+go tool pprof -svg --alloc_space dgraph dmem-"$TRAVIS_COMMIT".prof > topmem-alloc.svg
 
-go tool pprof --text dgraph dcpu-"$TRAVIS_COMMIT".prof | head -25 > topcpu.txt
-go tool pprof --svg dgraph dcpu-"$TRAVIS_COMMIT".prof > topcpu.svg
+go tool pprof -text dgraph dcpu-"$TRAVIS_COMMIT".prof | head -25 > topcpu.txt
+go tool pprof -svg dgraph dcpu-"$TRAVIS_COMMIT".prof > topcpu.svg
 
