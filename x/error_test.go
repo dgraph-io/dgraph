9c061d346a3429f34dd733342e6fda21c7b6d6bf
Jay Chiu <jchiu@dgraph.io>
2016-09-26 17:33:33 +0800

    Update

--- x/error_test.go
+++ x/error_test.go
@@ -2,19 +2,29 @@ package x
 
 import (
 	"flag"
+	"fmt"
 	"os"
-	"strings"
 	"testing"
 )
 
-func someTestFunc() error {
-	return Errorf("some nice error")
-}
+const originalError = `some nice error
+github.com/dgraph-io/dgraph/x.Errorf
+	/home/jchiu/go/src/github.com/dgraph-io/dgraph/x/error.go:90
+github.com/dgraph-io/dgraph/x.someTestFunc
+	/home/jchiu/go/src/github.com/dgraph-io/dgraph/x/error_test.go:12
+github.com/dgraph-io/dgraph/x.TestTraceError
+	/home/jchiu/go/src/github.com/dgraph-io/dgraph/x/error_test.go:16
+testing.tRunner
+	/usr/lib/go-1.7/src/testing/testing.go:610
+runtime.goexit
+	/usr/lib/go-1.7/src/runtime/asm_amd64.s:2086`
+
+const expectedError = `some nice error; x.Errorf (x/error.go:90) x.someTestFunc (x/error_test.go:12) x.TestTraceError (x/error_test.go:16) testing.tRunner (testing.go:610) runtime.goexit (asm_amd64.s:2086) `
 
 func TestTraceError(t *testing.T) {
-	s := shortenedErrorString(someTestFunc())
-	if !strings.HasPrefix(s, "some nice error; x.Errorf (x/error.go:90) x.someTestFunc (x/error_test.go:11)") {
-		t.Errorf("Error string has wrong prefix: %s", s)
+	s := shortenedErrorString(fmt.Errorf(originalError))
+	if s != expectedError {
+		t.Errorf("Error string is wrong: [%s]", s)
 		return
 	}
 }
