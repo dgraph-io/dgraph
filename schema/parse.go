720f88ac1c0899f927a1d6adabf4e136c01f4699
Ashwin Ramesh <ashwin@dgraph.io>
2016-10-04 16:45:18 +0530

    Add validation for empty object

--- schema/parse.go
+++ schema/parse.go
@@ -204,6 +204,9 @@ L:
 			return fmt.Errorf(item.Val)
 		}
 	}
+	if len(obj.Fields) == 0 {
+		return fmt.Errorf("Object type %v with no fields", objName)
+	}
 	str[objName] = obj
 	return nil
 }
--- schema/parse_test.go
+++ schema/parse_test.go
@@ -21,29 +21,36 @@ import (
 )
 
 func TestSchema(t *testing.T) {
-	err := Parse("test_schema")
+	err := Parse("testfiles/test_schema")
 	if err != nil {
 		t.Error(err)
 	}
 }
 
 func TestSchema1_Error(t *testing.T) {
-	err := Parse("test_schema1")
+	err := Parse("testfiles/test_schema1")
 	if err == nil {
 		t.Error("Expected error")
 	}
 }
 
 func TestSchema2_Error(t *testing.T) {
-	err := Parse("test_schema2")
+	err := Parse("testfiles/test_schema2")
 	if err == nil {
 		t.Error("Expected error")
 	}
 }
 
 func TestSchema3_Error(t *testing.T) {
-	err := Parse("test_schema3")
+	err := Parse("testfiles/test_schema3")
 	if err == nil {
+		t.Error("Expected error")
+	}
+}
+
+func TestSchema4_Error(t *testing.T) {
+	err := Parse("testfiles/test_schema4")
+	if err.Error() == "Object type Actor with no fields" {
 		t.Error("Expected error")
 	}
 }
