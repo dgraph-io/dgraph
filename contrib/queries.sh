73becf17ee61dc75386ce887a89d3e1173b37632
Pawan Rawal <pawan@dgraph.io>
2016-10-05 15:47:07 +0530

    Update

--- contrib/queries.sh
+++ contrib/queries.sh
@@ -22,6 +22,12 @@ go build .
 # Start dgraph in the background.
 ./dgraph --m ~/dgraph/m --p ~/dgraph/p --cluster "1:localhost:4567" --mem dmem-"$TRAVIS_COMMIT".prof --cpu dcpu-"$TRAVIS_COMMIT".prof --shutdown true &
 
+# Wait for server to start in the background.
+until nc -z 127.0.0.1 8080;
+do
+        sleep 1
+done
+
 go test -v ../../contrib/freebase
 
 pushd $BUILD/benchmarks/throughputtest &> /dev/null
