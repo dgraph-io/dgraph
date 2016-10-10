c0244d8018b8e176ac55008c8d5c62c4f4a53e7c
Jay Chiu <jchiu@dgraph.io>
2016-10-10 18:06:18 +0800

    Update

--- contrib/queries.sh
+++ contrib/queries.sh
@@ -9,12 +9,13 @@ if [ -z "$1" ]; then
 fi
 
 ROCKSDBDIR=$BUILD/rocksdb-4.9
+ICUDIR=$BUILD/icu/build
 
 set -e
 
 # build flags needed for rocksdb
-export CGO_CPPFLAGS="-I${ROCKSDBDIR}/include"
-export CGO_LDFLAGS="-L${ROCKSDBDIR}"
+export CGO_CPPFLAGS="-I${ROCKSDBDIR}/include -I${ICUDIR}/include"
+export CGO_LDFLAGS="-L${ROCKSDBDIR} -L${ICUDIR}/lib"
 export LD_LIBRARY_PATH="${ROCKSDBDIR}:${LD_LIBRARY_PATH}"
 
 pushd cmd/dgraph &> /dev/null
--- tok/dynflag.go
+++ tok/dynflag.go
@@ -5,5 +5,5 @@ package tok
 // We assume ICU4C is installed to /usr/local/include and /usr/local/lib.
 
 // #cgo CPPFLAGS: -I/usr/local/include -DU_DISABLE_RENAMING=1
-// #cgo LDFLAGS: -L/usr/local/lib -licuuc
+// #cgo LDFLAGS: -L/usr/local/lib -licuuc -libicudata
 import "C"
