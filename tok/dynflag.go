5b3ae79263aca04fa490f14c5177660074169037
Jay Chiu <jchiu@dgraph.io>
2016-10-10 18:59:55 +0800

    Update

--- tok/dynflag.go
+++ tok/dynflag.go
@@ -4,6 +4,6 @@ package tok
 
 // We assume ICU4C is installed to /usr/local/include and /usr/local/lib.
 
-// #cgo CPPFLAGS: -I/usr/local/include -DU_DISABLE_RENAMING=1
-// #cgo LDFLAGS: -L/usr/local/lib -licuuc -libicudata
+// #cgo CPPFLAGS: -DU_DISABLE_RENAMING=1
+// #cgo LDFLAGS: -licuuc -licudata
 import "C"
