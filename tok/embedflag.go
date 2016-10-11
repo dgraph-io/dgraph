ceb0ec10e0760dc4a6e011edc7e6ba767113edcd
Jay Chiu <jchiu@dgraph.io>
2016-10-11 10:54:13 +0800

    Update

--- tok/embedflag.go
+++ tok/embedflag.go
@@ -2,22 +2,18 @@
 
 package tok
 
-// #cgo CPPFLAGS: -I${SRCDIR}/icu/icuembed -DU_DISABLE_RENAMING=1
+//  /home/jchiu/go/src/github.com/dgraph-io/dgraph/vendor/github.com/dgraph-io/goicu/icuembed
+
+// #cgo CPPFLAGS: -I/home/jchiu/go/src/github.com/dgraph-io/dgraph/vendor/github.com/dgraph-io/goicu/icuembed -DU_DISABLE_RENAMING=1
 // #cgo darwin LDFLAGS: -Wl,-undefined -Wl,dynamic_lookup
 // #cgo !darwin LDFLAGS: -Wl,-unresolved-symbols=ignore-all -lrt
-// #include <unicode/utypes.h>
-// #include <unicode/udata.h>
 import "C"
 
 import (
 	"flag"
-	"io/ioutil"
-	"log"
-	"time"
-	"unsafe"
 
-	_ "github.com/dgraph-io/dgraph/tok/icu/icuembed" // Force ICU to be linked in.
 	"github.com/dgraph-io/dgraph/x"
+	"github.com/dgraph-io/goicu/icuembed"
 )
 
 var (
@@ -29,16 +25,6 @@ var (
 func init() {
 	x.AddInit(func() {
 		x.Assertf(len(*icuDataFile) > 0, "ICU data file empty")
-
-		start := time.Now()
-		icuData, err := ioutil.ReadFile(*icuDataFile)
-		x.Check(err)
-
-		var icuErr C.UErrorCode
-		C.udata_setCommonData(unsafe.Pointer(byteToChar(icuData)), &icuErr)
-		x.Assertf(int(icuErr) >= 0, "Error occurred with udata_setCommonData: %d",
-			int(icuErr))
-
-		log.Printf("Loaded ICU data from [%s] in %s", *icuDataFile, time.Since(start))
+		x.Check(icuembed.Load(*icuDataFile))
 	})
 }
