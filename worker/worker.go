cc5851b07bf4c2fef9e0bed86f5a8fb1bb31029e
Jay Chiu <jchiu@dgraph.io>
2016-10-27 20:38:23 +0800

    Move RocksDB stats to /debug/store for dgraph binary

--- worker/worker.go
+++ worker/worker.go
@@ -63,3 +63,8 @@ func RunServer() {
 	RegisterWorkerServer(s, &grpcWorker{})
 	s.Serve(ln)
 }
+
+// StoreStats returns stats for data store.
+func StoreStats() string {
+	return pstore.GetStats()
+}
