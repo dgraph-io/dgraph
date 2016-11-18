a7a095749bd46f4c0c2fc52c7cc25235350b4b4c
Jay Chiu <jchiu@dgraph.io>
2016-11-18 10:11:27 +0800

    Update

--- task/MembershipUpdate.go
+++ task/MembershipUpdate.go
@@ -28,6 +28,9 @@ func (rcv *MembershipUpdate) Members(obj *Membership, j int) bool {
 		x := rcv._tab.Vector(o)
 		x += flatbuffers.UOffsetT(j) * 4
 		x = rcv._tab.Indirect(x)
+		if obj == nil {
+			obj = new(Membership)
+		}
 		obj.Init(rcv._tab.Bytes, x)
 		return true
 	}
