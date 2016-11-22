dd84bb9b84998780371cf7e70a8dc5ba1f2195d3
Manish R Jain <manish@dgraph.io>
2016-11-22 23:49:39 +1100

    worker.backup compiles

--- worker/predicate.go
+++ worker/predicate.go
@@ -25,7 +25,6 @@ import (
 	"github.com/dgraph-io/dgraph/posting/types"
 	"github.com/dgraph-io/dgraph/task"
 	"github.com/dgraph-io/dgraph/x"
-	flatbuffers "github.com/google/flatbuffers/go"
 )
 
 const (
@@ -39,8 +38,8 @@ func writeBatch(ctx context.Context, kv chan *task.KV, che chan error) {
 	batchSize := 0
 	batchWriteNum := 1
 	for i := range kv {
-		wb.Put(i.KeyBytes(), i.ValBytes())
-		batchSize += len(i.KeyBytes()) + len(i.ValBytes())
+		wb.Put(i.Key, i.Val)
+		batchSize += len(i.Key) + len(i.Val)
 		// We write in batches of size 32MB.
 		if batchSize >= 32*MB {
 			x.Trace(ctx, "Doing batch write %d.", batchWriteNum)
@@ -67,56 +66,34 @@ func writeBatch(ctx context.Context, kv chan *task.KV, che chan error) {
 	che <- nil
 }
 
-func generateGroup(group uint64) ([]byte, error) {
+func generateGroup(group uint64) (*task.GroupKeys, error) {
 	it := pstore.NewIterator()
 	defer it.Close()
 
-	b := flatbuffers.NewBuilder(0)
-	uoffsets := make([]flatbuffers.UOffsetT, 0, 100)
-
+	g := &task.GroupKeys{}
 	for it.SeekToFirst(); it.Valid(); it.Next() {
 		// TODO: Check if this key belongs to the group.
 
 		k, v := it.Key(), it.Value()
-		pl := types.GetRootAsPostingList(v.Data(), 0)
+		var pl types.PostingList
+		x.Check(pl.Unmarshal(v.Data()))
 
-		ko := b.CreateByteVector(k.Data())
-		co := b.CreateByteVector(pl.Checksum())
-
-		task.KCStart(b)
-		task.KCAddKey(b, ko)
-		task.KCAddChecksum(b, co)
-		uo := task.KCEnd(b)
-
-		uoffsets = append(uoffsets, uo)
+		key := &task.KC{
+			Key:      k.Data(),
+			Checksum: pl.Checksum,
+		}
+		g.Keys = append(g.Keys, key)
 	}
-	if err := it.Err(); err != nil {
-		return []byte{}, nil
-	}
-
-	task.GroupKeysStartKeysVector(b, len(uoffsets))
-	for i := len(uoffsets) - 1; i >= 0; i-- {
-		b.PrependUOffsetT(uoffsets[i])
-	}
-	keysOffset := b.EndVector(len(uoffsets))
-
-	task.GroupKeysStart(b)
-	task.GroupKeysAddGroupid(b, group)
-	task.GroupKeysAddKeys(b, keysOffset)
-	rend := task.GroupKeysEnd(b)
-	b.Finish(rend)
-	return b.Bytes[b.Head():], nil
+	return g, it.Err()
 }
 
 // PopulateShard gets data for predicate pred from server with id serverId and
 // writes it to RocksDB.
 func populateShard(ctx context.Context, pl *pool, group uint64) (int, error) {
-	query := new(Payload)
-	data, err := generateGroup(group)
+	gkeys, err := generateGroup(group)
 	if err != nil {
 		return 0, x.Wrapf(err, "While generating keys group")
 	}
-	query.Data = data
 
 	conn, err := pl.Get()
 	if err != nil {
@@ -125,7 +102,7 @@ func populateShard(ctx context.Context, pl *pool, group uint64) (int, error) {
 	defer pl.Put(conn)
 	c := NewWorkerClient(conn)
 
-	stream, err := c.PredicateData(context.Background(), query)
+	stream, err := c.PredicateData(context.Background(), gkeys)
 	if err != nil {
 		return 0, err
 	}
@@ -138,7 +115,7 @@ func populateShard(ctx context.Context, pl *pool, group uint64) (int, error) {
 	// We can use count to check the number of posting lists returned in tests.
 	count := 0
 	for {
-		payload, err := stream.Recv()
+		kv, err := stream.Recv()
 		if err == io.EOF {
 			break
 		}
@@ -147,23 +124,19 @@ func populateShard(ctx context.Context, pl *pool, group uint64) (int, error) {
 			return count, err
 		}
 		count++
-		uo := flatbuffers.GetUOffsetT(payload.Data)
-		kv := new(task.KV)
-		kv.Init(payload.Data, uo)
 
 		// We check for errors, if there are no errors we send value to channel.
 		select {
+		case kvs <- kv:
+			// OK
 		case <-ctx.Done():
 			x.TraceError(ctx, x.Errorf("Context timed out while streaming group: %v", group))
 			close(kvs)
 			return count, ctx.Err()
-
 		case err := <-che:
 			x.TraceError(ctx, x.Errorf("Error while doing a batch write for group: %v", group))
 			close(kvs)
 			return count, err
-
-		case kvs <- kv:
 		}
 	}
 	close(kvs)
@@ -178,11 +151,8 @@ func populateShard(ctx context.Context, pl *pool, group uint64) (int, error) {
 
 // PredicateData can be used to return data corresponding to a predicate over
 // a stream.
-func (w *grpcWorker) PredicateData(query *Payload, stream Worker_PredicateDataServer) error {
-	var group task.GroupKeys
-	uo := flatbuffers.GetUOffsetT(query.Data)
-	group.Init(query.Data, uo)
-	_ = group.Groupid()
+func (w *grpcWorker) PredicateData(group *task.GroupKeys, stream Worker_PredicateDataServer) error {
+	// TODO: Use group id.
 
 	// TODO(pawan) - Shift to CheckPoints once we figure out how to add them to the
 	// RocksDB library we are using.
@@ -192,37 +162,30 @@ func (w *grpcWorker) PredicateData(query *Payload, stream Worker_PredicateDataSe
 
 	for it.SeekToFirst(); it.Valid(); it.Next() {
 		k, v := it.Key(), it.Value()
-		pl := types.GetRootAsPostingList(v.Data(), 0)
+		var pl types.PostingList
+		x.Check(pl.Unmarshal(v.Data()))
 
 		// TODO: Check that key is part of the specified group id.
-		i := sort.Search(group.KeysLength(), func(i int) bool {
-			var t task.KC
-			x.AssertTruef(group.Keys(&t, i), "Unable to parse task.KC")
-			return bytes.Compare(k.Data(), t.KeyBytes()) <= 0
+		i := sort.Search(len(group.Keys), func(i int) bool {
+			t := group.Keys[i]
+			return bytes.Compare(k.Data(), t.Key) <= 0
 		})
 
-		if i < group.KeysLength() {
+		if i < len(group.Keys) {
 			// Found a match.
-			var t task.KC
-			x.AssertTruef(group.Keys(&t, i), "Unable to parse task.KC")
-
-			if bytes.Equal(k.Data(), t.KeyBytes()) && bytes.Equal(pl.Checksum(), t.ChecksumBytes()) {
+			t := group.Keys[i]
+			if bytes.Equal(k.Data(), t.Key) && bytes.Equal(pl.Checksum, t.Checksum) {
 				// No need to send this.
 				continue
 			}
 		}
 
-		b := flatbuffers.NewBuilder(0)
-		bko := b.CreateByteVector(k.Data())
-		bvo := b.CreateByteVector(v.Data())
-		task.KVStart(b)
-		task.KVAddKey(b, bko)
-		task.KVAddVal(b, bvo)
-		kvoffset := task.KVEnd(b)
-		b.Finish(kvoffset)
+		kv := &task.KV{
+			Key: k.Data(),
+			Val: v.Data(),
+		}
 
-		p := Payload{Data: b.Bytes[b.Head():]}
-		if err := stream.Send(&p); err != nil {
+		if err := stream.Send(kv); err != nil {
 			return err
 		}
 		k.Free()
