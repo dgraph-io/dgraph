c912b3045157911fb3a1c9c08eaab8d89ded806e
Ashwin Ramesh <ashwin@dgraph.io>
2016-12-14 16:19:05 +1100

    Make sort tests work

--- types/sort_test.go
+++ types/sort_test.go
@@ -24,21 +24,21 @@ import (
 	"github.com/dgraph-io/dgraph/task"
 )
 
-func toString(t *testing.T, values []interface{}) []string {
+func toString(t *testing.T, values []interface{}, vID TypeID) []string {
 	out := make([]string, len(values))
 	for i, v := range values {
-		b, err := v.MarshalText()
-		require.NoError(t, err)
-		out[i] = string(b)
+		var b interface{}
+		require.NoError(t, ConvertFromInterface(vID, StringID, v, &b))
+		out[i] = b.(string)
 	}
 	return out
 }
 
-func getInput(tid TypeID, in []string) []interface{} {
-	list := make([]Value, len(in))
+func getInput(t *testing.T, tid TypeID, in []string) []interface{} {
+	list := make([]interface{}, len(in))
 	for i, s := range in {
 		v := ValueForType(tid)
-		v.UnmarshalText([]byte(s))
+		require.NoError(t, Convert(StringID, tid, []byte(s), &v))
 		list[i] = v
 	}
 	return list
@@ -53,42 +53,42 @@ func getUIDList(n int) *task.List {
 }
 
 func TestSortStrings(t *testing.T) {
-	list := getInput(StringID, []string{"bb", "aaa", "aa", "bab"})
+	list := getInput(t, StringID, []string{"bb", "aaa", "aa", "bab"})
 	ul := getUIDList(4)
 	require.NoError(t, Sort(StringID, list, ul))
 	require.EqualValues(t, []uint64{300, 200, 400, 100}, ul.Uids)
 	require.EqualValues(t, []string{"aa", "aaa", "bab", "bb"},
-		toString(t, list))
+		toString(t, list, StringID))
 }
 
 func TestSortInts(t *testing.T) {
-	list := getInput(Int32ID, []string{"22", "111", "11", "212"})
+	list := getInput(t, Int32ID, []string{"22", "111", "11", "212"})
 	ul := getUIDList(4)
 	require.NoError(t, Sort(Int32ID, list, ul))
 	require.EqualValues(t, []uint64{300, 100, 200, 400}, ul.Uids)
 	require.EqualValues(t, []string{"11", "22", "111", "212"},
-		toString(t, list))
+		toString(t, list, Int32ID))
 }
 
 func TestSortFloats(t *testing.T) {
-	list := getInput(FloatID, []string{"22.2", "11.2", "11.5", "2.12"})
+	list := getInput(t, FloatID, []string{"22.2", "11.2", "11.5", "2.12"})
 	ul := getUIDList(4)
 	require.NoError(t, Sort(FloatID, list, ul))
 	require.EqualValues(t, []uint64{400, 200, 300, 100}, ul.Uids)
 	require.EqualValues(t,
 		[]string{"2.12E+00", "1.12E+01", "1.15E+01", "2.22E+01"},
-		toString(t, list))
+		toString(t, list, FloatID))
 }
 
 func TestSortDates(t *testing.T) {
 	in := []string{"2022-01-01", "2022-02-03", "2021-01-05", "2021-01-07"}
-	list := getInput(DateID, in)
+	list := getInput(t, DateID, in)
 	ul := getUIDList(4)
 	require.NoError(t, Sort(DateID, list, ul))
 	require.EqualValues(t, []uint64{300, 400, 100, 200}, ul.Uids)
 	require.EqualValues(t,
 		[]string{"2021-01-05", "2021-01-07", "2022-01-01", "2022-02-03"},
-		toString(t, list))
+		toString(t, list, DateID))
 }
 
 func TestSortDateTimes(t *testing.T) {
@@ -98,12 +98,12 @@ func TestSortDateTimes(t *testing.T) {
 		"2006-01-02T15:04:06",
 		"2006-01-02T15:04:01",
 	}
-	list := getInput(DateTimeID, in)
+	list := getInput(t, DateTimeID, in)
 	ul := getUIDList(4)
 	require.NoError(t, Sort(DateTimeID, list, ul))
 	require.EqualValues(t, []uint64{400, 200, 300, 100}, ul.Uids)
 	require.EqualValues(t,
-		[]string{"2006-01-02T15:04:01Z", "2006-01-02T15:04:05Z",
-			"2006-01-02T15:04:06Z", "2016-01-02T15:04:05Z"},
-		toString(t, list))
+		[]string{"2006-01-02 15:04:01 +0000 UTC", "2006-01-02 15:04:05 +0000 UTC",
+			"2006-01-02 15:04:06 +0000 UTC", "2016-01-02 15:04:05 +0000 UTC"},
+		toString(t, list, DateTimeID))
 }
