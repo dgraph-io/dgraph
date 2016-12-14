5ed99eafe87c126c3744c657b3a424ce39681f03
Ashish Negi <ashishnegi33@gmail.com>
2016-12-14 12:40:45 +0530

    Refactor code.

--- algo/uidlist_test.go
+++ algo/uidlist_test.go
@@ -17,12 +17,12 @@
 package algo
 
 import (
-	"testing"
-	"strconv"
+	"github.com/stretchr/testify/require"
 	"math/rand"
 	"sort"
+	"strconv"
+	"testing"
 	"time"
-	"github.com/stretchr/testify/require"
 
 	"github.com/dgraph-io/dgraph/task"
 )
@@ -218,42 +218,39 @@ func TestApplyFilterUint(t *testing.T) {
 	require.Equal(t, u.Uids, []uint64{1, 3, 5})
 }
 
-// Benchmarks for IntersectWith
-// different data :
-// 1. random data : u and v having data within range [0,limit] where limit = N * sizeof-list
-// 2. uniformly distributed : u : 0,1,2.. and v : 0,100,200..
-// 3. non-uniformly distributed : u : 0,1,2,..100,1001,1002,..2000 and v : 0,1,2,3..1000
+// sort interface for []uint64
+type Uint64Slice []uint64
 
-func uintsFromInts(ints []int) []uint64 {
-	arrSz := len(ints)
-	uints := make([]uint64, arrSz, arrSz)
-	alwaysIncreasing := uint64(0)
-	for k := 0; k < arrSz; k++ {
-		uints[k] = uint64(ints[k])
-		if uints[k] < alwaysIncreasing {
-			panic("Bad data") // go test . -v
-		}
-		alwaysIncreasing = uints[k]
-	}
-	return uints
+func (xs Uint64Slice) Len() int {
+	return len(xs)
+}
+func (xs Uint64Slice) Less(i, j int) bool {
+	return xs[i] < xs[j]
+}
+func (xs Uint64Slice) Swap(i, j int) {
+	xs[i], xs[j] = xs[j], xs[i]
 }
 
-func benchmarkListIntersectRandom(arrSz int, limit int, r *rand.Rand, b *testing.B) {
-	u1, v1 := make([]int, arrSz, arrSz), make([]int, arrSz, arrSz)
-	abs := func(x int) int { if x < 0 { return -x } else { return x } }
-
+// Benchmarks for IntersectWith
+// random data : u and v having data within range [0,limit] where limit = N * sizeof-list
+func benchmarkListIntersectRandom(arrSz int, limit int64, r *rand.Rand, b *testing.B) {
+	u1, v1 := make([]uint64, arrSz, arrSz), make([]uint64, arrSz, arrSz)
 	for i := 0; i < arrSz; i++ {
-		u1[i] = abs(r.Int()) % limit
-		v1[i] = abs(r.Int()) % limit
+		u1[i] = uint64(r.Int63n(limit))
+		v1[i] = uint64(r.Int63n(limit))
 	}
-	sort.Ints(u1)
-	sort.Ints(v1)
+	sort.Sort(Uint64Slice(u1))
+	sort.Sort(Uint64Slice(v1))
 
-	v := newList(uintsFromInts(v1))
-
-	b.Run(":curr:" + strconv.Itoa(arrSz), func(b *testing.B) {
-		u := newList(uintsFromInts(u1))
-		ucopy := uintsFromInts(u1)
+	makeCopy := func(x []uint64) []uint64 {
+		c := make([]uint64, len(x), len(x))
+		copy(c, x)
+		return c
+	}
+	b.Run(":2forLoops:"+strconv.Itoa(arrSz), func(b *testing.B) {
+		u := newList(u1)
+		v := newList(v1)
+		ucopy := makeCopy(u1)
 		b.ResetTimer()
 		for k := 0; k < b.N; k++ {
 			IntersectWith(u, v)
@@ -261,10 +258,10 @@ func benchmarkListIntersectRandom(arrSz int, limit int, r *rand.Rand, b *testing
 			copy(u.Uids, ucopy)
 		}
 	})
-	b.Run(":curr:ordChg:" + strconv.Itoa(arrSz), func(b *testing.B) {
-		vArg := newList(uintsFromInts(u1))
-		uArg := newList(uintsFromInts(v1))
-		ucopy := uintsFromInts(v1)
+	b.Run(":2forLoops:ordChg:"+strconv.Itoa(arrSz), func(b *testing.B) {
+		uArg := newList(v1)
+		vArg := newList(u1)
+		ucopy := makeCopy(v1)
 		b.ResetTimer()
 		for k := 0; k < b.N; k++ {
 			IntersectWith(uArg, vArg)
@@ -276,132 +273,19 @@ func benchmarkListIntersectRandom(arrSz int, limit int, r *rand.Rand, b *testing
 
 func BenchmarkListIntersectRandom(b *testing.B) {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-
-	randomTests := func(factor int) {
-		b.Run(strconv.Itoa(factor) + ":random:size=500", func (b *testing.B) { benchmarkListIntersectRandom(500, 500*factor, r, b) })
-		b.Run(strconv.Itoa(factor) + ":random:size=10000", func (b *testing.B) { benchmarkListIntersectRandom(10000, 10000*factor, r, b) })
-		b.Run(strconv.Itoa(factor) + ":random:size=1000000", func (b *testing.B) { benchmarkListIntersectRandom(1000000, 1000000*factor, r, b) })
+	randomTests := func(sz int, factor int) {
+		sizeStr := strconv.Itoa(sz)
+		facStr := strconv.Itoa(factor)
+		b.Run("factor:"+facStr+":random:size="+sizeStr, func(b *testing.B) { benchmarkListIntersectRandom(sz, int64(sz*factor), r, b) })
 	}
 
-	randomTests(3);
-	randomTests(10);
-	randomTests(100);
-}
-
-func GenList(n int, f func(int, int) int) *task.List {
-	var arr = make([]uint64, n, n)
-	var incNos uint64
-	for i := 0; i < n; i++ {
-		arr[i] = uint64(f(i, n))
-		if (incNos > arr[i]) {
-			panic("Non increasing sequence of numbers")
-		}
-		incNos = arr[i]
-	}
-	return newList(arr)
-}
-
-func benchmarkListIntersectFn(sz int, f1 func(int, int) int, f2 func(int, int) int, intersectF func(u, v *task.List), b *testing.B) {
-	u, v := GenList(sz, f1), GenList(sz, f2)
-	ucopy := GenList(sz, f1)
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		intersectF(u, v)
-		// ~ this copy adds ~10% overhead in benchmarks for all
-		u.Uids = u.Uids[:sz]
-		copy(u.Uids, ucopy.Uids)
-	}
-}
-
-func benchmarkListIntersectAll(name string, ignoreBelowSz int, f1 func(int, int) int, f2 func(int, int) int, b *testing.B) {
-	runBenchmarks := func(n int) {
-		if ignoreBelowSz < n {
-			b.Run(name + ":curr:size=" + strconv.Itoa(n), func(b *testing.B) {
-				benchmarkListIntersectFn(n, f1, f2, IntersectWith, b)})
-		}
-	}
-	runBenchmarks(500)
-	runBenchmarks(10000)
-	runBenchmarks(1000000)
-}
-
-func BenchmarkListIntersectUniDist(b *testing.B) {
-	// All data generated is of uniform distribution
-	naturalNums := func(i int, sz int) int { return i }
-	// two lists match 0 %
-	match0Per := func(i int, sz int) int { return i + sz }
-	benchmarkListIntersectAll("nomatch:1", 0, match0Per, naturalNums, b)
-	benchmarkListIntersectAll("1:nomatch", 0, naturalNums, match0Per, b)
-	// two lists match 10 %
-	match10Per := func(i int, sz int) int { return 10*i }
-	benchmarkListIntersectAll("10:1", 0, match10Per, naturalNums, b)
-	benchmarkListIntersectAll("1:10", 0, naturalNums, match10Per, b)
-}
-
-func BenchmarkListIntersectNonUniform(b *testing.B) {
-	naturalNums := func(i int, sz int) int { return i }
-	numsAfterSz := func(i int, sz int) int {
-		return sz + i
-	}
-
-	type matchNpercfn func(int, int) int
-
-	matchNPercAtStart := func(n int) matchNpercfn {
-		return func(i int, sz int) int {
-			// for sz : 100, n : 10
-			// 0,1,2,3,..9,110,111,...
-			if (i < int((sz * n) / 100)) {
-				return i
-			}
-			return sz + i
-		}
-	}
-
-	// 20 % of list matches at start
-	benchmarkListIntersectAll("Same20%atStart:1", 0, matchNPercAtStart(20), naturalNums, b)
-	benchmarkListIntersectAll("1:Same20%atStart", 0, naturalNums, matchNPercAtStart(20) b)
-	// 50 % of list matches at start
-	benchmarkListIntersectAll("Same50%atStart:1", 0, matchNPercAtStart(50), naturalNums, b)
-	benchmarkListIntersectAll("1:Same50%atStart", 0, naturalNums, matchNPercAtStart(50), b)
-
-	matchNPercAtEnd := func (n int) matchNpercfn {
-		return func (i int, sz int) int {
-			// for sz : 100 and n : 10
-			// 0,1,2,....88,89,190,191,192..
-			if (i >= int(sz * (100 - n) / 100)) {
-				return sz + i
-			}
-			return i
-		}
-	}
-
-	// 20 % of list matches at end
-	benchmarkListIntersectAll("Same20%atEnd:1", 0, matchNPercAtEnd(20), numsAfterSz, b)
-	benchmarkListIntersectAll("1:Same20%atEnd", 0, numsAfterSz, matchNPercAtEnd(20), b)
-	// 50 % of list matches at end
-	benchmarkListIntersectAll("Same50%atEnd:1", 0, matchNPercAtEnd(50), numsAfterSz, b)
-	benchmarkListIntersectAll("1:Same50%atEnd", 0, numsAfterSz, matchNPercAtEnd(50), b)
-
-	matchNPercAtMid := func(n int) matchNpercfn {
-		nhalf := int(n / 2)
-		// for n : 50, sz : 100
-		// 0,1,..23,24,125,126,..173,174,276,277..299
-		return func(i int, sz int) int {
-			szhalf := int(sz / 2)
-			if (i < (szhalf - nhalf)) {
-				return i
-			} else if (i < (szhalf + nhalf)) {
-				return sz + i
-			} else {
-				return (2 * sz) + i
-			}
-		}
-	}
-
-	// 20 % of list matches at mid
-	benchmarkListIntersectAll("Same20%atMid:1", 0, matchNPercAtMid(20), numsAfterSz, b)
-	benchmarkListIntersectAll("1:Same20%atMid", 0, numsAfterSz, matchNPercAtMid(20), b)
-	// 50 % of list matches at Mid
-	benchmarkListIntersectAll("Same50%atMid:1", 0, matchNPercAtMid(50), numsAfterSz, b)
-	benchmarkListIntersectAll("1:Same50%atMid", 0, numsAfterSz, matchNPercAtMid(50), b)
+	randomTests(500, 3)
+	randomTests(10000, 3)
+	randomTests(1000000, 3)
+	randomTests(500, 10)
+	randomTests(10000, 10)
+	randomTests(1000000, 10)
+	randomTests(500, 100)
+	randomTests(10000, 100)
+	randomTests(1000000, 100)
 }
