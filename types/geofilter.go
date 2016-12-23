1dd7e11c249c48af6a644156190e391ea98ba2f0
Ashwin Ramesh <ashwin@dgraph.io>
2016-12-23 18:17:36 +1100

    Make near and within also return polygons approximately

--- types/geofilter.go
+++ types/geofilter.go
@@ -222,13 +222,33 @@ func WithinPolygon(g1 *s2.Loop, g2 *s2.Loop) bool {
 	return true
 }
 
+func WithinCapPolygon(g1 *s2.Loop, g2 *s2.Cap) bool {
+	for _, point := range g1.Vertices() {
+		if !g2.ContainsPoint(point) {
+			return false
+		}
+	}
+	return true
+}
+
 // returns true if the geometry represented by g is within the given loop or cap
 func (q GeoQueryData) isWithin(g geom.T) bool {
 	x.AssertTruef(q.pt != nil || q.loop != nil || q.cap != nil, "At least a point, loop or cap should be defined.")
 	gpt, ok := g.(*geom.Point)
 	if !ok {
 		// We will only consider points for within queries.
-		return false
+		gpoly, ok := g.(*geom.Polygon)
+		if !ok {
+			return false
+		}
+		s2loop, err := loopFromPolygon(gpoly)
+		if err != nil {
+			return false
+		}
+		if q.loop != nil {
+			return WithinPolygon(s2loop, q.loop)
+		}
+		return WithinCapPolygon(s2loop, q.cap)
 	}
 
 	s2pt := pointFromPoint(gpt)
