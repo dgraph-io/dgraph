b4a3d2dcf2de8347cf8123a86246866058ac21cb
Jay Chiu <jchiu@dgraph.io>
2017-01-04 01:16:47 -0800

    Update

--- worker/tmp.go
+++ worker/tmp.go
@@ -4,18 +4,19 @@ import (
 	"fmt"
 	"time"
 
-	_ "github.com/dgraph-io/dgraph/x"
+	"github.com/dgraph-io/dgraph/x"
 )
 
 func TempDebug() {
 	time.Sleep(time.Second)
 	fmt.Printf("~~~~~~~hi\n")
-	//	n := groups().Node(0)
+	n := groups().Node(0)
 	for {
-		//		lastIndex, err := n.store.LastIndex()
-		//		doneUntil := n.applied.DoneUntil()
-		//		x.Check(err)
-		//		fmt.Printf("~~~~ %d %d\n", doneUntil, lastIndex)
+		lastIndex, err := n.store.LastIndex()
+		doneUntil := n.applied.DoneUntil()
+		x.Check(err)
+		fmt.Printf("~~~~ %d %d\n", doneUntil, lastIndex)
+		// doneUntil can be < lastIndex
 		time.Sleep(100 * time.Millisecond)
 	}
 }
