1f61523e66a7c90aa666ad4059abcff4e005d303
Pawan Rawal <pawan@dgraph.io>
2017-01-04 17:48:57 +1100

    Compute and store checksum's

--- build.sh
+++ build.sh
@@ -6,6 +6,11 @@ set -e
 cur_dir=$(pwd);
 tmp_dir=/tmp/dgraph-build;
 release_version=$(git describe --abbrev=0);
+platform="$(uname | tr '[:upper:]' '[:lower:]')"
+checksum_file=$cur_dir/"dgraph-checksum-$platform-amd64-$release_version".md5
+if [ -f "$checksum_file" ]; then
+	rm $checksum_file
+fi
 
 # If temporary directory already exists delete it.
 if [ -d "$tmp_dir" ]; then
@@ -20,7 +25,7 @@ fi
 mkdir $tmp_dir;
 
 dgraph_cmd=$GOPATH/src/github.com/dgraph-io/dgraph/cmd;
-build_flags='-tags=embed -v'
+build_flags='-v -tags=embed'
 
 echo -e "\033[1;33mBuilding binaries\033[0m"
 echo "dgraph"
@@ -33,7 +38,6 @@ cd $tmp_dir;
 mkdir dgraph && pushd &> /dev/null dgraph;
 cp $dgraph_cmd/dgraph/dgraph $dgraph_cmd/dgraphloader/dgraphloader .;
 
-platform="$(uname | tr '[:upper:]' '[:lower:]')"
 # Stripping the binaries.
 # Stripping binaries on Mac doesn't lead to much reduction in size and
 # instead gives an error.
@@ -41,6 +45,11 @@ if [ "$platform" = "linux" ]; then
   strip dgraph dgraphloader
   echo -e "\n\033[1;34mSize of files after strip: $(du -sh)\033[0m"
 fi
+
+checksum=$(md5sum dgraph | awk '{print $1}')
+echo "$checksum /usr/local/bin/dgraph" >> $checksum_file
+checksum=$(md5sum dgraphloader | awk '{print $1}')
+echo "$checksum /usr/local/bin/dgraphloader" >> $checksum_file
 
 echo -e "\n\033[1;33mCreating tar file\033[0m"
 tar_file=dgraph-"$platform"-amd64-$release_version
@@ -52,3 +61,7 @@ echo -e "\n\033[1;34mSize of tar file: $(du -sh $tar_file.tar.gz)\033[0m"
 echo -e "\n\033[1;33mMoving tarfile to original directory\033[0m"
 mv $tar_file.tar.gz $cur_dir
 rm -rf $tmp_dir
+
+echo -e "\nCalculating and storing checksum for ICU data file."
+checksum=$(md5sum $GOPATH/src/github.com/dgraph-io/goicu/icudt58l.dat | awk '{print $1}')
+echo "$checksum /usr/local/share/icudt58l.dat" >> $checksum_file
