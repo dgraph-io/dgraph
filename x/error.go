e7cc8d7c52f895a8834757aff39bcb3d9ac21855
Ashish Negi <ashishnegi33@gmail.com>
2017-01-30 14:05:19 +0530

    Remove unused code in errors.go

--- x/error.go
+++ x/error.go
@@ -33,7 +33,6 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"strconv"
 	"strings"
 
 	"github.com/pkg/errors"
@@ -188,18 +187,4 @@ func TraceError(ctx context.Context, err error) {
 		return
 	}
 	Trace(ctx, shortenedErrorString(err))
-}
-
-// StrinfifyErrors returns all errors combined in string form.
-// Format : 1. <first error msg> \n 2. <second error msg> ...so on..
-func StringifyErrors(errs []error) string {
-	var buf bytes.Buffer
-	// error returned by buf.Write* are always nil : by doc
-	for i, err := range errs {
-		buf.WriteString(strconv.FormatInt(int64(i), 10))
-		buf.WriteString(": ")
-		buf.WriteString(err.Error())
-		buf.WriteRune('\n')
-	}
-	return buf.String()
 }
