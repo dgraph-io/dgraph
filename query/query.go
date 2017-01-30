08d438730d24a53823ee55c617a5d4fcf15e8008
Ashwin Ramesh <ashwin@dgraph.io>
2017-01-30 17:20:12 +1100

    Fix spelling

--- query/query.go
+++ query/query.go
@@ -660,7 +660,7 @@ func populateVarMap(sg *SubGraph, doneVars map[string]*task.List, isCascade bool
 		var exclude bool
 		for _, child := range sg.Children {
 			// If the length of child UID list is zero and it has no valid value, then the
-			// current UID should be removed form this level.
+			// current UID should be removed from this level.
 			if len(child.values[i].Val) == 0 && len(child.uidMatrix[i].Uids) == 0 {
 				exclude = true
 				break
