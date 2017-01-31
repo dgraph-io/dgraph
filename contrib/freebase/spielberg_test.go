c45d08e5cf8bc3aa27cb2dedb6705d5e50470311
Pawan Rawal <pawan@dgraph.io>
2017-02-01 09:49:27 +1100

    Modify some more freebase test files

--- contrib/freebase/spielberg_test.go
+++ contrib/freebase/spielberg_test.go
@@ -9,15 +9,15 @@ func TestSpielberg(t *testing.T) {
 	q := `
     {
       me(id: m.06pj8) {
-        type.object.name.en
-        film.director.film (first: 4)  {
-            type.object.name.en
+        name.en
+        director.film (first: 4)  {
+            name.en
         }
       }
     }`
 
 	res := decodeResponse(q)
-	expectedRes := `{"me":[{"film.director.film":[{"type.object.name.en":"Indiana Jones and the Temple of Doom"},{"type.object.name.en":"Jaws"},{"type.object.name.en":"Saving Private Ryan"},{"type.object.name.en":"Close Encounters of the Third Kind"}],"type.object.name.en":"Steven Spielberg"}]}`
+	expectedRes := `{"me":[{"director.film":[{"name.en":"Indiana Jones and the Temple of Doom"},{"name.en":"Jaws"},{"name.en":"Saving Private Ryan"},{"name.en":"Close Encounters of the Third Kind"}],"name.en":"Steven Spielberg"}]}`
 	if res != expectedRes {
 		log.Fatal("Query response is not as expected")
 	}
