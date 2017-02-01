2919a99e1c07aebd2cfc9c85151ea3e538db7806
Ganesh Acharya <ganeshacharya255@gmail.com>
2017-02-01 13:26:34 +0800

    refactor schema parsing

--- schema/parse.go
+++ schema/parse.go
@@ -34,8 +34,10 @@ func Parse(file string) (rerr error) {
 	return ParseBytes(b)
 }
 
-// ParseBytes parses the byte array which holds the schema.
+// ParseBytes parses the byte array which holds the schema. We will reset
+// all the globals.
 func ParseBytes(schema []byte) (rerr error) {
+	reset()
 	s := string(schema)
 
 	l := lex.NewLexer(s).Run(lexText)
@@ -43,91 +45,109 @@ func ParseBytes(schema []byte) (rerr error) {
 	it := l.NewIterator()
 	for it.Next() {
 		item := it.Item()
-		switch item.Typ {
-		case itemScalar:
-			{
-				if rerr = processScalar(it); rerr != nil {
-					return rerr
-				}
+		if item.Typ == lex.ItemEOF {
+			break
+		}
+		if item.Typ != itemText {
+			return x.Errorf("Expected text here but got: [%v] %v %v", item.Val, item.Typ, lex.EOF)
+		}
+		switch item.Val {
+		case "scalar":
+			if rerr = processScalar(it); rerr != nil {
+				return rerr
 			}
-		case itemType:
-			{
-				if rerr = processObject(it); rerr != nil {
-					return rerr
-				}
+		case "type":
+			if rerr = processObject(it); rerr != nil {
+				return rerr
 			}
-		case lex.ItemError:
-			return x.Errorf(item.Val)
+		default:
+			return x.Errorf("Expected either 'scalar' or 'type' but got: %v", item)
 		}
 	}
-
 	return nil
 }
 
+// processScalarBlock starts work on the inside of a scalar block.
 func processScalarBlock(it *lex.ItemIterator) error {
 	for it.Next() {
 		item := it.Item()
 		switch item.Typ {
 		case itemRightRound:
 			return nil
-		case itemScalarName:
-			{
-				var name, typ string
-				name = item.Val
-
-				it.Next()
-				if next := it.Item(); next.Typ != itemCollon {
-					return x.Errorf("Missing collon")
-				}
-
-				it.Next()
-				next := it.Item()
-				if next.Typ != itemScalarType {
-					return x.Errorf("Missing Type")
-				}
-				typ = next.Val
-
-				t, ok := types.TypeForName(typ)
-				if !ok {
-					return x.Errorf("Invalid type")
-				}
-				str[name] = t
-
-				// Check for index / reverse.
-				for it.Next() {
-					next = it.Item()
-					if next.Typ == lex.ItemError {
-						return x.Errorf(next.Val)
-					}
-					if next.Typ == itemDummy {
-						break
-					}
-					if next.Typ == itemAt {
-						it.Next()
-						next = it.Item()
-						if next.Typ == itemIndex {
-							if err := processIndexDirective(it, name, t); err != nil {
-								return err
-							}
-						} else if next.Typ == itemReverse {
-							if t != types.UidID {
-								return x.Errorf("Cannot reverse for non-UID type")
-							}
-							reversedFields[name] = true
-						} else {
-							return x.Errorf("Invalid index specification")
-						}
-					}
-				}
+		case itemText:
+			if err := processScalarPair(it, item.Val, true); err != nil {
+				return err
 			}
 		case lex.ItemError:
 			return x.Errorf(item.Val)
+		default:
+			return x.Errorf("Unexpected token: %v", item)
 		}
 	}
 
 	return nil
 }
 
+// processScalarPair processes "name: type (directive)" where name is already
+// consumed and is provided as input.
+func processScalarPair(it *lex.ItemIterator, name string, allowIndex bool) error {
+	it.Next()
+	if next := it.Item(); next.Typ != itemColon {
+		return x.Errorf("Missing colon")
+	}
+
+	it.Next()
+	next := it.Item()
+	if next.Typ != itemText {
+		return x.Errorf("Missing Type")
+	}
+	typ := next.Val
+	t, ok := types.TypeForName(typ)
+	if ok {
+		if t1, ok := str[name]; ok {
+			if t1 != t {
+				return x.Errorf("Same field cannot have multiple types")
+			}
+		} else {
+			str[name] = t
+		}
+	}
+
+	// Check for index / reverse.
+	for it.Next() {
+		next = it.Item()
+		if next.Typ == lex.ItemError {
+			return x.Errorf(next.Val)
+		}
+		if next.Typ == itemAt {
+			it.Next()
+			next = it.Item()
+			if next.Typ != itemText {
+				return x.Errorf("Missing directive name")
+			}
+			switch next.Val {
+			case "reverse":
+				if t != types.UidID {
+					return x.Errorf("Cannot reverse for non-UID type")
+				}
+				reversedFields[name] = true
+				return nil
+			case "index":
+				if !allowIndex {
+					return x.Errorf("@index not allowed")
+				}
+				return processIndexDirective(it, name, t)
+			default:
+				return x.Errorf("Invalid index specification")
+			}
+		}
+		it.Prev()
+		break
+	}
+	return nil
+}
+
+// processIndexDirective works on "@index" or "@index(customtokenizer)".
 func processIndexDirective(it *lex.ItemIterator, name string, typ types.TypeID) error {
 	indexedFields[name] = tok.Default(typ)
 	if !it.Next() {
@@ -161,80 +181,31 @@ func processIndexDirective(it *lex.ItemIterator, name string, typ types.TypeID) 
 	return nil
 }
 
+// processScalar works on either a single scalar pair or a scalar block.
+// A scalar block looks like "scalar ( .... )".
 func processScalar(it *lex.ItemIterator) error {
-
 	for it.Next() {
 		item := it.Item()
 		switch item.Typ {
 		case itemLeftRound:
-			{
-				return processScalarBlock(it)
-			}
-		case itemScalarName:
-			{
-				var name, typ string
-				name = item.Val
-
-				it.Next()
-				next := it.Item()
-				if next.Typ != itemCollon {
-					return x.Errorf("Missing collon")
-				}
-
-				it.Next()
-				next = it.Item()
-				if next.Typ != itemScalarType {
-					return x.Errorf("Missing Type")
-				}
-				typ = next.Val
-
-				t, ok := types.TypeForName(typ)
-				if ok {
-					str[name] = t
-				} else {
-					return x.Errorf("Invalid type")
-				}
-
-				// Check for index.
-				for it.Next() {
-					next = it.Item()
-					if next.Typ == lex.ItemError {
-						return x.Errorf(next.Val)
-					}
-					if next.Typ == itemDummy {
-						break
-					}
-					if next.Typ == itemAt {
-						it.Next()
-						next = it.Item()
-						if next.Typ == itemIndex {
-							if err := processIndexDirective(it, name, t); err != nil {
-								return err
-							}
-						} else if next.Typ == itemReverse {
-							if t != types.UidID {
-								return x.Errorf("Cannot reverse for non-UID type")
-							}
-							reversedFields[name] = true
-						} else {
-							return x.Errorf("Invalid index specification")
-						}
-					}
-				}
-				return nil
-			}
+			return processScalarBlock(it)
+		case itemText:
+			return processScalarPair(it, item.Val, true)
 		case lex.ItemError:
 			return x.Errorf(item.Val)
+		default:
+			return x.Errorf("Unexpected item: %v", item)
 		}
 	}
 	return nil
 }
 
+// processObject works on "type { ... }".
 func processObject(it *lex.ItemIterator) error {
 	var objName string
 	it.Next()
 	next := it.Item()
-	if next.Typ != itemObject {
+	if next.Typ != itemText {
 		return x.Errorf("Missing object name")
 	}
 	objName = next.Val
@@ -254,51 +225,11 @@ L:
 		switch item.Typ {
 		case itemRightCurl:
 			break L
-		case itemObjectName:
-			{
-				var name, typ string
-				name = item.Val
-
-				it.Next()
-				next := it.Item()
-				if next.Typ != itemCollon {
-					return x.Errorf("Missing collon")
-				}
-
-				it.Next()
-				next = it.Item()
-				if next.Typ != itemObjectType {
-					return x.Errorf("Missing Type")
-				}
-				typ = next.Val
-				t, ok := types.TypeForName(typ)
-				if ok {
-					if t1, ok := str[name]; ok {
-						if t1 != t {
-							return x.Errorf("Same field cant have multiple types")
-						}
-					} else {
-						str[name] = t
-					}
-				}
-				// Check for reverse.
-				it.Next()
-				next = it.Item()
-				if next.Typ == itemAt {
-					it.Next()
-					index := it.Item()
-					if index.Typ == itemReverse {
-						// TODO(jchiu): Add test for this check.
-						if t.IsScalar() /* && t != types.UidID */ {
-							return x.Errorf("Cannot reverse non-UID scalar")
-						}
-						reversedFields[name] = true
-					} else {
-						return x.Errorf("Invalid reverse specification")
-					}
-				}
-				fieldCount++
+		case itemText:
+			if err := processScalarPair(it, item.Val, false); err != nil {
+				return err
 			}
+			fieldCount++
 		case lex.ItemError:
 			return x.Errorf(item.Val)
 		}
--- schema/parse_test.go
+++ schema/parse_test.go
@@ -26,9 +26,37 @@ import (
 	"github.com/dgraph-io/dgraph/x"
 )
 
+type nameType struct {
+	name string
+	typ  types.TypeID
+}
+
+func checkSchema(t *testing.T, h map[string]types.TypeID, expected []nameType) {
+	require.Len(t, h, len(expected))
+	for _, nt := range expected {
+		typ, found := h[nt.name]
+		require.True(t, found, nt)
+		require.EqualValues(t, nt.typ, typ)
+	}
+}
+
 func TestSchema(t *testing.T) {
 	str = make(map[string]types.TypeID)
 	require.NoError(t, Parse("testfiles/test_schema"))
+	checkSchema(t, str, []nameType{
+		{"name", types.StringID},
+		{"address", types.StringID},
+		{"http://film.com/name", types.StringID},
+		{"http://scalar.com/helloworld/", types.StringID},
+		{"age", types.Int32ID},
+		{"budget", types.Int32ID},
+		{"http://film.com/budget", types.Int32ID},
+		{"NumFollower", types.Int32ID},
+		{"Person", types.UidID},
+		{"Actor", types.UidID},
+		{"Film", types.UidID},
+		{"http://film.com/", types.UidID},
+	})
 }
 
 func TestSchema1_Error(t *testing.T) {
@@ -87,6 +115,12 @@ func TestSchemaIndex_Error2(t *testing.T) {
 func TestSchemaIndexCustom(t *testing.T) {
 	str = make(map[string]types.TypeID)
 	require.NoError(t, Parse("testfiles/test_schema_index4"))
+	checkSchema(t, str, []nameType{
+		{"name", types.StringID},
+		{"address", types.StringID},
+		{"age", types.Int32ID},
+		{"Person", types.UidID},
+	})
 	require.Equal(t, 3, len(indexedFields))
 	require.Equal(t, "int", indexedFields["age"].Name())
 	require.Equal(t, "exact", indexedFields["name"].Name())
