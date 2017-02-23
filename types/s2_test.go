b050fd16513f30493ecfa78be2a6dfc6af711e01
Ashwin Ramesh <ashwin@dgraph.io>
2017-02-23 15:04:19 +1100

    Update

--- types/s2_test.go
+++ types/s2_test.go
@@ -22,7 +22,8 @@ func TestConvertToGeoJson_Poly(t *testing.T) {
 	require.Equal(t,
 		[][]geom.Coord{[]geom.Coord{geom.Coord{1.123, 2.543}, geom.Coord{-3.23, 4.123}, geom.Coord{4.43, -6.123}, geom.Coord{1.123, 2.543}}},
 		b.(*geom.Polygon).Coords())
-	x, err := geojson.Marshal(b)
+	_, err = geojson.Marshal(b)
+	require.NoError(t, err)
 }
 
 func TestConvertToGeoJson_PolyError1(t *testing.T) {
