18f9fc0f4f556da42e1e7b627a45f90b7d3aff96
Ashwin Ramesh <ashwin@dgraph.io>
2017-03-14 11:24:14 +1100

    Update

--- query/query_test.go
+++ query/query_test.go
@@ -2058,7 +2058,6 @@ func TestToFastJSONReverseDelSet(t *testing.T) {
 	require.JSONEq(t,
 		`{"me":[{"name":"Glenn Rhee","~friend":[{"name":"Daryl Dixon"},{"name":"Andrea"}]}]}`,
 		js)
-	delEdgeToUID(t, "friend", 25, 24)
 }
 
 func TestToFastJSONReverseDelSetCount(t *testing.T) {
@@ -2079,7 +2078,6 @@ func TestToFastJSONReverseDelSetCount(t *testing.T) {
 	require.JSONEq(t,
 		`{"me":[{"name":"Glenn Rhee","~friend":[{"count":2}]}]}`,
 		js)
-	delEdgeToUID(t, "friend", 25, 24)
 }
 
 func getProperty(properties []*graph.Property, prop string) *graph.Value {
