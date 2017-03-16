39d92dd127c7a1cba0edbd85bc37646353b03945
Ashwin Ramesh <ashwin@dgraph.io>
2017-03-16 18:57:12 +1100

    Fix

--- worker/task.go
+++ worker/task.go
@@ -289,7 +289,7 @@ func processTask(q *taskp.Query, gid uint32) (*taskp.Result, error) {
 			x.AssertTruef(pk.IsData() && pk.Attr == q.Attr,
 				"Invalid key obtained for comparison")
 			key := it.Key().Data()
-			pl, decr := posting.GetOrUnmarshal(key, it.Value().Data(), gid)
+			pl, decr := posting.GetOrUnmarshal(key, it.Value().Data())
 			count := int64(pl.Length(0))
 			decr()
 			if EvalCompare(srcFn.fname, count, srcFn.threshold) {
@@ -314,7 +314,7 @@ func processTask(q *taskp.Query, gid uint32) (*taskp.Result, error) {
 			x.AssertTrue(pk.Attr == q.Attr)
 			term := pk.Term[1:] // skip the first byte which is tokenizer prefix.
 			if srcFn.regex.MatchString(term) {
-				pl, decr := posting.GetOrUnmarshal(key, it.Value().Data(), gid)
+				pl, decr := posting.GetOrUnmarshal(key, it.Value().Data())
 				out.UidMatrix = append(out.UidMatrix, pl.Uids(opts))
 				decr()
 			}
