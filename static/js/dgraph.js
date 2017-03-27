f4c22c090649bffe192ddedbe499f516fe6ec8dd
Pawan Rawal <pawan0201@gmail.com>
2017-03-27 14:28:48 +1100

    Add some css, change base_url and open external links a new page.
    * Add css for warning and open external links in new tab
    
    * Change base url to match Hugo naming convention for folders and add CSS for incomplete

--- static/js/dgraph.js
+++ static/js/dgraph.js
@@ -105,4 +105,14 @@ $(document).ready(function() {
       pre.appendChild(showMore);
     }
   });
+
+  // Add target = _blank to all external links.
+  var links = document.links;
+
+  for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+     if (links[i].hostname != window.location.hostname) {
+         links[i].target = '_blank';
+     }
+  }
+
 });
