2148acb1dd1e13a5e952d982330e0d73ac4706b1
Pawan Rawal <pawan@dgraph.io>
2017-03-28 17:46:39 +1100

    Add build script for nginx

--- wiki/scripts/build.sh
+++ wiki/scripts/build.sh
@@ -1,8 +1,42 @@
-# HOST=https://wiki.dgraph.io
-#HOST=http://localhost:8080
-HOST=http://34.208.35.222
-VERSION=0.7.3
+#!/bin/bash
 
-HUGO_TITLE="Dgraph Doc ${VERSION}" hugo\
-     --destination=public/latest\
-     --baseURL=$HOST/latest
+HOST=http://docs.dgraph.io
+
+rebuild() {
+	echo "$(date) Updating docs for branch: $1"
+	# Generate new docs after merging.
+	HUGO_TITLE="Dgraph Doc ${2}" hugo\
+		--destination=public/$2\
+		--baseURL=$HOST/$2 1> /dev/null
+}
+
+checkAndUpdate()
+{
+	local branch=$1
+	git checkout -q $1
+	local tag=$2
+
+	UPSTREAM=$(git rev-parse "@{u}")
+	LOCAL=$(git rev-parse "@")
+
+	if [ $LOCAL != $UPSTREAM ] ; then
+		git merge -q origin/$branch
+		rebuild $branch $tag
+	fi
+
+	if [ ! -d "public/$tag" ] ; then
+		rebuild $branch $tag
+	fi
+}
+
+# Lets move to the Wiki directory.
+cd /home/ubuntu/dgraph/wiki
+
+while true; do
+	echo -e "Starting to check branches\n"
+	git remote update > /dev/null
+	checkAndUpdate "release/v0.7.4" "v0.7.4"
+	checkAndUpdate "master" "master"
+	echo -e "Done checking branches\n"
+	sleep 60
+done
