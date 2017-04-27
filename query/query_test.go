4dc3d36991c5b0df04a092234e6739d93cb60ed2
Pawan Rawal <pawan@dgraph.io>
2017-04-27 14:06:35 +1000

    Fix bugs related to string quoting. Also fix #862

--- query/query_test.go
+++ query/query_test.go
@@ -284,6 +284,8 @@ func populateGraph(t *testing.T) {
 	addEdgeToUID(t, "son", 1, 2300, nil)
 	addEdgeToValue(t, "name", 2300, "Andre", nil)
 
+	addEdgeToValue(t, "name", 2301, `Alice\"`, nil)
+
 	time.Sleep(5 * time.Millisecond)
 }
 
@@ -5993,4 +5995,19 @@ children: <
   >
 >
 `, proto.MarshalTextString(pb[0]))
+}
+
+func TestStringEscape(t *testing.T) {
+	populateGraph(t)
+	query := `
+		{
+			me(id: 2301) {
+				name
+			}
+		}
+	`
+	js := processToFastJSON(t, query)
+	require.JSONEq(t,
+		`{"me":[{"name":"Alice\""}]}`,
+		js)
 }
