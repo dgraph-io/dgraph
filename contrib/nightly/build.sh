5010d279cd20407cde30b850bebae31db7621dbf
Pawan Rawal <pawan@dgraph.io>
2017-05-02 12:15:23 +1000

    Add deps

--- contrib/nightly/build.sh
+++ contrib/nightly/build.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+go get -u golang.org/x/net/context
+go get -u golang.org/x/text/unicode/norm
+go get -u google.golang.org/grpc
+
 pushd contrib/releases &> /dev/null
 
 # Building embedded binaries.
