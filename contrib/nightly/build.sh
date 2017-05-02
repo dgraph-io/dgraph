508ce12a7656247f3989ece74dc6a27c3d3bd9d7
Pawan Rawal <pawan@dgraph.io>
2017-05-02 14:18:18 +1000

    upload release

--- contrib/nightly/build.sh
+++ contrib/nightly/build.sh
@@ -2,14 +2,67 @@
 
 set -e
 
-go get -u golang.org/x/net/context
-go get -u golang.org/x/text/unicode/norm
-go get -u google.golang.org/grpc
+BUILD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+echo ${BUILD_DIR}
+source ${BUILD_DIR}/contrib/nightly/github.sh
 
-pushd contrib/releases &> /dev/null
+NIGHTLY_TAG="nightly"
+DGRAPH_REPO="dgraph-io/dgraph"
+DGRAPH_VERSION=$(git describe --abbrev=0)
+DGRAPH_COMMIT=$(git rev-parse --short HEAD)
+TAR_FILE="dgraph-linuxplatform-amd64-${DGRAPH_VERSION}"
+NIGHTLY_FILE="${BUILD_DIR}/${TAR_FILE}"
+
+delete_old_nightly() {
+  local release_id
+  read release_id < <( \
+    send_gh_api_request repos/${DGRAPH_REPO}/releases \
+    | jq -r -c "(.[] | select(.tag_name == \"${NIGHTLY_TAG}\").id), \"\"") \
+    || exit
+
+  echo $release_id
+  if [[ ! -z "${release_id}" ]]; then
+    send_gh_api_request repos/${DGRAPH_REPO}/releases/${release_id} \
+        DELETE \
+        > /dev/null
+  fi
+}
+
+get_release_body() {
+  echo 'Dgraph development (pre-release) build. See **[Installing-Dgraph](http://docs.dgraph.io/master/deploy#manual-download-optional)**.'
+}
+
+upload_nightly() {
+  echo "Creating release for tag ${NIGHTLY_TAG}."
+  read release_id < <( \
+    send_gh_api_data_request repos/${DGRAPH_REPO}/releases POST \
+    "{ \"name\": \"Dgraph v${DGRAPH_VERSION}-dev\", \"tag_name\": \"${NIGHTLY_TAG}\", \
+    \"prerelease\": true }" \
+    | jq -r -c '.id') \
+    || exit
+
+  echo 'Updating release description.'
+  send_gh_api_data_request repos/${DGRAPH_REPO}/releases/${release_id} PATCH \
+    "{ \"body\": $(get_release_body | jq -s -c -R '.') }" \
+    > /dev/null
+
+  echo "Updating ${NIGHTLY_TAG} tag to point to ${DGRAPH_COMMIT}."
+  send_gh_api_data_request repos/${DGRAPH_REPO}/git/refs/tags/${NIGHTLY_TAG} PATCH \
+    "{ \"force\": true, \"sha\": \"${DGRAPH_COMMIT}\" }" \
+    > /dev/null
+
+  echo 'Uploading package.'
+  local name="dgraph-linux64.tar.gz"
+  upload_release_asset ${NIGHTLY_FILE} "$name" \
+    ${DGRAPH_REPO} ${release_id} \
+    > /dev/null
+}
+
+go get -u golang.org/x/net/context golang.org/x/text/unicode/norm google.golang.org/grpc
 
 # Building embedded binaries.
-./build.sh
-
+contrib/releases/build.sh
+pwd
 ls
-popd &> /dev/null
+delete_old_nightly
+upload_nightly
---
+++ contrib/nightly/github.sh
@@ -0,0 +1,71 @@
+# Ported over beautiful code from https://raw.githubusercontent.com/neovim/bot-ci/master/ci/common/github-api.sh.
+
+# Exit if there's an error message.
+# ${1}: Additional information about API call.
+_check_gh_error() {
+  local response="${1}"
+  local error_message="$(echo "${response}" | jq -r '.message?')"
+  if [[ -n "${error_message}" && "${error_message}" != 'null' ]]; then
+    >&2 echo "Error ${2}: ${error_message}."
+    return 1
+  else
+    echo "${response}"
+  fi
+}
+
+# Send a request to the Github API.
+# ${1}: API endpoint.
+# ${2}: HTTP verb (default: GET).
+send_gh_api_request() {
+  local endpoint="${1}"
+  local verb="${2:-GET}"
+
+  local response="$(curl -H "Accept: application/vnd.github.v3+json" \
+    -H "User-Agent: travis" \
+    -u "${GH_TOKEN}:x-oauth-basic" \
+    -X ${verb} \
+    https://api.github.com/${endpoint} \
+    2>/dev/null)"
+  _check_gh_error "${response}" "calling ${endpoint} (${verb})"
+}
+
+# Send a data request to the Github API.
+# ${1}: API endpoint.
+# ${2}: HTTP verb.
+# ${3}: JSON data.
+send_gh_api_data_request() {
+  local endpoint="${1}"
+  local verb="${2}"
+  local data="${3}"
+
+  local response="$(curl -H "Accept: application/vnd.github.v3+json" \
+    -H "User-Agent: travis" \
+    -u "${GH_TOKEN}:x-oauth-basic" \
+    -X ${verb} \
+    -d "${data}" \
+    https://api.github.com/${endpoint} \
+    2>/dev/null)"
+  _check_gh_error "${response}" "calling ${endpoint} (${verb})"
+}
+
+# Upload an asset to a Github release.
+# ${1}: Local path to file to upload.
+# ${2}: Desired file name on Github.
+# ${3}: Repository to upload the asset to.
+# ${4}: Release ID to upload the asset to.
+upload_release_asset() {
+  local file="${1}"
+  local file_name="${2}"
+  local repository="${3}"
+  local release_id="${4}"
+  local mime_type="$(file --mime-type -b "${file}")"
+
+  local response="$(curl -H "Accept: application/vnd.github.v3+json" \
+    -H "User-Agent: travis" \
+    -H "Content-Type: ${mime_type}" \
+    -u "${GH_TOKEN}:x-oauth-basic" \
+    -T "${file}"\
+    https://uploads.github.com/repos/${repository}/releases/${release_id}/assets?name=${file_name} \
+    2>/dev/null)"
+  _check_gh_error "${response}" 'uploading release assets'
+}
