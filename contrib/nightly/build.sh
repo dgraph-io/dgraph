33d0bd113e29f69c785d885387d11c218e0f6cb0
Pawan Rawal <pawan@dgraph.io>
2017-05-02 12:05:49 +1000

    Try building embedded binaries

---
+++ contrib/nightly/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SRC="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
+
+BUILD=$1
+# If build variable is empty then we set it.
+if [ -z "$1" ]; then
+  BUILD=$SRC/build
+fi
+
+ROCKSDBDIR=$BUILD/rocksdb-5.1.4
+
+set -e
+
+pushd $BUILD &> /dev/null
+
+benchmark=$(pwd)
+popd &> /dev/null
+
+# build flags needed for rocksdb
+export CGO_CPPFLAGS="-I${ROCKSDBDIR}/include"
+export CGO_LDFLAGS="-L${ROCKSDBDIR}"
+export LD_LIBRARY_PATH="${ROCKSDBDIR}:${LD_LIBRARY_PATH}"
+
+pushd contrib/releases &> /dev/null
+
+# Building embedded binaries.
+./build.sh
+
+ls
+popd &> /dev/null
