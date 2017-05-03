a22811a2cd3b07f860606b95630e5a5da82836aa
Pawan Rawal <pawan@dgraph.io>
2017-05-03 12:52:45 +1000

    add pass to travis

--- contrib/nightly/build.sh
+++ contrib/nightly/build.sh
@@ -86,16 +86,16 @@ upload_nightly() {
 }
 
 upload_docker_image() {
-  pushd ${GOPATH}/src/github.com/dgraph-io/dgraph/contrib/nightly > /dev/null
-  # Extract dgraph folder from the tar as its required by the Dockerfile.
-  tar -xzf ${NIGHTLY_FILE}
-  cp ${ASSETS_FILE} .
-  echo "Building the dgraph master image."
-  docker build -t dgraph/dgraph:master .
+#  pushd ${GOPATH}/src/github.com/dgraph-io/dgraph/contrib/nightly > /dev/null
+#  # Extract dgraph folder from the tar as its required by the Dockerfile.
+#  tar -xzf ${NIGHTLY_FILE}
+#  cp ${ASSETS_FILE} .
+#  echo "Building the dgraph master image."
+#  docker build -t dgraph/dgraph:master .
   echo "Logging into Docker."
   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-  echo "Pushing the image"
-  docker push dgraph/dgraph:master
+#  echo "Pushing the image"
+#  docker push dgraph/dgraph:master
   popd > /dev/null
 }
 
@@ -103,8 +103,8 @@ go get -u golang.org/x/net/context golang.org/x/text/unicode/norm google.golang.
 
 # Building embedded binaries.
 echo "Building embedded binaries"
-contrib/releases/build.sh
-delete_old_nightly
-upload_nightly
+#contrib/releases/build.sh
+#delete_old_nightly
+#upload_nightly
 
 upload_docker_image
