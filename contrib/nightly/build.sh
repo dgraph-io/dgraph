8e1628dc6b6404cf3e8fbb589882b7bc80fbddab
Pawan Rawal <pawan@dgraph.io>
2017-05-04 11:35:50 +1000

    build binary for all platforms

--- contrib/nightly/build.sh
+++ contrib/nightly/build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
-  exit 0;
-fi
-
 if [[ $TRAVIS_TAG == "nightly" ]]; then
   # We create nightly tag using the script so we don't want to run this script
   # when the tagged build is triggered.
@@ -16,14 +12,20 @@ fi
 
 set -e
 
+if [[ $TRAVIS_OS_NAME == "osx" ]]; then
+  OS="darwin"
+else
+  OS="linux"
+fi
+
 BUILD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 source ${BUILD_DIR}/nightly/github.sh
 
 NIGHTLY_TAG="nightly"
 DGRAPH_REPO="dgraph-io/dgraph"
 DGRAPH_VERSION=$(git describe --abbrev=0)
-DGRAPH_COMMIT=$(git rev-parse HEAD)
-TAR_FILE="dgraph-linux-amd64-${DGRAPH_VERSION}.tar.gz"
+DGRAPH_COMMIT=$(git rev-parse master)
+TAR_FILE="dgraph-${OS}-amd64-${DGRAPH_VERSION}.tar.gz"
 NIGHTLY_FILE="${GOPATH}/src/github.com/dgraph-io/dgraph/${TAR_FILE}"
 ASSETS_FILE="${GOPATH}/src/github.com/dgraph-io/dgraph/assets.tar.gz"
 
@@ -59,36 +61,56 @@ get_release_body() {
 }
 
 upload_nightly() {
-  echo "Creating release for tag ${NIGHTLY_TAG}."
+  local release_id
+  # We check if a release with tag nightly already exists, else we create it.
   read release_id < <( \
-    send_gh_api_data_request repos/${DGRAPH_REPO}/releases POST \
-    "{ \"name\": \"Dgraph ${DGRAPH_VERSION}-dev\", \"tag_name\": \"${NIGHTLY_TAG}\", \
-    \"prerelease\": true }" \
-    | jq -r -c '.id') \
+    send_gh_api_request repos/${DGRAPH_REPO}/releases \
+    | jq -r -c "(.[] | select(.tag_name == \"${NIGHTLY_TAG}\").id), \"\"") \
     || exit
 
-  echo 'Updating release description.'
-  send_gh_api_data_request repos/${DGRAPH_REPO}/releases/${release_id} PATCH \
-    "{ \"body\": $(get_release_body | jq -s -c -R '.') }" \
-    > /dev/null
+  if [[ -z "${release_id}" ]]; then
+    echo "Creating release for tag ${NIGHTLY_TAG}."
+    read release_id < <( \
+      send_gh_api_data_request repos/${DGRAPH_REPO}/releases POST \
+      "{ \"name\": \"Dgraph ${DGRAPH_VERSION}-dev\", \"tag_name\": \"${NIGHTLY_TAG}\", \
+      \"prerelease\": true }" \
+      | jq -r -c '.id') \
+      || exit
+  else
+    # We upload the tar binary.
+    echo 'Uploading package.'
+    local name="dgraph-${OS}-amd64-${DGRAPH_VERSION}-dev.tar.gz"
+    upload_release_asset ${NIGHTLY_FILE} "$name" \
+      ${DGRAPH_REPO} ${release_id} \
+      > /dev/null
 
-  echo "Updating ${NIGHTLY_TAG} tag to point to ${DGRAPH_COMMIT}."
-  send_gh_api_data_request repos/${DGRAPH_REPO}/git/refs/tags/${NIGHTLY_TAG} PATCH \
-    "{ \"force\": true, \"sha\": \"${DGRAPH_COMMIT}\" }" \
-    > /dev/null
+    if [[ $TRAVIS_OS_NAME == "linux" ]]; then
+      # As asset would be the same on both platforms, we only upload it from linux.
+      upload_release_asset ${ASSETS_FILE} "assets.tar.gz" \
+        ${DGRAPH_REPO} ${release_id} \
+        > /dev/null
+    fi
+  fi
 
-  echo 'Uploading package.'
-  local name="dgraph-linux64.tar.gz"
-  upload_release_asset ${NIGHTLY_FILE} "$name" \
-    ${DGRAPH_REPO} ${release_id} \
-    > /dev/null
+  if [[ $TRAVIS_OS_NAME == "linux" ]]; then
+    echo 'Updating release description.'
+    send_gh_api_data_request repos/${DGRAPH_REPO}/releases/${release_id} PATCH \
+      "{ \"body\": $(get_release_body | jq -s -c -R '.') }" \
+      > /dev/null
 
-  upload_release_asset ${ASSETS_FILE} "assets.tar.gz" \
-    ${DGRAPH_REPO} ${release_id} \
-    > /dev/null
+    echo "Updating ${NIGHTLY_TAG} tag to point to ${DGRAPH_COMMIT}."
+    send_gh_api_data_request repos/${DGRAPH_REPO}/git/refs/tags/${NIGHTLY_TAG} PATCH \
+      "{ \"force\": true, \"sha\": \"${DGRAPH_COMMIT}\" }" \
+      > /dev/null
+
+  fi
 }
 
 upload_docker_image() {
+  if [[ $TRAVIS_OS_NAME == "osx" ]]; then
+    return 0
+  fi
+
   pushd ${GOPATH}/src/github.com/dgraph-io/dgraph/contrib/nightly > /dev/null
   # Extract dgraph folder from the tar as its required by the Dockerfile.
   tar -xzf ${NIGHTLY_FILE}
@@ -102,11 +124,21 @@ upload_docker_image() {
   popd > /dev/null
 }
 
+local nightly_sha
+read nightly_sha < <( \
+  send_gh_api_request repos/${DGRAPH_REPO}/git/refs/tags/${NIGHTLY_TAG} \
+  | jq -r '.object.sha') || true
+
+if [[ $nightly_sha == $DGRAPH_COMMIT ]]; then
+  echo "nightly $nightly_sha, dgraph commit $DGRAPH_COMMIT"
+  echo "Latest commit on master hasn't changed. Exiting"
+  exit 0
+fi
 go get -u golang.org/x/net/context golang.org/x/text/unicode/norm google.golang.org/grpc
 
 echo "Building embedded binaries"
 contrib/releases/build.sh
-delete_old_nightly
+# delete_old_nightly
 upload_nightly
 
 upload_docker_image
