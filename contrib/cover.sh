ae591c9ed688c030fb94796a8691a92ee8e1559b
Pawan Rawal <pawan@dgraph.io>
2017-05-19 17:16:34 +1000

    Update

--- contrib/cover.sh
+++ contrib/cover.sh
@@ -17,11 +17,6 @@ rm -f $OUT
 
 set -e
 
-# Lets install the dependencies that are not vendored in anymore.
-go get -d golang.org/x/net/context
-go get -d google.golang.org/grpc/...
-go get -u github.com/dgraph-io/badger/...
-
 pushd $SRC &> /dev/null
 
 # create coverage output
