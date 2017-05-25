7946571b02bf1ed16edd7ce38b44fe14a1aecd3e
Szymon Matejczyk <smatejczyk@gmail.com>
2017-05-25 13:39:34 +0200

    load test - common posting lists dir

--- contrib/queries.sh
+++ contrib/queries.sh
@@ -25,7 +25,7 @@ set -e
 pushd cmd/dgraph &> /dev/null
 go build .
 # Start dgraph in the background.
-./dgraph --w ~/dgraph/w2 &
+./dgraph -w ~/dgraph/w2 -p $BUILD/p &
 
 # Wait for server to start in the background.
 until nc -z 127.0.0.1 8080;
