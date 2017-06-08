bb4f97cedbad68b268fa755901a490ab55d81fca
Sung Won Cho <mikeswcho@gmail.com>
2017-06-08 16:15:37 +1000

    Implement root page for docs

--- wiki/scripts/local.sh
+++ wiki/scripts/local.sh
@@ -2,6 +2,7 @@
 
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 VERSIONS=(
+  'v0.7.7'
   'v0.7.6'
   'master'
   'v0.7.5'
@@ -15,15 +16,6 @@ joinVersions() {
 
 VERSION_STRING=$(joinVersions)
 
-# ensure_doc_branch exits the script if doc is run on a branch not supported
-ensure_doc_branch() {
-  if [ $CURRENT_BRANCH != "master" ] && ! [[ $CURRENT_BRANCH =~ ^release\/v[0-9]\.[0-9]\.[0-9]$ ]]; then
-    echo "You can only run documentation on 'master' or 'release/vx.x.x' branches."
-    echo "See: https://github.com/dgraph-io/dgraph/tree/doc/wiki#branch"
-    exit 1
-  fi
-}
-
 run() {
   export CURRENT_BRANCH=${CURRENT_BRANCH}
   export VERSIONS=${VERSION_STRING}
@@ -33,5 +25,4 @@ run() {
   CURRENT_BRANCH=${CURRENT_BRANCH} hugo server -w
 }
 
-ensure_doc_branch
 run
