5b1a5c660ce3b6bfdd575011030d25d56c78aab6
Pawan Rawal <pawan@dgraph.io>
2017-06-09 10:46:12 +1000

    Fix issues related to tracing

--- cmd/dgraph/main.go
+++ cmd/dgraph/main.go
@@ -542,6 +542,7 @@ func queryHandler(w http.ResponseWriter, r *http.Request) {
 
 	if rand.Float64() < *tracing {
 		tr := trace.New("Dgraph", "Query")
+		tr.SetMaxEvents(1000)
 		defer tr.Finish()
 		ctx = trace.NewContext(ctx, tr)
 	}
@@ -570,9 +571,8 @@ func queryHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// set timeout if schema mutation not present
+	// set timeout for read-only queries.
 	if res.Mutation == nil || len(res.Mutation.Schema) == 0 {
-		// If schema mutation is not present
 		var cancel context.CancelFunc
 		ctx, cancel = context.WithTimeout(ctx, time.Minute)
 		defer cancel()
@@ -809,6 +809,7 @@ func (s *grpcServer) Run(ctx context.Context,
 	var schemaNodes []*protos.SchemaNode
 	if rand.Float64() < *tracing {
 		tr := trace.New("Dgraph", "GrpcQuery")
+		tr.SetMaxEvents(1000)
 		defer tr.Finish()
 		ctx = trace.NewContext(ctx, tr)
 	}
@@ -834,6 +835,13 @@ func (s *grpcServer) Run(ctx context.Context,
 		return resp, err
 	}
 	l.Parsing += time.Since(l.Start)
+
+	// set timeout for read-only queries.
+	if res.Mutation == nil || len(res.Mutation.Schema) == 0 {
+		var cancel context.CancelFunc
+		ctx, cancel = context.WithTimeout(ctx, time.Minute)
+		defer cancel()
+	}
 
 	// If mutations are part of the query, we run them through the mutation handler
 	// same as the http client.
