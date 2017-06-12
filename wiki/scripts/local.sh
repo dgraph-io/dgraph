5862dc967543105e6eb2223a684d31ed5635f294
Sung Won Cho <mikeswcho@gmail.com>
2017-06-12 17:35:15 +1000

    Pass current version so that theme can generate warning for older version of doc

--- wiki/scripts/local.sh
+++ wiki/scripts/local.sh
@@ -18,11 +18,14 @@ VERSION_STRING=$(joinVersions)
 
 run() {
   export CURRENT_BRANCH=${CURRENT_BRANCH}
+  export CURRENT_VERSION=${VERSIONS[0]}
   export VERSIONS=${VERSION_STRING}
+
 
   HUGO_TITLE="Dgraph Doc - local" \
   VERSIONS=${VERSION_STRING} \
-  CURRENT_BRANCH=${CURRENT_BRANCH} hugo server -w
+  CURRENT_BRANCH=${CURRENT_BRANCH} \
+  CURRENT_VERSION=${CURRENT_VERSION} hugo server -w
 }
 
 run
