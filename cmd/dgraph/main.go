48380dbe896b1b5a89e5c37b65ae5bce24bb3405
Janardhan Reddy <janardhan@dgraph.io>
2017-07-14 11:51:51 +1000

    100 chars

--- cmd/dgraph/main.go
+++ cmd/dgraph/main.go
@@ -88,8 +88,9 @@ func setupConfigOpts() {
 	flag.StringVar(&config.PostingDir, "p", defaults.PostingDir,
 		"Directory to store posting lists.")
 	flag.StringVar(&config.PostingTables, "posting_tables", defaults.PostingTables,
-		"Specifies how Badger LSM tree is stored. Options are loadtoram, memorymap and nothing;"+
-			" which consume most to least RAM while providing best to worst performance respectively")
+		"Specifies how Badger LSM tree is stored. Options are loadtoram, memorymap and "+
+			"nothing; which consume most to least RAM while providing best to worst "+
+			"performance respectively")
 	flag.StringVar(&config.WALDir, "w", defaults.WALDir,
 		"Directory to store raft write-ahead logs.")
 	flag.BoolVar(&config.Nomutations, "nomutations", defaults.Nomutations,
