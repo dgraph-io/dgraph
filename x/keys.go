dfa3aeeb692bde4c39afa198885b08424c48a94b
Pawan Rawal <pawan@dgraph.io>
2017-07-18 15:59:59 +1000

    Update

--- x/keys.go
+++ x/keys.go
@@ -161,13 +161,7 @@ func (p ParsedKey) SkipRangeOfSameType() []byte {
 	rest := buf[1:]
 	k := writeAttr(rest, p.Attr)
 	AssertTrue(len(k) == 1)
-	if p.byteType == byteCountRev {
-		// We need to move to next predicate.
-		k[0] = p.byteType + 1
-	} else {
-		// We need to move to next type of key.
-		k[0] = p.byteType * 2
-	}
+	k[0] = p.byteType + 1
 	return buf
 }
 
