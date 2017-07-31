476cb0fa40581052b410c6413195860ca2375352
Pawan Rawal <pawan@dgraph.io>
2017-07-31 12:26:08 +1000

    Some changes

--- worker/backup.go
+++ worker/backup.go
@@ -1,18 +1,18 @@
 /*
- * Copyright (C) 2017 Dgraph Labs, Inc. and Contributors
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* Copyright (C) 2017 Dgraph Labs, Inc. and Contributors
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package worker
@@ -31,6 +31,7 @@ import (
 	"golang.org/x/net/context"
 
 	"github.com/dgraph-io/dgraph/group"
+	"github.com/dgraph-io/dgraph/protos"
 	"github.com/dgraph-io/dgraph/x"
 	"golang.org/x/net/trace"
 )
@@ -56,22 +57,25 @@ func writeBackupToFile(f *os.File, ch chan []byte) error {
 	return w.Flush()
 }
 
-// Export creates a export of data by exporting it as an RDF gzip.
-func backup(gids []uint32, bdir string) error {
-	// Use a goroutine to write to file.
-	err := os.MkdirAll(bdir, 0700)
-	fmt.Println(bdir)
-	if err != nil {
-		return err
+func handleBackupForGroup(ctx context.Context, gid uint32, bdir string) *protos.ExportPayload {
+	reply := &protos.ExportPayload{
+		Status: protos.ExportPayload_FAILED,
 	}
 
-	fileMap := make(map[uint32]chan []byte)
+	if ctx.Err() != nil {
+		return reply, ctx.Err()
+	}
 
-	errChan := make(chan error, len(gids))
+	// Use a goroutine to write to file.
+	if err := os.MkdirAll(bdir, 0700); err != nil {
+		return err
+	}
+	fmt.Println(bdir)
+
 	curTime := time.Now().Format("2006-01-02-15-04")
+	fpath := path.Join(bdir, fmt.Sprintf("dgraph-backup-%d-%s.rdf.gz", gid, curTime))
 	var allPaths []string
 	for _, gid := range gids {
-		fpath := path.Join(bdir, fmt.Sprintf("dgraph-%d-%s.rdf.gz", gid, curTime))
 		fmt.Printf("Backing up to: %v", fpath)
 		chb := make(chan []byte, 1000)
 		f, err := os.Create(fpath)
@@ -93,12 +97,14 @@ func backup(gids []uint32, bdir string) error {
 	prefix := new(bytes.Buffer)
 	prefix.Grow(100)
 	var debugCount int
-	for it.Rewind(); it.Valid(); debugCount++ {
+	for it.Rewind(); it.Valid(); debugCount++; it.Next() {
 		item := it.Item()
 		key := item.Key()
 		pk := x.Parse(key)
 
-		if chb, ok := fileMap[group.BelongsTo(pk.Attr)]; ok {
+		if g := group.BelongsTo(pk.Attr); g != gid {
+			continue
+		}
 			k := item.Key()
 			v := item.Value()
 			binary.LittleEndian.PutUint32(b, uint32(len(k)))
@@ -110,9 +116,6 @@ func backup(gids []uint32, bdir string) error {
 			prefix.WriteRune('\n')
 			chb <- prefix.Bytes()
 			prefix.Reset()
-		}
-
-		it.Next()
 	}
 
 	for _, v := range fileMap {
@@ -149,8 +152,28 @@ func Backup(ctx context.Context) error {
 	}
 	fmt.Println(gids)
 
-	if err := backup(gids, Config.BackupPath); err != nil {
-		return err
+	// TODO - Maybe later change to BackupPayload
+	ch := make(chan *protos.ExportPayload, len(gids))
+	for _, gid := range gids {
+		go func(group uint32) {
+			ch <- handleBackupForGroup(ctx, group)
+		}(gid)
+	}
+
+	for i := 0; i < len(gids); i++ {
+		bp := <-ch
+		if bp.Status != protos.ExportPayload_SUCCESS {
+			if tr, ok := trace.FromContext(ctx); ok {
+				tr.LazyPrintf("Backup status: %v for group id: %d", bp.Status, bp.GroupId)
+			}
+			return fmt.Errorf("Backup status: %v for group id: %d", bp.Status, bp.GroupId)
+		}
+		if tr, ok := trace.FromContext(ctx); ok {
+			tr.LazyPrintf("Backup successful for group: %v", bp.GroupId)
+		}
+	}
+	if tr, ok := trace.FromContext(ctx); ok {
+		tr.LazyPrintf("DONE export")
 	}
 	return nil
 }
