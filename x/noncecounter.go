c10a2e6c54bfb7090c28b4f525d8fcfd603d6691
Sam Hughes <srh@dgraph.io>
2017-08-25 12:08:18 -0700

    Make NonceCounter not use atomic ops

--- x/noncecounter.go
+++ x/noncecounter.go
@@ -3,10 +3,9 @@ package x
 import (
 	cryptoRand "crypto/rand"
 	"encoding/binary"
-	"sync/atomic"
 )
 
-// NonceCounter can generate unique 128-bit numbers, and it can be accessed concurrently.
+// NonceCounter can generate unique 128-bit numbers.  Only usable on a single thread.
 type NonceCounter struct {
 	lo uint64
 	hi uint64 // Only this gets incremented, atomically, and it wraps around.
@@ -23,11 +22,11 @@ func NewNonceCounter() NonceCounter {
 	return ret
 }
 
-// Generate returns a new nonce value.  It uses an atomic operation.
+// Generate returns a new nonce value.
 func (nc *NonceCounter) Generate() [16]byte {
 	var buf [16]byte
 	binary.LittleEndian.PutUint64(buf[0:8], nc.lo)
-	value := atomic.AddUint64(&nc.hi, 1)
-	binary.LittleEndian.PutUint64(buf[8:16], value)
+	nc.hi++
+	binary.LittleEndian.PutUint64(buf[8:16], nc.hi)
 	return buf
 }
