d8fe2d00335e59d575fd9b1437ec9d2d1257705d
Pawan Rawal <pawan@dgraph.io>
2017-08-30 15:46:41 +1000

    Update

--- contrib/loader.sh
+++ contrib/loader.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-function start {
-  echo -e "Starting first server.\n"
-  ./dgraph -p $BUILD/p -w $BUILD/w -memory_mb 2048 --group_conf groups.conf --groups "0,1" --idx 1 --my "127.0.0.1:12345" > $BUILD/server.log &
-  sleep 5
-  echo -e "Starting second server.\n"
-  ./dgraph -p $BUILD/p2 -w $BUILD/w2 -memory_mb 2048 --group_conf groups.conf --groups "2" --idx 2 --my "127.0.0.1:12346" --peer "127.0.0.1:12345" --port 8082 --grpc_port 9082 --workerport 12346 > $BUILD/server2.log &
-  return 0
-}
-
+source ./contrib/functions.sh
 
 SRC="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 
@@ -68,12 +60,6 @@ go build .
 popd &> /dev/null
 
 pushd $GOPATH/src/github.com/dgraph-io/dgraph/contrib/indextest &> /dev/null
-
-function quit {
-  curl localhost:8080/admin/shutdown
-  curl localhost:8082/admin/shutdown
-  return $1
-}
 
 function run_index_test {
   local max_attempts=${ATTEMPTS-5}
