83dd5dd1aa39cf5d2c31120ea588eb55e084c863
Sam Hughes <srh@dgraph.io>
2017-08-30 15:23:37 -0700

    Make noncecounter expose counter value

--- x/noncecounter.go
+++ x/noncecounter.go
@@ -5,10 +5,12 @@ import (
 	"encoding/binary"
 )
 
-// NonceCounter can generate unique 128-bit numbers.  Only usable on a single thread.
+// NonceCounter can generate unique 128-bit numbers.  Only usable on a single thread.  (All it
+// would take to be multithreaded is to increment hi atomically.)
 type NonceCounter struct {
-	lo uint64
-	hi uint64 // Only this gets incremented, atomically, and it wraps around.
+	initialLo uint64
+	initialHi uint64
+	counter   uint64
 }
 
 // NewNonceCounter reads from /dev/urandom (or such) and generates a new randomized NonceCounter.
@@ -17,16 +19,25 @@ func NewNonceCounter() NonceCounter {
 	_, err := cryptoRand.Read(buf[:])
 	Check(err)
 	var ret NonceCounter
-	ret.lo = binary.LittleEndian.Uint64(buf[0:8])
-	ret.hi = binary.LittleEndian.Uint64(buf[8:16])
+	ret.initialLo = binary.LittleEndian.Uint64(buf[0:8])
+	ret.initialHi = binary.LittleEndian.Uint64(buf[8:16])
+	ret.counter = 0
 	return ret
 }
 
 // Generate returns a new nonce value.
 func (nc *NonceCounter) Generate() [16]byte {
 	var buf [16]byte
-	binary.LittleEndian.PutUint64(buf[0:8], nc.lo)
-	nc.hi++
-	binary.LittleEndian.PutUint64(buf[8:16], nc.hi)
+	binary.LittleEndian.PutUint64(buf[0:8], nc.initialLo)
+	hi := nc.counter + nc.initialHi
+	nc.counter++
+	binary.LittleEndian.PutUint64(buf[8:16], hi)
 	return buf
+}
+
+// GenerationNumber returns N+C when buf was produced by the Nth call to Generate(), for some
+// global constant C.
+func (nc *NonceCounter) GenerationNumber(buf []byte) uint64 {
+	hi := binary.LittleEndian.Uint64(buf[8:16])
+	return hi - nc.initialHi
 }
