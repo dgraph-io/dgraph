9be5fec5b86a55ec0b7fc40aa8029227dd3eb433
Peter Stace <peter@dgraph.io>
2017-09-04 18:32:26 +1000

    One goroutine per file write

--- cmd/bulkloader/denormalised_posting_writer.go
+++ cmd/bulkloader/denormalised_posting_writer.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"path/filepath"
 	"sort"
+	"sync"
 
 	"github.com/dgraph-io/dgraph/protos"
 	"github.com/dgraph-io/dgraph/x"
@@ -15,35 +16,46 @@ import (
 func writeDenormalisedPostings(dir string, postingsIn <-chan *protos.DenormalisedPosting) {
 
 	var fileNum int
-	var buf proto.Buffer
 	var postings []*protos.DenormalisedPosting
+	var wg sync.WaitGroup
 
-	dump := func() {
-		sort.Slice(postings, func(i, j int) bool {
-			return bytes.Compare(postings[i].PostingListKey, postings[j].PostingListKey) < 0
-		})
-		for _, posting := range postings {
-			x.Check(buf.EncodeMessage(posting))
-		}
-
+	processBatch := func() {
+		wg.Add(1)
 		filename := filepath.Join(dir, fmt.Sprintf("%06d.bin", fileNum))
-		fd, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
-		x.Checkf(err, "Could not open tmp file.")
-		x.Check2(fd.Write(buf.Bytes()))
-		x.Check(fd.Close())
-
 		fileNum++
-		buf.Reset()
-		postings = postings[:0]
+		go func() {
+			sortAndDump(filename, postings)
+			wg.Done()
+		}()
+		postings = nil
 	}
 
 	for posting := range postingsIn {
 		postings = append(postings, posting)
 		if len(postings) > 4<<20 {
-			dump()
+			processBatch()
 		}
 	}
-	if len(buf.Bytes()) > 0 {
-		dump()
+	if len(postings) > 0 {
+		processBatch()
 	}
+
+	wg.Wait()
+}
+
+func sortAndDump(filename string, postings []*protos.DenormalisedPosting) {
+
+	sort.Slice(postings, func(i, j int) bool {
+		return bytes.Compare(postings[i].PostingListKey, postings[j].PostingListKey) < 0
+	})
+
+	var buf proto.Buffer
+	for _, posting := range postings {
+		x.Check(buf.EncodeMessage(posting))
+	}
+
+	fd, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+	x.Checkf(err, "Could not open tmp file.")
+	x.Check2(fd.Write(buf.Bytes()))
+	x.Check(fd.Close())
 }
