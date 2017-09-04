195e11db0b580457a11014b37971ba1828e837ff
Peter Stace <peter@dgraph.io>
2017-09-04 18:04:30 +1000

    Add sorting

--- cmd/bulkloader/denormalised_posting_writer.go
+++ cmd/bulkloader/denormalised_posting_writer.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"path/filepath"
+	"sort"
 
 	"github.com/dgraph-io/dgraph/protos"
 	"github.com/dgraph-io/dgraph/x"
@@ -14,22 +16,38 @@ func writeDenormalisedPostings(dir string, postingsIn <-chan *protos.Denormalise
 
 	var fileNum int
 	var buf proto.Buffer
+	var postings []*protos.DenormalisedPosting
 
 	dump := func() {
+		sort.Slice(postings, func(i, j int) bool {
+			return bytes.Compare(postings[i].PostingListKey, postings[j].PostingListKey) < 0
+		})
+		for _, posting := range postings {
+			x.Check(buf.EncodeMessage(posting))
+		}
+
 		filename := filepath.Join(dir, fmt.Sprintf("%06d.bin", fileNum))
 		fd, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
 		x.Checkf(err, "Could not open tmp file.")
 		x.Check2(fd.Write(buf.Bytes()))
 		x.Check(fd.Close())
-		buf.Reset()
+
 		fileNum++
+		buf.Reset()
+		postings = postings[:0]
 	}
 
 	for posting := range postingsIn {
-		x.Check(buf.EncodeMessage(posting))
-		if len(buf.Bytes()) > 128<<20 {
+
+		postings = append(postings, posting)
+		if len(postings) > 1<<20 {
 			dump()
 		}
+
+		//x.Check(buf.EncodeMessage(posting))
+		//if len(buf.Bytes()) > 128<<20 {
+		//dump()
+		//}
 	}
 	if len(buf.Bytes()) > 0 {
 		dump()
