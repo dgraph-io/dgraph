53130d4cc1de256f628cfae7d5f319106cf95679
Peter Stace <peter@dgraph.io>
2017-09-11 18:36:25 +1000

    Sort and encode inside the goroutine

--- cmd/bulkloader/mapper.go
+++ cmd/bulkloader/mapper.go
@@ -23,53 +23,31 @@ import (
 
 type mapper struct {
 	*state
-	mapEntries     []*protos.MapEntry
-	sz             int64
-	buf            bytes.Buffer
-	freePostings   []*protos.Posting
-	freeMapEntries []*protos.MapEntry
-	mu             sync.Mutex
+	mapEntries []*protos.MapEntry
+	sz         int64
+	buf        bytes.Buffer
+	mu         sync.Mutex
 }
 
-func (m *mapper) getPosting() *protos.Posting {
-	ln := len(m.freePostings)
-	if ln > 0 {
-		p := m.freePostings[ln-1]
-		m.freePostings = m.freePostings[:ln-1]
-		p.Reset()
-		return p
-	}
-	return new(protos.Posting)
-}
-
-func (m *mapper) writeMapEntriesToFile() {
-	sort.Slice(m.mapEntries, func(i, j int) bool {
-		return bytes.Compare(m.mapEntries[i].Key, m.mapEntries[j].Key) < 0
+func (m *mapper) writeMapEntriesToFile(mapEntries []*protos.MapEntry) {
+	sort.Slice(mapEntries, func(i, j int) bool {
+		return bytes.Compare(mapEntries[i].Key, mapEntries[j].Key) < 0
 	})
 
-	m.mu.Lock()
-	m.buf.Reset()
 	var varintBuf [binary.MaxVarintLen64]byte
-	for _, me := range m.mapEntries {
+	for _, me := range mapEntries {
 		n := binary.PutUvarint(varintBuf[:], uint64(me.Size()))
 		m.buf.Write(varintBuf[:n])
 		postBuf, err := me.Marshal()
 		x.Check(err)
 		m.buf.Write(postBuf)
-		if me.Posting != nil {
-			m.freePostings = append(m.freePostings, me.Posting)
-		}
 	}
-	m.freeMapEntries = m.mapEntries
-	m.mapEntries = make([]*protos.MapEntry, 0, len(m.freeMapEntries))
-	m.sz = 0
 
 	fileNum := atomic.AddUint32(&m.mapFileId, 1)
 	filename := filepath.Join(m.opt.tmpDir, fmt.Sprintf("%06d.map", fileNum))
-	go func() {
-		x.Check(x.WriteFileSync(filename, m.buf.Bytes(), 0644))
-		m.mu.Unlock()
-	}()
+	x.Check(x.WriteFileSync(filename, m.buf.Bytes(), 0644))
+	m.buf.Reset()
+	m.mu.Unlock()
 }
 
 func (m *mapper) run() {
@@ -77,11 +55,15 @@ func (m *mapper) run() {
 		x.Check(m.parseRDF(rdf))
 		atomic.AddInt64(&m.prog.rdfCount, 1)
 		if m.sz >= m.opt.mapBufSize {
-			m.writeMapEntriesToFile()
+			m.mu.Lock() // One write at a time.
+			go m.writeMapEntriesToFile(m.mapEntries)
+			m.mapEntries = nil
+			m.sz = 0
 		}
 	}
 	if len(m.mapEntries) > 0 {
-		m.writeMapEntriesToFile()
+		m.mu.Lock() // One write at a time.
+		m.writeMapEntriesToFile(m.mapEntries)
 	}
 	m.mu.Lock() // Ensure that the last file write finishes.
 }
@@ -89,20 +71,9 @@ func (m *mapper) run() {
 func (m *mapper) addMapEntry(key []byte, posting *protos.Posting) {
 	atomic.AddInt64(&m.prog.mapEdgeCount, 1)
 
-	var me *protos.MapEntry
-	if len(m.freeMapEntries) > 0 {
-		// Try to reuse memory.
-		ln := len(m.freeMapEntries)
-		me = m.freeMapEntries[ln-1]
-		me.Reset()
-		m.freeMapEntries = m.freeMapEntries[:ln-1]
-		me.Key = key
-	} else {
-		me = &protos.MapEntry{
-			Key: key,
-		}
+	me := &protos.MapEntry{
+		Key: key,
 	}
-
 	if posting.PostingType == protos.Posting_REF {
 		me.Uid = posting.Uid
 	} else {
@@ -159,7 +130,7 @@ func parseNQuad(line string) (gql.NQuad, error) {
 
 func (m *mapper) createPredicatePosting(predicate string) *protos.Posting {
 	fp := farm.Fingerprint64([]byte(predicate))
-	p := m.getPosting()
+	p := new(protos.Posting)
 	p.Uid = fp
 	p.Value = []byte(predicate)
 	p.ValType = protos.Posting_DEFAULT
@@ -172,7 +143,7 @@ func (m *mapper) createPostings(nq gql.NQuad,
 
 	m.ss.validateType(de, nq.ObjectValue == nil)
 
-	p := m.getPosting()
+	p := new(protos.Posting)
 	posting.SetPosting(de, p)
 	if nq.GetObjectValue() != nil {
 		if lang := de.GetLang(); lang == "" {
@@ -192,7 +163,7 @@ func (m *mapper) createPostings(nq gql.NQuad,
 	x.AssertTruef(nq.GetObjectValue() == nil, "only has reverse schema if object is UID")
 	de.Entity, de.ValueId = de.ValueId, de.Entity
 	m.ss.validateType(de, true)
-	rp := m.getPosting()
+	rp := new(protos.Posting)
 	posting.SetPosting(de, p)
 
 	de.Entity, de.ValueId = de.ValueId, de.Entity // de reused so swap back.
