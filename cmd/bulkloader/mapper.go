95b4c582fe3d7675ced5cb4d137d4e353b81f717
Peter Stace <peter@dgraph.io>
2017-09-13 14:24:02 +1000

    Revert "Wrap error for rdf.Parse"
    This reverts commit 139c8fabefab20cfd8022ddae4a18a1d6a526f3e.

--- cmd/bulkloader/mapper.go
+++ cmd/bulkloader/mapper.go
@@ -21,7 +21,6 @@ import (
 	"github.com/dgraph-io/dgraph/types"
 	"github.com/dgraph-io/dgraph/x"
 	farm "github.com/dgryski/go-farm"
-	"github.com/pkg/errors"
 )
 
 type mapper struct {
@@ -145,7 +144,7 @@ func (m *mapper) parseRDF(rdfLine string) error {
 func parseNQuad(line string) (gql.NQuad, error) {
 	nq, err := rdf.Parse(line)
 	if err != nil {
-		return gql.NQuad{}, errors.Wrapf(err, "while parsing line %q:", line)
+		return gql.NQuad{}, err
 	}
 	return gql.NQuad{NQuad: &nq}, nil
 }
