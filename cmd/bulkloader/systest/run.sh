d08b3e812553aaeca64b08284ad25c85960beafd
Peter Stace <peter@dgraph.io>
2017-09-19 17:42:12 +1000

    First 2 suits of systests

---
+++ cmd/bulkloader/systest/run.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -euo pipefail
+
+script_dir=$(dirname $(readlink -f "$0"))
+
+echo "Installing binaries..."
+go install github.com/dgraph-io/dgraph/cmd/bulkloader
+go install github.com/dgraph-io/dgraph/cmd/dgraph
+go install github.com/dgraph-io/dgraph/cmd/dgraphzero
+echo "Done."
+
+fail=false
+for suite in $script_dir/suite*; do
+	echo Running test suite: $(basename $suite)
+
+	rm -rf tmp
+	mkdir tmp
+	pushd tmp >/dev/null
+	mkdir dg
+	pushd dg >/dev/null
+	$GOPATH/bin/bulkloader -r $suite/rdfs.rdf -s $suite/schema.txt >/dev/null 2>&1
+	mv out/0 p
+	popd >/dev/null
+
+	mkdir dgz
+	pushd dgz >/dev/null
+	$GOPATH/bin/dgraphzero -id 1 >/dev/null 2>&1 &
+	dgzPid=$!
+	popd >/dev/null
+	sleep 5
+
+	pushd dg >/dev/null
+	$GOPATH/bin/dgraph -peer localhost:8888 -memory_mb=1024 >/dev/null 2>&1 &
+	dgPid=$!
+	popd >/dev/null
+	sleep 5
+
+	popd >/dev/null # out of tmp
+	for test_case in $suite/test*; do
+		echo Running test case: $(basename $test_case)
+		result=$(curl --silent localhost:8080/query -XPOST -d @$test_case/query.json)
+		if ! $(jq --argfile a <(echo $result) --argfile b $test_case/result.json -n 'def post_recurse(f): def r: (f | select(. != null) | r), .; r; def post_recurse: post_recurse(.[]?); ($a | (post_recurse | arrays) |= sort) as $a | ($b | (post_recurse | arrays) |= sort) as $b | $a == $b')
+		then
+			echo "Actual result doesn't match expected result:"
+			echo "Actual: $result"
+			echo "Expected: $(cat $test_case/result.json)"
+			fail=true
+		fi
+	done
+
+	kill $dgPid
+	kill $dgzPid
+	sleep 2
+done
+
+if $fail; then
+	exit 1
+fi
