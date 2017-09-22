acbf669f29576a3fd002a1705340d416a12947ba
Peter Stace <peter@dgraph.io>
2017-09-22 17:34:03 +1000

    Sort slices in JSON before comparing

--- systest/run_test.go
+++ systest/run_test.go
@@ -12,6 +12,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"reflect"
 	"sort"
 	"strconv"
 	"testing"
@@ -147,21 +148,7 @@ func (s *suite) strtest(query, wantResult string) func(*testing.T) {
 			t.Fatal("Could not read response:", err)
 		}
 
-		gotCanon, err := canonicalJSON(body)
-		if err != nil {
-			t.Fatal("Could not convert response body to JSON:", err)
-		}
-
-		wantCanon, err := canonicalJSON([]byte(wantResult))
-		if err != nil {
-			t.Fatal("Could not convert want result to JSON:", err)
-		}
-
-		// TODO: Actually need to compare using map[string]interface{}, since
-		// map iteration order could cause different textual representations.
-		if gotCanon != wantCanon {
-			t.Errorf("Want result and got result different:\nWant:\n%v\nGot:\n%v", wantCanon, gotCanon)
-		}
+		compareJSON(t, wantResult, string(body))
 	}
 }
 
@@ -200,49 +187,85 @@ func freePort() string {
 	}
 }
 
-func canonicalJSON(in []byte) (string, error) {
-	m := make(map[string]interface{})
-	if err := json.Unmarshal(in, &m); err != nil {
-		return "", err
-	}
-	// TODO: Sorting on m. Do a post traversal, and sorting each array by the hash of its children.
-	out, err := json.MarshalIndent(m, "", "  ")
+func compareJSON(t *testing.T, want, got string) {
+	wantMap := map[string]interface{}{}
+	err := json.Unmarshal([]byte(want), &wantMap)
 	if err != nil {
-		return "", err
+		t.Fatalf("Could not unmarshal want JSON: %v", err)
 	}
-	return string(out), nil
+	gotMap := map[string]interface{}{}
+	err = json.Unmarshal([]byte(got), &gotMap)
+	if err != nil {
+		t.Fatalf("Could not unmarshal got JSON: %v", err)
+	}
+
+	sortJSON(wantMap)
+	sortJSON(gotMap)
+
+	fmt.Println(wantMap)
+	fmt.Println(gotMap)
+
+	if !reflect.DeepEqual(wantMap, gotMap) {
+		wantBuf, err := json.MarshalIndent(wantMap, "", "  ")
+		if err != nil {
+			t.Error("Could not marshal JSON:", err)
+		}
+		gotBuf, err := json.MarshalIndent(gotMap, "", "  ")
+		if err != nil {
+			t.Error("Could not marshal JSON:", err)
+		}
+		t.Errorf("Want JSON and Got JSON not equal\nWant:\n%v\nGot:\n%v",
+			string(wantBuf), string(gotBuf))
+	}
 }
 
+// sortJSON looks for any arrays in the unmarshalled JSON and sorts them in an
+// arbitrary but deterministic order based on their content.
 func sortJSON(i interface{}) uint64 {
+	fmt.Println("sortJSON:", i)
+	if i == nil {
+		return 0
+	}
 	switch i := i.(type) {
 	case map[string]interface{}:
 		return sortJSONMap(i)
 	case []interface{}:
 		return sortJSONArray(i)
 	default:
-		// TODO: Just take a hash
+		h := crc64.New(crc64.MakeTable(crc64.ISO))
+		fmt.Fprint(h, i)
+		return h.Sum64()
 	}
 }
 
 func sortJSONMap(m map[string]interface{}) uint64 {
-	h := crc64.New(crc64.ISO)
-	for _, k := range sortedKeys(m) {
-		h.Write([]byte(k))
-		v := m[k]
-		if slc, ok := v.(type); ok {
-			// TODO: Sort the slice
-		}
+	h := uint64(0)
+	for _, k := range m {
+		// Because xor is commutative, it doesn't matter that map iteration
+		// is in random order.
+		h ^= sortJSON(k)
 	}
+	return h
+}
+
+type arrayElement struct {
+	elem   interface{}
+	sortBy uint64
 }
 
 func sortJSONArray(a []interface{}) uint64 {
-}
-
-func sortedKeys(m map[string]interface{}) []string {
-	ks := make([]string, 0, len(m))
-	for k := range m {
-		ks = append(ks, k)
+	h := uint64(0)
+	elements := make([]arrayElement, len(a))
+	for i, elem := range a {
+		elements[i] = arrayElement{elem, sortJSON(elem)}
+		h ^= elements[i].sortBy
+		fmt.Println(elements[i].sortBy)
 	}
-	sort.Strings(ks)
-	return ks
+	sort.Slice(elements, func(i, j int) bool {
+		return elements[i].sortBy < elements[j].sortBy
+	})
+	for i := range a {
+		a[i] = elements[i].elem
+	}
+	return h
 }
