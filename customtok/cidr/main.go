032b13dd85df4708c545d0f40eae92e318020922
Peter Stace <peter@dgraph.io>
2017-10-11 18:20:23 +1100

    Add better cidr plugin

--- customtok/cidr/main.go
+++ customtok/cidr/main.go
@@ -5,9 +5,19 @@ import (
 )
 
 func Tokens(s string) ([]string, error) {
-	_, network, err := net.ParseCIDR(s)
+	_, ipnet, err := net.ParseCIDR(s)
 	if err != nil {
 		return nil, err
 	}
-	return []string{network.String()}, nil
+	ones, bits := ipnet.Mask.Size()
+	var toks []string
+	for i := ones; i >= 1; i-- {
+		m := net.CIDRMask(i, bits)
+		tok := net.IPNet{
+			IP:   ipnet.IP.Mask(m),
+			Mask: m,
+		}
+		toks = append(toks, tok.String())
+	}
+	return toks, nil
 }
