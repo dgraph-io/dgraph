55e3d35bdf0e2bedc1b05b8cba0b82f4cb45c8cc
Peter Stace <peter@dgraph.io>
2017-10-12 17:15:29 +1100

    Fix rune example

--- customtok/letter/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-func Tokens(s string) ([]string, error) {
-	var toks []string
-	for _, r := range s {
-		toks = append(toks, string(r))
-	}
-	return toks, nil
-}
---
+++ customtok/rune/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/binary"
+)
+
+func Tokenizer() interface{} {
+	return RuneTokenizer{}
+}
+
+type RuneTokenizer struct {
+}
+
+func (RuneTokenizer) Name() string     { return "rune" }
+func (RuneTokenizer) Type() string     { return "string" }
+func (RuneTokenizer) Identifier() byte { return 0xfd }
+
+func (t RuneTokenizer) Tokens(value interface{}) ([]string, error) {
+	var toks []string
+	for _, r := range value.(string) {
+		var buf [4]byte
+		n := binary.PutVarint(buf[:], int64(r))
+		tok := string(buf[:n])
+		toks = append(toks, tok)
+	}
+	return toks, nil
+}
