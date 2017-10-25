15e564d4ba1cd06677bb45f12b41efd871c2349b
Janardhan Reddy <janardhan@dgraph.io>
2017-10-25 14:28:18 +1100

    add test to ensure txn gets aborted if client dies before aborting.

--- client/testtxn/main.go
+++ client/testtxn/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"sync"
+	"time"
 
 	"github.com/dgraph-io/dgraph/client"
 	"github.com/dgraph-io/dgraph/protos"
@@ -45,6 +46,7 @@ func main() {
 	TestTxnRead4(ctx, dg)
 	TestConflict(ctx, dg)
 	TestConflictTimeout(ctx, dg)
+	TestConflictTimeout2(ctx, dg)
 }
 
 // readTs == startTs
@@ -263,6 +265,54 @@ func TestConflictTimeout(ctx context.Context, dg *client.Dgraph) {
 	txn3 := dg.NewTxn()
 	q = fmt.Sprintf(`{ me(func: uid(%d)) { name }}`, uid)
 	resp, err = txn3.Query(ctx, q, nil)
+	x.Check(err)
+	fmt.Printf("Final Response JSON: %q\n", resp.Json)
+}
+
+func TestConflictTimeout2(ctx context.Context, dg *client.Dgraph) {
+	fmt.Println("TestConflictTimeout2")
+	var uid uint64
+	{
+		txn := dg.NewTxn()
+
+		mu := &protos.Mutation{}
+		mu.SetJson = []byte(`{"name": "Manish"}`)
+		assigned, err := txn.Mutate(ctx, mu)
+		if err != nil {
+			log.Fatalf("Error while running mutation: %v\n", err)
+		}
+		if len(assigned.Uids) != 1 {
+			log.Fatalf("Error. Nothing assigned. %+v\n", assigned)
+		}
+		for _, u := range assigned.Uids {
+			uid = u
+		}
+	}
+
+	txn2 := dg.NewTxn()
+	mu := &protos.Mutation{}
+	mu.SetJson = []byte(fmt.Sprintf("{\"_uid_\": %d, \"name\": \"Jan the man\"}", uid))
+	_, err := txn2.Mutate(ctx, mu)
+	fmt.Printf("This txn should fail with error. Err got: %v\n", err)
+	x.AssertTrue(err != nil)
+
+	time.Sleep(time.Second * 15)
+
+	txn3 := dg.NewTxn()
+	mu = &protos.Mutation{}
+	mu.SetJson = []byte(fmt.Sprintf("{\"_uid_\": %d, \"name\": \"Jan the man\"}", uid))
+	assigned, err := txn3.Mutate(ctx, mu)
+	fmt.Printf("txn2.mutate error: %v\n", err)
+	if err == nil {
+		x.Check(txn3.Commit(ctx))
+	}
+	for _, u := range assigned.Uids {
+		uid = u
+	}
+
+	txn4 := dg.NewTxn()
+	q := fmt.Sprintf(`{ me(func: uid(%d)) { name }}`, uid)
+	resp, err := txn4.Query(ctx, q, nil)
 	x.Check(err)
 	fmt.Printf("Final Response JSON: %q\n", resp.Json)
 }
