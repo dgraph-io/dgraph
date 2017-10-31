25964bc46a8e5edada92c90ef5e475cfca6d90fa
Peter Stace <peter@dgraph.io>
2017-10-31 19:12:11 +1100

    Exponential backoff when requesting timestamps in client (#1708)

--- client/client.go
+++ client/client.go
@@ -100,6 +100,11 @@ func (d *Dgraph) getLinRead() *protos.LinRead {
 
 func (d *Dgraph) fillTimestampRequests() {
 	var chs []chan uint64
+	const (
+		initDelay = 10 * time.Millisecond
+		maxDelay  = 10 * time.Second
+	)
+	delay := initDelay
 	for range d.notify {
 	RETRY:
 		d.mu.Lock()
@@ -115,9 +120,15 @@ func (d *Dgraph) fillTimestampRequests() {
 		ts, err := d.zero.Timestamps(ctx, num)
 		cancel()
 		if err != nil {
-			log.Printf("Error while retrieving timestamps: %v. Will retry...\n", err)
+			log.Printf("Error while retrieving timestamps: %v. Will retry in %v\n", err, delay)
+			time.Sleep(delay)
+			delay *= 2
+			if delay > maxDelay {
+				delay = maxDelay
+			}
 			goto RETRY
 		}
+		delay = initDelay
 		x.AssertTrue(ts.EndId-ts.StartId+1 == uint64(len(chs)))
 		for i, ch := range chs {
 			ch <- ts.StartId + uint64(i)
