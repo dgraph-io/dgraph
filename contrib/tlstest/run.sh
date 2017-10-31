1b77f4405d5f5042f2f0ff22b9610d12f54ef407
Pawan Rawal <pawan@dgraph.io>
2017-10-31 14:13:26 +1100

    Update again

--- contrib/tlstest/run.sh
+++ contrib/tlstest/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # We run the TLS tests only when a commit is made on master/release
- if [ $TRAVIS_BRANCH =~ master|release\/ ] ; then
+ if [[ $TRAVIS_BRANCH =~ master|release\/ ]] ; then
 	make test
  else
  	echo "TLS tests skipped."
