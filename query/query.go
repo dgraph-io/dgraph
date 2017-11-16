1f0273364a37eff8b1a56cb318f9766670cf0152
Pawan Rawal <pawan@dgraph.io>
2017-11-16 17:33:18 +1100

    Support alias in key too.

--- query/query.go
+++ query/query.go
@@ -1971,6 +1971,7 @@ func ProcessGraph(ctx context.Context, sg, parent *SubGraph, rch chan error) {
 				ReadTs:  sg.ReadTs,
 				LinRead: sg.LinRead,
 				Params: params{
+					Alias:        it.Alias,
 					ignoreResult: true,
 					Langs:        it.Langs,
 				},
