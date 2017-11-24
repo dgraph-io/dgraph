ae3ed8db302246a17292fb368101fccbb5f371a0
Peter Stace <peter@dgraph.io>
2017-11-24 11:43:47 +1100

    Break out into two packages (breaks *everything*)

--- protos/gen.sh
+++ protos/gen.sh
@@ -4,4 +4,5 @@
 
 protos=${GOPATH-$HOME/go}/src/github.com/dgraph-io/dgraph/protos
 pushd $protos > /dev/null
-protoc --gofast_out=plugins=grpc:. -I=. *.proto
+protoc --gofast_out=plugins=grpc:api -I=. api.proto
+protoc --gofast_out=plugins=grpc,Mapi.proto=github.com/dgraph-io/dgraph/protos/api:intern -I=. internal.proto
