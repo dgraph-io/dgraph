2b7c7a22eb65c6533173be26c98735cb45b9ae22
Pawan Rawal <pawan@dgraph.io>
2017-12-04 18:59:08 +1100

    Update docs latest version

--- wiki/scripts/build.sh
+++ wiki/scripts/build.sh
@@ -19,8 +19,9 @@ HOST=https://docs.dgraph.io
 # append '(latest)' to the version string, and build script can place the
 # artifact in an appropriate location
 VERSIONS_ARRAY=(
-'v0.9.3'
+'v0.9.4'
 'master'
+'v0.9.3'
 'v0.9.2'
 'v0.9.1'
 'v0.9.0'
