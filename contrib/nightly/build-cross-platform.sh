7cb3282c907988e4c3e37987ac57cc408150a580
Pawan Rawal <pawan@dgraph.io>
2017-12-04 16:20:41 +1100

    Fix small issue

--- contrib/nightly/build-cross-platform.sh
+++ contrib/nightly/build-cross-platform.sh
@@ -34,40 +34,33 @@ source $GOPATH/src/github.com/dgraph-io/dgraph/contrib/nightly/constants.sh
 pushd $GOPATH/src/github.com/dgraph-io/dgraph/dgraph > /dev/null
 
 if [[ $platform == "windows" ]]; then
-  xgo_target="$platform/amd64"
+  xgo_target="windows/amd64"
 else
-  xgo_target="$platform-10.9/amd64"
+  xgo_target="darwin-10.9/amd64"
 fi
 
 echo -e "\n\n\033[1;33mBuilding binaries for $platform\033[0m"
 xgo --go 1.8.3 --targets $xgo_target -ldflags \
   "-X $release=$release_version -X $branch=$gitBranch -X $commitSHA1=$lastCommitSHA1 -X '$commitTime=$lastCommitTime' -X $uiDir=$ui" .;
 
+echo -e "\n\033[1;33mCopying binaries to tmp folder\033[0m"
 if [[ $platform == "windows" ]]; then
-  cp dgraph-windows-4.0-amd64.exe dgraph.exe
+  cp dgraph-windows-4.0-amd64.exe $tmp_dir/dgraph.exe
 else
-  cp dgraph-darwin-10.9-amd64 dgraph
+  cp dgraph-darwin-10.9-amd64 $tmp_dir/dgraph
 fi
 
-echo -e "\n\033[1;33mCopying binaries to tmp folder\033[0m"
-pushd $tmp_dir > /dev/null
-if [[ $platfrom == "windows" ]]; then
-  cp $GOPATH/src/github.com/dgraph-io/dgraph/dgraph/dgraph.exe .
-else
-  cp $GOPATH/src/github.com/dgraph-io/dgraph/dgraph/dgraph .
-fi
-echo -e "\n\033[1;34mSize of files: $(du -sh)\033[0m"
-popd &> /dev/null
+echo -e "\n\033[1;34mSize of files: $(du -sh $tmp_dir)\033[0m"
 
 echo -e "\n\033[1;33mCreating tar file\033[0m"
 tar_file=dgraph-"$platform"-amd64-$release_version.tar.gz
+
 # Create a tar file with the contents of the dgraph folder (i.e the binaries)
 pushd $tmp_dir > /dev/null
-
 if [[ $platform == "windows" ]]; then
-  tar -zcvf $tar_file dgraph
-else
   tar -zcvf $tar_file dgraph.exe
+else
+  tar -zcvf $tar_file dgraph
 fi
 
 echo -e "\n\033[1;34mSize of tar file: $(du -sh $tar_file)\033[0m"
