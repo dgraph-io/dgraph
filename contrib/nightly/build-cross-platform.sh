3deda8d973030e975395b743a666a4415470fa46
Pawan Rawal <pawan@dgraph.io>
2017-12-04 17:15:05 +1100

    Last changes

--- contrib/nightly/build-cross-platform.sh
+++ contrib/nightly/build-cross-platform.sh
@@ -61,7 +61,6 @@ if [[ $platform == "windows" ]]; then
   tar -zcvf $tar_file dgraph.exe
 else
   checksum=$(shasum -a 256 dgraph | awk '{print $1}')
-  echo -e "Checksum $checksum"
   echo "$checksum /usr/local/bin/dgraph" >> $cur_dir/"dgraph-checksum-darwin-amd64-$release_version".sha256
   tar -zcvf $tar_file dgraph
 fi
