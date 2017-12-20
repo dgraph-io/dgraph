3bfca1030babbc866f91be12f213eede394c4bc5
Pawan Rawal <pawan@dgraph.io>
2017-12-20 09:38:50 +0530

    Couple of fixes to NquadFromJson

--- edgraph/server.go
+++ edgraph/server.go
@@ -626,8 +626,10 @@ func mapToNquads(m map[string]interface{}, idx *int, op int, parentPred string) 
 		var uid uint64
 		if id, ok := uidVal.(float64); ok {
 			uid = uint64(id)
-		} else if id, ok := uidVal.(string); ok {
-			if u, err := strconv.ParseInt(id, 0, 64); err == nil {
+		} else if id, ok := uidVal.(string); ok && len(id) > 0 {
+			if u, err := strconv.ParseInt(id, 0, 64); err != nil {
+				return mr, err
+			} else {
 				uid = uint64(u)
 			}
 		}
