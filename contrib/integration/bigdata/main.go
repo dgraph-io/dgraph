ad0f05b0772b683b4a507a4d93dd55557a8001b4
Peter Stace <peter@dgraph.io>
2018-01-05 17:18:59 +1100

    Dgraph exerciser with many predicates (#1981)
    * WIP
    
    * Add attr data
    
    * add reverse to uid edges
    
    * WIP for delete
    
    * Start to delete nodes
    
    * Get delete working properly
    
    * small refactor
    
    * Bit of refactoring
    
    * Fix for Q check
    
    * Fix a few things
    
    * more like a tree
    
    * Remove some old code
    
    * Split into modes
    
    * parallel
    
    * Add graph expansion
    
    * Add some debug
    
    * Ge 1
    
    * Show error
    
    * Revert "Show error"
    
    This reverts commit 34653bb2363a53098840f57933a2aecaa115c8ff.
    
    * Round robin the schema to different groups
    
    * Comment out log about unhealthy connection
    
    * Add some extra logging
    
    * Comment out more noisy logging
    
    * Revert "Add some extra logging"
    
    This reverts commit 97410debad2aaa3c21a49c36a6e37bbc3f9f3bef.
    
    * Simplify
    
    * show error
    
    * ptr to map
    
    * Remove error logging for query
    
    * Add some logging to see which part of the mutation blocks
    
    * Revert "Add some logging to see which part of the mutation blocks"
    
    This reverts commit 51a2f4b051df3eb19536b889cde9a248944b772b.
    
    * Debug log lines
    
    * Log after graceful stop
    
    * Two kinds of stops
    
    * Better logging
    
    * logging
    
    * Add some debugging to see issue where xid or count (?) is missing
    
    * fix compile errors
    
    * Add uid to query
    
    * Fix error handling
    
    * Fix compile
    
    * more log
    
    * Remove a heap of debugging code
    
    * cleanup
    
    * Suggestions from Jan

---
+++ contrib/integration/bigdata/main.go
@@ -0,0 +1,237 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"math/rand"
+	"net/url"
+	"os"
+	"strings"
+	"sync/atomic"
+	"time"
+
+	"github.com/dgraph-io/dgraph/client"
+	"github.com/dgraph-io/dgraph/protos/api"
+	"github.com/dgraph-io/dgraph/x"
+	"google.golang.org/grpc"
+)
+
+var addrs = flag.String("addrs", "", "comma separated dgraph addresses")
+var mode = flag.String("mode", "", "mode to run in ('mutate' or 'query')")
+var conc = flag.Int("j", 1, "number of operations to run in parallel")
+
+func init() {
+	rand.Seed(time.Now().Unix())
+}
+
+var ctx = context.Background()
+
+func main() {
+	flag.Parse()
+	c := makeClient()
+
+	// Install the schema automatically on the first run. This allows the same
+	// command to be used when running this program for the first and
+	// subsequent times. We guess if it's the first run based on the number of
+	// schema items.
+	resp, err := c.NewTxn().Query(ctx, "schema {}")
+	x.Check(err)
+	if len(resp.Schema) < 5 {
+		// Run each schema alter separately so that there is an even
+		// distribution among all groups.
+		for _, s := range schema() {
+			x.Check(c.Alter(ctx, &api.Operation{
+				Schema: s,
+			}))
+		}
+		x.Check2(c.NewTxn().Mutate(ctx, &api.Mutation{
+			CommitNow: true,
+			SetNquads: []byte(initialData()),
+		}))
+	}
+
+	switch *mode {
+	case "mutate":
+		var errCount int64
+		var mutateCount int64
+		for i := 0; i < *conc; i++ {
+			go func() {
+				for {
+					err := mutate(c)
+					if err == nil {
+						atomic.AddInt64(&mutateCount, 1)
+					} else {
+						atomic.AddInt64(&errCount, 1)
+					}
+				}
+			}()
+		}
+		for {
+			time.Sleep(time.Second)
+			fmt.Printf("Status: success_mutations=%d errors=%d\n",
+				atomic.LoadInt64(&mutateCount), atomic.LoadInt64(&errCount))
+		}
+	case "query":
+		var errCount int64
+		var queryCount int64
+		for i := 0; i < *conc; i++ {
+			go func() {
+				for {
+					err := showNode(c)
+					if err == nil {
+						atomic.AddInt64(&queryCount, 1)
+					} else {
+						atomic.AddInt64(&errCount, 1)
+					}
+				}
+			}()
+		}
+		for {
+			time.Sleep(time.Second)
+			fmt.Printf("Status: success_queries=%d errors=%d\n",
+				atomic.LoadInt64(&queryCount), atomic.LoadInt64(&errCount))
+		}
+	default:
+		fmt.Printf("unknown mode: %q\n", *mode)
+		os.Exit(1)
+	}
+}
+
+func schema() []string {
+	s := []string{"xid: string @index(exact) .\n"}
+	for char := 'a'; char <= 'z'; char++ {
+		s = append(s, fmt.Sprintf("count_%c: int .\n", char))
+	}
+	for char := 'a'; char <= 'z'; char++ {
+		s = append(s, fmt.Sprintf("attr_%c: string .\n", char))
+	}
+	return s
+}
+
+func initialData() string {
+	rdfs := "_:root <xid> \"root\" .\n"
+	for char := 'a'; char <= 'z'; char++ {
+		rdfs += fmt.Sprintf("_:root <count_%c> \"0\" .\n", char)
+	}
+	return rdfs
+}
+
+func makeClient() *client.Dgraph {
+	var dgcs []api.DgraphClient
+	for _, addr := range strings.Split(*addrs, ",") {
+		c, err := grpc.Dial(addr, grpc.WithInsecure())
+		x.Check(err)
+		dgcs = append(dgcs, api.NewDgraphClient(c))
+	}
+	return client.NewDgraphClient(dgcs...)
+}
+
+type runner struct {
+	txn *client.Txn
+}
+
+func mutate(c *client.Dgraph) error {
+	r := &runner{
+		txn: c.NewTxn(),
+	}
+	defer r.txn.Discard(ctx)
+
+	char := 'a' + rune(rand.Intn(26))
+
+	var result struct {
+		Q []struct {
+			Uid   *string
+			Count *int
+		}
+	}
+	if err := r.query(&result, `
+	{
+		q(func: eq(xid, "root")) {
+			uid
+			count: count_%c
+		}
+	}
+	`, char); err != nil {
+		return err
+	}
+
+	x.AssertTrue(len(result.Q) > 0 && result.Q[0].Count != nil && result.Q[0].Uid != nil)
+
+	if _, err := r.txn.Mutate(ctx, &api.Mutation{
+		SetNquads: []byte(fmt.Sprintf("<%s> <count_%c> \"%d\" .\n",
+			*result.Q[0].Uid, char, *result.Q[0].Count+1)),
+	}); err != nil {
+		return err
+	}
+
+	rdfs := fmt.Sprintf("_:node <xid> \"%c_%d\" .\n", char, *result.Q[0].Count)
+	for char := 'a'; char <= 'z'; char++ {
+		if rand.Float64() < 0.9 {
+			continue
+		}
+		payload := make([]byte, 16+rand.Intn(16))
+		rand.Read(payload)
+		rdfs += fmt.Sprintf("_:node <attr_%c> \"%s\" .\n", char, url.QueryEscape(string(payload)))
+	}
+	if _, err := r.txn.Mutate(ctx, &api.Mutation{
+		SetNquads: []byte(rdfs),
+	}); err != nil {
+		return err
+	}
+
+	return r.txn.Commit(ctx)
+}
+
+func showNode(c *client.Dgraph) error {
+	r := &runner{
+		txn: c.NewTxn(),
+	}
+	defer r.txn.Discard(ctx)
+
+	char := 'a' + rune(rand.Intn(26))
+	var result struct {
+		Q []struct {
+			Count *int
+		}
+	}
+
+	q := fmt.Sprintf(`
+	{
+		q(func: eq(xid, "root")) {
+			uid
+			count: count_%c
+		}
+	}
+	`, char)
+	resp, err := r.txn.Query(ctx, q)
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(resp.Json, &result); err != nil {
+		return err
+	}
+	x.AssertTruef(len(result.Q) > 0 && result.Q[0].Count != nil, "%v %+v", string(resp.Json), result)
+
+	var m map[string]interface{}
+	if err := r.query(&m, `
+	{
+		q(func: eq(xid, "%c_%d")) {
+			expand(_all_)
+		}
+	}
+	`, char, rand.Intn(*result.Q[0].Count)); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *runner) query(out interface{}, q string, args ...interface{}) error {
+	q = fmt.Sprintf(q, args...)
+	resp, err := r.txn.Query(ctx, q)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(resp.Json, out)
+}
