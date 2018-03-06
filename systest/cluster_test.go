91480ed787e0e7310a72dde0dfc51b394a8b515a
Pawan Rawal <pawan@dgraph.io>
2018-03-06 17:16:28 +1100

    Fix bug in dgraph live where some batches could be skipped on an error.

--- systest/cluster_test.go
+++ systest/cluster_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -12,7 +13,116 @@ import (
 	"syscall"
 	"testing"
 	"time"
+
+	"github.com/dgraph-io/dgraph/x"
+	"github.com/stretchr/testify/require"
 )
+
+type Member struct {
+	Id      string `json:"id"`
+	GroupId int    `json:"groupId"`
+	Leader  bool   `json:"leader"`
+}
+
+type GroupState struct {
+	Members map[string]Member `json:"members"`
+}
+
+type State struct {
+	Groups map[string]GroupState `json:"groups"`
+}
+
+func waitForConvergence(t *testing.T, c *DgraphCluster) {
+	for i := 0; i < 60; i++ {
+		resp, err := http.Get(fmt.Sprintf("http://localhost:%d/state", c.zeroPortOffset+6080))
+		require.NoError(t, err)
+		b, err := ioutil.ReadAll(resp.Body)
+		require.NoError(t, err)
+
+		var s State
+		require.NoError(t, json.Unmarshal(b, &s))
+		members := s.Groups["1"].Members
+		if len(members) == 2 && (members["1"].Leader || members["2"].Leader) {
+			break
+		}
+
+		x.Println("Couldn't find leader, waiting...")
+		time.Sleep(time.Second)
+	}
+}
+
+type matchExport struct {
+	expectedRDF    int
+	expectedSchema int
+	dir            string
+	port           int
+}
+
+func matchExportCount(opts matchExport) error {
+	// Now try and export data from second server.
+	resp, err := http.Get(fmt.Sprintf("http://localhost:%d/admin/export", opts.port))
+	if err != nil {
+		return err
+	}
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	expected := `{"code": "Success", "message": "Export completed."}`
+	if string(b) != expected {
+		return x.Errorf("Unexpected message while exporting: %v", string(b))
+	}
+
+	dataFile, err := findFile(filepath.Join(opts.dir, "export"), ".rdf.gz")
+	if err != nil {
+		return err
+	}
+	cmd := fmt.Sprintf("gunzip -c %s | wc -l", dataFile)
+	out, err := exec.Command("sh", "-c", cmd).Output()
+	if err != nil {
+		return err
+	}
+	count := strings.TrimSpace(string(out))
+	if count != strconv.Itoa(opts.expectedRDF) {
+		return x.Errorf("Export count mismatch. Got: %s", count)
+	}
+
+	schemaFile, err := findFile(filepath.Join(opts.dir, "export"), ".schema.gz")
+	if err != nil {
+		return err
+	}
+	cmd = fmt.Sprintf("gunzip -c %s | wc -l", schemaFile)
+	out, err = exec.Command("sh", "-c", cmd).Output()
+	if err != nil {
+		return err
+	}
+	count = strings.TrimSpace(string(out))
+	if count != strconv.Itoa(opts.expectedSchema) {
+		return x.Errorf("Schema export count mismatch. Got: %s", count)
+	}
+	return nil
+}
+
+func waitForNodeToBeHealthy(t *testing.T, port int) {
+	for i := 0; i < 90; i++ {
+		// Ignore error, server might be unhealthy temporarily.
+		resp, err := http.Get(fmt.Sprintf("http://localhost:%d/health", port))
+		if err != nil {
+			x.Printf("Server running on: [%v] is not up yet, waiting...\n", port)
+			time.Sleep(2 * time.Second)
+			continue
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		require.NoError(t, err)
+
+		if string(b) == "OK" {
+			break
+		}
+
+		x.Printf("Server running on: [%v] not healthy, retrying...\n", port)
+		time.Sleep(2 * time.Second)
+	}
+}
 
 func TestClusterSnapshot(t *testing.T) {
 	if testing.Short() {
@@ -27,7 +137,7 @@ func TestClusterSnapshot(t *testing.T) {
 	defer cluster.Close()
 
 	schema := os.ExpandEnv("$GOPATH/src/github.com/dgraph-io/dgraph/systest/data/goldendata.schema")
-	data := os.ExpandEnv("$GOPATH/src/github.com/dgraph-io/dgraph/systest/data/goldendata.rdf.gz")
+	data := os.ExpandEnv("$GOPATH/src/github.com/dgraph-io/dgraph/systest/data/goldendata_first_200k.rdf.gz")
 
 	liveCmd := exec.Command(os.ExpandEnv("$GOPATH/bin/dgraph"), "live",
 		"--rdfs", data,
@@ -58,14 +168,17 @@ func TestClusterSnapshot(t *testing.T) {
 		t.Fatalf("Couldn't add server: %v\n", err)
 	}
 
-	// TODO - Make this more deterministic.
-	dur := 90 * time.Second
-	// TODO - Remove later when we move nightly to Teamcity
-	if ok := os.Getenv("TRAVIS"); ok == "true" {
-		dur = 2 * dur
+	quickCheck := func(err error) {
+		if err != nil {
+			shutdownCluster()
+			t.Fatalf("Got error: %v\n", err)
+		}
 	}
-	// Approx time for snapshot to be transferred to the second instance.
-	time.Sleep(dur)
+
+	o, err := strconv.Atoi(n.offset)
+	quickCheck(err)
+
+	waitForNodeToBeHealthy(t, o+x.PortHTTP)
 
 	cluster.dgraph.Process.Signal(syscall.SIGINT)
 	if _, err = cluster.dgraph.Process.Wait(); err != nil {
@@ -80,53 +193,19 @@ func TestClusterSnapshot(t *testing.T) {
 		t.Fatalf("Couldn't start Dgraph server again: %v\n", err)
 	}
 
-	dur = 15 * time.Second
-	if ok := os.Getenv("TRAVIS"); ok == "true" {
-		dur = 2 * dur
-	}
-	// Wait for leader election to happen again.
-	time.Sleep(dur)
+	waitForNodeToBeHealthy(t, cluster.dgraphPortOffset+x.PortHTTP)
+	waitForNodeToBeHealthy(t, o+x.PortHTTP)
+	waitForConvergence(t, cluster)
 
-	quickCheck := func(err error) {
-		if err != nil {
-			shutdownCluster()
-			t.Fatalf("Got error: %v\n", err)
-		}
-	}
-
-	// Now try and export data from second server.
-	o, err := strconv.Atoi(n.offset)
-	quickCheck(err)
-	resp, err := http.Get(fmt.Sprintf("http://localhost:%d/admin/export", o+8080))
-	quickCheck(err)
-	b, err := ioutil.ReadAll(resp.Body)
-	quickCheck(err)
-	expected := `{"code": "Success", "message": "Export completed."}`
-	if string(b) != expected {
+	err = matchExportCount(matchExport{
+		expectedRDF:    2e5,
+		expectedSchema: 10,
+		dir:            dgraphDir,
+		port:           o + x.PortHTTP,
+	})
+	if err != nil {
 		shutdownCluster()
-		t.Fatalf("Unexpected message while exporting: %v", string(b))
-	}
-
-	dataFile, err := findFile(filepath.Join(dgraphDir, "export"), ".rdf.gz")
-	quickCheck(err)
-	cmd := fmt.Sprintf("gunzip -c %s | wc -l", dataFile)
-	out, err := exec.Command("sh", "-c", cmd).Output()
-	quickCheck(err)
-	count := strings.TrimSpace(string(out))
-	if count != "1120879" {
-		shutdownCluster()
-		t.Fatalf("Export count mismatch. Got: %s", count)
-	}
-
-	schemaFile, err := findFile(filepath.Join(dgraphDir, "export"), ".schema.gz")
-	quickCheck(err)
-	cmd = fmt.Sprintf("gunzip -c %s | wc -l", schemaFile)
-	out, err = exec.Command("sh", "-c", cmd).Output()
-	quickCheck(err)
-	count = strings.TrimSpace(string(out))
-	if count != "10" {
-		shutdownCluster()
-		t.Fatalf("Schema export count mismatch. Got: %s", count)
+		t.Fatal(err)
 	}
 }
 
