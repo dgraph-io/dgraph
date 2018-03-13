3c12263bae0a6631ba6a1eeeec07d9d95038e977
Pawan Rawal <pawan@dgraph.io>
2018-03-13 17:20:06 +1100

    Update install.sh

--- contrib/scripts/install.sh
+++ contrib/scripts/install.sh
@@ -8,6 +8,9 @@ set -ex
 go get -d golang.org/x/net/context
 go get -d google.golang.org/grpc
 
+go get github.com/dgraph-io/dgo
+go get github.com/stretchr/testify/require
+
 pushd $GOPATH/src/google.golang.org/grpc
   git checkout v1.8.2
 popd
