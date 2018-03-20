96821d4f8d75093db7bbe74b960d25f00831a78d
Pawan Rawal <pawan@dgraph.io>
2018-03-20 12:30:28 +1100

    Throw error instead of asserting in query.

--- worker/task.go
+++ worker/task.go
@@ -1258,8 +1258,10 @@ func (w *grpcWorker) ServeTask(ctx context.Context, q *intern.Query) (*intern.Re
 		tr.LazyPrintf("Attribute: %q NumUids: %v groupId: %v ServeTask", q.Attr, numUids, gid)
 	}
 
-	x.AssertTruef(groups().ServesGroup(gid),
-		"attr: %q groupId: %v Request sent to wrong server.", q.Attr, gid)
+	if !groups().ServesGroup(gid) {
+		return nil, fmt.Errorf("Temporary error, attr: %q groupId: %v Request sent to wrong server",
+			q.Attr, gid)
+	}
 
 	type reply struct {
 		result *intern.Result
