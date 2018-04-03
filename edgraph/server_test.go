b81b2780ade0317ab6208dadfdf197e29317106a
Manish R Jain <manish@dgraph.io>
2018-04-03 02:43:16 -0700

    Add license notice to files without the notice.

--- edgraph/server_test.go
+++ edgraph/server_test.go
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2017-2018 Dgraph Labs, Inc. and Contributors
+ *
+ * This file is available under the Apache License, Version 2.0,
+ * with the Commons Clause restriction.
+ */
+
 package edgraph
 
 import (
