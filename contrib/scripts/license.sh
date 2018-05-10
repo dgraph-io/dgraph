597e6675684a0a505d53f8a28ac194514847ff6a
Manish R Jain <manish@dgraph.io>
2018-05-10 13:57:44 -0700

    Remove notice from shell script.

--- contrib/scripts/license.sh
+++ contrib/scripts/license.sh
@@ -1,18 +1,5 @@
 files=$(find . ! -path "./vendor/*" ! -path "./bp128/*" -type f -name "*.go")
 
-cat > /tmp/notice << EOF
-/*
- * Copyright 2018 Dgraph Labs, Inc.
- *
- * This file is available under the Apache License, Version 2.0,
- * with the Commons Clause restriction.
- */
-
-EOF
-
-echo "NOTICE IS:"
-cat /tmp/notice
-
 for f in $files; do
   echo "Processing $f"
   python2 contrib/scripts/license.py $f
