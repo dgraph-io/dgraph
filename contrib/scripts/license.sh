5a35f0e4716f945dd54da3f707d960aa2cd96ff7
Manish R Jain <manish@dgraph.io>
2018-05-10 13:08:09 -0700

    Update contrib script

--- contrib/scripts/license.sh
+++ contrib/scripts/license.sh
@@ -14,17 +14,23 @@ echo "NOTICE IS:"
 cat /tmp/notice
 
 for f in $files; do
+  echo "Processing $f"
+  # Remove the copyright notice.
+  sed -i -e '/\/\**Copyright*Dgraph/,/\*\//d' $f
+
   if ! grep -L "Copyright" $f; then
-    echo "Couldn't find copyright in $f. Adding it."
     cat /tmp/notice > /tmp/codefile
     cat $f >> /tmp/codefile
     mv /tmp/codefile $f
   fi
 
+  # Start from year.
   year=$(git log --format=%aD $f | tail -1 | awk '{print $4}')
-  echo $year, $f
   if [ "$year" != "2018" ]; then
     sed -i "s/Copyright 2018 Dgraph/Copyright $year-2018 Dgraph/g" $f
   fi
+
+  # Format it.
+  gofmt -w $f
 done
 
