c3f21fa167baf4b55f901de0816d3505b3da24f1
Manish R Jain <manish@dgraph.io>
2018-07-12 15:04:13 -0700

    Pull in Badger with the new DropAll method.

--- vendor/github.com/dgraph-io/badger/levels.go
+++ vendor/github.com/dgraph-io/badger/levels.go
@@ -170,6 +170,62 @@ func (s *levelsController) cleanupLevels() error {
 	return firstErr
 }
 
+// This function picks all tables from all levels, creates a manifest changeset,
+// applies it, and then decrements the refs of these tables, which would result
+// in their deletion. It spares one table from L0, to keep the badgerHead key
+// persisted, so we don't lose where we are w.r.t. value log.
+// NOTE: This function in itself isn't sufficient to completely delete all the
+// data. After this, one would still need to iterate over the KV pairs and mark
+// them as deleted.
+func (s *levelsController) deleteLSMTree() (int, error) {
+	var all []*table.Table
+	var keepOne *table.Table
+	for _, l := range s.levels {
+		l.RLock()
+		if l.level == 0 && len(l.tables) > 1 {
+			// Skip the last table. We do this to keep the badgerMove key persisted.
+			lastIdx := len(l.tables) - 1
+			keepOne = l.tables[lastIdx]
+			all = append(all, l.tables[:lastIdx]...)
+		} else {
+			all = append(all, l.tables...)
+		}
+		l.RUnlock()
+	}
+	if len(all) == 0 {
+		return 0, nil
+	}
+
+	// Generate the manifest changes.
+	changes := []*protos.ManifestChange{}
+	for _, table := range all {
+		changes = append(changes, makeTableDeleteChange(table.ID()))
+	}
+	changeSet := protos.ManifestChangeSet{Changes: changes}
+	if err := s.kv.manifest.addChanges(changeSet.Changes); err != nil {
+		return 0, err
+	}
+
+	for _, l := range s.levels {
+		l.Lock()
+		l.totalSize = 0
+		if l.level == 0 && len(l.tables) > 1 {
+			l.tables = []*table.Table{keepOne}
+			l.totalSize += keepOne.Size()
+		} else {
+			l.tables = l.tables[:0]
+		}
+		l.Unlock()
+	}
+	// Now allow deletion of tables.
+	for _, table := range all {
+		if err := table.DecrRef(); err != nil {
+			return 0, err
+		}
+	}
+	return len(all), nil
+}
+
 func (s *levelsController) startCompact(lc *y.Closer) {
 	n := s.kv.opt.NumCompactors
 	lc.AddRunning(n - 1)
