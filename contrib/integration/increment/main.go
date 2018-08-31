e654d845396510c452acd9be5738b9f4dee58db1
Manish R Jain <manish@dgraph.io>
2018-08-30 18:54:41 -0700

    Add a new tool which retrieves and increments a counter by 1 transactionally. Can be used to verify that the cluster is GOOOOD.

---
+++ contrib/integration/increment/main.go
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2018 Dgraph Labs, Inc.
+ *
+ * This file is available under the Apache License, Version 2.0,
+ * with the Commons Clause restriction.
+ */
+
+// This binary would retrieve a value for UID=0x01, and increment it by 1. If
+// successful, it would print out the incremented value. It assumes that it has
+// access to UID=0x01, and that `val` predicate is of type int.
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/dgraph-io/dgo"
+	"github.com/dgraph-io/dgo/protos/api"
+	"google.golang.org/grpc"
+)
+
+var addr = flag.String("addr", "localhost:9080", "Address of Dgraph server.")
+
+type Counter struct {
+	Uid string `json:"uid"`
+	Val int    `json:"val"`
+}
+
+func increment(dg *dgo.Dgraph) (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	txn := dg.NewTxn()
+	resp, err := txn.Query(ctx, `{ q(func: uid(0x01)) { uid, val }}`)
+	if err != nil {
+		return 0, err
+	}
+	m := make(map[string][]Counter)
+	if err := json.Unmarshal(resp.Json, &m); err != nil {
+		return 0, err
+	}
+	var counter Counter
+	if len(m["q"]) == 0 {
+		counter.Uid = "0x01"
+	} else if len(m["q"]) == 1 {
+		counter = m["q"][0]
+	} else {
+		log.Fatalf("Invalid response: %q", resp.Json)
+	}
+	counter.Val += 1
+
+	var mu api.Mutation
+	data, err := json.Marshal(counter)
+	if err != nil {
+		return 0, err
+	}
+	mu.SetJson = data
+	_, err = txn.Mutate(ctx, &mu)
+	if err != nil {
+		return 0, err
+	}
+	return counter.Val, txn.Commit(ctx)
+}
+
+func main() {
+	flag.Parse()
+
+	conn, err := grpc.Dial(*addr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+	dc := api.NewDgraphClient(conn)
+	dg := dgo.NewDgraphClient(dc)
+
+	for {
+		val, err := increment(dg)
+		if err != nil {
+			fmt.Printf("While trying to increment counter: %v. Retrying...\n", err)
+			time.Sleep(time.Second)
+			continue
+		}
+		fmt.Printf("Counter SET OK: %d\n", val)
+		return
+	}
+}
