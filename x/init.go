3d0b2cd84f04b263f6c5128e682a63be8dcb220d
Manish R Jain <manish@dgraph.io>
2018-10-10 17:17:03 -0700

    Switch Dgraph back to Apache 2.0 (#2652)
    While the promise of Commons Clause is a great one, i.e. restricting resale while allowing proprietary usage, its subjective implementation generates a lot of friction and confusion for the open source community.
    
    Being an open source developer at heart, I'm glad to be bringing Dgraph back to Apache 2.0 license. It is something that we started off with, but then got lost into the AGPL woods. At the end of the day, Apache license is the gold standard of licenses, and something open source users (i.e. the world) feel very comfortable with. The vision for Dgraph is to bring graph databases mainstream, allowing them to be the primary databases holding the source of truth, and that's only possible if it can be adopted by any company, big or small, without any concerns.
    
    **Apache license lets achieve that vision.**
    
    We will be adding proprietary features later on under an enterprise license, but as long as I'm in charge, we're not moving the core away from Apache 2.0 license. This is it.

--- x/init.go
+++ x/init.go
@@ -1,8 +1,17 @@
 /*
- * Copyright 2016-2018 Dgraph Labs, Inc.
+ * Copyright 2016-2018 Dgraph Labs, Inc. and Contributors
  *
- * This file is available under the Apache License, Version 2.0,
- * with the Commons Clause restriction.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package x
@@ -59,7 +68,7 @@ For Dgraph official documentation, visit https://docs.dgraph.io.
 For discussions about Dgraph     , visit https://discuss.dgraph.io.
 To say hi to the community       , visit https://dgraph.slack.com.
 
-Licensed under Apache 2.0 + Commons Clause. Copyright 2015-2018 Dgraph Labs, Inc.
+Licensed under Apache 2.0. Copyright 2015-2018 Dgraph Labs, Inc.
 
 `,
 		dgraphVersion, lastCommitSHA, lastCommitTime, gitBranch)
