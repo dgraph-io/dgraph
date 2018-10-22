b37861c66d9712252e56a2500776e0b3545a1ec0
Daniel Mai <daniel@dgraph.io>
2018-10-22 11:26:08 -0700

    Fix typo.

--- systest/cluster_test.go
+++ systest/cluster_test.go
@@ -246,7 +246,7 @@ func DONOTRUN_TestClusterSnapshot(t *testing.T) {
 	time.Sleep(10 * time.Second)
 
 	cmd.Process = nil
-	glog.Infoln("Trying to restart Dgraph Alpha
+	glog.Infoln("Trying to restart Dgraph Alpha")
 	if err := cmd.Start(); err != nil {
 		shutdownCluster()
 		log.Fatal(err)
