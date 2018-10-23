8a98cfcf4fe6d30a2a828de391e23cec37ef17ba
Manish R Jain <manish@dgraph.io>
2018-10-22 19:54:41 -0700

    Improve the debug tool to be able to output all keys, in a better format, and lookup an individual key.

--- dgraph/cmd/debug/run.go
+++ dgraph/cmd/debug/run.go
@@ -17,18 +17,31 @@
 package debug
 
 import (
+	"bytes"
+	"encoding/hex"
 	"fmt"
 	"log"
 	"math"
-	"sort"
 
 	"github.com/dgraph-io/badger"
 	"github.com/dgraph-io/badger/options"
+	"github.com/dgraph-io/dgraph/posting"
+	"github.com/dgraph-io/dgraph/protos/pb"
 	"github.com/dgraph-io/dgraph/x"
 	"github.com/spf13/cobra"
 )
 
 var Debug x.SubCommand
+
+var opt flagOptions
+
+type flagOptions struct {
+	vals      bool
+	keyLookup string
+	predicate string
+	readOnly  bool
+	pdir      string
+}
 
 func init() {
 	Debug.Cmd = &cobra.Command{
@@ -40,94 +53,120 @@ func init() {
 	}
 
 	flag := Debug.Cmd.Flags()
-	flag.StringP("postings", "p", "", "Directory where posting lists are stored.")
-	flag.BoolP("predicates", "s", false, "List all the predicates.")
-	flag.BoolP("readonly", "o", true, "Open in read only mode.")
+	flag.StringVarP(&opt.pdir, "postings", "p", "", "Directory where posting lists are stored.")
+	flag.StringVar(&opt.predicate, "pred", "", "Only output specified predicate.")
+	flag.BoolVar(&opt.vals, "vals", false, "Output values along with keys.")
+	flag.StringVarP(&opt.keyLookup, "lookup", "l", "", "Hex of key to lookup.")
+	flag.BoolVarP(&opt.readOnly, "readonly", "o", true, "Open in read only mode.")
 }
 
-type Stats struct {
-	Data    int
-	Index   int
-	Schema  int
-	Reverse int
-	Count   int
-	Total   int
+func lookup(db *badger.DB) {
+	txn := db.NewTransactionAt(math.MaxUint64, false)
+	defer txn.Discard()
+
+	iopts := badger.DefaultIteratorOptions
+	iopts.AllVersions = true
+	iopts.PrefetchValues = false
+	itr := txn.NewIterator(iopts)
+	defer itr.Close()
+
+	key, err := hex.DecodeString(opt.keyLookup)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for itr.Seek(key); itr.Valid(); {
+		item := itr.Item()
+		pl, err := posting.ReadPostingList(item.KeyCopy(nil), itr)
+		if err != nil {
+			log.Fatal(err)
+		}
+		var buf bytes.Buffer
+		fmt.Fprintf(&buf, " Key: %x", item.Key())
+		fmt.Fprintf(&buf, " Length: %d\n", pl.Length(math.MaxUint64, 0))
+		err = pl.Iterate(math.MaxUint64, 0, func(o *pb.Posting) error {
+			fmt.Fprintf(&buf, " Uid: %d\n", o.Uid)
+			// TODO: Extend this to output more fields.
+			return nil
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(buf.String())
+		return
+	}
+}
+
+func printKeys(db *badger.DB) {
+	txn := db.NewTransactionAt(math.MaxUint64, false)
+	defer txn.Discard()
+
+	iopts := badger.DefaultIteratorOptions
+	iopts.PrefetchValues = false
+	itr := txn.NewIterator(iopts)
+	defer itr.Close()
+
+	var prefix []byte
+	if len(opt.predicate) > 0 {
+		prefix = x.PredicatePrefix(opt.predicate)
+	}
+
+	var loop int
+	for itr.Seek(prefix); itr.ValidForPrefix(prefix); itr.Next() {
+		item := itr.Item()
+		pk := x.Parse(item.Key())
+		var buf bytes.Buffer
+
+		// Don't use a switch case here. Because multiple of these can be true. In particular,
+		// IsSchema can be true alongside IsData.
+		if pk.IsData() {
+			buf.WriteString("{d}")
+		}
+		if pk.IsIndex() {
+			buf.WriteString("{i}")
+		}
+		if pk.IsCount() {
+			buf.WriteString("{c}")
+		}
+		if pk.IsSchema() {
+			buf.WriteString("{s}")
+		}
+		if pk.IsReverse() {
+			buf.WriteString("{r}")
+		}
+
+		buf.WriteString(" attr: " + pk.Attr)
+		if len(pk.Term) > 0 {
+			fmt.Fprintf(&buf, " term: [%d] %s ", pk.Term[0], pk.Term[1:])
+		}
+		if pk.Uid > 0 {
+			fmt.Fprintf(&buf, " uid: %d ", pk.Uid)
+		}
+		fmt.Fprintf(&buf, " item: [%d, b%04b]", item.EstimatedSize(), item.UserMeta())
+		fmt.Fprintf(&buf, " key: %s", hex.EncodeToString(item.Key()))
+		fmt.Println(buf.String())
+		loop++
+	}
+	fmt.Printf("Found %d keys\n", loop)
 }
 
 func run() {
-	opts := badger.DefaultOptions
-	opts.Dir = Debug.Conf.GetString("postings")
-	opts.ValueDir = Debug.Conf.GetString("postings")
-	opts.TableLoadingMode = options.MemoryMap
-	opts.ReadOnly = Debug.Conf.GetBool("readonly")
+	bopts := badger.DefaultOptions
+	bopts.Dir = opt.pdir
+	bopts.ValueDir = opt.pdir
+	bopts.TableLoadingMode = options.MemoryMap
+	bopts.ReadOnly = opt.readOnly
 
-	x.AssertTruef(len(opts.Dir) > 0, "No posting dir specified.")
-	fmt.Printf("Opening DB: %s\n", opts.Dir)
+	x.AssertTruef(len(bopts.Dir) > 0, "No posting dir specified.")
+	fmt.Printf("Opening DB: %s\n", bopts.Dir)
 
-	db, err := badger.OpenManaged(opts)
+	db, err := badger.OpenManaged(bopts)
 	x.Check(err)
 	defer db.Close()
 
-	if Debug.Conf.GetBool("predicates") {
-		txn := db.NewTransactionAt(math.MaxUint64, false)
-		defer txn.Discard()
-
-		iopts := badger.DefaultIteratorOptions
-		iopts.PrefetchValues = false
-		itr := txn.NewIterator(iopts)
-		defer itr.Close()
-
-		var loop int
-		m := make(map[string]*Stats)
-		for itr.Rewind(); itr.Valid(); itr.Next() {
-			item := itr.Item()
-			pk := x.Parse(item.Key())
-			stats, ok := m[pk.Attr]
-			if !ok {
-				stats = new(Stats)
-				m[pk.Attr] = stats
-			}
-			stats.Total += 1
-			// Don't use a switch case here. Because multiple of these can be true. In particular,
-			// IsSchema can be true alongside IsData.
-			if pk.IsData() {
-				stats.Data += 1
-			}
-			if pk.IsIndex() {
-				stats.Index += 1
-			}
-			if pk.IsCount() {
-				stats.Count += 1
-			}
-			if pk.IsSchema() {
-				stats.Schema += 1
-			}
-			if pk.IsReverse() {
-				stats.Reverse += 1
-			}
-			loop++
-		}
-
-		type C struct {
-			pred  string
-			stats *Stats
-		}
-
-		var counts []C
-		for pred, stats := range m {
-			counts = append(counts, C{pred, stats})
-		}
-		sort.Slice(counts, func(i, j int) bool {
-			return counts[i].stats.Total > counts[j].stats.Total
-		})
-		for _, c := range counts {
-			st := c.stats
-			fmt.Printf("Total: %-8d. Predicate: %-20s\n", st.Total, c.pred)
-			fmt.Printf("  Data: %d Index: %d Reverse: %d Schema: %d Count: %d Predicate: %s\n\n",
-				st.Data, st.Index, st.Reverse, st.Schema, st.Count, c.pred)
-		}
-		fmt.Printf("Found %d keys\n", loop)
-		return
+	switch {
+	case len(opt.keyLookup) > 0:
+		lookup(db)
+	default:
+		printKeys(db)
 	}
-	log.Fatalln("Please provide a valid option for diagnosis.")
 }
