dgraph-io/dgraph

commit 63689045b3d00f0e66f396c5d80dccccbc7f3b47

Make has function correct w.r.t. transactions (#2585)
Generate the full posting list based on the read timestamp, so we can determine if it is empty or not. If empty, then has should not pick up the UID. Similarly, use Btree to pick up posting lists from memory, also considering whether they have a valid posting keeping transactional properties intact.

This fixes https://github.com/dgraph-io/dgraph/issues/2574.

worker/task.go
@@ -8,6 +8,7 @@
 package worker
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"math/rand"
@@ -19,6 +20,7 @@ import (
 	"google.golang.org/grpc/metadata"
 
 	"github.com/dgraph-io/badger"
+	"github.com/golang/glog"
 	"golang.org/x/net/context"
 	"golang.org/x/net/trace"
 
@@ -1609,43 +1611,98 @@ func (cp *countParams) evaluate(out *intern.Result) error {
 	return nil
 }
 
-// TODO - Check meta for empty PL and skip it.
-// This is not transactionally isolated, add to docs
+// handleHasFunction looks at both the inmemory btree populated by
+// posting/lists.go, and Badger. Thus, it can capture both committed
+// transactions and in-progress transactions. It also accounts for transaction
+// start ts.
 func handleHasFunction(ctx context.Context, q *intern.Query, out *intern.Result) error {
-	tlist := &intern.List{}
-
 	txn := pstore.NewTransactionAt(q.ReadTs, false)
 	defer txn.Discard()
 
-	itOpt := badger.DefaultIteratorOptions
-	itOpt.PrefetchValues = false
-
-	pk := x.ParsedKey{
+	initKey := x.ParsedKey{
 		Attr: q.Attr,
 	}
 	startKey := x.DataKey(q.Attr, q.AfterUid+1)
-	prefix := pk.DataPrefix()
+	prefix := initKey.DataPrefix()
 	if q.Reverse {
-		startKey = x.ReverseKey(q.Attr, q.AfterUid+1)
-		prefix = pk.ReversePrefix()
+		glog.Warningln("has function does not handle reverse iteration.")
 	}
 
-	w := 0
-	it := posting.NewTxnPrefixIterator(txn, itOpt, prefix, startKey)
+	var cachedUids []uint64
+	bitr := &posting.BTreeIterator{
+		// Reverse: q.Reverse, // We don't handle reverse iteration.
+		Prefix: prefix,
+	}
+	for bitr.Seek(startKey); bitr.Valid(); bitr.Next() {
+		key := bitr.Key()
+		pl := posting.GetLru(key)
+		if pl == nil {
+			continue
+		}
+		pk := x.Parse(key)
+		var num int
+		if err := pl.Iterate(q.ReadTs, 0, func(_ *intern.Posting) bool {
+			num++
+			return false
+		}); err != nil {
+			return err
+		}
+		if num > 0 {
+			cachedUids = append(cachedUids, pk.Uid)
+		}
+	}
+
+	result := &intern.List{}
+	var prevKey []byte
+	var cidx, w int
+	itOpt := badger.DefaultIteratorOptions
+	itOpt.PrefetchValues = false
+	itOpt.AllVersions = true
+	it := txn.NewIterator(itOpt)
 	defer it.Close()
-	for ; it.Valid(); it.Next() {
-		if it.UserMeta() == posting.BitEmptyPosting {
+
+	for it.Seek(startKey); it.ValidForPrefix(prefix); {
+		item := it.Item()
+		if bytes.Equal(item.Key(), prevKey) {
+			it.Next()
+			continue
+		}
+		prevKey = append(prevKey[:0], item.Key()...)
+
+		// Parse the key upfront, otherwise ReadPostingList would advance the
+		// iterator.
+		pk := x.Parse(item.Key())
+
+		// Pick up all the uids found in the Btree, which are <= this Uid.
+		for cidx < len(cachedUids) && cachedUids[cidx] < pk.Uid {
+			result.Uids = append(result.Uids, pk.Uid)
+			cidx++
+		}
+		if cidx < len(cachedUids) && cachedUids[cidx] == pk.Uid {
+			// No need to check this key on disk. We already found it in the
+			// Btree.
+			result.Uids = append(result.Uids, pk.Uid)
+			cidx++
 			continue
 		}
 
-		pl := posting.GetLru(it.Key())
-		if pl != nil && pl.IsEmpty() {
-			// empty pl's can be present in lru
-			// We merge the keys on badger and the keys present in memory so
-			// we need to skip empty pls
-			continue
+		// We do need to copy over the key for ReadPostingList.
+		l, err := posting.ReadPostingList(item.KeyCopy(nil), it)
+		if err != nil {
+			return err
 		}
-		pk := x.Parse(it.Key())
+		var num int
+		if err = l.Iterate(q.ReadTs, 0, func(_ *intern.Posting) bool {
+			num++
+			return false
+		}); err != nil {
+			return err
+		}
+		if num > 0 {
+			result.Uids = append(result.Uids, pk.Uid)
+			w++
+		}
+
 		if w%1000 == 0 {
 			select {
 			case <-ctx.Done():
@@ -1657,10 +1714,12 @@ func handleHasFunction(ctx context.Context, q *intern.Query, out *intern.Result)
 				}
 			}
 		}
-		w++
-		tlist.Uids = append(tlist.Uids, pk.Uid)
+	}
+	// Pick up any remaining UIDs from the Btree.
+	if cidx < len(cachedUids) {
+		result.Uids = append(result.Uids, cachedUids[cidx:]...)
 	}
 
-	out.UidMatrix = append(out.UidMatrix, tlist)
+	out.UidMatrix = append(out.UidMatrix, result)
 	return nil
 }

systest/mutations_test.go
@@ -12,31 +12,31 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"os"
 	"sort"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/dgraph-io/dgo"
 	"github.com/dgraph-io/dgo/protos/api"
+	"github.com/dgraph-io/dgraph/x"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
 )
 
+// TestSystem uses the externally run Dgraph cluster for testing. Most other
+// tests in this package are using a suite struct system, which runs Dgraph and
+// loads data with bulk and live loader.
 func TestSystem(t *testing.T) {
-	dir, err := ioutil.TempDir("", "")
-	require.NoError(t, err)
-	defer os.RemoveAll(dir)
-
-	cluster := NewDgraphCluster(dir)
-	require.NoError(t, cluster.Start())
-	defer cluster.Close()
-
 	wrap := func(fn func(*testing.T, *dgo.Dgraph)) func(*testing.T) {
 		return func(t *testing.T) {
-			require.NoError(t, cluster.client.Alter(
+			conn, err := grpc.Dial("localhost:9180", grpc.WithInsecure())
+			x.Check(err)
+			dg := dgo.NewDgraphClient(api.NewDgraphClient(conn))
+
+			require.NoError(t, dg.Alter(
 				context.Background(), &api.Operation{DropAll: true}))
-			fn(t, cluster.client)
+			fn(t, dg)
 		}
 	}
 
@@ -67,6 +67,7 @@ func TestSystem(t *testing.T) {
 	t.Run("regex query vars", wrap(RegexQueryWithVars))
 	t.Run("graphql var child", wrap(GraphQLVarChild))
 	t.Run("math ge", wrap(MathGe))
+	t.Run("has should not have deleted edge", wrap(HasDeletedEdge))
 }
 
 func ExpandAllLangTest(t *testing.T, c *dgo.Dgraph) {
@@ -1459,4 +1460,100 @@ func MathGe(t *testing.T, c *dgo.Dgraph) {
 		  ]
 		}
 	`, string(resp.GetJson()))
+}
+
+func HasDeletedEdge(t *testing.T, c *dgo.Dgraph) {
+	ctx := context.Background()
+
+	txn := c.NewTxn()
+	defer txn.Discard(ctx)
+
+	assigned, err := txn.Mutate(ctx, &api.Mutation{
+		CommitNow: true,
+		SetNquads: []byte(`
+			_:a <end> "" .
+			_:b <end> "" .
+			_:c <end> "" .
+			_:d <start> "" .
+			_:d2 <start> "" .
+		`),
+	})
+	check(t, err)
+
+	var ids []string
+	for key, uid := range assigned.Uids {
+		if !strings.HasPrefix(key, "d") {
+			ids = append(ids, uid)
+		}
+	}
+	sort.Strings(ids)
+
+	type U struct {
+		Uid string `json:"uid"`
+	}
+
+	getUids := func(txn *dgo.Txn) []string {
+		resp, err := txn.Query(ctx, `{
+			me(func: has(end)) { uid }
+			you(func: has(end)) { count(uid) }
+		}`)
+		check(t, err)
+		t.Logf("resp: %s\n", resp.GetJson())
+		m := make(map[string][]U)
+		err = json.Unmarshal(resp.GetJson(), &m)
+		check(t, err)
+		uids := m["me"]
+		var result []string
+		for _, uid := range uids {
+			result = append(result, uid.Uid)
+		}
+		return result
+	}
+
+	txn = c.NewTxn()
+	defer txn.Discard(ctx)
+	uids := getUids(txn)
+	require.Equal(t, 3, len(uids))
+	for _, uid := range uids {
+		require.Contains(t, ids, uid)
+	}
+
+	deleteMu := &api.Mutation{
+		CommitNow: true,
+	}
+	deleteMu.DelNquads = []byte(fmt.Sprintf(`
+		<%s> <end> * .
+	`, ids[len(ids)-1]))
+	t.Logf("deleteMu: %+v\n", deleteMu)
+	_, err = txn.Mutate(ctx, deleteMu)
+	check(t, err)
+
+	txn = c.NewTxn()
+	defer txn.Discard(ctx)
+	uids = getUids(txn)
+	require.Equal(t, 2, len(uids))
+	for _, uid := range uids {
+		require.Contains(t, ids, uid)
+	}
+	// Remove the last entry from ids.
+	ids = ids[:len(ids)-1]
+
+	// This time we didn't commit the txn.
+	assigned, err = txn.Mutate(ctx, &api.Mutation{
+		SetNquads: []byte(`
+			_:d <end> "" .
+		`),
+	})
+	check(t, err)
+
+	require.Equal(t, 1, len(assigned.Uids))
+	for _, uid := range assigned.Uids {
+		ids = append(ids, uid)
+	}
+
+	uids = getUids(txn)
+	require.Equal(t, 3, len(uids))
+	for _, uid := range uids {
+		require.Contains(t, ids, uid)
+	}
 }

posting/mvcc.go
@@ -257,35 +257,39 @@ func getNew(key []byte, pstore *badger.ManagedDB) (*List, error) {
 	return l, nil
 }
 
-type bTreeIterator struct {
+type BTreeIterator struct {
 	keys    [][]byte
 	idx     int
-	reverse bool
-	prefix  []byte
+	Reverse bool
+	Prefix  []byte
 }
 
-func (bi *bTreeIterator) Next() {
+func (bi *BTreeIterator) Next() {
 	bi.idx++
 }
 
-func (bi *bTreeIterator) Key() []byte {
+func (bi *BTreeIterator) Key() []byte {
 	x.AssertTrue(bi.Valid())
 	return bi.keys[bi.idx]
 }
 
-func (bi *bTreeIterator) Valid() bool {
+func (bi *BTreeIterator) Valid() bool {
+	// No need to check HasPrefix here, because we are only picking those keys
+	// which have the right prefix in Seek.
 	return bi.idx < len(bi.keys)
 }
 
-func (bi *bTreeIterator) Seek(key []byte) {
+func (bi *BTreeIterator) Seek(key []byte) {
+	bi.keys = bi.keys[:0]
+	bi.idx = 0
 	cont := func(key []byte) bool {
-		if !bytes.HasPrefix(key, bi.prefix) {
+		if !bytes.HasPrefix(key, bi.Prefix) {
 			return false
 		}
 		bi.keys = append(bi.keys, key)
 		return true
 	}
-	if !bi.reverse {
+	if !bi.Reverse {
 		btree.AscendGreaterOrEqual(key, cont)
 	} else {
 		btree.DescendLessOrEqual(key, cont)
@@ -293,7 +297,7 @@ func (bi *bTreeIterator) Seek(key []byte) {
 }
 
 type TxnPrefixIterator struct {
-	btreeIter  *bTreeIterator
+	btreeIter  *BTreeIterator
 	badgerIter *badger.Iterator
 	prefix     []byte
 	reverse    bool
@@ -307,9 +311,9 @@ func NewTxnPrefixIterator(txn *badger.Txn,
 	txnIt := new(TxnPrefixIterator)
 	txnIt.reverse = iterOpts.Reverse
 	txnIt.prefix = prefix
-	txnIt.btreeIter = &bTreeIterator{
-		reverse: iterOpts.Reverse,
-		prefix:  prefix,
+	txnIt.btreeIter = &BTreeIterator{
+		Reverse: iterOpts.Reverse,
+		Prefix:  prefix,
 	}
 	txnIt.btreeIter.Seek(key)
 	// Create iterator only after copying the keys from btree, or else there could

commit 901cb2588cb6af57fdb8009af54cbb42642f0ef4

Bug Fix: Has function should consider reverses
Fix a bug caused by #2585, where I mistook reverse to mean reverse
iteration, when it actually meant reverse index. Fixed it to its
original meaning.

worker/task.go
@@ -39,7 +39,6 @@ import (
 	"github.com/dgraph-io/dgraph/types/facets"
 	"github.com/dgraph-io/dgraph/x"
 
-	"github.com/golang/glog"
 	cindex "github.com/google/codesearch/index"
 	cregexp "github.com/google/codesearch/regexp"
 	"golang.org/x/net/context"
@@ -1630,7 +1629,10 @@ func handleHasFunction(ctx context.Context, q *pb.Query, out *pb.Result) error {
 	startKey := x.DataKey(q.Attr, q.AfterUid+1)
 	prefix := initKey.DataPrefix()
 	if q.Reverse {
-		glog.Warningln("has function does not handle reverse iteration.")
+		// Reverse does not mean reverse iteration. It means we're looking for
+		// the reverse index.
+		startKey = x.ReverseKey(q.Attr, q.AfterUid+1)
+		prefix = initKey.ReversePrefix()
 	}
 
 	result := &pb.List{}

systest/mutations_test.go
@@ -77,6 +77,7 @@ func TestSystem(t *testing.T) {
 	t.Run("graphql var child", wrap(GraphQLVarChild))
 	t.Run("math ge", wrap(MathGe))
 	t.Run("has should not have deleted edge", wrap(HasDeletedEdge))
+	t.Run("has should have reverse edges", wrap(HasReverseEdge))
 }
 
 func ExpandAllLangTest(t *testing.T, c *dgo.Dgraph) {
@@ -1522,4 +1523,56 @@ func HasDeletedEdge(t *testing.T, c *dgo.Dgraph) {
 	for _, uid := range uids {
 		require.Contains(t, ids, uid)
 	}
+}
+
+func HasReverseEdge(t *testing.T, c *dgo.Dgraph) {
+	ctx := context.Background()
+
+	check(t, c.Alter(ctx, &api.Operation{
+		Schema: `
+			follow: uid @reverse .
+		`,
+	}))
+	txn := c.NewTxn()
+	defer txn.Discard(ctx)
+
+	_, err := txn.Mutate(ctx, &api.Mutation{
+		CommitNow: true,
+		SetNquads: []byte(`
+			_:alice <name> "alice" .
+			_:bob <name> "bob" .
+			_:carol <name> "carol" .
+			_:alice <follow> _:carol .
+			_:bob <follow> _:carol .
+		`),
+	})
+	check(t, err)
+
+	type F struct {
+		Name string `json:"name"`
+	}
+
+	txn = c.NewTxn()
+	defer txn.Discard(ctx)
+	resp, err := txn.Query(ctx, `{
+		fwd(func: has(follow)) { name }
+		rev(func: has(~follow)) { name }
+		}`)
+	check(t, err)
+
+	t.Logf("resp: %s\n", resp.GetJson())
+	m := make(map[string][]F)
+	err = json.Unmarshal(resp.GetJson(), &m)
+	check(t, err)
+
+	fwds := m["fwd"]
+	revs := m["rev"]
+
+	require.Equal(t, len(fwds), 2)
+	require.Contains(t, []string{"alice", "bob"}, fwds[0].Name)
+	require.Contains(t, []string{"alice", "bob"}, fwds[1].Name)
+	require.NotEqual(t, fwds[0].Name, fwds[1].Name)
+
+	require.Equal(t, len(revs), 1)
+	require.Equal(t, revs[0].Name, "carol")
 }