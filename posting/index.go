dgraph-io/dgraph

commit 3c293a75fc94030e56c571e421135025bfac4456

Cleanup and logging during index rebuilds (#2695)
* Reuse TxnWriter for deletes. Fix a bug in TxnWriter which would have caused it to not capture any errors. Log all errors encountered in TxnWriter callbacks.
* A lot more logging info about index rebuilds.

posting/index.go
@@ -19,9 +19,9 @@ package posting
 import (
 	"bytes"
 	"context"
+	"encoding/hex"
 	"fmt"
 	"math"
-	"sync"
 	"time"
 
 	"golang.org/x/net/trace"
@@ -412,46 +412,22 @@ func (l *List) AddMutationWithIndex(ctx context.Context, t *pb.DirectedEdge,
 }
 
 func deleteEntries(prefix []byte, remove func(key []byte) bool) error {
-	iterOpt := badger.DefaultIteratorOptions
-	iterOpt.PrefetchValues = false
-	txn := pstore.NewTransactionAt(math.MaxUint64, false)
-	defer txn.Discard()
-	idxIt := txn.NewIterator(iterOpt)
-	defer idxIt.Close()
+	return pstore.View(func(txn *badger.Txn) error {
+		opt := badger.DefaultIteratorOptions
+		opt.PrefetchValues = false
 
-	var m sync.Mutex
-	var err error
-	setError := func(e error) {
-		m.Lock()
-		err = e
-		m.Unlock()
-	}
-	var wg sync.WaitGroup
-	for idxIt.Seek(prefix); idxIt.ValidForPrefix(prefix); idxIt.Next() {
-		item := idxIt.Item()
-		if !remove(item.Key()) {
-			continue
-		}
-		nkey := item.KeyCopy(nil)
-		version := item.Version()
+		itr := txn.NewIterator(opt)
+		defer itr.Close()
 
-		txn := pstore.NewTransactionAt(version, true)
-		txn.Delete(nkey)
-		wg.Add(1)
-		err := txn.CommitAt(version, func(e error) {
-			defer wg.Done()
-			if e != nil {
-				setError(e)
-				return
+		writer := x.NewTxnWriter(pstore)
+		for itr.Seek(prefix); itr.ValidForPrefix(prefix); itr.Next() {
+			item := itr.Item()
+			if err := writer.Delete(item.KeyCopy(nil), item.Version()); err != nil {
+				return err
 			}
-		})
-		txn.Discard()
-		if err != nil {
-			break
 		}
-	}
-	wg.Wait()
-	return err
+		return writer.Flush()
+	})
 }
 
 func compareAttrAndType(key []byte, attr string, typ byte) bool {
@@ -527,6 +503,8 @@ func (r *rebuild) Run(ctx context.Context) error {
 	t := pstore.NewTransactionAt(r.startTs, false)
 	defer t.Discard()
 
+	glog.V(1).Infof("Rebuild: Starting process. StartTs=%d. Prefix=\n%s\n",
+		r.startTs, hex.Dump(r.prefix))
 	opts := badger.DefaultIteratorOptions
 	opts.AllVersions = true
 	it := t.NewIterator(opts)
@@ -585,6 +563,7 @@ func (r *rebuild) Run(ctx context.Context) error {
 			return err
 		}
 	}
+	glog.V(1).Infof("Rebuild: Iteration done. Now commiting at ts=%d\n", r.startTs)
 
 	// We must commit all the posting lists to memory, so they'd be picked up
 	// during posting list rollup below.
@@ -593,26 +572,33 @@ func (r *rebuild) Run(ctx context.Context) error {
 	}
 
 	// Now we write all the created posting lists to disk.
-	writer := x.TxnWriter{DB: pstore}
+	writer := x.NewTxnWriter(pstore)
 	for key := range txn.deltas {
 		pl, err := txn.Get([]byte(key))
 		if err != nil {
 			return err
 		}
 
+		le := pl.Length(r.startTs, 0)
+		if le == 0 {
+			y.AssertTruef(le > 0, "Unexpected list of size zero: %q", key)
+		}
+		kv, err := pl.MarshalToKv()
+		if err != nil {
+			return err
+		}
+		// We choose to write the PL at r.startTs, so it won't be read by txns,
+		// which occurred before this schema mutation. Typically, we use
+		// kv.Version as the timestamp.
+		if err = writer.SetAt(kv.Key, kv.Val, kv.UserMeta[0], r.startTs); err != nil {
+			return err
+		}
+		// This locking is just to catch any future issues.  We shouldn't need
+		// to release this lock, because each posting list must only be accessed
+		// once and never again.
 		pl.Lock()
-		// We shouldn't need to release this lock, because each posting list
-		// must only be accessed once and never again.
-		le := pl.length(r.startTs, 0)
-		y.AssertTruef(le > 0, "Unexpected list of size zero: %q", key)
-		if err := pl.rollup(); err != nil {
-			return err
-		}
-		data, meta := marshalPostingList(pl.plist)
-		if err = writer.SetAt([]byte(key), data, meta, r.startTs); err != nil {
-			return err
-		}
 	}
+	glog.V(1).Infoln("Rebuild: Flushing all writes.")
 	return writer.Flush()
 }
 

worker/index.go
@@ -22,6 +22,7 @@ import (
 	"github.com/dgraph-io/dgraph/posting"
 	"github.com/dgraph-io/dgraph/schema"
 	"github.com/dgraph-io/dgraph/x"
+	"github.com/golang/glog"
 )
 
 func (n *node) rebuildOrDelIndex(ctx context.Context, attr string, rebuild bool, startTs uint64) error {
@@ -29,13 +30,13 @@ func (n *node) rebuildOrDelIndex(ctx context.Context, attr string, rebuild bool,
 		return x.Errorf("Predicate %s index mismatch, rebuild %v", attr, rebuild)
 	}
 	// Remove index edges
+	glog.Infof("Deleting index for %s", attr)
 	if err := posting.DeleteIndex(attr); err != nil {
 		return err
 	}
 	if rebuild {
-		if err := posting.RebuildIndex(ctx, attr, startTs); err != nil {
-			return err
-		}
+		glog.Infof("Rebuilding index for %s", attr)
+		return posting.RebuildIndex(ctx, attr, startTs)
 	}
 	return nil
 }
@@ -44,26 +45,26 @@ func (n *node) rebuildOrDelRevEdge(ctx context.Context, attr string, rebuild boo
 	if schema.State().IsReversed(attr) != rebuild {
 		return x.Errorf("Predicate %s reverse mismatch, rebuild %v", attr, rebuild)
 	}
+	glog.Infof("Deleting reverse index for %s", attr)
 	if err := posting.DeleteReverseEdges(attr); err != nil {
 		return err
 	}
 	if rebuild {
 		// Remove reverse edges
-		if err := posting.RebuildReverseEdges(ctx, attr, startTs); err != nil {
-			return err
-		}
+		glog.Infof("Rebuilding reverse index for %s", attr)
+		return posting.RebuildReverseEdges(ctx, attr, startTs)
 	}
 	return nil
 }
 
 func (n *node) rebuildOrDelCountIndex(ctx context.Context, attr string, rebuild bool, startTs uint64) error {
+	glog.Infof("Deleting count index for %s", attr)
 	if err := posting.DeleteCountIndex(attr); err != nil {
 		return err
 	}
 	if rebuild {
-		if err := posting.RebuildCountIndex(ctx, attr, startTs); err != nil {
-			return err
-		}
+		glog.Infof("Rebuilding count index for %s", attr)
+		return posting.RebuildCountIndex(ctx, attr, startTs)
 	}
 	return nil
 }

posting/index_test.go
@@ -137,7 +137,7 @@ func addMutation(t *testing.T, l *List, edge *pb.DirectedEdge, op uint32,
 		require.NoError(t, err)
 	}
 
-	writer := &x.TxnWriter{DB: pstore}
+	writer := x.NewTxnWriter(pstore)
 	require.NoError(t, txn.CommitToDisk(writer, commitTs))
 	require.NoError(t, writer.Flush())
 	require.NoError(t, txn.CommitToMemory(commitTs))

query/common_test.go
@@ -296,7 +296,7 @@ func addPassword(t *testing.T, uid uint64, attr, password string) {
 func populateGraph(t *testing.T) {
 	x.AssertTrue(ps != nil)
 	// Initialize a TxnWriter, so CommitToDisk can use it to write to Badger.
-	writer = &x.TxnWriter{DB: ps}
+	writer = x.NewTxnWriter(ps)
 	defer x.Check(writer.Flush())
 
 	const schemaStr = `

query/mutation.go
@@ -22,14 +22,15 @@ import (
 	"fmt"
 	"strings"
 
-	"golang.org/x/net/trace"
-
 	"github.com/dgraph-io/dgo/protos/api"
 	"github.com/dgraph-io/dgraph/gql"
 	"github.com/dgraph-io/dgraph/protos/pb"
 	"github.com/dgraph-io/dgraph/types/facets"
 	"github.com/dgraph-io/dgraph/worker"
 	"github.com/dgraph-io/dgraph/x"
+
+	"github.com/golang/glog"
+	"golang.org/x/net/trace"
 )
 
 func ApplyMutations(ctx context.Context, m *pb.Mutations) (*api.TxnContext, error) {
@@ -52,6 +53,7 @@ func ApplyMutations(ctx context.Context, m *pb.Mutations) (*api.TxnContext, erro
 	}
 	tctx, err := worker.MutateOverNetwork(ctx, m)
 	if err != nil {
+		glog.Errorf("MutateOverNetwork Error: %v. Mutation: %v.", err, m)
 		if tr, ok := trace.FromContext(ctx); ok {
 			tr.LazyPrintf("Error while MutateOverNetwork: %+v", err)
 		}

worker/draft.go
@@ -403,6 +403,9 @@ func (n *node) processApplyCh() {
 						p := &P{err: perr, size: psz, seen: time.Now()}
 						previous[proposal.Key] = p
 					}
+					if perr != nil {
+						glog.Errorf("Applying proposal. Error: %v. Proposal: %q.", perr, proposal)
+					}
 					n.elog.Printf("Applied proposal with key: %s, index: %d. Err: %v",
 						proposal.Key, e.Index, perr)
 				}
@@ -439,7 +442,7 @@ func (n *node) processApplyCh() {
 
 func (n *node) commitOrAbort(pkey string, delta *pb.OracleDelta) error {
 	// First let's commit all mutations to disk.
-	writer := x.TxnWriter{DB: pstore}
+	writer := x.NewTxnWriter(pstore)
 	toDisk := func(start, commit uint64) {
 		txn := posting.Oracle().GetTxn(start)
 		if txn == nil {
@@ -447,7 +450,7 @@ func (n *node) commitOrAbort(pkey string, delta *pb.OracleDelta) error {
 		}
 		var err error
 		for retry := Config.MaxRetries; retry != 0; retry-- {
-			err = txn.CommitToDisk(&writer, commit)
+			err = txn.CommitToDisk(writer, commit)
 			if err == nil {
 				break
 			}
@@ -748,7 +751,9 @@ func (n *node) Run() {
 // rollupLists would consolidate all the deltas that constitute one posting
 // list, and write back a complete posting list.
 func (n *node) rollupLists(readTs uint64) error {
-	writer := &x.TxnWriter{DB: pstore, BlindWrite: true}
+	writer := x.NewTxnWriter(pstore)
+	writer.BlindWrite = true // Do overwrite keys.
+
 	sl := streamLists{stream: writer, db: pstore}
 	sl.chooseKey = func(item *badger.Item) bool {
 		pk := x.Parse(item.Key())

worker/mutation.go
@@ -100,8 +100,7 @@ func runSchemaMutation(ctx context.Context, update *pb.SchemaUpdate, startTs uin
 		return err
 	}
 
-	updateSchema(update.Predicate, *update)
-	return nil
+	return updateSchema(update.Predicate, *update)
 }
 
 func runSchemaMutationHelper(ctx context.Context, update *pb.SchemaUpdate, startTs uint64) error {

worker/predicate.go
@@ -57,7 +57,8 @@ func (n *node) populateSnapshot(ps *badger.DB, pl *conn.Pool) (int, error) {
 
 	// We can use count to check the number of posting lists returned in tests.
 	count := 0
-	writer := &x.TxnWriter{DB: ps, BlindWrite: true}
+	writer := x.NewTxnWriter(ps)
+	writer.BlindWrite = true // Do overwrite keys.
 	for {
 		kvs, err := stream.Recv()
 		if err == io.EOF {

commit 594ad3e31479e85ad6aaf30b1b0fef2aa01050cb

Fix a bug caused by #2695.

posting/index.go
@@ -422,6 +422,9 @@ func deleteEntries(prefix []byte, remove func(key []byte) bool) error {
 		writer := x.NewTxnWriter(pstore)
 		for itr.Seek(prefix); itr.ValidForPrefix(prefix); itr.Next() {
 			item := itr.Item()
+			if !remove(item.Key()) {
+				continue
+			}
 			if err := writer.Delete(item.KeyCopy(nil), item.Version()); err != nil {
 				return err
 			}