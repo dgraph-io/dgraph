60825014f02d3cd4da8da4efde3808c666b525c8
Manish R Jain <manish@dgraph.io>
2018-10-31 16:14:34 -0700

    Create a lot simpler sorted uint64 codec (#2716)
    This PR contains a simple encoder/decoder using concepts from binary packing building on protobuf's default varint encoding.
    
    We currently use bp128 package, a modification on a 3rd party library, which uses a complex piece of code, that hasn't been touched since it was written. It is hard to understand and debug, given a lot of it is using SIMD instructions generated via a python library.
    
    We have been seeing panics during rollups, and reads which indicate bugs in this implementation. Instead of trying to fix that up, I wrote this PR, which uses concepts from Binary Packing and the fact that protobufs use Varint encoding to write a very simple encoder/decoder for posting lists.
    
    This PR just contains the implementation, tests and benchmarks. Next PR would modify posting lists to replace the current bp128 with the new binary package.
    
    Benchmark output with artificial dataset list:
    
    ```
    goos: linux
    goarch: amd64
    pkg: github.com/dgraph-io/dgraph/binary
    BenchmarkGzip-4      	      10	 112428082 ns/op
    --- BENCH: BenchmarkGzip-4
    	codec_test.go:77: Dataset Len=1000000. Size: 8.0 MB
    	codec_test.go:95: Output size: 2.6 MB. Compression: 0.33
    	codec_test.go:77: Dataset Len=1000000. Size: 8.0 MB
    	codec_test.go:95: Output size: 2.6 MB. Compression: 0.33
    BenchmarkUidPack/encode/128-4         	     100	  14213068 ns/op
    --- BENCH: BenchmarkUidPack/encode/128-4
    	codec_test.go:105: Dataset Len=1000000. Size: 8.0 MB
    	codec_test.go:115: Output size: 1.1 MB. Compression: 0.13
    	codec_test.go:105: Dataset Len=1000000. Size: 8.0 MB
    	codec_test.go:115: Output size: 1.1 MB. Compression: 0.13
    BenchmarkUidPack/encode/256-4         	     100	  12399005 ns/op
    --- BENCH: BenchmarkUidPack/encode/256-4
    	codec_test.go:105: Dataset Len=1000000. Size: 8.0 MB
    	codec_test.go:115: Output size: 1.0 MB. Compression: 0.13
    	codec_test.go:105: Dataset Len=1000000. Size: 8.0 MB
    	codec_test.go:115: Output size: 1.0 MB. Compression: 0.13
    BenchmarkUidPack/decode/128-4         	    1000	   2213527 ns/op
    --- BENCH: BenchmarkUidPack/decode/128-4
    	codec_test.go:140: Dataset Len=1000000. Size: 8.0 MB
    	codec_test.go:145: Output size: 1.1 MB. Compression: 0.13
    	codec_test.go:140: Dataset Len=1000000. Size: 8.0 MB
    	codec_test.go:145: Output size: 1.1 MB. Compression: 0.13
    	codec_test.go:140: Dataset Len=1000000. Size: 8.0 MB
    	codec_test.go:145: Output size: 1.1 MB. Compression: 0.13
    BenchmarkUidPack/decode/256-4         	    1000	   2014434 ns/op
    --- BENCH: BenchmarkUidPack/decode/256-4
    	codec_test.go:140: Dataset Len=1000000. Size: 8.0 MB
    	codec_test.go:145: Output size: 1.0 MB. Compression: 0.13
    	codec_test.go:140: Dataset Len=1000000. Size: 8.0 MB
    	codec_test.go:145: Output size: 1.0 MB. Compression: 0.13
    	codec_test.go:140: Dataset Len=1000000. Size: 8.0 MB
    	codec_test.go:145: Output size: 1.0 MB. Compression: 0.13
    PASS
    ok  	github.com/dgraph-io/dgraph/binary	17.786s
    ```

---
+++ binary/codec_test.go
@@ -0,0 +1,153 @@
+/*
+ * Copyright 2018 Dgraph Labs, Inc. and Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package binary
+
+import (
+	"bytes"
+	"compress/gzip"
+	"encoding/binary"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/dgraph-io/dgraph/protos/pb"
+	"github.com/dgraph-io/dgraph/x"
+	humanize "github.com/dustin/go-humanize"
+	"github.com/stretchr/testify/require"
+)
+
+func getUids(size int) []uint64 {
+	var uids []uint64
+	last := uint64(rand.Intn(100))
+	uids = append(uids, last)
+	for i := 1; i < size; i++ {
+		last += uint64(rand.Intn(33))
+		uids = append(uids, last)
+	}
+	return uids
+}
+
+func TestUidPack(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+
+	// Some edge case tests.
+	Encode([]uint64{}, 128)
+	require.Equal(t, 0, NumUids(pb.UidPack{}))
+	require.Equal(t, 0, len(Decode(pb.UidPack{})))
+
+	for i := 0; i < 13; i++ {
+		size := rand.Intn(10e6)
+		if size < 0 {
+			size = 1e6
+		}
+		t.Logf("Testing with size = %d", size)
+
+		expected := getUids(size)
+		pack := Encode(expected, 256)
+		for _, block := range pack.Blocks {
+			require.True(t, len(block.Deltas) <= 255)
+		}
+		require.Equal(t, len(expected), NumUids(pack))
+		actual := Decode(pack)
+		require.Equal(t, expected, actual)
+	}
+}
+
+func BenchmarkGzip(b *testing.B) {
+	rand.Seed(time.Now().UnixNano())
+
+	uids := getUids(1e6)
+	b.ResetTimer()
+	sz := uint64(len(uids)) * 8
+
+	b.Logf("Dataset Len=%d. Size: %s", len(uids), humanize.Bytes(sz))
+	var data []byte
+	for i := 0; i < b.N; i++ {
+		tmp := make([]byte, binary.MaxVarintLen64)
+		var buf bytes.Buffer
+		for _, uid := range uids {
+			n := binary.PutUvarint(tmp, uid)
+			_, err := buf.Write(tmp[:n])
+			x.Check(err)
+		}
+
+		var out bytes.Buffer
+		zw := gzip.NewWriter(&out)
+		_, err := zw.Write(buf.Bytes())
+		x.Check(err)
+
+		data = out.Bytes()
+	}
+	b.Logf("Output size: %s. Compression: %.2f",
+		humanize.Bytes(uint64(len(data))),
+		float64(len(data))/float64(sz))
+}
+
+func benchmarkUidPackEncode(b *testing.B, blockSize int) {
+	rand.Seed(time.Now().UnixNano())
+
+	uids := getUids(1e6)
+	sz := uint64(len(uids)) * 8
+	b.Logf("Dataset Len=%d. Size: %s", len(uids), humanize.Bytes(sz))
+	b.ResetTimer()
+
+	var data []byte
+	for i := 0; i < b.N; i++ {
+		pack := Encode(uids, blockSize)
+		out, err := pack.Marshal()
+		x.Check(err)
+		data = out
+	}
+	b.Logf("Output size: %s. Compression: %.2f",
+		humanize.Bytes(uint64(len(data))),
+		float64(len(data))/float64(sz))
+}
+
+func BenchmarkUidPack(b *testing.B) {
+	b.Run("encode/128", func(b *testing.B) {
+		benchmarkUidPackEncode(b, 128)
+	})
+	b.Run("encode/256", func(b *testing.B) {
+		benchmarkUidPackEncode(b, 256)
+	})
+	b.Run("decode/128", func(b *testing.B) {
+		benchmarkUidPackDecode(b, 128)
+	})
+	b.Run("decode/256", func(b *testing.B) {
+		benchmarkUidPackDecode(b, 256)
+	})
+}
+
+func benchmarkUidPackDecode(b *testing.B, blockSize int) {
+	rand.Seed(time.Now().UnixNano())
+
+	uids := getUids(1e6)
+	sz := uint64(len(uids)) * 8
+	b.Logf("Dataset Len=%d. Size: %s", len(uids), humanize.Bytes(sz))
+
+	pack := Encode(uids, blockSize)
+	data, err := pack.Marshal()
+	x.Check(err)
+	b.Logf("Output size: %s. Compression: %.2f",
+		humanize.Bytes(uint64(len(data))),
+		float64(len(data))/float64(sz))
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = Decode(pack)
+	}
+}
