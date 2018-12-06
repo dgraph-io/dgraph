29735b36e194bd81bd0e5ee9a16cae0c90dd6f89
Lucas Wang <lucas@dgraph.io>
2018-12-06 14:24:35 -0800

    addressing comments

--- dgraph/run-single.sh
+++ dgraph/run-single.sh
@@ -1,2 +1,2 @@
-md5sum ~/go/bin/dgraph; go build . && go install . && md5sum dgraph ~/go/bin/dgraph &&\
+md5sum ~/go/bin/dgraph; go build . && go install . && md5sum dgraph ~/go/bin/dgraph
 docker-compose -f docker-compose-single.yml down && docker-compose -f docker-compose-single.yml up --force-recreate --remove-orphans
