7a798e8477c05c9c215bf144ad2dc0fc8d9d2cd5
Manish R Jain <manish@dgraph.io>
2018-12-19 13:50:54 -0800

    Fix the breaking test by switching BinaryId back to using fmt.Sprintf(%q).

--- query/outputnode.go
+++ query/outputnode.go
@@ -145,8 +145,10 @@ func (fj *fastJsonNode) IsEmpty() bool {
 
 func valToBytes(v types.Val) ([]byte, error) {
 	switch v.Tid {
-	case types.BinaryID, types.StringID, types.DefaultID:
+	case types.StringID, types.DefaultID:
 		return json.Marshal(v.Value)
+	case types.BinaryID:
+		return []byte(fmt.Sprintf("%q", v.Value)), nil
 	case types.IntID:
 		return []byte(fmt.Sprintf("%d", v.Value)), nil
 	case types.FloatID:
