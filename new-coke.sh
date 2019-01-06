a07081e7a91c035fe45df7dbf293ee9519bc2636
Javier Alvarado <javier@dgraph.io>
2019-01-06 15:28:03 -0800

    Use restartCluster instead of cluster.sh. Clean up.

--- new-coke.sh
+++ new-coke.sh
@@ -3,6 +3,8 @@
 readonly ME=${0##*/}
 readonly DGRAPH_ROOT=${GOPATH:-$HOME}/src/github.com/dgraph-io/dgraph
 
+source $DGRAPH_ROOT/contrib/scripts/functions.sh
+
 PATH+=:$DGRAPH_ROOT/contrib/scripts/
 CUSTOM_CLUSTER_TEST=()
 
@@ -11,28 +13,21 @@ function Info {
 }
 
 function Run {
-    go test -short=true $@ |\
-        GREP_COLORS='mt=01;32' egrep --line-buffered --color=always '^ok\ .*|$' |\
-        GREP_COLORS='mt=00;38;5;226' egrep --line-buffered --color=always '^\?\ .*|$' |\
-        GREP_COLORS='mt=01;31' egrep --line-buffered --color=always '.*FAIL.*|$'
+    go test -short=true $@ \
+    | GREP_COLORS='mt=01;32' egrep --line-buffered --color=always '^ok\ .*|$' \
+    | GREP_COLORS='mt=00;38;5;226' egrep --line-buffered --color=always '^\?\ .*|$' \
+    | GREP_COLORS='mt=01;31' egrep --line-buffered --color=always '.*FAIL.*|$'
 }
 
-function RunDefaultTests {
+function RunPackageTests {
     for PKG in $(go list ./...); do
-        DIR=$(echo $PKG | cut -d/ -f4-)
-        Info "PKG=$PKG  DIR=$DIR"
-        if [[ -e $DIR/docker-compose.yml ]]; then
-            Info "Skipping $PKG because it requires custom cluster"
-        else
-            Info "Running test for $PKG"
-            Run $PKG
-        fi
+        Info "Running test for $PKG"
+        Run $PKG
     done
 }
 
 cd $DGRAPH_ROOT
 
 Info "Running tests using the default cluster"
-#cluster.sh -b restart
-#sleep 5
-RunDefaultTests
+restartCluster
+RunPackageTests
