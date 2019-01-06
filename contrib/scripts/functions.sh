6e44349dfccf64de9d508daae862a34b773dfc5e
Javier Alvarado <javier@dgraph.io>
2019-01-06 15:26:48 -0800

    Remove sudo and unused data dir from restartCluster.

--- contrib/scripts/functions.sh
+++ contrib/scripts/functions.sh
@@ -3,15 +3,15 @@
 function restartCluster {
   basedir=$GOPATH/src/github.com/dgraph-io/dgraph
   pushd $basedir/dgraph
-    sudo rm -Rf /tmp/dg
-    sudo mkdir /tmp/dg
     go build . && go install . && md5sum dgraph $GOPATH/bin/dgraph
-    docker-compose down
-    DATA="/tmp/dg" docker-compose up --force-recreate --remove-orphans --detach
+    docker-compose --log-level=CRITICAL down
+    docker-compose up --force-recreate --remove-orphans --detach
   popd
-  $basedir/contrib/wait-for-it.sh -t 60 localhost:6080
-  $basedir/contrib/wait-for-it.sh -t 60 localhost:9180
-  sleep 10 # Sleep 10 seconds to get things ready.
+
+  $basedir/contrib/wait-for-it.sh -t 60 localhost:6080 || exit 1
+  $basedir/contrib/wait-for-it.sh -t 60 localhost:9180 || exit 1
+  echo "Waiting 10 seconds for cluster to come up"
+  sleep 10 || exit 1
 }
 
 function stopCluster {
