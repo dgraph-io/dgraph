a27bfeb17a1129006a706d869efe3999d0ad5b1d
Javier Alvarado <javier@dgraph.io>
2019-01-06 23:53:57 -0800

    Allow restarting cluster with additional docker compose file.

--- contrib/scripts/functions.sh
+++ contrib/scripts/functions.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+# may be called with an argument which is a docker compose file
+# to use *in addition* to the default docker-compose.yml
 function restartCluster {
+  compose_files=("-f" "docker-compose.yml")
+  if [[ -n $1 ]]; then
+    compose_files+=("-f" "$(readlink -f $1)")
+  fi
+
   basedir=$GOPATH/src/github.com/dgraph-io/dgraph
-  pushd $basedir/dgraph
+  pushd $basedir/dgraph >/dev/null
     go build . && go install . && md5sum dgraph $GOPATH/bin/dgraph
     docker-compose --log-level=CRITICAL down
-    docker-compose up --force-recreate --remove-orphans --detach
-  popd
+    docker-compose ${compose_files[@]} up \
+                   --force-recreate --remove-orphans --detach
+  popd >/dev/null
 
   $basedir/contrib/wait-for-it.sh -t 60 localhost:6080 || exit 1
   $basedir/contrib/wait-for-it.sh -t 60 localhost:9180 || exit 1
@@ -16,7 +24,7 @@ function restartCluster {
 
 function stopCluster {
   basedir=$GOPATH/src/github.com/dgraph-io/dgraph
-  pushd $basedir/dgraph
+  pushd $basedir/dgraph >/dev/null
     docker-compose down
-  popd
+  popd >/dev/null
 }
