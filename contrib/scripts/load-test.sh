f819edf0ea90172deeb871206548c11e42b5f154
Javier Alvarado <javier@dgraph.io>
2019-01-11 10:44:11 -0800

    Fix error message.

--- contrib/scripts/load-test.sh
+++ contrib/scripts/load-test.sh
@@ -4,7 +4,7 @@ set -e
 
 total_mem_kb=`cat /proc/meminfo | awk '/MemTotal:/ {print $2}'`
 if [[ $total_mem_kb -lt 32000000 ]]; then
-    echo >&2 "Load test requires system with at least 16GB of memory"
+    echo >&2 "Load test requires system with at least 32GB of memory"
     exit 1
 fi
 
