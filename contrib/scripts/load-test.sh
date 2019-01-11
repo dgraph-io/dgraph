bf26e948d0853f6645057a43b63e449f9659df2b
Javier Alvarado <javier@dgraph.io>
2019-01-11 09:50:22 -0800

    Fix missnamed variable.

--- contrib/scripts/load-test.sh
+++ contrib/scripts/load-test.sh
@@ -3,7 +3,7 @@
 set -e
 
 total_mem_kb=`cat /proc/meminfo | awk '/MemTotal:/ {print $2}'`
-if [[ $mem_total -lt 16000000 ]]; then
+if [[ $total_mem_kb -lt 16000000 ]]; then
     echo >&2 "Load test requires system with at least 16GB of memory"
     exit 1
 fi
