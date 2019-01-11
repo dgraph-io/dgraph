fe0ec9ec9367622dac62ab844707f5f4fbcae900
Javier Alvarado <javier@dgraph.io>
2019-01-10 21:15:59 -0800

    Try to prevent VM thrashing when not enough memory for load test.

--- contrib/scripts/load-test.sh
+++ contrib/scripts/load-test.sh
@@ -2,5 +2,11 @@
 
 set -e
 
+total_mem_kb=`cat /proc/meminfo | awk '/MemTotal:/ {print $2}'`
+if [[ $mem_total -lt 16000000 ]]; then
+    echo >&2 "Load test requires system with at least 16GB of memory"
+    exit 1
+fi
+
 bash contrib/scripts/loader.sh $1
 bash contrib/scripts/transactions.sh $1
