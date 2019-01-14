0f164a2b7ce69efba60e711816cc75ca74301a40
Manish R Jain <manish@dgraph.io>
2019-01-14 13:46:13 -0800

    Remove Long term for exact index warning.

--- posting/index.go
+++ posting/index.go
@@ -55,13 +55,6 @@ func indexTokens(attr, lang string, src types.Val) ([]string, error) {
 	// Schema will know the mapping from attr to tokenizer.
 	var tokens []string
 	for _, it := range schema.State().Tokenizer(attr) {
-		if it.Identifier() == tok.IdentExact &&
-			schemaType == types.StringID && len(sv.Value.(string)) > 100 {
-			// Exact index can only be applied for strings so we can safely try to convert Value to
-			// string.
-			glog.Infof("Long term for exact index on predicate: [%s]. "+
-				"Consider switching to hash for better performance.\n", attr)
-		}
 		toks, err := tok.BuildTokens(sv.Value, tok.GetLangTokenizer(it, lang))
 		if err != nil {
 			return tokens, err
