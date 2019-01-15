41a3d4d3494b70f392d5c45e141ca7492a995de5
srfrog <gus@dgraph.io>
2019-01-14 18:50:53 -0700

    re-fixed this slippery lil bug

--- tok/tok.go
+++ tok/tok.go
@@ -124,7 +124,7 @@ func LoadCustomTokenizer(soFile string) {
 	tokenizer := symb.(func() interface{})().(PluginTokenizer)
 
 	id := tokenizer.Identifier()
-	x.AssertTruef(id < IdentCustom,
+	x.AssertTruef(id >= IdentCustom,
 		"custom tokenizer identifier byte must be >= 0x80, but was %#x", id)
 	registerTokenizer(CustomTokenizer{PluginTokenizer: tokenizer})
 }
