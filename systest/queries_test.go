103411aca93187bfac92449bc371deed27b71c2c
srfrog <gus@dgraph.io>
2019-01-17 21:05:52 -0700

    added regexp trigram tests

--- systest/queries_test.go
+++ systest/queries_test.go
@@ -50,6 +50,7 @@ func TestQuery(t *testing.T) {
 	t.Run("multiple block eval", wrap(MultipleBlockEval))
 	t.Run("unmatched var assignment eval", wrap(UnmatchedVarEval))
 	t.Run("hash index queries", wrap(QueryHashIndex))
+	t.Run("regexp with toggled trigram index", wrap(RegexpToggleTrigramIndex))
 	t.Run("cleanup", wrap(SchemaQueryCleanup))
 }
 
@@ -682,6 +683,93 @@ func QueryHashIndex(t *testing.T, c *dgo.Dgraph) {
 		},
 	}
 
+	for _, tc := range tests {
+		resp, err := c.NewTxn().Query(ctx, tc.in)
+		require.NoError(t, err)
+		CompareJSON(t, tc.out, string(resp.Json))
+	}
+}
+
+func RegexpToggleTrigramIndex(t *testing.T, c *dgo.Dgraph) {
+	ctx := context.Background()
+
+	require.NoError(t, c.Alter(ctx, &api.Operation{
+		Schema: `
+      name: string @index(term) @lang .
+    `,
+	}))
+
+	txn := c.NewTxn()
+	_, err := txn.Mutate(ctx, &api.Mutation{
+		SetNquads: []byte(`
+      _:x1 <name> "The luck is in the details" .
+      _:x1 <name> "The art is in the details"@en .
+      _:x1 <name> "L'art est dans les détails"@fr .
+
+      _:x2 <name> "Detach yourself from ostentation" .
+      _:x2 <name> "Detach yourself from artificiality"@en .
+      _:x2 <name> "Desprenderse de la artificialidad"@es .
+    `),
+	})
+	require.NoError(t, err)
+	require.NoError(t, txn.Commit(ctx))
+
+	tests := []struct {
+		in, out string
+	}{
+		{
+			in:  `{q(func:regexp(name, /art/)) {name}}`,
+			out: `{"q":[]}`,
+		},
+		{
+			in:  `{q(func:regexp(name@es, /art/)){name@es}}`,
+			out: `{"q":[{"name@es": "Desprenderse de la artificialidad"}]}`,
+		},
+		{
+			in:  `{q(func:regexp(name@fr, /art/)) {name@fr}}`,
+			out: `{"q":[{"name@fr": "L'art est dans les détails"}]}`,
+		},
+		{
+			in:  `{q(func:has(name)) @filter(regexp(name, /art/)) {name}}`,
+			out: `{"q":[]}`,
+		},
+		{
+			in:  `{q(func:has(name)) @filter(regexp(name@es, /art/)) {name@es}}`,
+			out: `{"q":[{"name@es": "Desprenderse de la artificialidad"}]}`,
+		},
+		{
+			in:  `{q(func:has(name)) @filter(regexp(name@fr, /art/)) {name@fr}}`,
+			out: `{"q":[{"name@fr": "L'art est dans les détails"}]}`,
+		},
+	}
+
+	t.Log("testing without trigram index")
+	for _, tc := range tests {
+		resp, err := c.NewTxn().Query(ctx, tc.in)
+		require.NoError(t, err)
+		CompareJSON(t, tc.out, string(resp.Json))
+	}
+
+	require.NoError(t, c.Alter(ctx, &api.Operation{
+		Schema: `
+      name: string @index(trigram) @lang .
+    `,
+	}))
+
+	t.Log("testing with trigram index")
+	for _, tc := range tests {
+		resp, err := c.NewTxn().Query(ctx, tc.in)
+		require.NoError(t, err)
+		CompareJSON(t, tc.out, string(resp.Json))
+	}
+
+	require.NoError(t, c.Alter(ctx, &api.Operation{
+		Schema: `
+      name: string @index(term) @lang .
+    `,
+	}))
+
+	t.Log("testing again without trigram index")
 	for _, tc := range tests {
 		resp, err := c.NewTxn().Query(ctx, tc.in)
 		require.NoError(t, err)
