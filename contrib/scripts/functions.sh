7de4a28160dc235d6429730ba54763bd3f01a556
Lucas Wang <lucas@dgraph.io>
2019-01-21 16:18:55 -0800

    don't run docker rm when no previous test containers are found

--- contrib/scripts/functions.sh
+++ contrib/scripts/functions.sh
@@ -14,7 +14,7 @@ function restartCluster {
   basedir=$GOPATH/src/github.com/dgraph-io/dgraph
   pushd $basedir/dgraph >/dev/null
   go build . && go install . && md5sum dgraph $GOPATH/bin/dgraph
-  docker ps -a --filter label="cluster=test" --format "{{.Names}}" | xargs docker rm -f
+  docker ps -a --filter label="cluster=test" --format "{{.Names}}" | xargs -r docker rm -f
   docker-compose -f $compose_file up --force-recreate --remove-orphans --detach
   popd >/dev/null
 
