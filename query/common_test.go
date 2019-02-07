69bc58dc01485bc5aceb6c8659b741555a7ed323
Martin Martinez Rivera <martinmr@dgraph.io>
2019-02-06 18:37:03 -0800

    Move tests in query1_test.go and query2_test.go to use the cluster.
    Some of the tests in query1_test.go were checking the subgraphs produced
    by the query directly. I have removed these tests since such internal
    state will not be available when we moved to use the cluster for all
    tests and the queries being performed by these tests are simple and
    should be already covered by the remaining tests.

--- query/common_test.go
+++ query/common_test.go
@@ -86,13 +86,94 @@ func processQueryNoErr(t *testing.T, query string) string {
 	return res
 }
 
-func addTriples(t *testing.T, triples string) {
+func processQueryWithVars(t *testing.T, query string,
+	vars map[string]string) (string, error) {
+	txn := client.NewTxn()
+	res, err := txn.QueryWithVars(context.Background(), query, vars)
+	if err != nil {
+		return "", err
+	}
+
+	response := map[string]interface{}{}
+	response["data"] = json.RawMessage(string(res.Json))
+
+	jsonResponse, err := json.Marshal(response)
+	require.NoError(t, err)
+	return string(jsonResponse), err
+}
+
+func addTriplesToCluster(t *testing.T, triples string) {
 	txn := client.NewTxn()
 	_, err := txn.Mutate(context.Background(), &api.Mutation{
 		SetNquads: []byte(triples),
 		CommitNow: true,
 	})
 	require.NoError(t, err)
+}
+
+func addGeoPointToCluster(t *testing.T, uid uint64, pred string, point []float64) {
+	triple := fmt.Sprintf(
+		`<%d> <%s> "{'type':'Point', 'coordinates':[%v, %v]}"^^<geo:geojson> .`,
+		uid, pred, point[0], point[1])
+	addTriplesToCluster(t, triple)
+}
+
+func addGeoPolygonToCluster(t *testing.T, uid uint64, pred string, polygon [][][]float64) {
+	coordinates := "["
+	for i, ring := range polygon {
+		coordinates += "["
+		for j, point := range ring {
+			coordinates += fmt.Sprintf("[%v, %v]", point[0], point[1])
+
+			if j != len(ring)-1 {
+				coordinates += ","
+			}
+		}
+
+		coordinates += "]"
+		if i != len(polygon)-1 {
+			coordinates += ","
+		}
+	}
+	coordinates += "]"
+
+	triple := fmt.Sprintf(
+		`<%d> <%s> "{'type':'Polygon', 'coordinates': %s}"^^<geo:geojson> .`,
+		uid, pred, coordinates)
+	addTriplesToCluster(t, triple)
+}
+
+func addGeoMultiPolygonToCluster(t *testing.T, uid uint64, polygons [][][][]float64) {
+	coordinates := "["
+	for i, polygon := range polygons {
+		coordinates += "["
+		for j, ring := range polygon {
+			coordinates += "["
+			for k, point := range ring {
+				coordinates += fmt.Sprintf("[%v, %v]", point[0], point[1])
+
+				if k != len(ring)-1 {
+					coordinates += ","
+				}
+			}
+
+			coordinates += "]"
+			if j != len(polygon)-1 {
+				coordinates += ","
+			}
+		}
+
+		coordinates += "]"
+		if i != len(polygons)-1 {
+			coordinates += ","
+		}
+	}
+	coordinates += "]"
+
+	triple := fmt.Sprintf(
+		`<%d> <geometry> "{'type':'MultiPolygon', 'coordinates': %s}"^^<geo:geojson> .`,
+		uid, coordinates)
+	addTriplesToCluster(t, triple)
 }
 
 func childAttrs(sg *SubGraph) []string {
@@ -390,28 +471,52 @@ type                           : string @index(exact) .
 func populateCluster(t *testing.T) {
 	require.NoError(t, client.Alter(context.Background(), &api.Operation{DropAll: true}))
 	setSchema(t, testSchema)
-	assignUids(t, 15000)
+	assignUids(t, 100000)
 
-	addTriples(t, `
+	addTriplesToCluster(t, `
 		<1> <name> "Michonne" .
 		<23> <name> "Rick Grimes" .
 		<24> <name> "Glenn Rhee" .
 		<25> <name> "Daryl Dixon" .
 		<31> <name> "Andrea" .
+		<110> <name> "Alice" .
 		<240> <name> "Andrea With no friends" .
 		<1000> <name> "Alice" .
 		<1001> <name> "Bob" .
 		<1002> <name> "Matt" .
 		<1003> <name> "John" .
+		<2300> <name> "Andre" .
+		<2301> <name> "Alice\"" .
+		<2333> <name> "Helmut" .
 		<3500> <name> "" .
 		<3500> <name@ko> "상현" .
+		<3501> <name> "Alex" .
+		<3501> <name@en> "Alex" .
 		<3502> <name> "" .
 		<3502> <name@en> "Amit" .
 		<3502> <name@hi> "अमित" .
 		<3503> <name@en> "Andrew" .
 		<3503> <name@hi> "" .
+		<4097> <name> "Badger" .
+		<4097> <name@en> "European badger" .
+		<4097> <name@xx> "European badger barger European" .
+		<4097> <name@pl> "Borsuk europejski" .
+		<4097> <name@de> "Europäischer Dachs" .
+		<4097> <name@ru> "Барсук" .
+		<4097> <name@fr> "Blaireau européen" .
+		<4098> <name@en> "Honey badger" .
+		<4099> <name@en> "Honey bee" .
+		<4100> <name@en> "Artem Tkachenko" .
+		<4100> <name@ru> "Артём Ткаченко" .
 		<5000> <name> "School A" .
 		<5001> <name> "School B" .
+		<5101> <name> "Googleplex" .
+		<5102> <name> "Shoreline Amphitheater" .
+		<5103> <name> "San Carlos Airport" .
+		<5104> <name> "SF Bay area" .
+		<5105> <name> "Mountain View" .
+		<5106> <name> "San Carlos" .
+		<5107> <name> "New York" .
 		<10000> <name> "Alice" .
 		<10001> <name> "Elizabeth" .
 		<10002> <name> "Alice" .
@@ -420,6 +525,10 @@ func populateCluster(t *testing.T) {
 		<10005> <name> "Bob" .
 		<10006> <name> "Colin" .
 		<10007> <name> "Elizabeth" .
+
+		<1> <full_name> "Michonne's large name for hashing" .
+
+		<1> <noindex_name> "Michonne's name not indexed" .
 
 		<1> <friend> <23> .
 		<1> <friend> <24> .
@@ -446,8 +555,12 @@ func populateCluster(t *testing.T) {
 		<10007> <age> "25" .
 
 		<1> <alive> "true" .
+		<23> <alive> "true" .
+		<25> <alive> "false" .
+		<31> <alive> "false" .
 
 		<1> <gender> "female" .
+		<23> <gender> "male" .
 
 		<4001> <office> "office 1" .
 		<4002> <room> "room 1" .
@@ -500,7 +613,93 @@ func populateCluster(t *testing.T) {
 		<25> <school> <5000> .
 		<31> <school> <5001> .
 		<101> <school> <5001> .
+
+		<1> <_xid_> "mich" .
+		<24> <_xid_> "g\"lenn" .
+		<110> <_xid_> "a.bc" .
+
+		<23> <alias> "Zambo Alice" .
+		<24> <alias> "John Alice" .
+		<25> <alias> "Bob Joe" .
+		<31> <alias> "Allan Matt" .
+		<101> <alias> "John Oliver" .
+
+		<1> <bin_data> "YmluLWRhdGE=" .
+
+		<1> <graduation> "1932-01-01" .
+		<31> <graduation> "1933-01-01" .
+		<31> <graduation> "1935-01-01" .
+
+		<10000> <salary> "10000" .
+		<10002> <salary> "10002" .
+
+		<1> <address> "31, 32 street, Jupiter" .
+		<23> <address> "21, mark street, Mars" .
+
+		<1> <dob_day> "1910-01-01" .
+		<23> <dob_day> "1910-01-02" .
+		<24> <dob_day> "1909-05-05" .
+		<25> <dob_day> "1909-01-10" .
+		<31> <dob_day> "1901-01-15" .
+
+		<1> <power> "13.25"^^<xs:float> .
+
+		<1> <sword_present> "true" .
+
+		<1> <son> <2300> .
+		<1> <son> <2333> .
+
+		<5010> <nick_name> "Two Terms" .
+
+		<4097> <lossy> "Badger" .
+		<4097> <lossy@en> "European badger" .
+		<4097> <lossy@xx> "European badger barger European" .
+		<4097> <lossy@pl> "Borsuk europejski" .
+		<4097> <lossy@de> "Europäischer Dachs" .
+		<4097> <lossy@ru> "Барсук" .
+		<4097> <lossy@fr> "Blaireau européen" .
+		<4098> <lossy@en> "Honey badger" .
+
+		<23> <film.film.initial_release_date> "1900-01-02" .
+		<24> <film.film.initial_release_date> "1909-05-05" .
+		<25> <film.film.initial_release_date> "1929-01-10" .
+		<31> <film.film.initial_release_date> "1801-01-15" .
+
+		<0x10000> <royal_title@en> "Her Majesty Elizabeth the Second, by the Grace of God of the United Kingdom of Great Britain and Northern Ireland and of Her other Realms and Territories Queen, Head of the Commonwealth, Defender of the Faith" .
+		<0x10000> <royal_title@fr> "Sa Majesté Elizabeth Deux, par la grâce de Dieu Reine du Royaume-Uni, du Canada et de ses autres royaumes et territoires, Chef du Commonwealth, Défenseur de la Foi" .
 	`)
+
+	addGeoPointToCluster(t, 1, "loc", []float64{1.1, 2.0})
+	addGeoPointToCluster(t, 24, "loc", []float64{1.10001, 2.000001})
+	addGeoPointToCluster(t, 25, "loc", []float64{1.1, 2.0})
+	addGeoPointToCluster(t, 5101, "geometry", []float64{-122.082506, 37.4249518})
+	addGeoPointToCluster(t, 5102, "geometry", []float64{-122.080668, 37.426753})
+	addGeoPointToCluster(t, 5103, "geometry", []float64{-122.2527428, 37.513653})
+
+	addGeoPolygonToCluster(t, 23, "loc", [][][]float64{
+		{{0.0, 0.0}, {2.0, 0.0}, {2.0, 2.0}, {0.0, 2.0}, {0.0, 0.0}},
+	})
+	addGeoPolygonToCluster(t, 5104, "geometry", [][][]float64{
+		{{-121.6, 37.1}, {-122.4, 37.3}, {-122.6, 37.8}, {-122.5, 38.3}, {-121.9, 38},
+			{-121.6, 37.1}},
+	})
+	addGeoPolygonToCluster(t, 5105, "geometry", [][][]float64{
+		{{-122.06, 37.37}, {-122.1, 37.36}, {-122.12, 37.4}, {-122.11, 37.43},
+			{-122.04, 37.43}, {-122.06, 37.37}},
+	})
+	addGeoPolygonToCluster(t, 5106, "geometry", [][][]float64{
+		{{-122.25, 37.49}, {-122.28, 37.49}, {-122.27, 37.51}, {-122.25, 37.52},
+			{-122.25, 37.49}},
+	})
+
+	addGeoMultiPolygonToCluster(t, 5107, [][][][]float64{
+		{{{-74.29504394531249, 40.19146303804063}, {-74.59716796875, 40.39258071969131},
+			{-74.6466064453125, 40.20824570152502}, {-74.454345703125, 40.06125658140474},
+			{-74.28955078125, 40.17467622056341}, {-74.29504394531249, 40.19146303804063}}},
+		{{{-74.102783203125, 40.8595252289932}, {-74.2730712890625, 40.718119379753446},
+			{-74.0478515625, 40.66813955408042}, {-73.98193359375, 40.772221877329024},
+			{-74.102783203125, 40.8595252289932}}},
+	})
 }
 
 func populateGraph(t *testing.T) {
