66faa2a6b69e34a86175a5d5f46e0f47bce85b95
srfrog <gus@dgraph.io>
2019-02-07 19:16:51 -0700

    protos/depcheck.sh: simplified using for loop and renamed vars

--- protos/depcheck.sh
+++ protos/depcheck.sh
@@ -8,35 +8,22 @@ which protoc &>/dev/null || (echo "Error: protoc not found" ; exit 1)
 
 PROTOCVER=`protoc --version | awk '{printf $2}'`
 
-# CompareSemVer compares the minimum version ver1 against another version ver2.
+# CompareSemVer compares the minimum version minver against another version curver.
 # If the version is below our min it will exit with non-zero to trigger error in make.
 function CompareSemVer() {
-	local ver1=(${1//./ })
-	local ver2=(${2//./ })
+	local minver=(${1//./ })
+	local curver=(${2//./ })
 
 	echo "Checking for semantic version $1 or newer"
 
-	# check major
-	if [ ${ver1[0]} -gt ${ver2[0]} ]; then
-		echo "Error: major version is '${ver2[0]}'"
-		exit 1
-	elif [ ${ver2[0]} -gt ${ver1[0]} ]; then
-		exit 0
-	fi
-
-	# check minor
-	if [ ${ver1[1]} -gt ${ver2[1]} ]; then
-		echo "Error: minor version is '${ver2[1]}'"
-		exit 1
-	elif [ ${ver2[1]} -gt ${ver1[1]} ]; then
-		exit 0
-	fi
-
-	# check patch
-	if [ ${ver1[2]} -gt ${ver2[2]} ]; then
-		echo "Error patch version is '${ver2[2]}'"
-		exit 1
-	fi
+	for i in 0 1 2; do
+		if [ ${minver[$i]} -gt ${curver[$i]} ]; then
+			echo "Error: version $2 is lower than the required version $1"
+			exit 1
+		elif [ ${curver[$i]} -gt ${minver[$i]} ]; then
+			break
+		fi
+	done
 }
 
 CompareSemVer $PROTOCMINVER $PROTOCVER
@@ -46,4 +33,3 @@ CompareSemVer $PROTOCMINVER $PROTOCVER
 echo "OK"
 
 exit 0
-
