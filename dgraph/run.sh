f2efc79cd52781f6bcf779827b48a9fca428645f
Daniel Mai <daniel@dgraph.io>
2019-02-07 16:39:17 -0800

    Update test cluster configuration with --data and --tmpfs options. (#2984)
    * ./run.sh --data <path> will mount all Zero and Alpha directories to the host
      path mounted at <path>.
    * ./run.sh --tmpfs will mount WAL (w/zw) directories in tmpfs.

--- dgraph/run.sh
+++ dgraph/run.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 COMPOSE_FILES=( -f docker-compose.yml )
 SERVICES=()
-for o in $@; do
-    case $o in
+
+while (( "$#" )); do
+    case "$1" in
         '-h'|'--help' )
             cat <<EOF
 Runs the Dgraph test cluster.
 
 Available options:
 
-    --jaeger   Run with Jaeger tracing enabled.
-    --single   Run a single Zero and Alpha (2-node cluster).
-    --metrics  Run with metrics collection and dashboard (Prometheus/Grafana/Node Exporter).
+    --jaeger       Run with Jaeger tracing enabled.
+    --single       Run a single Zero and Alpha (2-node cluster).
+    --metrics      Run with metrics collection and dashboard (Prometheus/Grafana/Node Exporter).
+    --data <path>  Run with Dgraph data directories mounted to <path>.
+    --tmpfs        Run with WAL directories (w/zw) in-memory with tmpfs.
 EOF
             exit 0
             ;;
@@ -22,13 +25,29 @@ EOF
             COMPOSE_FILES+=( -f docker-compose-metrics.yml )
             ;;
         '--single' )
-            SERVICES=( zero1 dg1 )
+            SERVICES+=( zero1 dg1 )
             if [[ " ${COMPOSE_FILES[@]} " =~ "docker-compose-jaeger.yml" ]]; then
                 SERVICES+=( jaeger )
             fi
             if [[ " ${COMPOSE_FILES[@]} " =~ "docker-compose-metrics.yml" ]]; then
                 SERVICES+=( node-exporter prometheus grafana )
             fi
+            ;;
+        '--data' )
+            DATA="$2"
+            shift
+            if [ -z "${DATA}" ]; then
+                echo "--data option missing <path>"
+                exit 1
+            fi
+            if [ ! -d "$DATA" ]; then
+                echo "Directory does not exist: $DATA"
+            fi
+            COMPOSE_FILES+=( -f docker-compose-data.yml )
+            echo "Running with Dgraph data directories mounted at $DATA"
+            ;;
+        '--tmpfs' )
+            COMPOSE_FILES+=( -f docker-compose-tmpfs.yml )
             ;;
         *)
             echo "Unknown option $1"
@@ -40,5 +59,4 @@ done
 
 make install
 docker-compose ${COMPOSE_FILES[@]} down
-DATA=$HOME/dg docker-compose ${COMPOSE_FILES[@]} up --force-recreate --remove-orphans ${SERVICES[@]}
-
+DATA=$DATA docker-compose ${COMPOSE_FILES[@]} up --force-recreate --remove-orphans ${SERVICES[@]}
