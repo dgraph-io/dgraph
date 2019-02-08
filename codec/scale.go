e70991f85a502d7c168a9be0e8fc03179f5fdb4b
noot <elizabethjbinks@gmail.com>
2019-02-08 10:30:39 -0500

    stub functions and test for Encode and Decode

--- codec/scale.go
+++ codec/scale.go
@@ -1,1 +1,9 @@
 package codec
+
+func Encode(b []byte) []byte {
+	return []byte{}
+}
+
+func Decode(b []byte) []byte {
+	return []byte{}
+}
--- codec/scale_test.go
+++ codec/scale_test.go
@@ -1,1 +1,13 @@
 package codec
+
+import (
+	"testing"
+)
+
+func TestEncode(t *testing.T) {
+
+}
+
+func TestDecode(t *testing.T) {
+
+}
