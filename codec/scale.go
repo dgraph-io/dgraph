ca89e8fcaaa61f7f866a4ec98b444ccb5ac8006c
noot <elizabethjbinks@gmail.com>
2019-02-13 15:39:29 -0500

    implemented encoding of integers of bit length > 30

--- codec/scale.go
+++ codec/scale.go
@@ -12,7 +12,7 @@ func Encode(b interface{}) ([]byte, error) {
 		case int64:
 			return encodeInteger(v)
 		default:
-			return []byte{}, errors.New("Unsupported type!!")
+			return []byte{}, errors.New("Unsupported type!")
 	}
 	return []byte{}, nil
 }
@@ -32,29 +32,45 @@ func encodeByteArray(b []byte) ([]byte, error) {
 // if n < 2^6 return i^n...i^2 00 [ 8 bits = 1 byte output ]
 // if 2^6 <= n < 2^14 return i^n...i^2 01 [ 16 bits = 2 byte output ]
 // if 2^14 <= n < 2^30 return i^n...i^2 10 [ 32 bits = 4 byte output ]
-// if n >= 2^30 return 
-func encodeInteger(n int64) ([]byte, error) {
-	if n < 1 << 6 { 
-		o := byte(n) << 2
+// if n >= 2^30 return [top 6 bits of first byte = # of bytes following less 4] [bottom 2 bits of first byte = 11]
+// [append i as a byte array to the first byte]
+func encodeInteger(i int64) ([]byte, error) {
+	if i < 1 << 6 { 
+		o := byte(i) << 2
 		return []byte{o}, nil
-	} else if n < 1 << 14 {
+	} else if i < 1 << 14 {
 		o := make([]byte, 2)
-		binary.LittleEndian.PutUint16(o, uint16(n<<2)+1)
+		binary.LittleEndian.PutUint16(o, uint16(i<<2)+1)
 		return o, nil
-	} else if n < 1 << 30 {
+	} else if i < 1 << 30 {
 		o := make([]byte, 4)
-		binary.LittleEndian.PutUint16(o, uint16(n<<2)+2)
+		binary.LittleEndian.PutUint32(o, uint32(i<<2)+2)
 		return o, nil
 	} else {
-		return []byte{}, nil
-	}
-}
+		o := make([]byte, 8)
+		m := i
+		var numBytes uint
 
-func encodeLargeInteger(n int) ([]byte, error) {
-	if n < 1 << 30 {
-		return encodeInteger(int64(n))
-	} else {
-		return []byte{}, nil
+		// calculate the number of bytes needed to store i
+		// the most significant byte cannot be zero
+		// each iteration, shift by 1 byte until the number is zero
+		// then break and save the numBytes needed
+		for numBytes = 1; numBytes < 256; numBytes++ {
+			m = m >> 8
+			if m == 0 {
+				break
+			}
+		}
+
+		topSixBits := uint8(numBytes - 4)
+		lengthByte := topSixBits<<2 + 3
+
+		l := make([]byte, 2)
+
+		binary.LittleEndian.PutUint16(l, uint16(lengthByte))
+		binary.LittleEndian.PutUint64(o, uint64(i))
+
+		return append([]byte{l[0]}, o[0:numBytes]...), nil
 	}
 }
 
--- codec/scale_test.go
+++ codec/scale_test.go
@@ -13,16 +13,20 @@ type encodeVec struct {
 
 var encodeTests = []encodeVec{
 	// compact integers
-	{val: int64(0), output: []byte{0x00}},
-	{val: int64(1), output: []byte{0x04}},
-	{val: int64(42), output: []byte{0xa8}},
-	{val: int64(69), output: []byte{0x15, 0x01}},
-	{val: int64(16383), output: []byte{0xfd, 0xff}},
+	{val: int64(0), 			output: []byte{0x00}},
+	{val: int64(1), 			output: []byte{0x04}},
+	{val: int64(42), 			output: []byte{0xa8}},
+	{val: int64(69), 			output: []byte{0x15, 0x01}},
+	{val: int64(16383), 		output: []byte{0xfd, 0xff}},
+	{val: int64(1073741823), 	output: []byte{0xfe, 0xff, 0xff, 0xff}},
+	{val: int64(1073741824), 	output: []byte{0x03, 0x00, 0x00, 0x00, 0x40}},
+	{val: int64(1<<32-1), 		output: []byte{0x03, 0xff, 0xff, 0xff, 0xff}},
+	{val: int64(1<<32), 		output: []byte{0x07, 0x00, 0x00, 0x00, 0x00, 0x01}},
 
 	// byte arrays
-	{val: []byte{0x01}, output: []byte{0x04, 0x01}},
-	{val: []byte{0xff}, output: []byte{0x04, 0xff}},	
-	{val: []byte{0x01, 0x01}, output: []byte{0x08, 0x01, 0x01}},	
+	{val: []byte{0x01}, 		output: []byte{0x04, 0x01}},
+	{val: []byte{0xff}, 		output: []byte{0x04, 0xff}},	
+	{val: []byte{0x01, 0x01}, 	output: []byte{0x08, 0x01, 0x01}},	
 }
 
 func TestEncode(t *testing.T) {
