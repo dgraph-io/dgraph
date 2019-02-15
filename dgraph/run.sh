916f7c440c5f6b52839f422043aad623064e2997
Daniel Mai <daniel@dgraph.io>
2019-02-14 17:37:25 -0800

    Add test cluster option to run a single group cluster.

--- dgraph/run.sh
+++ dgraph/run.sh
@@ -12,7 +12,7 @@ Available options:
 
     --jaeger       Run with Jaeger tracing enabled.
     --single       Run a single Zero and Alpha (2-node cluster).
-    --six
+    --one-group    Run a single replicated Alpha group (3 Zeros, 3 Alphas).
     --metrics      Run with metrics collection and dashboard (Prometheus/Grafana/Node Exporter).
     --data <path>  Run with Dgraph data directories mounted to <path>.
     --tmpfs        Run with WAL directories (w/zw) in-memory with tmpfs.
@@ -33,6 +33,19 @@ EOF
             ;;
         '--single' )
             SERVICES+=( zero1 dg1 )
+            SINGLE_FLAG=""
+            if [ ! -z "${ONE_GROUP_FLAG+x}" ]; then
+                echo '--single and --one-group not allowed at the same time'
+                exit 1
+            fi
+            ;;
+        '--one-group' )
+            SERVICES+=( zero1 zero2 zero3 dg1 dg2 dg3 )
+            ONE_GROUP_FLAG=""
+            if [ ! -z "${SINGLE_FLAG+x}" ]; then
+                echo '--single and --one-group not allowed at the same time'
+                exit 1
+            fi
             ;;
         '--data' )
             DATA="$2"
@@ -58,8 +71,8 @@ EOF
     shift
 done
 
-# If the --single flag was set, then make sure that services from other flags
-# are started too.
+# If the --single or --one-group flag was set, then make sure that services from
+# other flags are started too.
 if [ "${#SERVICES[@]}" -gt 0 ]; then
     if [[ " ${COMPOSE_FILES[@]} " =~ "docker-compose-jaeger.yml" ]]; then
         SERVICES+=( jaeger )
