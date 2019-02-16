1eb1fc8341572098288e29856f0d184dcc91baba
srfrog <gus@dgraph.io>
2019-02-15 17:19:43 -0700

    ee/backup/backup.go: decouple Process and Request, add Manifest.
    Process is a one-shot process and shouldn't depend on the handler.
    The new Manifest object is used to record the backup details and
    complete the backup.

--- ee/backup/backup.go
+++ ee/backup/backup.go
@@ -14,83 +14,77 @@ package backup
 
 import (
 	"context"
-	"net/url"
+	"encoding/json"
+	"fmt"
 
 	"github.com/dgraph-io/badger"
 	"github.com/dgraph-io/dgraph/protos/pb"
-	"github.com/dgraph-io/dgraph/x"
 
 	"github.com/golang/glog"
 )
-
-// Request has all the information needed to perform a backup.
-type Request struct {
-	DB     *badger.DB // Badger pstore managed by this node.
-	Sizex  uint64     // approximate upload size
-	Backup *pb.BackupRequest
-}
 
 // Process uses the request values to create a stream writer then hand off the data
 // retrieval to stream.Orchestrate. The writer will create all the fd's needed to
 // collect the data and later move to the target.
 // Returns errors on failure, nil on success.
-func (r *Request) Process(ctx context.Context) error {
-	h, err := r.newHandler()
-	if err != nil {
-		glog.Errorf("Unable to get handler for request: %+v. Error: %v", r, err)
+func Process(ctx context.Context, db *badger.DB, req *pb.BackupRequest) error {
+	if err := ctx.Err(); err != nil {
 		return err
 	}
 
-	stream := r.DB.NewStreamAt(r.Backup.ReadTs)
+	h, err := create(object{
+		uri:  req.Location,
+		path: fmt.Sprintf(backupPathFmt, req.UnixTs),
+		name: fmt.Sprintf(backupNameFmt, req.ReadTs, req.GroupId),
+	})
+	if err != nil {
+		glog.Errorf("Unable to get handler for request: %+v. Error: %v", req, err)
+		return err
+	}
+
+	stream := db.NewStreamAt(req.ReadTs)
 	stream.LogPrefix = "Dgraph.Backup"
-	if _, err := stream.Backup(h, r.Backup.Since); err != nil {
+	// Here we return the max version in the original request obejct. We will use this
+	// to create our manifest after the backup is complete.
+	req.Since, err = stream.Backup(h, req.Since)
+	if err != nil {
 		glog.Errorf("While taking backup: %v", err)
 		return err
 	}
-	if err := h.Close(); err != nil {
+	glog.V(3).Infof("Backup maximum version: %d", req.Since)
+	if err = h.Close(); err != nil {
 		glog.Errorf("While closing handler: %v", err)
 		return err
 	}
-	glog.Infof("Backup complete: group %d at %d", r.Backup.GroupId, r.Backup.ReadTs)
+	glog.Infof("Backup complete: group %d at %d", req.GroupId, req.ReadTs)
 	return err
 }
 
-// newHandler parses the requested target URI, finds a handler and then tries to create a session.
-// Target URI formats:
-//   [scheme]://[host]/[path]?[args]
-//   [scheme]:///[path]?[args]
-//   /[path]?[args] (only for local or NFS)
-//
-// Target URI parts:
-//   scheme - service handler, one of: "s3", "gs", "az", "http", "file"
-//     host - remote address. ex: "dgraph.s3.amazonaws.com"
-//     path - directory, bucket or container at target. ex: "/dgraph/backups/"
-//     args - specific arguments that are ok to appear in logs.
-//
-// Global args (might not be support by all handlers):
-//     secure - true|false turn on/off TLS.
-//   compress - true|false turn on/off data compression.
-//
-// Examples:
-//   s3://dgraph.s3.amazonaws.com/dgraph/backups?secure=true
-//   gs://dgraph/backups/
-//   as://dgraph-container/backups/
-//   http://backups.dgraph.io/upload
-//   file:///tmp/dgraph/backups or /tmp/dgraph/backups?compress=gzip
-func (r *Request) newHandler() (handler, error) {
-	uri, err := url.Parse(r.Backup.Location)
+// Manifest records backup details, these are values used during restore.
+// Since is the maximum version seen.
+// Groups are the IDs of the groups involved.
+// Request is the original backup request.
+type Manifest struct {
+	Since   uint64           `json:"since"`
+	Groups  []uint32         `json:"groups"`
+	Request pb.BackupRequest `json:"request"`
+}
+
+// Complete will finalize a backup by writing the manifest at the backup destination.
+func (m *Manifest) Complete(ctx context.Context) error {
+	if err := ctx.Err(); err != nil {
+		return err
+	}
+	h, err := create(object{
+		uri:  m.Request.Location,
+		path: fmt.Sprintf(backupPathFmt, m.Request.UnixTs),
+		name: `manifest.json`,
+	})
 	if err != nil {
-		return nil, err
+		return err
 	}
-
-	// find handler for this URI scheme
-	h := getHandler(uri.Scheme)
-	if h == nil {
-		return nil, x.Errorf("Unable to handle url: %v", uri)
+	if err := json.NewEncoder(h).Encode(&m); err != nil {
+		return err
 	}
-
-	if err := h.Create(uri, r); err != nil {
-		return nil, err
-	}
-	return h, nil
+	return h.Close()
 }
