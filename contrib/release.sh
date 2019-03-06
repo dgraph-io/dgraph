7fb236805b880e7f092e7070fa39040d0a155236
Daniel Mai <daniel@dgraph.io>
2019-03-06 13:41:20 -0800

    Update release script to build with go 1.11.5.

--- contrib/release.sh
+++ contrib/release.sh
@@ -11,6 +11,8 @@ GOPATH="/tmp/go"
 rm -Rf $GOPATH
 mkdir $GOPATH
 
+GOVERSION="1.11.5"
+
 TAG=$1
 # The Docker tag should not contain a slash e.g. feature/issue1234
 # The initial slash is taken from the repository name dgraph/dgraph:tag
@@ -100,33 +102,33 @@ popd
 
 # Build Windows.
 pushd $basedir/dgraph/dgraph
-	xgo --targets=windows/amd64 -ldflags \
+	xgo -go=$GOVERSION --targets=windows/amd64 -ldflags \
   "-X $release=$release_version -X $branch=$gitBranch -X $commitSHA1=$lastCommitSHA1 -X '$commitTime=$lastCommitTime'" .
   mkdir $TMP/windows
   mv dgraph-windows-4.0-amd64.exe $TMP/windows/dgraph.exe
 popd
 
 pushd $basedir/ratel
-	xgo --targets=windows/amd64 -ldflags "-X $ratel_release=$release_version" .
+	xgo -go=$GOVERSION --targets=windows/amd64 -ldflags "-X $ratel_release=$release_version" .
 	mv ratel-windows-4.0-amd64.exe $TMP/windows/dgraph-ratel.exe
 popd
 
 # Build Darwin.
 pushd $basedir/dgraph/dgraph
-	xgo --targets=darwin-10.9/amd64 -ldflags \
+	xgo -go=$GOVERSION --targets=darwin-10.9/amd64 -ldflags \
   "-X $release=$release_version -X $branch=$gitBranch -X $commitSHA1=$lastCommitSHA1 -X '$commitTime=$lastCommitTime'" .
   mkdir $TMP/darwin
   mv dgraph-darwin-10.9-amd64 $TMP/darwin/dgraph
 popd
 
 pushd $basedir/ratel
-	xgo --targets=darwin-10.9/amd64 -ldflags "-X $ratel_release=$release_version" .
+	xgo -go=$GOVERSION --targets=darwin-10.9/amd64 -ldflags "-X $ratel_release=$release_version" .
 	mv ratel-darwin-10.9-amd64 $TMP/darwin/dgraph-ratel
 popd
 
 # Build Linux.
 pushd $basedir/dgraph/dgraph
-	xgo --targets=linux/amd64 -ldflags \
+	xgo -go=$GOVERSION --targets=linux/amd64 -ldflags \
     "-X $release=$release_version -X $branch=$gitBranch -X $commitSHA1=$lastCommitSHA1 -X '$commitTime=$lastCommitTime'" .
   strip -x dgraph-linux-amd64
   mkdir $TMP/linux
@@ -134,7 +136,7 @@ pushd $basedir/dgraph/dgraph
 popd
 
 pushd $basedir/ratel
-	xgo --targets=linux/amd64 -ldflags "-X $ratel_release=$release_version" .
+	xgo -go=$GOVERSION --targets=linux/amd64 -ldflags "-X $ratel_release=$release_version" .
   strip -x ratel-linux-amd64
 	mv ratel-linux-amd64 $TMP/linux/dgraph-ratel
 popd
