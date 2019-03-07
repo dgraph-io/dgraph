614ca119a1adaa7deb3b2304dc640764fd1cb3c6
Daniel Mai <daniel@dgraph.io>
2019-03-06 17:09:20 -0800

    docs: Set v1.0.12 as latest docs.

--- wiki/scripts/build.sh
+++ wiki/scripts/build.sh
@@ -19,8 +19,9 @@ HOST=https://docs.dgraph.io
 # append '(latest)' to the version string, and build script can place the
 # artifact in an appropriate location
 VERSIONS_ARRAY=(
-'v1.0.11'
+'v1.0.12'
 'master'
+'v1.0.11'
 'v1.0.10'
 'v1.0.9'
 'v1.0.8'
