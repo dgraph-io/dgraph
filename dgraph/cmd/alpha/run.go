32238cba6832d2ea4e153caa7f810b9f27bfc9a0
Manish R Jain <manish@dgraph.io>
2019-03-11 18:53:27 -0700

    Formatting changes

--- dgraph/cmd/alpha/run.go
+++ dgraph/cmd/alpha/run.go
@@ -516,8 +516,9 @@ func run() {
 		}
 	}
 	otrace.ApplyConfig(otrace.Config{
+		DefaultSampler:             otrace.ProbabilitySampler(x.WorkerConfig.Tracing),
 		MaxAnnotationEventsPerSpan: 64,
-		DefaultSampler:             otrace.ProbabilitySampler(x.WorkerConfig.Tracing)})
+	})
 
 	// Posting will initialize index which requires schema. Hence, initialize
 	// schema before calling posting.Init().
