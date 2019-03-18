db65dc80459b570b74dc93d568050ec1abd039c8
Lucas Wang <lucas@dgraph.io>
2019-03-18 16:33:34 -0700

    Reduce the value of ACL TTLs to reduce the test running time (#3164)

--- edgraph/access_ee.go
+++ edgraph/access_ee.go
@@ -288,7 +288,7 @@ func RefreshAcls(closer *y.Closer) {
 		return
 	}
 
-	ticker := time.NewTicker(30 * time.Second)
+	ticker := time.NewTicker(Config.AclRefreshInterval)
 	defer ticker.Stop()
 
 	// retrieve the full data set of ACLs from the corresponding alpha server, and update the
