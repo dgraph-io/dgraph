f068b79a49e5623c695a326f9169dca8475182b0
Amer Ameen <amerameen@gmail.com>
2019-03-20 19:50:44 -0400

    Amer/encode strings (#59)
    * added support for strings in encodeTests and added preliminary string tests
    
    * added string tests for mode = 1 and mode = 2. mode = 3 test commented out because num_bytes_n = 3 results in a negative number for num_bytes_n - 4
    
    * Refactor string tests. Create seperate function for setup of tests.
    
    * Changed TestEncode so it doesn't try to print a very long string upon a failed test
    
    * Added test for special characters
    
    * Commented out long running test (testString3)
    
    * Fixed issue that caused linter to fail CI
    
    * Added in comment on long running test being omitted for the CI and uncommented a test that was accidentilly commented out

--- codec/encode_test.go
+++ codec/encode_test.go
@@ -2,6 +2,7 @@ package codec
 
 import (
 	"bytes"
+	"strings"
 	"math/big"
 	"testing"
 )
@@ -74,16 +75,54 @@ var encodeTests = []encodeTest{
 	{val: []*big.Int{big.NewInt(0), big.NewInt(1)}, output: []byte{0x08, 0x00, 0x04}, bytesEncoded: 3},
 }
 
+// Test strings for various values of n & mode. Also test strings with special characters
+// Commented out testString3 since it causes the CI to fail.
+func setUpStringTests() {
+
+	testString1 := "We love you! We believe in open source as wonderful form of giving." 	// n = 67
+	testString2 := strings.Repeat("We need a longer string to test with. Let's multiple this several times.", 230) 		// n = 72 * 230 = 16560
+	//testString3 := strings.Repeat("We need a longer string to test with. Let's multiple this several times.", 14913081) 			// n = 72 * 14913081 = 1073741832 (> 2^30 = 1073741824)
+	testString4 := "Let's test some special ASCII characters: ~  · © ÿ" 															// n = 55 (UTF-8 encoding versus n = 51 with ASCII encoding)
+
+	testStrings := []encodeTest{
+		{val: string("a"),
+			output: []byte{0x04, 0x61}, bytesEncoded: 2},
+		{val: string("go-pre"),																			// n = 6, mode = 0
+			output: append([]byte{0x18}, string("go-pre")...), bytesEncoded: 7}, 			 			// n|mode = 0x18
+		{val: testString1,																				// n = 67, mode = 1
+			output: append([]byte{0x0D,0x01}, testString1...), bytesEncoded: 69}, 						// n|mode = 0x010D (BE) = 0x0D01 (LE)
+		{val: testString2,																				// n = 16560, mode = 2
+			output: append([]byte{0xC2,0x02,0x01,0x00}, testString2...), bytesEncoded: 16564},			// n|mode = 0x102C2 (BE) = 0xC20201 (LE)
+		//{val: testString3,																			// n = 1073741832, mode = 3, num_bytes_n = 4
+		//output: append([]byte{0x03,0x08,0x00,0x00,0x40}, testString3...), bytesEncoded: 1073741837},	// (num_bytes_n - 4)|mode|n = 0x40 00 00 08 03 (BE) = 0x03 08 00 00 40 (LE)
+		{val: testString4,																				// n = 55, mode = 0
+			output: append([]byte{0xDC}, testString4...), bytesEncoded: 56},							// n|mode = 0xDC (BE/LE)
+	}
+
+	//Append stringTests to all other tests
+	encodeTests = append(encodeTests, testStrings...)
+}
+
 func TestEncode(t *testing.T) {
+
+	setUpStringTests()
+
 	for _, test := range encodeTests {
+	    
 		buffer := bytes.Buffer{}
 		se := Encoder{&buffer}
 		bytesEncoded, err := se.Encode(test.val)
 		output := buffer.Bytes()
+		
 		if err != nil {
 			t.Error(err)
 		} else if !bytes.Equal(output, test.output) {
-			t.Errorf("Fail: input %x got %x expected %x", test.val, output, test.output)
+			if len(test.output) < 1<<15 {
+				t.Errorf("Fail: input %x got %x expected %x", test.val, output, test.output)
+			} else {
+				//Only prints first 10 bytes of a failed test if output is > 2^15 bytes
+				t.Errorf("Failed test with large output. First 10 bytes: got %x... expected %x...", output[0:10], test.output[0:10] )
+			}
 		} else if bytesEncoded != test.bytesEncoded {
 			t.Errorf("Fail: input %x  got %d bytes encoded expected %d", test.val, bytesEncoded, test.bytesEncoded)
 		}
