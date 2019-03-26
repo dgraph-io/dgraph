1f40701ea1eac0b80414ddbb49aeb6c7a7b9ea8f
Martin Martinez Rivera <martinmr@users.noreply.github.com>
2019-03-26 13:43:59 -0700

    Rename type predicate to dgraph.type (#3204)

--- schema/schema.go
+++ schema/schema.go
@@ -421,7 +421,7 @@ func InitialSchema() []*pb.SchemaUpdate {
 	}
 
 	initialSchema = append(initialSchema, &pb.SchemaUpdate{
-		Predicate: "type",
+		Predicate: "dgraph.type",
 		ValueType: pb.Posting_STRING,
 		Directive: pb.SchemaUpdate_INDEX,
 		Tokenizer: []string{"exact"},
