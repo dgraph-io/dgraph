57360d7139ba09e77a461b2373bcaa29f1190141
Dustin Brickwood <dustinbrickwood204@gmail.com>
2019-03-27 16:31:16 -0500

    MemoryDB  (#80)
    * basic memory database ops in place
    
    * memorydb tests
    
    * memDB: in place for testing
    
    * resolved conflicts
    
    * added data struct

---
+++ polkadb/memoryDB.go
@@ -0,0 +1,69 @@
+package polkadb
+
+import (
+	"sync"
+	"errors"
+)
+
+// MemDatabase test memory database, data is not persisted
+type MemDatabase struct {
+	db map[string][]byte
+	lock sync.RWMutex
+}
+
+// NewMemDatabase returns an initialized mapping used for test database
+func NewMemDatabase() (*MemDatabase, error) {
+	return &MemDatabase{
+		db: make(map[string][]byte),
+	}, nil
+}
+
+// Put puts the given key / value into the mapping
+func (db *MemDatabase) Put(k []byte, v []byte) error {
+	db.lock.Lock()
+	defer db.lock.Unlock()
+
+	db.db[string(k)] = v
+	return nil
+}
+
+// Has checks the given key exists already; returning true or false
+func (db *MemDatabase) Has(k []byte) (bool, error) {
+	db.lock.RLock()
+	defer db.lock.RUnlock()
+
+	_, ok := db.db[string(k)]
+	return ok, nil
+}
+
+// Get returns the given key []byte
+func (db *MemDatabase) Get(k []byte) ([]byte, error) {
+	db.lock.RLock()
+	defer db.lock.RUnlock()
+
+	if v, ok := db.db[string(k)]; ok {
+		return v, nil
+	}
+	return nil, errors.New("not found")
+}
+
+// Keys returns [][]byte of mapping keys
+func (db *MemDatabase) Keys() [][]byte {
+	db.lock.RLock()
+	defer db.lock.RUnlock()
+
+	keys := [][]byte{}
+	for key := range db.db {
+		keys = append(keys, []byte(key))
+	}
+	return keys
+}
+
+// Delete removes the key from the mapping
+func (db *MemDatabase) Delete(key []byte) error {
+	db.lock.Lock()
+	defer db.lock.Unlock()
+
+	delete(db.db, string(key))
+	return nil
+}
---
+++ polkadb/memoryDB_test.go
@@ -0,0 +1,96 @@
+package polkadb
+
+import (
+	"testing"
+	"bytes"
+)
+
+type memData struct {
+	input    string
+	expected string
+}
+
+
+func testData() []memData {
+	tests := []memData{
+		{"camel", "camel"},
+		{"walrus", "walrus"},
+		{"296204", "296204"},
+		{"\x00123\x00", "\x00123\x00"},
+	}
+	return tests
+}
+
+func TestMemoryDB_PutGet(t *testing.T) {
+	memDB, err := NewMemDatabase()
+	if err != nil {
+		t.Fatalf("failed to create memoryDB %q", err)
+	}
+	testPutGet(memDB, t)
+	testHasGet(memDB, t)
+	testDelGet(memDB, t)
+}
+
+func testPutGet(db *MemDatabase, t *testing.T) {
+	tests := testData()
+
+	for _, v := range tests {
+		err := db.Put([]byte(v.input), []byte(v.input))
+		if err != nil {
+			t.Fatalf("put failed: %v", err)
+		}
+	}
+	for _, v := range tests {
+		data, err := db.Get([]byte(v.input))
+		if err != nil {
+			t.Fatalf("get failed: %v", err)
+		}
+		if !bytes.Equal(data, []byte(v.expected)) {
+			t.Fatalf("get returned wrong result, got %q expected %q", string(data), v.expected)
+		}
+	}
+}
+
+func testHasGet(db *MemDatabase, t *testing.T) {
+	tests := testData()
+
+	for _, v := range tests {
+		exists, err := db.Has([]byte(v.input))
+		if err != nil {
+			t.Fatalf("has operation failed: %v", err)
+		}
+		if !exists {
+			t.Fatalf("has operation returned wrong result, got %t expected %t", exists, true)
+		}
+	}
+	k := db.Keys()
+	if len(k) != 4 {
+		t.Fatalf("failed to retrieve keys, expected %v, got %v", 4, len(k[0]))
+	}
+}
+
+func testDelGet(db *MemDatabase, t *testing.T) {
+	tests := testData()
+
+	for _, v := range tests {
+		err := db.Put([]byte(v.input), []byte(v.input))
+		if err != nil {
+			t.Fatalf("put failed: %v", err)
+		}
+	}
+
+	for _, v := range tests {
+		err := db.Delete([]byte(v.input))
+		if err != nil {
+			t.Fatalf("delete %q failed: %v", v.input, err)
+		}
+	}
+
+	for _, v := range tests {
+		d, _ := db.Get([]byte(v.input))
+		if len(d) > 1 {
+			t.Fatalf("failed to delete value %q", v.input)
+		}
+	}
+}
+
