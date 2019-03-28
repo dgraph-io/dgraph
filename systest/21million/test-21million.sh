b992486e0cfce6074aa35bdd7347b8155ee8109c
Javier Alvarado <javier@dgraph.io>
2019-03-28 13:25:22 -0700

    Replace environment variables with proper options.

--- systest/21million/test-21million.sh
+++ systest/21million/test-21million.sh
@@ -22,19 +22,59 @@ function DockerCompose {
     docker-compose -p dgraph "$@"
 }
 
-if [[ $DGRAPH_LOADER != bulk && $DGRAPH_LOADER != live ]]; then
-    echo >&2 "$ME: loader must be 'bulk' or 'live' -- $DGRAPH_LOADER"
+HELP= LOADER=bulk CLEANUP=
+
+ARGS=$(/usr/bin/getopt -n$ME -o"h" -l"help,loader:,cleanup:" -- "$@") || exit 1
+eval set -- "$ARGS"
+while true; do
+    case "$1" in
+        -h|--help)  HELP=yes;              ;;
+        --loader)   LOADER=${2,,}; shift   ;;
+        --cleanup)  CLEANUP=${2,,}; shift  ;;
+        --)         shift; break           ;;
+    esac
+    shift
+done
+
+if [[ $HELP ]]; then
+    cat <<EOF
+usage: $ME [-h|--help] [--loader=<bulk|live|none>] [--cleanup=<all|none|servers>]
+
+options:
+
+    --loader        bulk = use dgraph bulk (default)
+                    live = use dgraph live
+                    none = use data loaded by previous run
+    --cleanup       all = take down containers and data volume (default)
+                    servers = take down dgraph zero and alpha but leave data volume up
+                    none = leave up containers and data volume
+EOF
+    exit 0
+fi
+
+if [[ $LOADER != bulk && $LOADER != live && $LOADER != none ]]; then
+    echo >&2 "$ME: loader must be 'bulk' or 'live' or 'none' -- $LOADER"
     exit 1
 fi
 
-if [[ ${DGRAPH_RELOAD,,} != yes ]]; then
-    unset DGRAPH_RELOAD
+# default to leaving the data around for another run
+# if already re-using it from a previous run
+if [[ $LOADER == none && -z $CLEANUP ]]; then
+    CLEANUP=servers
+fi
+
+# default to cleaning up both services and volume
+if [[ -z $CLEANUP  ]]; then
+    CLEANUP=all
+elif [[ $CLEANUP != all && $CLEANUP != servers && $CLEANUP != none ]]; then
+    echo >&2 "$ME: cleanup must be 'all' or 'servers' or 'none' -- $LOADER"
+    exit 1
 fi
 
 Info "entering directory $SRCDIR"
 cd $SRCDIR
 
-if [[ $DGRAPH_RELOAD ]]; then
+if [[ $LOADER != none ]]; then
     Info "removing old data (if any)"
     DockerCompose down -v
 else
@@ -42,14 +82,14 @@ else
 fi
 
 Info "bringing up zero container"
-DockerCompose up -d zero1
+DockerCompose up -d --force-recreate zero1
 
 Info "waiting for zero to become leader"
 DockerCompose logs -f zero1 | grep -q -m1 "I've become the leader"
 
-if [[ $DGRAPH_RELOAD && $DGRAPH_LOADER == bulk ]]; then
-    Info "bulk loading 21million data set"
-    DockerCompose run --rm dg1 \
+if [[ $LOADER == bulk ]]; then
+    Info "bulk loading data set"
+    DockerCompose run --name bulk_load --rm dg1 \
         bash -s <<EOF
             /gobin/dgraph bulk --schema=<(curl -LSs $SCHEMA_URL) --files=<(curl -LSs $DATA_URL) \
                                --format=rdf --zero=zero1:5080 --out=/data/dg1/bulk
@@ -58,27 +98,25 @@ EOF
 fi
 
 Info "bringing up alpha container"
-DockerCompose up -d dg1
+DockerCompose up -d --force-recreate dg1
 
 Info "waiting for alpha to be ready"
 DockerCompose logs -f dg1 | grep -q -m1 "Server is ready"
 
-if [[ $DGRAPH_RELOAD && $DGRAPH_LOADER == live ]]; then
-    Info "live loading 21million data set"
+if [[ $LOADER == live ]]; then
+    Info "live loading data set"
     dgraph live --schema=<(curl -LSs $SCHEMA_URL) --files=<(curl -LSs $DATA_URL) \
                 --format=rdf --zero=:5080 --dgraph=:9180 --logtostderr
 fi
 
-# if env var is set, exit after loading data
-[[ $DGRAPH_LOAD_ONLY ]] && exit 0
-
 Info "running benchmarks/regression queries"
 go test -v -tags standalone || FOUND_DIFFS=1
 
-Info "bringing down zero and alpha containers"
-if [[ $DGRAPH_RELOAD ]]; then
+if [[ $CLEANUP == all ]]; then
+    Info "bringing down zero and alpha and data volumes"
     DockerCompose down -v
-else
+elif [[ $CLEANUP != none ]]; then
+    Info "bringing down zero and alpha only"
     DockerCompose down
 fi
 
