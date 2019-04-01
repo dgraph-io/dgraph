6e3e9ac8499e769b5681cdd8c060280e9917af50
Lucas Wang <lucas@dgraph.io>
2019-04-01 16:18:32 -0700

    ACL over TLS (#3207)

--- ee/acl/acl_curl_test.go
+++ ee/acl/acl_curl_test.go
@@ -13,10 +13,7 @@
 package acl
 
 import (
-	"encoding/json"
 	"fmt"
-	"os/exec"
-	"strings"
 	"testing"
 	"time"
 
@@ -49,8 +46,8 @@ func TestCurlAuthorization(t *testing.T) {
 		return []string{"-H", fmt.Sprintf("X-Dgraph-AccessToken:%s", jwt),
 			"-d", query, curlQueryEndpoint}
 	}
-	verifyCurlCmd(t, queryArgs(accessJwt), &FailureConfig{
-		shouldFail: false,
+	z.VerifyCurlCmd(t, queryArgs(accessJwt), &z.FailureConfig{
+		ShouldFail: false,
 	})
 
 	mutateArgs := func(jwt string) []string {
@@ -60,16 +57,17 @@ func TestCurlAuthorization(t *testing.T) {
 	   }}`, predicateToWrite), curlMutateEndpoint}
 
 	}
-	verifyCurlCmd(t, mutateArgs(accessJwt), &FailureConfig{
-		shouldFail: false,
+
+	z.VerifyCurlCmd(t, mutateArgs(accessJwt), &z.FailureConfig{
+		ShouldFail: false,
 	})
 
 	alterArgs := func(jwt string) []string {
 		return []string{"-H", fmt.Sprintf("X-Dgraph-AccessToken:%s", jwt),
 			"-d", fmt.Sprintf(`%s: int .`, predicateToAlter), curlAlterEndpoint}
 	}
-	verifyCurlCmd(t, alterArgs(accessJwt), &FailureConfig{
-		shouldFail: false,
+	z.VerifyCurlCmd(t, alterArgs(accessJwt), &z.FailureConfig{
+		ShouldFail: false,
 	})
 
 	// sleep long enough (longer than 10s, the access JWT TTL defined in the docker-compose.yml
@@ -77,17 +75,17 @@ func TestCurlAuthorization(t *testing.T) {
 	// JWT
 	glog.Infof("Sleeping for 4 seconds for accessJwt to expire")
 	time.Sleep(4 * time.Second)
-	verifyCurlCmd(t, queryArgs(accessJwt), &FailureConfig{
-		shouldFail: true,
-		failMsg:    "Token is expired",
+	z.VerifyCurlCmd(t, queryArgs(accessJwt), &z.FailureConfig{
+		ShouldFail:   true,
+		DgraphErrMsg: "Token is expired",
 	})
-	verifyCurlCmd(t, mutateArgs(accessJwt), &FailureConfig{
-		shouldFail: true,
-		failMsg:    "Token is expired",
+	z.VerifyCurlCmd(t, mutateArgs(accessJwt), &z.FailureConfig{
+		ShouldFail:   true,
+		DgraphErrMsg: "Token is expired",
 	})
-	verifyCurlCmd(t, alterArgs(accessJwt), &FailureConfig{
-		shouldFail: true,
-		failMsg:    "Token is expired",
+	z.VerifyCurlCmd(t, alterArgs(accessJwt), &z.FailureConfig{
+		ShouldFail:   true,
+		DgraphErrMsg: "Token is expired",
 	})
 	// login again using the refreshJwt
 	accessJwt, refreshJwt, err = z.HttpLogin(&z.LoginParams{
@@ -96,17 +94,17 @@ func TestCurlAuthorization(t *testing.T) {
 	})
 	require.NoError(t, err, fmt.Sprintf("login through refresh token failed: %v", err))
 	// verify that the query works again with the new access jwt
-	verifyCurlCmd(t, queryArgs(accessJwt), &FailureConfig{
-		shouldFail: false,
+	z.VerifyCurlCmd(t, queryArgs(accessJwt), &z.FailureConfig{
+		ShouldFail: false,
 	})
 
 	createGroupAndAcls(t, unusedGroup, false)
 	// wait for 6 seconds to ensure the new acl have reached all acl caches
 	glog.Infof("Sleeping for 6 seconds for acl caches to be refreshed")
 	time.Sleep(6 * time.Second)
-	verifyCurlCmd(t, queryArgs(accessJwt), &FailureConfig{
-		shouldFail: true,
-		failMsg:    "Token is expired",
+	z.VerifyCurlCmd(t, queryArgs(accessJwt), &z.FailureConfig{
+		ShouldFail:   true,
+		DgraphErrMsg: "Token is expired",
 	})
 	// refresh the jwts again
 	accessJwt, refreshJwt, err = z.HttpLogin(&z.LoginParams{
@@ -116,17 +114,17 @@ func TestCurlAuthorization(t *testing.T) {
 	require.NoError(t, err, fmt.Sprintf("login through refresh token failed: %v", err))
 	// verify that with an ACL rule defined, all the operations should be denied when the acsess JWT
 	// does not have the required permissions
-	verifyCurlCmd(t, queryArgs(accessJwt), &FailureConfig{
-		shouldFail: true,
-		failMsg:    "PermissionDenied",
+	z.VerifyCurlCmd(t, queryArgs(accessJwt), &z.FailureConfig{
+		ShouldFail:   true,
+		DgraphErrMsg: "PermissionDenied",
 	})
-	verifyCurlCmd(t, mutateArgs(accessJwt), &FailureConfig{
-		shouldFail: true,
-		failMsg:    "PermissionDenied",
+	z.VerifyCurlCmd(t, mutateArgs(accessJwt), &z.FailureConfig{
+		ShouldFail:   true,
+		DgraphErrMsg: "PermissionDenied",
 	})
-	verifyCurlCmd(t, alterArgs(accessJwt), &FailureConfig{
-		shouldFail: true,
-		failMsg:    "PermissionDenied",
+	z.VerifyCurlCmd(t, alterArgs(accessJwt), &z.FailureConfig{
+		ShouldFail:   true,
+		DgraphErrMsg: "PermissionDenied",
 	})
 
 	createGroupAndAcls(t, devGroup, true)
@@ -139,14 +137,14 @@ func TestCurlAuthorization(t *testing.T) {
 	})
 	require.NoError(t, err, fmt.Sprintf("login through refresh token failed: %v", err))
 	// verify that the operations should be allowed again through the dev group
-	verifyCurlCmd(t, queryArgs(accessJwt), &FailureConfig{
-		shouldFail: false,
+	z.VerifyCurlCmd(t, queryArgs(accessJwt), &z.FailureConfig{
+		ShouldFail: false,
 	})
-	verifyCurlCmd(t, mutateArgs(accessJwt), &FailureConfig{
-		shouldFail: false,
+	z.VerifyCurlCmd(t, mutateArgs(accessJwt), &z.FailureConfig{
+		ShouldFail: false,
 	})
-	verifyCurlCmd(t, alterArgs(accessJwt), &FailureConfig{
-		shouldFail: false,
+	z.VerifyCurlCmd(t, alterArgs(accessJwt), &z.FailureConfig{
+		ShouldFail: false,
 	})
 }
 
@@ -156,47 +154,3 @@ const (
 	curlMutateEndpoint = "localhost:8180/mutate"
 	curlAlterEndpoint  = "localhost:8180/alter"
 )
-
-type ErrorEntry struct {
-	Code    string `json:"code"`
-	Message string `json:"message"`
-}
-
-type Output struct {
-	Data   map[string]interface{} `json:"data"`
-	Errors []ErrorEntry           `json:"errors"`
-}
-
-type FailureConfig struct {
-	shouldFail bool
-	failMsg    string
-}
-
-func verifyOutput(t *testing.T, bytes []byte, failureConfig *FailureConfig) {
-	output := Output{}
-	require.NoError(t, json.Unmarshal(bytes, &output),
-		"unable to unmarshal the curl output")
-
-	if failureConfig.shouldFail {
-		require.True(t, len(output.Errors) > 0, "no error entry found")
-		if len(failureConfig.failMsg) > 0 {
-			errorEntry := output.Errors[0]
-			require.True(t, strings.Contains(errorEntry.Message, failureConfig.failMsg),
-				fmt.Sprintf("the failure msg\n%s\nis not part of the curl error output:%s\n",
-					failureConfig.failMsg, errorEntry.Message))
-		}
-	} else {
-		require.True(t, len(output.Data) > 0,
-			fmt.Sprintf("no data entry found in the output:%+v", output))
-	}
-}
-
-func verifyCurlCmd(t *testing.T, args []string,
-	failureConfig *FailureConfig) {
-	queryCmd := exec.Command("curl", args...)
-
-	output, err := queryCmd.Output()
-	// the curl command should always succeed
-	require.NoError(t, err, "the curl command should have succeeded")
-	verifyOutput(t, output, failureConfig)
-}
