f86557ce7029e81d7e0eab0c4b208f02c8a2ba53
srfrog <gus@dgraph.io>
2019-04-04 15:41:28 -0700

    gql/state.go: make error a bit clearer when unbalanced braces are detected

--- gql/state.go
+++ gql/state.go
@@ -483,7 +483,7 @@ func lexTextCondQuery(l *lex.Lexer) lex.StateFn {
 		if r == rightCurl {
 			depth--
 			if l.Depth < depth {
-				return l.Errorf("Invalid character '}' inside query text")
+				return l.Errorf("Unbalanced '}' found inside query text")
 			}
 		}
 		if depth > 0 {
