1ee701394c86db1b032e8c4e4c805ecec21ceed5
Manish R Jain <manish@dgraph.io>
2019-05-01 18:37:23 -0700

    Revert unnecessary changes

--- contrib/blockade/main.go
+++ contrib/blockade/main.go
@@ -41,7 +41,7 @@ func partition(instance string) error {
 
 func increment(atLeast int, args string) error {
 	errCh := make(chan error, 1)
-	ctx, cancel := context.WithTimeout(ctxb, 2*time.Minute)
+	ctx, cancel := context.WithTimeout(ctxb, 1*time.Minute)
 	defer cancel()
 
 	addrs := []string{"localhost:9180", "localhost:9182", "localhost:9183"}
