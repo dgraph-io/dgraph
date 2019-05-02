5b7dcd7091d57afba6cdedb36de94e4142fec556
Martin Martinez Rivera <martinmr@users.noreply.github.com>
2019-05-02 10:51:51 -0700

    Rename blockade to embargo. (#3353)

--- contrib/embargo/run.sh
+++ contrib/embargo/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Builds ./blockade and runs the blockade tests.
+# Builds ./embargo and runs the embargo tests.
 #
 # Usage:
 # Run the test 32 times (about 8 hours):
@@ -7,12 +7,12 @@
 # Run the test once:
 #     ./run.sh 1
 
-function cleanup_blockade {
-    blockade destroy || true
+function cleanup_embargo {
+    embargo destroy || true
     docker container prune -f
-    if docker network ls | grep -q 'blockade_net'; then
+    if docker network ls | grep -q 'embargo_net'; then
         docker network ls |
-            awk '/blockade_net/ { print $1 }' |
+            awk '/embargo_net/ { print $1 }' |
             xargs docker network rm
     fi
 }
@@ -24,13 +24,13 @@ times=${1:-32}
 
 go build -v .
 
-cleanup_blockade
+cleanup_embargo
 # Each run takes about 15 minutes, so running 32 times will take about 8 hours.
 for i in $(seq 1 $times)
 do
-    echo "===> Running Blockade #$i"
-    if ! ./blockade 2>&1 | tee blockade$i.log; then
-        echo "===> Blockade test failed"
+    echo "===> Running Embargo #$i"
+    if ! ./embargo 2>&1 | tee embargo$i.log; then
+        echo "===> Embargo test failed"
         docker logs zero1 2>&1 | tee zero1.log
         docker logs zero2 2>&1 | tee zero2.log
         docker logs zero3 2>&1 | tee zero3.log
@@ -38,12 +38,12 @@ do
         docker logs dg2 2>&1 | tee dg2.log
         docker logs dg3 2>&1 | tee dg3.log
 
-        cleanup_blockade
+        cleanup_embargo
         exit 1
     fi
 done
 
-echo "Blockade log summary:"
-grep '===>' blockade*.log
+echo "Embargo log summary:"
+grep '===>' embargo*.log
 
-cleanup_blockade
+cleanup_embargo
