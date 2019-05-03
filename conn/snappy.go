e787ed713b593382ed953d6c855040ffac287d44
Daniel Mai <daniel@dgraph.io>
2019-05-03 15:12:23 -0700

    Copy snappy.go from cockroachdb/cockroach/pkg/rpc/snappy.go.

--- conn/snappy.go
+++ conn/snappy.go
@@ -1,18 +1,16 @@
-/*
- * Copyright 2019 Dgraph Labs, Inc. and Contributors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2017 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
 
 package conn
 
@@ -24,6 +22,9 @@ import (
 	"google.golang.org/grpc/encoding"
 )
 
+// NB: The encoding.Compressor implementation needs to be goroutine
+// safe as multiple goroutines may be using the same compressor for
+// different streams on the same connection.
 var snappyWriterPool sync.Pool
 var snappyReaderPool sync.Pool
 
