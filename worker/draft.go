f21fff90d64e3ca180591641b66096389ee1614f
Manish R Jain <manish@dgraph.io>
2019-05-03 16:05:04 -0700

    Revert "Fix issue caused by the new RaftKey (#3370)"
    This reverts commit 809a08f2f77e01db5457744959216b849d5206ac.

--- worker/draft.go
+++ worker/draft.go
@@ -924,11 +924,19 @@ func (n *node) rollupLists(readTs uint64) error {
 	amLeader := n.AmLeader()
 	m := new(sync.Map)
 
-	addTo := func(attr string, delta int64) {
-		val, ok := m.Load(attr)
+	addTo := func(key []byte, delta int64) {
+		if !amLeader {
+			// Only leader needs to calculate the tablet sizes.
+			return
+		}
+		pk := x.Parse(key)
+		if pk == nil {
+			return
+		}
+		val, ok := m.Load(pk.Attr)
 		if !ok {
 			sz := new(int64)
-			val, _ = m.LoadOrStore(attr, sz)
+			val, _ = m.LoadOrStore(pk.Attr, sz)
 		}
 		size := val.(*int64)
 		atomic.AddInt64(size, delta)
@@ -937,16 +945,9 @@ func (n *node) rollupLists(readTs uint64) error {
 	stream := pstore.NewStreamAt(readTs)
 	stream.LogPrefix = "Rolling up"
 	stream.ChooseKey = func(item *badger.Item) bool {
-		pk := x.Parse(item.Key())
-		if pk == nil || len(pk.Attr) == 0 {
-			return false
-		}
 		switch item.UserMeta() {
 		case posting.BitSchemaPosting, posting.BitCompletePosting, posting.BitEmptyPosting:
-			if amLeader {
-				// Only leader needs to calculate the tablet sizes.
-				addTo(pk.Attr, item.EstimatedSize())
-			}
+			addTo(item.Key(), item.EstimatedSize())
 			return false
 		default:
 			return true
@@ -960,12 +961,7 @@ func (n *node) rollupLists(readTs uint64) error {
 		}
 		atomic.AddUint64(&numKeys, 1)
 		kv, err := l.MarshalToKv()
-		if amLeader {
-			pk := x.Parse(kv.Key)
-			if pk != nil && len(pk.Attr) > 0 {
-				addTo(pk.Attr, int64(kv.Size()))
-			}
-		}
+		addTo(key, int64(kv.Size()))
 		return listWrap(kv), err
 	}
 	stream.Send = func(list *bpb.KVList) error {
@@ -1142,6 +1138,9 @@ func (n *node) calculateSnapshot(discardN int) (*pb.Snapshot, error) {
 		return nil, err
 	}
 
+	if num := posting.Oracle().NumPendingTxns(); num > 0 {
+		glog.Infof("Num pending txns: %d", num)
+	}
 	// We can't rely upon the Raft entries to determine the minPendingStart,
 	// because there are many cases during mutations where we don't commit or
 	// abort the transaction. This might happen due to an early error thrown.
