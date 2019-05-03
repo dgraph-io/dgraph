9086d00252e0fed3460f15c30f7249d69ad40729
Manish R Jain <manish@dgraph.io>
2019-05-03 15:53:20 -0700

    Revert "Fix issue caused by the new RaftKey (#3370)"
    This reverts commit 9daff210fa4dedefb5ae8622fe3f7c049041c31e.

--- worker/draft.go
+++ worker/draft.go
@@ -990,11 +990,19 @@ func (n *node) rollupLists(readTs uint64) error {
 	amLeader := n.AmLeader()
 	m := new(sync.Map)
 
-	addTo := func(attr string, delta int64) {
-		val, ok := m.Load(attr)
+	addTo := func(key []byte, delta int64) {
+		if !amLeader {
+			// Only leader needs to calculate the tablet sizes.
+			return
+		}
+		pk := x.Parse(key)
+		if pk == nil {
+			return
+		}
+		val, ok := m.Load(pk.Attr)
 		if !ok {
 			sz := new(int64)
-			val, _ = m.LoadOrStore(attr, sz)
+			val, _ = m.LoadOrStore(pk.Attr, sz)
 		}
 		size := val.(*int64)
 		atomic.AddInt64(size, delta)
@@ -1003,16 +1011,9 @@ func (n *node) rollupLists(readTs uint64) error {
 	stream := pstore.NewStreamAt(readTs)
 	stream.LogPrefix = "Rolling up"
 	stream.ChooseKey = func(item *badger.Item) bool {
-		pk := x.Parse(item.Key())
-		if pk == nil || len(pk.Attr) == 0 {
-			return false
-		}
 		switch item.UserMeta() {
 		case posting.BitSchemaPosting, posting.BitCompletePosting, posting.BitEmptyPosting:
-			if amLeader {
-				// Only leader needs to calculate the tablet sizes.
-				addTo(pk.Attr, item.EstimatedSize())
-			}
+			addTo(item.Key(), item.EstimatedSize())
 			return false
 		default:
 			return true
@@ -1030,13 +1031,8 @@ func (n *node) rollupLists(readTs uint64) error {
 		// If there are multiple keys, the posting list was split into multiple
 		// parts. The key of the first part is the right key to use for tablet
 		// size calculations.
-		if amLeader && len(kvs) > 0 {
-			pk := x.Parse(kvs[0].Key)
-			if pk != nil && len(pk.Attr) > 0 {
-				for _, kv := range kvs {
-					addTo(pk.Attr, int64(kv.Size()))
-				}
-			}
+		for _, kv := range kvs {
+			addTo(kvs[0].Key, int64(kv.Size()))
 		}
 
 		return &bpb.KVList{Kv: kvs}, err
@@ -1215,6 +1211,9 @@ func (n *node) calculateSnapshot(discardN int) (*pb.Snapshot, error) {
 		return nil, err
 	}
 
+	if num := posting.Oracle().NumPendingTxns(); num > 0 {
+		glog.Infof("Num pending txns: %d", num)
+	}
 	// We can't rely upon the Raft entries to determine the minPendingStart,
 	// because there are many cases during mutations where we don't commit or
 	// abort the transaction. This might happen due to an early error thrown.
