95ad4c1dd261ff6375e7f116e8fb90c702efba27
Daniel Mai <daniel@dgraph.io>
2019-05-06 17:40:22 -0700

    Update to the badger version vendored in release/v1.0.

--- vendor/github.com/dgraph-io/badger/batch.go
+++ vendor/github.com/dgraph-io/badger/batch.go
@@ -36,14 +36,7 @@ type WriteBatch struct {
 // creating and committing transactions. Due to the nature of SSI guaratees provided by Badger,
 // blind writes can never encounter transaction conflicts (ErrConflict).
 func (db *DB) NewWriteBatch() *WriteBatch {
-	txn := db.newTransaction(true, true)
-	// If we let it stay at zero, compactions would not allow older key versions to be deleted,
-	// because the read timestamps of pending txns, would be zero. Therefore, we set it to the
-	// maximum read timestamp that's done. This allows compactions to discard key versions below
-	// this read timestamp, while also not blocking on pending txns to finish before starting this
-	// one.
-	txn.readTs = db.orc.readMark.DoneUntil()
-	return &WriteBatch{db: db, txn: txn}
+	return &WriteBatch{db: db, txn: db.newTransaction(true, true)}
 }
 
 // Cancel function must be called if there's a chance that Flush might not get
@@ -135,8 +128,7 @@ func (wb *WriteBatch) commit() error {
 	wb.wg.Add(1)
 	wb.txn.CommitWith(wb.callback)
 	wb.txn = wb.db.newTransaction(true, true)
-	// See comment about readTs in NewWriteBatch.
-	wb.txn.readTs = wb.db.orc.readMark.DoneUntil()
+	wb.txn.readTs = 0 // We're not reading anything.
 	return wb.err
 }
 
