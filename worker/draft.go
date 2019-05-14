ba0791df108c5ef9ef23c780800bdddf77adfea9
Manish R Jain <manish@dgraph.io>
2019-05-14 16:09:33 -0700

    Call Sync explicitly to improve performance (#3415)
    Instead of setting SyncWrites=true, we should call db.Sync() at the end of TxnWriter
    or when raft.Ready().MustSync is set to true. This is expected to be faster than when
    SyncWrites is blanket set to true without any change in data consistency behavior.

--- worker/draft.go
+++ worker/draft.go
@@ -891,13 +891,19 @@ func (n *node) Run() {
 
 			// Store the hardstate and entries. Note that these are not CommittedEntries.
 			n.SaveToStorage(rd.HardState, rd.Entries, rd.Snapshot)
+			timer.Record("disk")
+			if rd.MustSync {
+				if err := n.Store.Sync(); err != nil {
+					glog.Errorf("Error while calling Store.Sync: %v", err)
+				}
+				timer.Record("sync")
+			}
 			if span != nil {
 				span.Annotatef(nil, "Saved %d entries. Snapshot, HardState empty? (%v, %v)",
 					len(rd.Entries),
 					raft.IsEmptySnap(rd.Snapshot),
 					raft.IsEmptyHardState(rd.HardState))
 			}
-			timer.Record("disk")
 
 			// Now schedule or apply committed entries.
 			var proposals []*pb.Proposal
