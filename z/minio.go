14fa8274499b01602343a90a726a7744db691fd5
Martin Martinez Rivera <martinmr@users.noreply.github.com>
2019-05-24 10:06:09 -0700

    Add basic test utils for creating minio clients. (#3459)

---
+++ z/minio.go
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2019 Dgraph Labs, Inc. and Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package z
+
+import (
+	"github.com/minio/minio-go"
+)
+
+var (
+	accessKey     = "accesskey"
+	secretKey     = "secretkey"
+	minioEndpoint = "localhost:9001"
+)
+
+// NewMinioClient returns a minio client.
+func NewMinioClient() (*minio.Client, error) {
+	return minio.New(minioEndpoint, accessKey, secretKey, false)
+}
