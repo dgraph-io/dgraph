d048d5c4b009cff63e25fb8807747cf13d4ce037
Lucas Wang <lucas@dgraph.io>
2019-05-31 15:59:22 -0700

    Bring in latest changes from badger and fix broken API calls (#3502)

--- dgraph/cmd/bulk/reduce.go
+++ dgraph/cmd/bulk/reduce.go
@@ -20,6 +20,8 @@ import (
 	"bytes"
 	"sync/atomic"
 
+	"github.com/dgraph-io/badger"
+
 	"github.com/dgraph-io/dgraph/codec"
 	"github.com/dgraph-io/dgraph/posting"
 	"github.com/dgraph-io/dgraph/protos/pb"
@@ -67,7 +69,11 @@ func (r *reducer) reduce(job shuffleOutput) {
 		pl.Pack = codec.Encode(uids, 256)
 		val, err := pl.Marshal()
 		x.Check(err)
-		x.Check(txn.SetWithMeta(currentKey, val, meta))
+		x.Check(txn.SetEntry(&badger.Entry{
+			Key:      currentKey,
+			Value:    val,
+			UserMeta: meta,
+		}))
 
 		uids = uids[:0]
 		pl.Reset()
--- vendor/github.com/dgraph-io/badger/structs.go
+++ vendor/github.com/dgraph-io/badger/structs.go
@@ -5,6 +5,7 @@ import (
 	"encoding/binary"
 	"fmt"
 	"hash/crc32"
+	"time"
 
 	"github.com/dgraph-io/badger/y"
 )
@@ -130,4 +131,49 @@ func encodeEntry(e *Entry, buf *bytes.Buffer) (int, error) {
 func (e Entry) print(prefix string) {
 	fmt.Printf("%s Key: %s Meta: %d UserMeta: %d Offset: %d len(val)=%d",
 		prefix, e.Key, e.meta, e.UserMeta, e.offset, len(e.Value))
+}
+
+// NewEntry creates a new entry with key and value passed in args. This newly created entry can be
+// set in a transaction by calling txn.SetEntry(). All other properties of Entry can be set by
+// calling WithMeta, WithDiscard, WithTTL methods on it.
+// This function uses key and value reference, hence users must
+// not modify key and value until the end of transaction.
+func NewEntry(key, value []byte) *Entry {
+	return &Entry{
+		Key:   key,
+		Value: value,
+	}
+}
+
+// WithMeta adds meta data to Entry e. This byte is stored alongside the key
+// and can be used as an aid to interpret the value or store other contextual
+// bits corresponding to the key-value pair of entry.
+func (e *Entry) WithMeta(meta byte) *Entry {
+	e.UserMeta = meta
+	return e
+}
+
+// WithDiscard adds a marker to Entry e. This means all the previous versions of the key (of the
+// Entry) will be eligible for garbage collection.
+// This method is only useful if you have set a higher limit for options.NumVersionsToKeep. The
+// default setting is 1, in which case, this function doesn't add any more benefit. If however, you
+// have a higher setting for NumVersionsToKeep (in Dgraph, we set it to infinity), you can use this
+// method to indicate that all the older versions can be discarded and removed during compactions.
+func (e *Entry) WithDiscard() *Entry {
+	e.meta = bitDiscardEarlierVersions
+	return e
+}
+
+// WithTTL adds time to live duration to Entry e. Entry stored with a TTL would automatically expire
+// after the time has elapsed, and will be eligible for garbage collection.
+func (e *Entry) WithTTL(dur time.Duration) *Entry {
+	e.ExpiresAt = uint64(time.Now().Add(dur).Unix())
+	return e
+}
+
+// withMergeBit sets merge bit in entry's metadata. This
+// function is called by MergeOperator's Add method.
+func (e *Entry) withMergeBit() *Entry {
+	e.meta = bitMergeEntry
+	return e
 }
