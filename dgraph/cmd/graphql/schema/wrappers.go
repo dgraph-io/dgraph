61b9322409e1118db82bc016329738596730c173
MichaelJCompton <MichaelJCompton@users.noreply.github.com>
2019-06-12 16:20:50 +1000

    wrap the bits of ast.schema we need

--- dgraph/cmd/graphql/schema/wrappers.go
+++ dgraph/cmd/graphql/schema/wrappers.go
@@ -17,25 +17,61 @@
 package schema
 
 import (
+	"strings"
+
 	"github.com/vektah/gqlparser/ast"
 )
 
-// wrap the github.com/vektah/gqlparser/ast defintions so that the bulk of the GraphQL
-// algorithm and interface is dependent on behaviours not the exact structure in there.
+// Wrap the github.com/vektah/gqlparser/ast defintions so that the bulk of the GraphQL
+// algorithm and interface is dependent on behaviours we expect from a GraphQL schema
+// and validation, but not dependent the exact structure in the gqlparser.
+//
+// This also auto hooks up some bookkeeping that's otherwise no fun.  E.g. getting values for
+// field arguments requires the variable map from the operation - so we'd need to carry vars
+// through all the resolver functions.  Much nicer if they are resolved by magic here.
 
+// QueryType is current queries supported
+type QueryType string
+
+const (
+	GetQuery    QueryType = "get"
+	FilterQuery QueryType = "query"
+)
+
+// Schema is a GraphQL schema
 type Schema interface {
 	Operation(r *Request) (Operation, *Response)
 }
 
+// An Operation is a single valid GraphQL operation.  It contains either Queries or Mutations.
 type Operation interface {
 	Queries() []Query
 	Mutations() []Mutation
+	IsQuery() bool
+	IsMutation() bool
+	IsSubscription() bool
 }
 
+// A Field is one field from an Operation.
+type Field interface {
+	Name() string
+	Alias() string
+	ResponseName() string
+	ArgValue(name string) (interface{}, error)
+	TypeName() string
+	SelectionSet() []Field
+}
+
+// A Mutation is a field (from the schema's Mutation type) from an Operation
 type Mutation interface {
+	Field
+	MutatedTypeName() string
 }
 
+// A Query is a field (from the schema's Query type) from an Operation
 type Query interface {
+	Field
+	QueryType() QueryType
 }
 
 type schema struct {
@@ -47,24 +83,153 @@ type operation struct {
 	vars map[string]interface{}
 }
 
-type mutation struct {
-	schema *ast.Field
+type field struct {
+	field *ast.Field
+	op    *operation
+}
+type mutation field
+type query field
+
+func (o *operation) IsQuery() bool {
+	return o.op.Operation == ast.Query
 }
 
-type query struct {
-	schema *ast.Field
+func (o *operation) IsMutation() bool {
+	return o.op.Operation == ast.Mutation
 }
 
-func (*operation) Queries() []Query {
-	// TODO:
-	return nil
+func (o *operation) IsSubscription() bool {
+	return o.op.Operation == ast.Subscription
 }
 
-func (*operation) Mutations() []Mutation {
-	// TODO:
-	return nil
+func (o *operation) Queries() (qs []Query) {
+	if !o.IsQuery() {
+		return
+	}
+
+	for _, s := range o.op.SelectionSet {
+		if f, ok := s.(*ast.Field); ok {
+			qs = append(qs, &query{field: f, op: o})
+		}
+	}
+
+	return
 }
 
+func (o *operation) Mutations() (ms []Mutation) {
+	if !o.IsQuery() {
+		return
+	}
+
+	for _, s := range o.op.SelectionSet {
+		if f, ok := s.(*ast.Field); ok {
+			ms = append(ms, &mutation{field: f, op: o})
+		}
+	}
+
+	return
+}
+
+// AsSchema wraps a github.com/vektah/gqlparser/ast.Schema.
 func AsSchema(s *ast.Schema) Schema {
 	return &schema{schema: s}
+}
+
+func responseName(f *ast.Field) string {
+	if f.Alias == "" {
+		return f.Name
+	}
+	return f.Alias
+}
+
+func (f *field) Name() string {
+	return f.field.Name
+}
+
+func (f *field) Alias() string {
+	return f.field.Alias
+}
+
+func (f *field) ResponseName() string {
+	return responseName(f.field)
+}
+
+func (f *field) ArgValue(name string) (interface{}, error) {
+	// FIXME: cache this
+	return f.field.ArgumentMap(f.op.vars)[name], nil
+}
+
+func (f *field) TypeName() string {
+	return f.field.Definition.Type.NamedType
+}
+
+func (f *field) SelectionSet() (flds []Field) {
+	for _, s := range f.field.SelectionSet {
+		if fld, ok := s.(*ast.Field); ok {
+			flds = append(flds, &field{field: fld, op: f.op})
+		}
+	}
+
+	return
+}
+
+func (q *query) Name() string {
+	return (*field)(q).Name()
+}
+
+func (q *query) Alias() string {
+	return (*field)(q).Alias()
+}
+
+func (q *query) ArgValue(name string) (interface{}, error) {
+	return (*field)(q).ArgValue(name)
+}
+
+func (q *query) TypeName() string {
+	return (*field)(q).TypeName()
+}
+
+func (q *query) SelectionSet() []Field {
+	return (*field)(q).SelectionSet()
+}
+
+func (q *query) ResponseName() string {
+	return (*field)(q).ResponseName()
+}
+
+func (q *query) QueryType() QueryType {
+	if strings.HasPrefix(q.Name(), "get") {
+		return GetQuery
+	}
+	return FilterQuery
+}
+
+func (m *mutation) Name() string {
+	return (*field)(m).Name()
+}
+
+func (m *mutation) Alias() string {
+	return (*field)(m).Alias()
+}
+
+func (m *mutation) ArgValue(name string) (interface{}, error) {
+	return (*field)(m).ArgValue(name)
+}
+
+func (m *mutation) TypeName() string {
+	return (*field)(m).TypeName()
+}
+
+func (m *mutation) SelectionSet() []Field {
+	return (*field)(m).SelectionSet()
+}
+
+func (m *mutation) ResponseName() string {
+	return (*field)(m).ResponseName()
+}
+
+func (m *mutation) MutatedTypeName() string {
+	// Currently only supporting addT(...) mutations.
+	// This'll change.
+	return strings.TrimSuffix(strings.TrimPrefix(m.TypeName(), "Add"), "PayLoad")
 }
