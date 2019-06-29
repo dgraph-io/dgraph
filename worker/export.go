64988bff25fbe76c889579697c174acfd4fd16c3
Lucas Wang <lucas@dgraph.io>
2019-06-28 18:05:50 -0700

    Moved the separator logic into the Send function, since the KeyToList is called concurrently, and adding locks to protect the counter would be too expensive

--- worker/export.go
+++ worker/export.go
@@ -68,11 +68,10 @@ var exportFormats = map[string]exportFormat{
 }
 
 type exporter struct {
-	pl          *posting.List
-	uid         uint64
-	attr        string
-	readTs      uint64
-	dataCounter int
+	pl     *posting.List
+	uid    uint64
+	attr   string
+	readTs uint64
 }
 
 // Map from our types to RDF type. Useful when writing storage types
@@ -149,10 +148,6 @@ func escapedString(str string) string {
 
 func (e *exporter) toJSON() (*bpb.KVList, error) {
 	bp := new(bytes.Buffer)
-	if e.dataCounter != 1 {
-		fmt.Fprint(bp, ",\n")
-	}
-
 	// We could output more compact JSON at the cost of code complexity.
 	// Leaving it simple for now.
 
@@ -474,8 +469,7 @@ func export(ctx context.Context, in *pb.ExportRequest) error {
 	}
 
 	e := &exporter{
-		readTs:      in.ReadTs,
-		dataCounter: 0,
+		readTs: in.ReadTs,
 	}
 
 	stream := pstore.NewStreamAt(in.ReadTs)
@@ -534,12 +528,10 @@ func export(ctx context.Context, in *pb.ExportRequest) error {
 			return toType(pk.Attr, update)
 
 		case pk.IsData():
-			e.dataCounter += 1
 			e.pl, err = posting.ReadPostingList(key, itr)
 			if err != nil {
 				return nil, err
 			}
-
 			switch in.Format {
 			case "json":
 				return e.toJSON()
@@ -555,6 +547,18 @@ func export(ctx context.Context, in *pb.ExportRequest) error {
 		return nil, nil
 	}
 
+	hasDataBefore := false
+	var separator []byte
+	switch in.Format {
+	case "json":
+		separator = []byte(",\n")
+	case "rdf":
+		// the separator for RDF should be empty since the toRDF function already
+		// adds newline to each RDF entry
+	default:
+		glog.Fatalf("Invalid export format found: %s", in.Format)
+	}
+
 	stream.Send = func(list *bpb.KVList) error {
 		for _, kv := range list.Kv {
 			var writer *fileWriter
@@ -567,9 +571,15 @@ func export(ctx context.Context, in *pb.ExportRequest) error {
 				glog.Fatalf("Invalid data type found: %x", kv.Key)
 			}
 
+			if hasDataBefore {
+				if _, err := writer.gw.Write(separator); err != nil {
+					return err
+				}
+			}
 			if _, err := writer.gw.Write(kv.Value); err != nil {
 				return err
 			}
+			hasDataBefore = true
 		}
 		// Once all the sends are done, writers must be flushed and closed in order.
 		return nil
