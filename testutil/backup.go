44a9042f92ca7a41e697449b4b0d9e4e7583f250
Martin Martinez Rivera <martinmr@dgraph.io>
2019-08-13 12:38:17 -0700

    Fix flaky backup tests.
    The backup folders were using the timestamp up to the second but when
    the test runs with no pauses, multiple backup might get the same folder
    name. This PR changes the format to use milliseconds in the name to
    avoid collisions. Users won't be making backups that fast so the chances
    of a collision happening in real-time use is very small.

--- testutil/backup.go
+++ testutil/backup.go
@@ -95,16 +95,3 @@ func GetPValues(pdir, attr string, readTs uint64) (map[string]string, error) {
 	}
 	return values, err
 }
-
-// GetError reads the response from a backup request and returns an error if it indicates
-// that the request failed.
-func GetError(rc io.ReadCloser) error {
-	b, err := ioutil.ReadAll(rc)
-	if err != nil {
-		return errors.Wrapf(err, "while reading")
-	}
-	if bytes.Contains(b, []byte("Error")) {
-		return errors.Errorf("%s", string(b))
-	}
-	return nil
-}
