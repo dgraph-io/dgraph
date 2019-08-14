767e5be1d01caadfa90fabdf0974d474e820f7fd
Aman Mangal <aman@dgraph.io>
2019-08-14 10:02:35 +0530

    vendor in new api.proto from dgo

--- vendor/github.com/dgraph-io/dgo/client.go
+++ vendor/github.com/dgraph-io/dgo/client.go
@@ -29,19 +29,18 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-// Dgraph is a transaction aware client to a set of dgraph server instances.
+// Dgraph is a transaction aware client to a set of Dgraph server instances.
 type Dgraph struct {
 	jwtMutex sync.RWMutex
 	jwt      api.Jwt
 	dc       []api.DgraphClient
 }
 
-// NewDgraphClient creates a new Dgraph for interacting with the Dgraph store connected to in
-// conns.
-// The client can be backed by multiple connections (to the same server, or multiple servers in a
-// cluster).
+// NewDgraphClient creates a new Dgraph (client) for interacting with Alphas.
+// The client is backed by multiple connections to the same or different
+// servers in a cluster.
 //
-// A single client is thread safe for sharing with multiple go routines.
+// A single Dgraph (client) is thread safe for sharing with multiple goroutines.
 func NewDgraphClient(clients ...api.DgraphClient) *Dgraph {
 	dg := &Dgraph{
 		dc: clients,
@@ -50,6 +49,8 @@ func NewDgraphClient(clients ...api.DgraphClient) *Dgraph {
 	return dg
 }
 
+// Login logs in the current client using the provided credentials.
+// Valid for the duration the client is alive.
 func (d *Dgraph) Login(ctx context.Context, userid string, password string) error {
 	d.jwtMutex.Lock()
 	defer d.jwtMutex.Unlock()
@@ -65,6 +66,29 @@ func (d *Dgraph) Login(ctx context.Context, userid string, password string) erro
 	}
 
 	return d.jwt.Unmarshal(resp.Json)
+}
+
+// Alter can be used to do the following by setting various fields of api.Operation:
+//   1. Modify the schema.
+//   2. Drop a predicate.
+//   3. Drop the database.
+func (d *Dgraph) Alter(ctx context.Context, op *api.Operation) error {
+	dc := d.anyClient()
+
+	ctx = d.getContext(ctx)
+	_, err := dc.Alter(ctx, op)
+
+	if isJwtExpired(err) {
+		err = d.retryLogin(ctx)
+		if err != nil {
+			return err
+		}
+
+		ctx = d.getContext(ctx)
+		_, err = dc.Alter(ctx, op)
+	}
+
+	return err
 }
 
 func (d *Dgraph) retryLogin(ctx context.Context) error {
@@ -83,18 +107,21 @@ func (d *Dgraph) retryLogin(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
+
 	return d.jwt.Unmarshal(resp.Json)
 }
 
 func (d *Dgraph) getContext(ctx context.Context) context.Context {
 	d.jwtMutex.RLock()
 	defer d.jwtMutex.RUnlock()
+
 	if len(d.jwt.AccessJwt) > 0 {
 		md, ok := metadata.FromOutgoingContext(ctx)
 		if !ok {
 			// no metadata key is in the context, add one
 			md = metadata.New(nil)
 		}
+
 		md.Set("accessJwt", d.jwt.AccessJwt)
 		return metadata.NewOutgoingContext(ctx, md)
 	}
@@ -102,33 +129,7 @@ func (d *Dgraph) getContext(ctx context.Context) context.Context {
 	return ctx
 }
 
-// By setting various fields of api.Operation, Alter can be used to do the
-// following:
-//
-// 1. Modify the schema.
-//
-// 2. Drop a predicate.
-//
-// 3. Drop the database.
-func (d *Dgraph) Alter(ctx context.Context, op *api.Operation) error {
-	dc := d.anyClient()
-
-	ctx = d.getContext(ctx)
-	_, err := dc.Alter(ctx, op)
-
-	if isJwtExpired(err) {
-		err = d.retryLogin(ctx)
-		if err != nil {
-			return err
-		}
-		ctx = d.getContext(ctx)
-		_, err = dc.Alter(ctx, op)
-	}
-
-	return err
-}
-
-// isJwtExpired returns true if the error indicates that the jwt has expired
+// isJwtExpired returns true if the error indicates that the jwt has expired.
 func isJwtExpired(err error) bool {
 	if err == nil {
 		return false
@@ -143,10 +144,10 @@ func (d *Dgraph) anyClient() api.DgraphClient {
 	return d.dc[rand.Intn(len(d.dc))]
 }
 
-// DeleteEdges sets the edges corresponding to predicates on the node with the given uid
-// for deletion.
-// This helper function doesn't run the mutation on the server. It must be done by the user
-// after the function returns.
+// DeleteEdges sets the edges corresponding to predicates
+// on the node with the given uid for deletion.
+// This helper function doesn't run the mutation on the server.
+// It must be done by the user after the function returns.
 func DeleteEdges(mu *api.Mutation, uid string, predicates ...string) {
 	for _, predicate := range predicates {
 		mu.Del = append(mu.Del, &api.NQuad{
