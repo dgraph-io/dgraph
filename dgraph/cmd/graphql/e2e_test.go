e08671668a6c6183bdcf3797863af344f3ac1f6c
Karthic Rao <kartronics85@gmail.com>
2019-08-14 19:20:22 +0530

    Adding more cases and fixing the compose path issue
    - Created test for validating the mutation response.
    - Fixed the path issues found with the init GraphQL server.

--- dgraph/cmd/graphql/e2e_test.go
+++ dgraph/cmd/graphql/e2e_test.go
@@ -19,8 +19,10 @@ package graphql
 import (
 	"bytes"
 	"encoding/json"
-	"net/http"
+	//"fmt"
 	"io/ioutil"
+	//"log"
+	"net/http"
 	"strings"
 	"testing"
 
@@ -28,18 +30,42 @@ import (
 )
 
 /*
-The docker-compose config  in dgraph/docker-compose.yml runs the GrapQL from container.
+The docker-compose config  in dgraph/docker-compose.yml intializes and runs the GraphQL server.
 
 Check the compose file to find the graphql schema file used to initialize the server.
-
-These tests follow table driven format.
-
-Check table driven test examples from https://dave.cheney.net/2019/05/07/prefer-table-driven-tests
 */
 
-type params struct {
+// Parameters for the constructing http post body of a graphql query.
+type graphqlParams struct {
 	Query     string                       `json:"query"`
 	Variables map[string]map[string]string `json:"variables"`
+}
+
+// GraphQL types.
+// Data structures below are used to match the response from the GraphQL server.
+// These structs match the types present and generated in the GraphQL schema
+// which is used to initialize the GraphQL server.
+
+// Type corresponding to the Message type from the GraphQL schema.
+// check dgraph/docker-compose.yml to find the schema file used for tests.
+type message struct {
+	ID         string `json:"id"`
+	Content    string `json:"content"`
+	Author     string `json:"author"`
+	DatePosted string `json:"datePosted"`
+}
+
+type addMessage struct {
+	Messages []message `json:"message"`
+}
+
+type data struct {
+	AddMessage addMessage `json:"addMessage"`
+}
+
+// Data structure respresenting the json response for the auto generated mutation type.
+type MessageMutation struct {
+	Data data `json:"data"`
 }
 
 var graphqlURL = "http://localhost:9000/graphql"
@@ -55,7 +81,21 @@ const (
 
 // Graphql queries/mutations to be used in test cases.
 var queries = []string{
+	// Query 1.
+	// Valid Graphql mutation for the test schema.
 	`mutation addMessage($message: MessageInput!) { 
+		addMessage(input: $message) {
+			message {
+				id 
+				content 
+				author 
+				datePosted
+			}
+		}
+	}`,
+	// Query 2.
+	// Invalid Graphql mutation for the test schema.
+	`mutation additionMessages($message: MessageInput!) { 
 		addMessage(input: $message) {
 			message {
 				id 
@@ -79,25 +119,80 @@ func toSingleLine(graphqlBody string) string {
 
 // Runs mutations derived from the test schema.
 // Verifies the response.
+// The Graphql layer is initialized with the test schema in the docker-compose file.
 func TestGraphQLMutation(t *testing.T) {
 
 	type testData struct {
+		// Simple name for the test case.
+		// For example [Simple mutations 1, Simple mutations 2, wrong mutation body]\
+		name string
 		// POST data for the GraphQL request.
-		graphqlReq params
+		graphqlReq graphqlParams
 		// GraphQL mutation or Query
 		queryType graphqlQueryType
 		// Short Description of the test case.
 		explain string
 		// expected http response status code.
 		wantCode int
+		// expected Result.
+		wantResult MessageMutation
+	}
+
+	// Expected data from the mutations.
+	// Will be used to validate the mutation responses.
+	expectedData := []MessageMutation{
+		// Expected result 1.
+		MessageMutation{
+			Data: data{
+				AddMessage: addMessage{
+					Messages: []message{
+						message{
+							Content:    "Unit test post",
+							Author:     "Karthic Rao",
+							DatePosted: "2019-07-07T00:00:00Z",
+						},
+					},
+				},
+			},
+		},
+		// Expected result 2.
+		MessageMutation{
+			Data: data{
+				AddMessage: addMessage{
+					Messages: []message{
+						message{
+							Content:    "Unit test second post",
+							Author:     "Michael Compton",
+							DatePosted: "2019-08-08T05:04:33Z",
+						},
+					},
+				},
+			},
+		},
+		// Expected result 3.
+		MessageMutation{
+			Data: data{
+				AddMessage: addMessage{
+					Messages: []message{
+						message{
+							Content:    "Invalid mutation",
+							Author:     "Prashant Shahi",
+							DatePosted: "2019-08-08T05:04:33Z",
+						},
+					},
+				},
+			},
+		},
 	}
 
 	// The following image shows an example HTTP POST request body for a graphql query/mutation.
 	// https://user-images.githubusercontent.com/2609511/62793240-b731fc80-baee-11e9-915f-2c19f0396229.png
 	// In the testData we will construct different Graphql queries and mutations.
 	tests := []testData{
+		// Test case 1
 		{
-			graphqlReq: params{
+			name: "Simple mutation 1",
+			graphqlReq: graphqlParams{
 				Query: toSingleLine(queries[0]),
 				Variables: map[string]map[string]string{
 					"message": map[string]string{
@@ -107,20 +202,63 @@ func TestGraphQLMutation(t *testing.T) {
 					},
 				},
 			},
-			queryType: QUERY,
-			explain:   "Add data with Mutation and verify return data",
-			wantCode:  200,
+			queryType:  QUERY,
+			explain:    "Send Mutation to the GraphQL server and verify response data",
+			wantCode:   200,
+			wantResult: expectedData[0],
+		},
+		// Test case 2.
+		{
 
+			name: "Simple mutation 2",
+			graphqlReq: graphqlParams{
+				Query: toSingleLine(queries[0]),
+				Variables: map[string]map[string]string{
+					"message": map[string]string{
+						"content":    "Unit test second post",
+						"author":     "Michael Compton",
+						"datePosted": "2019-08-08T05:04:33Z",
+					},
+				},
+			},
+			queryType: QUERY,
+			explain: "Add data with Mutation and verify return data." +
+				" Time is set to 2019-08-08T00:00:00Z format",
+			wantCode:   200,
+			wantResult: expectedData[1],
+		},
+		// Test case 3.
+		{
+
+			name: "Invalid Mutation function name",
+			graphqlReq: graphqlParams{
+				Query: toSingleLine(queries[1]),
+				Variables: map[string]map[string]string{
+					"message": map[string]string{
+						"content":    "Invalid mutation",
+						"author":     "Prashant Shahi",
+						"datePosted": "2019-08-08T05:04:33Z",
+					},
+				},
+			},
+			queryType: QUERY,
+			explain: "The mutation name is invalid. Will be checking for " +
+				"valid error response when the mutation function doesn't of given name doesn't exist",
+			wantCode:   200,
+			wantResult: expectedData[2],
 		},
 	}
 
+	// Iterate through each test case and validate the response.
 	for _, test := range tests {
-		t.Run(test.explain, func(t *testing.T) {
+		t.Run(test.name, func(t *testing.T) {
+			// Construct the post request body for the GraphQL request.
 			d2, err := json.Marshal(test.graphqlReq)
 
 			require.NoError(t, err)
 
 			buf := bytes.NewBuffer(d2)
+			// Send the request to the GraphQL server running at "graphqlURL".
 			req, err := http.NewRequest("POST", graphqlURL, buf)
 
 			require.NoError(t, err)
@@ -128,14 +266,35 @@ func TestGraphQLMutation(t *testing.T) {
 			req.Header.Set("Content-Type", "application/json")
 			client := &http.Client{}
 			resp, err := client.Do(req)
+
+			require.NoError(t, err)
+
 			bodyBytes, err := ioutil.ReadAll(resp.Body)
 
 			require.NoError(t, err)
 
+			//log.Println(string(bodyBytes))
+
+			var mutationResponse MessageMutation
+			err = json.Unmarshal(bodyBytes, &mutationResponse)
+
 			require.NoError(t, err)
+			//fmt.Printf("%+v", mutationResponse)
+
+			// Verify the response status code.
 			require.Equal(t, test.wantCode, resp.StatusCode)
+			// Verify that the response data exist.
+			require.Equal(t, len(mutationResponse.Data.AddMessage.Messages), 1)
+			// Verify that the GraphQL mutation has returned UID of the node from Dgraph.
+			require.NotEmpty(t, mutationResponse.Data.AddMessage.Messages[0].ID)
 
+			// Since the node ID is generated after the mutation,
+			// We cannot have it before hand in the expected data.
+			// We fetch it from the response json of the mutation.
+			test.wantResult.Data.AddMessage.Messages[0].ID = mutationResponse.Data.AddMessage.Messages[0].ID
+
+			// Match and verify the mutation response from the GraphQL server.
+			require.Equal(t, test.wantResult, mutationResponse)
 		})
-
 	}
 }
