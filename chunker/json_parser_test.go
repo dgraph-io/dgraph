08a949817aaf98447265e1661fa190d3c632c597
Martin Martinez Rivera <martinmr@dgraph.io>
2019-08-15 14:11:16 -0700

    Fix ineffassign warnings.

--- chunker/json_parser_test.go
+++ chunker/json_parser_test.go
@@ -199,6 +199,7 @@ func TestNquadsFromJson3(t *testing.T) {
 	b, err := json.Marshal(p)
 	require.NoError(t, err)
 	nq, err := Parse(b, SetNquads)
+	require.NoError(t, err)
 	outputNq(nq)
 	exp := &Experiment{
 		t:      t,
