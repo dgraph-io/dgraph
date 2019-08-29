8d88d3a052f62f86bfbd75b61a29629e3cac3926
Pawan Rawal <pawan0201@gmail.com>
2019-08-29 14:16:51 +1000

    Read output from a file in the test

--- dgraph/cmd/graphql/schema/introspection_test.go
+++ dgraph/cmd/graphql/schema/introspection_test.go
@@ -220,6 +220,7 @@ func TestIntrospectioNQuery_full(t *testing.T) {
 	resp, err := Introspect(ctx, oper, AsSchema(sch))
 	require.NoError(t, err)
 
-	expected := `{"__schema":{"queryType":{"name":"TestType"},"mutationType":null,"subscriptionType":null,"types":[{"kind":"SCALAR","name":"Float","fields":[],"inputFields":[],"interfaces":[],"enumValues":[],"possibleTypes":[]},{"kind":"OBJECT","name":"TestType","fields":[{"name":"testField","args":[],"type":{"kind":"SCALAR","name":"String","ofType":null},"isDeprecated":false,"deprecationReason":null}],"inputFields":[],"interfaces":[],"enumValues":[],"possibleTypes":[]},{"kind":"SCALAR","name":"ID","fields":[],"inputFields":[],"interfaces":[],"enumValues":[],"possibleTypes":[]},{"kind":"SCALAR","name":"String","fields":[],"inputFields":[],"interfaces":[],"enumValues":[],"possibleTypes":[]},{"kind":"SCALAR","name":"Boolean","fields":[],"inputFields":[],"interfaces":[],"enumValues":[],"possibleTypes":[]},{"kind":"SCALAR","name":"Int","fields":[],"inputFields":[],"interfaces":[],"enumValues":[],"possibleTypes":[]}],"directives":[{"name":"include","locations":["FIELD","FRAGMENT_SPREAD","INLINE_FRAGMENT"],"args":[{"name":"if","type":{"kind":"NON_NULL","name":null,"ofType":{"kind":"SCALAR","name":"Boolean","ofType":null}},"defaultValue":null}]},{"name":"skip","locations":["FIELD","FRAGMENT_SPREAD","INLINE_FRAGMENT"],"args":[{"name":"if","type":{"kind":"NON_NULL","name":null,"ofType":{"kind":"SCALAR","name":"Boolean","ofType":null}},"defaultValue":null}]},{"name":"deprecated","locations":["FIELD_DEFINITION","ENUM_VALUE"],"args":[{"name":"reason","type":{"kind":"SCALAR","name":"String","ofType":null},"defaultValue":"\"No longer supported\""}]}]}}`
-	testutil.CompareJSON(t, string(expected), string(resp))
+	expectedBuf, err := ioutil.ReadFile("testdata/output/introspection_full_query.json")
+	require.NoError(t, err)
+	testutil.CompareJSON(t, string(expectedBuf), string(resp))
 }
