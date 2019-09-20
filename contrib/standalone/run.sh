effb7b2bd60178f387e014d909d88e594db73644
Daniel Mai <daniel@dgraph.io>
2019-09-19 17:10:22 -0700

    Add TODO for SIGTERM handling.

--- contrib/standalone/run.sh
+++ contrib/standalone/run.sh
@@ -6,4 +6,5 @@ set -e
 lru_kb=$(cat /proc/meminfo | grep MemTotal | sed "s/.* \([0-9]*\) .*/\1/")
 lru_mb=$(expr $lru_kb / 1024)
 echo "running alpha with LRU size of $lru_mb MB"
+# TODO properly handle SIGTERM for all three processes.
 dgraph-ratel & dgraph zero & dgraph alpha --lru_mb $lru_mb
