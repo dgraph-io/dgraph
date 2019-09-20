9e69f29483ba85ace1d5a02c205f0bf3f1449040
Daniel Mai <daniel@dgraph.io>
2019-09-20 11:30:43 -0700

    Set one-third of host memory for lru_mb.

--- contrib/standalone/run.sh
+++ contrib/standalone/run.sh
@@ -4,7 +4,7 @@
 set -e
 
 lru_kb=$(cat /proc/meminfo | grep MemTotal | sed "s/.* \([0-9]*\) .*/\1/")
-lru_mb=$(expr $lru_kb / 1024)
+lru_mb=$(expr $lru_kb / 1024 / 3) # one-third of host memory
 
 echo -e "\033[0;33m
 Warning: This standalone version is meant for quickstart purposes only.
