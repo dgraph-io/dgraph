bab5a3d1b8b91997b5798dd5de6006a3ac8238b3
Daniel Mai <daniel@dgraph.io>
2019-09-20 11:07:10 -0700

    Add warning message to log output.

--- contrib/standalone/run.sh
+++ contrib/standalone/run.sh
@@ -6,5 +6,9 @@ set -e
 lru_kb=$(cat /proc/meminfo | grep MemTotal | sed "s/.* \([0-9]*\) .*/\1/")
 lru_mb=$(expr $lru_kb / 1024)
 echo "running alpha with LRU size of $lru_mb MB"
+echo ""
+echo "The standalone version is meant for quickstart purposes only. It is NOT
+RECOMMENDED for production environments."
+echo ""
 # TODO properly handle SIGTERM for all three processes.
 dgraph-ratel & dgraph zero & dgraph alpha --lru_mb $lru_mb
