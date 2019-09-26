dd95df72cac23549c5687802eadf0ab777477de0
Animesh <animesh@dgraph.io>
2019-09-26 17:43:56 +0530

    Minimal implementation of validator

--- dgraph/cmd/validator/run.go
+++ dgraph/cmd/validator/run.go
@@ -1,19 +1,31 @@
 package validator
 
 import (
+	"bytes"
 	"fmt"
-	"math"
+	"io"
 	"os"
-	"runtime"
 	"strings"
+	"sync"
 
+	"github.com/dgraph-io/dgraph/chunker"
+	"github.com/dgraph-io/dgraph/lex"
 	"github.com/dgraph-io/dgraph/x"
+	"github.com/golang/glog"
 	"github.com/spf13/cobra"
 )
 
 // Validator is the sub-command for validating input files which can later be
 // used with bulk/live loader to insert data into dgraph.
 var Validator x.SubCommand
+
+type options struct {
+	DataFiles     string
+	TmpDir        string
+	NumGoroutines int
+	CleanupTmp    bool
+	DataFormat    string
+}
 
 func init() {
 	Validator.Cmd = &cobra.Command{
@@ -29,8 +41,6 @@ func init() {
 	flag := Validator.Cmd.Flags()
 	flag.StringP("files", "f", "",
 		"Location of *.rdf(.gz) or *.json(.gz) files(s) to validate.")
-	flag.IntP("num_go_routines", "j", int(math.Ceil(float64(runtime.NumCPU())/4.0)),
-		"Number of worker threads to use. MORE THREADS LEAD TO HIGHER RAM USAGE.")
 	flag.String("format", "",
 		"Specify file format (rdf or json) instead of getting it from filename.")
 }
@@ -56,5 +66,79 @@ func run() {
 		}
 	}
 
-	newLoader(opt).mapStage()
+	allDataFiles := getFiles(opt.DataFiles)
+	loaderType := getLoaderType(allDataFiles[0], opt.DataFormat)
+
+	var validatorWg sync.WaitGroup
+	validatorWg.Add(len(allDataFiles))
+	for _, file := range allDataFiles {
+		go validateDataFile(file, loaderType, &validatorWg)
+	}
+
+	validatorWg.Wait()
+}
+
+func validateDataFile(file string, loadType chunker.InputFormat, validatorWg *sync.WaitGroup) {
+	defer validatorWg.Done()
+	glog.Infof("Processing file %s\n", file)
+
+	fReader, cleanup := chunker.FileReader(file)
+	defer cleanup()
+
+	fChunker := chunker.NewChunker(loadType, 1000)
+	x.Check(fChunker.Begin(fReader))
+	for {
+		chunkBuf, err := fChunker.Chunk(fReader)
+		if chunkBuf != nil && chunkBuf.Len() > 0 {
+			parseChunkbuf(fChunker, chunkBuf, file)
+		}
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			x.Check(err) // Most likely the specified file format is incorrect.
+		}
+	}
+	x.Check(fChunker.End(fReader))
+}
+
+func parseChunkbuf(fChunker chunker.Chunker, chunkBuf *bytes.Buffer, file string) {
+	if chunkBuf == nil || chunkBuf.Len() == 0 {
+		return
+	}
+
+	lexer := &lex.Lexer{}
+
+	for chunkBuf.Len() > 0 {
+		str, err := chunkBuf.ReadString('\n')
+		if err != nil && err != io.EOF {
+			x.Check(err)
+		}
+
+		_, err = chunker.ParseRDF(str, lexer)
+		if err == chunker.ErrEmpty {
+			continue // blank line or comment
+		} else if err != nil {
+			glog.Errorf("Error found in file %s: %s while parsing line %q\n",
+				file, err, str)
+		}
+	}
+}
+
+func getFiles(dataFiles string) []string {
+	files := x.FindDataFiles(dataFiles, []string{".rdf", ".rdf.gz", ".json", ".json.gz"})
+	if len(files) == 0 {
+		x.Fatalf("No data files found in %s\n", dataFiles)
+	}
+
+	return files
+}
+
+func getLoaderType(file, dataFormat string) chunker.InputFormat {
+	loaderType := chunker.DataFormat(file, dataFormat)
+	if loaderType == chunker.UnknownFormat {
+		// Dont't try to detect JSON input in bulk loader.
+		x.Fatalf("Need --format=rdf or --format=json to load %s\n", file)
+	}
+
+	return loaderType
 }
