094549ad38864839ce7a8517862de7a15d83edc0
MichaelJCompton <MichaelJCompton@users.noreply.github.com>
2019-10-10 13:48:08 +1100

    build the mutation and query resolvers

--- dgraph/cmd/graphql/resolve/query.go
+++ dgraph/cmd/graphql/resolve/query.go
@@ -23,73 +23,54 @@ import (
 	otrace "go.opencensus.io/trace"
 
 	"github.com/dgraph-io/dgraph/dgraph/cmd/graphql/api"
-	"github.com/dgraph-io/dgraph/dgraph/cmd/graphql/dgraph"
 	"github.com/dgraph-io/dgraph/dgraph/cmd/graphql/schema"
+	"github.com/dgraph-io/dgraph/gql"
 	"github.com/dgraph-io/dgraph/x"
 )
 
-// queryResolver can resolve a single GraphQL query field
+// a queryResolver can resolve a single GraphQL query field.
 type queryResolver struct {
-	query         schema.Query
-	schema        schema.Schema
-	dgraph        dgraph.Client
-	queryRewriter dgraph.QueryRewriter
-	operation     schema.Operation
+	query           schema.Query
+	queryRewriter   QueryRewriter
+	queryExecutor   QueryExecutor
+	resultCompleter ResultCompleter
 }
 
-// resolve a query.
-func (qr *queryResolver) resolve(ctx context.Context) *resolved {
-	res := &resolved{}
+// a schemaIntrospector is the QueryExecutor function of schema introspection.
+type schemaIntrospector struct {
+	query     schema.Query
+	schema    schema.Schema
+	operation schema.Operation
+}
+
+func (qr *queryResolver) Resolve(ctx context.Context) (*Resolved, bool) {
 
 	span := otrace.FromContext(ctx)
 	stop := x.SpanTimer(span, "resolveQuery")
 	defer stop()
 
-	if qr.query.QueryType() == schema.SchemaQuery {
-		resp, err := schema.Introspect(qr.operation, qr.query, qr.schema)
-		if err != nil {
-			res.err = err
-			return res
-		}
-		// This is because Introspect returns an object.
-		if len(resp) >= 2 {
-			res.data = resp[1 : len(resp)-1]
-		}
-		return res
-	}
-
 	dgQuery, err := qr.queryRewriter.Rewrite(qr.query)
 	if err != nil {
-		res.err = schema.GQLWrapf(err, "couldn't rewrite query %s", qr.query.ResponseName())
-		return res
+		return &Resolved{
+			Err: schema.GQLWrapf(err, "couldn't rewrite query %s", qr.query.ResponseName()),
+		}, resolverFailed
 	}
 
-	resp, err := qr.dgraph.Query(ctx, dgQuery)
+	resp, err := qr.queryExecutor.Query(ctx, dgQuery)
 	if err != nil {
 		glog.Infof("[%s] Dgraph query failed : %s", api.RequestID(ctx), err)
-		res.err = schema.GQLWrapf(err, "[%s] failed to resolve query", api.RequestID(ctx))
-		return res
+		return &Resolved{
+			Err: schema.GQLWrapf(err, "[%s] failed to resolve query", api.RequestID(ctx)),
+		}, resolverFailed
 	}
 
-	completed, err := completeDgraphResult(ctx, qr.query, resp)
-	res.err = err
+	completed, err := qr.resultCompleter.Complete(ctx, qr.query, resp)
+	return &Resolved{
+		Data: completed,
+		Err:  err,
+	}, resolverSucceeded
+}
 
-	// chop leading '{' and trailing '}' from JSON object
-	//
-	// The final GraphQL result gets built like
-	// { data:
-	//    {
-	//      q1: {...},
-	//      q2: [ {...}, {...} ],
-	//      ...
-	//    }
-	// }
-	// Here we are building a single one of the q's, so the fully resolved
-	// result should be q1: {...}, rather than {q1: {...}}.
-	//
-	// completeDgraphResult() always returns a valid JSON object.  At least
-	// { q: null }
-	// even in error cases, so this is safe.
-	res.data = completed[1 : len(completed)-1]
-	return res
+func (si *schemaIntrospector) Query(ctx context.Context, query *gql.GraphQuery) ([]byte, error) {
+	return schema.Introspect(si.operation, si.query, si.schema)
 }
