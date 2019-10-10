f34084baac8b6c5e345743f30cc8641d99ac1542
MichaelJCompton <MichaelJCompton@users.noreply.github.com>
2019-10-10 13:47:02 +1100

    make a query executor type

--- dgraph/cmd/graphql/dgraph/dgo.go
+++ dgraph/cmd/graphql/dgraph/dgo.go
@@ -18,7 +18,6 @@ package dgraph
 
 import (
 	"context"
-	"encoding/json"
 
 	"github.com/golang/glog"
 	"go.opencensus.io/trace"
@@ -40,10 +39,14 @@ import (
 // GraphQuery to alpha, so we don't have to stringify -> reparse etc.  Also
 // allows exercising some of the particulars around GraphQL error processing
 // without needing a Dgraph instance the reproduces the exact error conditions.
+//
+// A Dgraph client is an implementation of both the resolve.QueryExecutor and the
+// resolve.MutationExecutor interfaces.
 type Client interface {
 	Query(ctx context.Context, query *gql.GraphQuery) ([]byte, error)
-	Mutate(ctx context.Context, val interface{}) (map[string]string, error)
-	DeleteNodes(ctx context.Context, query, mutation string) error
+	Mutate(ctx context.Context,
+		query *gql.GraphQuery,
+		mutations []*dgoapi.Mutation) (map[string]string, map[string][]string, error)
 }
 
 type dgraph struct {
@@ -83,53 +86,30 @@ func (dg *dgraph) Query(ctx context.Context, query *gql.GraphQuery) ([]byte, err
 	return resp.GetJson(), schema.GQLWrapf(err, "Dgraph query failed")
 }
 
-func (dg *dgraph) Mutate(ctx context.Context, val interface{}) (map[string]string, error) {
+func (dg *dgraph) Mutate(
+	ctx context.Context,
+	query *gql.GraphQuery,
+	mutations []*dgoapi.Mutation) (map[string]string, map[string][]string, error) {
+
 	span := trace.FromContext(ctx)
 	stop := x.SpanTimer(span, "dgraph.Mutate")
 	defer stop()
 
-	jsonMu, err := json.Marshal(val)
-	if err != nil {
-		return nil, schema.GQLWrapf(err, "couldn't marshal mutation")
-	}
+	// if it's got just a query, then it's an error ???
+	// ...this indicates a bug...
 
 	if glog.V(3) {
-		glog.Infof("[%s] Executing Dgraph mutation: \n%s\n", api.RequestID(ctx), jsonMu)
-	}
+		// FIXME: ...write out the whole thing
 
-	mu := &dgoapi.Mutation{
-		CommitNow: true,
-		SetJson:   jsonMu,
-	}
-
-	resp, err := dg.client.NewTxn().Mutate(ctx, mu)
-	return resp.GetUids(), schema.GQLWrapf(err, "couldn't execute mutation")
-}
-
-// DeleteNodes deletes nodes from the graph based on the result of the filter query.
-func (dg *dgraph) DeleteNodes(ctx context.Context, query, mutation string) error {
-	// TODO: Note this simple cut that just removes it's outgoing edges.
-	// If we are to do referential integrity or ensuring the type constraints
-	// on the nodes, or cascading deletes, then a whole bunch more needs to be
-	// done on deletion.
-	//
-	// This is safe though in the sense that once we have proper error propagation
-	// we'd remove the node and if that caused any errors in the graph, that
-	// would be picked up and handled as GraphQL errors in future queries, etc.
-
-	if glog.V(3) {
-		glog.Infof("[%s] Executing Dgraph upsert : \n%s\nwith delete - %s\n",
-			api.RequestID(ctx), query, mutation)
+		// glog.Infof("[%s] Executing Dgraph upsert : \n%s\nwith set - %s\n",
+		// 	api.RequestID(ctx), query, string(b))
 	}
 
 	req := &dgoapi.Request{
-		Query:     query,
+		Query:     asString(query),
 		CommitNow: true,
-		Mutations: []*dgoapi.Mutation{{
-			DelNquads: []byte(mutation),
-		}},
+		Mutations: mutations,
 	}
-
-	_, err := dg.client.NewTxn().Do(ctx, req)
-	return err
+	resp, err := dg.client.NewTxn().Do(ctx, req)
+	return resp.GetUids(), nil, schema.GQLWrapf(err, "Dgraph mutation failed")
 }
