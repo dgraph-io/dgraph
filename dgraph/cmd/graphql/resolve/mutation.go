07ef9194aaac25006c15570b52fcb47f306cbe72
MichaelJCompton <MichaelJCompton@users.noreply.github.com>
2019-10-14 22:55:28 +1100

    add resolver pipeline

--- dgraph/cmd/graphql/resolve/mutation.go
+++ dgraph/cmd/graphql/resolve/mutation.go
@@ -17,13 +17,10 @@
 package resolve
 
 import (
-	"bytes"
 	"context"
 
-	"github.com/dgraph-io/dgraph/dgraph/cmd/graphql/dgraph"
 	"github.com/dgraph-io/dgraph/dgraph/cmd/graphql/schema"
 	"github.com/dgraph-io/dgraph/x"
-	"github.com/pkg/errors"
 	otrace "go.opencensus.io/trace"
 )
 
@@ -55,131 +52,68 @@ import (
 
 // mutationResolver can resolve a single GraphQL mutation field
 type mutationResolver struct {
-	mutation         schema.Mutation
-	schema           schema.Schema
-	mutationRewriter dgraph.MutationRewriter
-	queryRewriter    dgraph.QueryRewriter
-	dgraph           dgraph.Client
+	mutationRewriter MutationRewriter
+	queryExecutor    QueryExecutor
+	mutationExecutor MutationExecutor
+	resultCompleter  ResultCompleter
 }
 
-const (
-	mutationFailed    = false
-	mutationSucceeded = true
-)
+func (mr *mutationResolver) Resolve(
+	ctx context.Context, mutation schema.Mutation) (*Resolved, bool) {
 
-// resolve a single mutation, returning the result of resolving the mutation and
-// a bool where true indicates that the mutation itself succeeded and false indicates
-// that some error prevented the actual mutation.
-func (mr *mutationResolver) resolve(ctx context.Context) (*resolved, bool) {
-
-	var res *resolved
-	var mutationSucceeded bool
-	switch mr.mutation.MutationType() {
-	case schema.AddMutation:
-		res, mutationSucceeded = mr.resolveMutation(ctx)
-	case schema.DeleteMutation:
-		res, mutationSucceeded = mr.resolveDeleteMutation(ctx)
-	case schema.UpdateMutation:
-		// TODO: this should typecheck the input before resolving (like delete does)
-		res, mutationSucceeded = mr.resolveMutation(ctx)
-	default:
-		return &resolved{
-			err: errors.Errorf(
-				"Mutation %s was not executed because only add, delete and update "+
-					"mutations are implemented.", mr.mutation.Name())}, mutationFailed
-	}
-
-	// Mutations should have an extra element to their result path.  Because the mutation
-	// always looks like `addFoo(...) { foo { ... } }` and what's resolved above
-	// is the `foo { ... }` part, so both the result and any error paths need a prefix
-	// of `addFoo` added.
-
-	// Add prefix to result
-	var b bytes.Buffer
-	b.WriteString("\"")
-	b.WriteString(mr.mutation.ResponseName())
-	b.WriteString(`": `)
-	if len(res.data) > 0 {
-		b.Write(res.data)
-	} else {
-		b.WriteString("null")
-	}
-	res.data = b.Bytes()
-
-	// Add prefix to all error paths
-	resErrs := schema.AsGQLErrors(res.err)
-	for _, err := range resErrs {
-		if len(err.Path) > 0 {
-			err.Path = append([]interface{}{mr.mutation.ResponseName()}, err.Path...)
-		}
-	}
-	res.err = resErrs
-
-	return res, mutationSucceeded
-}
-
-func (mr *mutationResolver) resolveMutation(ctx context.Context) (*resolved, bool) {
-	res := &resolved{}
 	span := otrace.FromContext(ctx)
 	stop := x.SpanTimer(span, "resolveMutation")
 	defer stop()
 	if span != nil {
-		span.Annotatef(nil, "mutation alias: [%s] type: [%s]", mr.mutation.Alias(),
-			mr.mutation.MutationType())
+		span.Annotatef(nil, "mutation alias: [%s] type: [%s]", mutation.Alias(),
+			mutation.MutationType())
 	}
 
-	mut, err := mr.mutationRewriter.Rewrite(mr.mutation)
-	if err != nil {
-		res.err = schema.GQLWrapf(err, "couldn't rewrite mutation %s", mr.mutation.Name())
-		return res, mutationFailed
-	}
+	resCtx := &ResolverContext{Ctx: ctx, Field: mutation}
 
-	assigned, err := mr.dgraph.Mutate(ctx, mut)
-	if err != nil {
-		res.err = schema.GQLWrapLocationf(
-			err, mr.mutation.Location(), "mutation %s failed", mr.mutation.Name())
-		return res, mutationFailed
-	}
+	res, success, err := mr.rewriteAndExecute(resCtx, mutation)
 
-	dgQuery, err := mr.queryRewriter.FromMutationResult(mr.mutation, assigned)
-	if err != nil {
-		res.err = schema.GQLWrapf(err, "couldn't rewrite query for mutation %s",
-			mr.mutation.Name())
-		return res, mutationSucceeded
-	}
-
-	resp, err := mr.dgraph.Query(ctx, dgQuery)
-	if err != nil {
-		res.err = schema.GQLWrapf(err, "mutation %s succeeded but query failed",
-			mr.mutation.Name())
-		return res, mutationSucceeded
-	}
-
-	res.data, res.err = completeDgraphResult(ctx, mr.mutation.QueryField(), resp)
-	return res, mutationSucceeded
+	completed, err := mr.resultCompleter.Complete(resCtx, mutation.QueryField(), res, err)
+	return &Resolved{
+		Data: completed,
+		Err:  err,
+	}, success
 }
 
-func (mr *mutationResolver) resolveDeleteMutation(ctx context.Context) (*resolved, bool) {
-	res := &resolved{}
-	span := otrace.FromContext(ctx)
-	stop := x.SpanTimer(span, "resolveDeleteMutation")
-	defer stop()
-	if span != nil {
-		span.Annotatef(nil, "mutation alias: [%s] type: [%s]", mr.mutation.Alias(),
-			mr.mutation.MutationType())
-	}
+func (mr *mutationResolver) rewriteAndExecute(
+	resCtx *ResolverContext, mutation schema.Mutation) ([]byte, bool, error) {
 
-	query, mut, err := mr.mutationRewriter.RewriteDelete(mr.mutation)
+	query, mutations, err := mr.mutationRewriter.Rewrite(mutation)
 	if err != nil {
-		res.err = schema.GQLWrapf(err, "couldn't rewrite mutation %s", mr.mutation.Name())
-		return res, mutationFailed
+		return nil, resolverFailed,
+			schema.GQLWrapf(err, "couldn't rewrite mutation %s", mutation.Name())
 	}
 
-	if err = mr.dgraph.DeleteNodes(ctx, query, mut); err != nil {
-		res.err = schema.GQLWrapf(err, "mutation %s failed", mr.mutation.Name())
-		return res, mutationFailed
+	assigned, mutated, err := mr.mutationExecutor.Mutate(resCtx, query, mutations)
+	if err != nil {
+		return nil, resolverFailed,
+			schema.GQLWrapLocationf(err, mutation.Location(), "mutation %s failed", mutation.Name())
 	}
 
-	res.data = []byte(`{ "msg": "Deleted" }`)
-	return res, mutationSucceeded
+	dgQuery, err := mr.mutationRewriter.FromMutationResult(mutation, assigned, mutated)
+	if err != nil {
+		return nil, resolverFailed,
+			schema.GQLWrapf(err, "couldn't rewrite query for mutation %s", mutation.Name())
+	}
+
+	resp, err := mr.queryExecutor.Query(resCtx, dgQuery)
+
+	return resp, resolverSucceeded,
+		schema.GQLWrapf(err, "mutation %s succeeded but query failed", mutation.Name())
+}
+
+// a deleteCompletion returns `{ "msg": "Deleted" }`
+// FIXME: after upsert mutations changes are done, it will return info about
+// the result of a deletion.
+func deleteCompletion() CompletionFunc {
+	return CompletionFunc(func(
+		resCtx *ResolverContext, field schema.Field, result []byte, err error) ([]byte, error) {
+
+		return []byte(`{ "msg": "Deleted" }`), err
+	})
 }
