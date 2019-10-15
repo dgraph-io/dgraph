c49f421f3556edc70240a7bb85568a3b0c722b50
Manish R Jain <manish@dgraph.io>
2019-10-14 17:58:46 -0700

    Do not wait for timestamp during reads.

--- worker/task.go
+++ worker/task.go
@@ -726,15 +726,16 @@ func processTask(ctx context.Context, q *pb.Query, gid uint32) (*pb.Result, erro
 	stop := x.SpanTimer(span, "processTask"+q.Attr)
 	defer stop()
 
-	span.Annotatef(nil, "Waiting for startTs: %d", q.ReadTs)
-	if err := posting.Oracle().WaitForTs(ctx, q.ReadTs); err != nil {
-		return &pb.Result{}, err
-	}
-	if span != nil {
-		maxAssigned := posting.Oracle().MaxAssigned()
-		span.Annotatef(nil, "Done waiting for maxAssigned. Attr: %q ReadTs: %d Max: %d",
-			q.Attr, q.ReadTs, maxAssigned)
-	}
+	// HACK: Don't block on WaitForTs for this mode.
+	// span.Annotatef(nil, "Waiting for startTs: %d", q.ReadTs)
+	// if err := posting.Oracle().WaitForTs(ctx, q.ReadTs); err != nil {
+	// 	return &pb.Result{}, err
+	// }
+	// if span != nil {
+	// 	maxAssigned := posting.Oracle().MaxAssigned()
+	// 	span.Annotatef(nil, "Done waiting for maxAssigned. Attr: %q ReadTs: %d Max: %d",
+	// 		q.Attr, q.ReadTs, maxAssigned)
+	// }
 	if err := groups().ChecksumsMatch(ctx); err != nil {
 		return &pb.Result{}, err
 	}
