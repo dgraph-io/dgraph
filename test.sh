ed4f91ae6a3638b67d2ea3f8e469381b9378fbc0
Martin Martinez Rivera <martinmr@dgraph.io>
2019-10-18 14:14:56 -0700

    make DGRAPH_ROOT the location of the test.sh script.

--- test.sh
+++ test.sh
@@ -25,7 +25,7 @@
 
 set -e
 readonly ME=${0##*/}
-readonly DGRAPH_ROOT=${GOPATH:-$HOME}/src/github.com/dgraph-io/dgraph
+readonly DGRAPH_ROOT=$(dirname $0)
 
 source $DGRAPH_ROOT/contrib/scripts/functions.sh
 
