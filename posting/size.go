bac475a7bfebf6d03951ef921cce912ca125be4e
Aman Mangal <aman@dgraph.io>
2019-10-21 11:24:01 +0530

    more progress

--- posting/size.go
+++ posting/size.go
@@ -20,18 +20,6 @@ import (
 	"unsafe"
 )
 
-type mapHeader struct {
-	count      int
-	flags      uint8
-	B          uint8
-	noverflow  uint16
-	hash0      uint32
-	buckets    unsafe.Pointer
-	oldbuckets unsafe.Pointer
-	nevacuate  uintptr
-	extra      *int // just so that we don't have to define mapExtra
-}
-
 // DeepSize computes the memory taken by a Posting List
 func (l *List) DeepSize() int {
 	l.RLock()
@@ -56,31 +44,91 @@ func (l *List) DeepSize() int {
 	size += l.plist.DeepSize()
 
 	// List.mutationMap
-	// map has maptype and hmap
+	if l.mutationMap != nil {
+		size += mapDeepSize(l.mutationMap)
+	}
+
+	// memory taken in PostingList in Map
+	// TODO: double calculation here? first in List.PostingList
+	for _, v := range l.mutationMap {
+		size += v.DeepSize()
+	}
+
+	return size
+}
+
+// MAP Usage calculation
+type emptyInterface struct {
+	typ unsafe.Pointer
+	val unsafe.Pointer
+}
+
+type _type struct {
+	size       uintptr
+	ptrdata    uintptr // size of memory prefix holding all pointers
+	hash       uint32
+	tflag      uint8
+	align      uint8
+	fieldalign uint8
+	kind       uint8
+	alg        uintptr
+	// gcdata stores the GC type data for the garbage collector.
+	// If the KindGCProg bit is set in kind, gcdata is a GC program.
+	// Otherwise it is a ptrmask bitmap. See mbitmap.go for details.
+	gcdata    *byte
+	str       int32
+	ptrToThis int32
+}
+
+type maptype struct {
+	typ        _type
+	key        *_type
+	elem       *_type
+	bucket     *_type // internal type representing a hash bucket
+	keysize    uint8  // size of key slot
+	elemsize   uint8  // size of elem slot
+	bucketsize uint16 // size of bucket
+	flags      uint32
+}
+
+type hmap struct {
+	count      int
+	flags      uint8
+	B          uint8
+	noverflow  uint16
+	hash0      uint32
+	buckets    unsafe.Pointer
+	oldbuckets unsafe.Pointer
+	nevacuate  uintptr
+	// it's *mapExtra but we are not interested in that for now.
+	extra uintptr
+}
+
+// compute the space consumed by a map
+func mapDeepSize(m interface{}) int {
+	var size = 0
+
+	// map has maptype and hmap, let's extract those first.
+	ei := (*emptyInterface)(unsafe.Pointer(&m))
+	mt, h := (*maptype)(ei.typ), (*hmap)(ei.val)
+	if h == nil {
+		return size
+	}
+
 	// maptype is defined at compile time and is hardcoded in the compiled code.
 	// Hence, it doesn't consume any extra memory.
 	// Ref: https://dave.cheney.net/2018/05/29/how-the-go-runtime-implements-maps-efficiently-without-generics
-	// Now, let's look at hmap stuct.
+	size += 0
+
 	// size of hmap struct
 	// Ref: https://golang.org/src/runtime/map.go?#L114
 	size += 6 * 8
 
-	// A map bucket is 16 + 8*sizeof(key) + 8*sizeof(value) bytes.
-	// size of each bucket is 16 + 1 + 1 = 18
-	// TODO: This doesn't seem accurate
-	if l.mutationMap != nil {
-		hh := (*mapHeader)(unsafe.Pointer(*(*uintptr)(unsafe.Pointer(&l.mutationMap))))
-		size += 16 * ((1 << hh.B) + int(hh.noverflow))
-		size += 2 * 8 * hh.count
-	}
+	// hmap.buckets
+	size += int(mt.bucket.size)
 
-	// memory consumed by keys and values
-	size += len(l.mutationMap) * 16
-
-	// memory taken in PostingList in Map
-	for _, v := range l.mutationMap {
-		size += v.DeepSize()
-	}
+	// TODO: hmap.oldbuckets
+	// TODO: hmap.extra
 
 	return size
 }
