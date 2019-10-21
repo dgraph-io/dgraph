7bfdb64aca7f43bfdf0e93369f09ffa5cef18139
Ibrahim Jarif <ibrahim@dgraph.io>
2019-10-21 19:51:43 +0530

    Fix failing build

--- dgraph/cmd/zero/oracle.go
+++ dgraph/cmd/zero/oracle.go
@@ -61,7 +61,7 @@ func (o *Oracle) Init() {
 	o.keyCommit = make(map[string]uint64)
 	o.subscribers = make(map[int]chan pb.OracleDelta)
 	o.updates = make(chan *pb.OracleDelta, 100000) // Keeping 1 second worth of updates.
-	o.doneUntil.Init(nil)
+	o.doneUntil.Init(nil, true)
 	go o.sendDeltasToSubscribers()
 }
 
