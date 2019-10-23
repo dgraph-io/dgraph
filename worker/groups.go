9bb2947a2c13e08bf26839b3a3951a4dd0fb8649
Martin Martinez Rivera <martinmr@dgraph.io>
2019-10-23 14:38:54 -0700

    Force alpha to be in proposed group.

--- worker/groups.go
+++ worker/groups.go
@@ -107,7 +107,7 @@ func StartRaftNodes(walStore *badger.DB, bindall bool) {
 
 	// Connect with Zero leader and figure out what group we should belong to.
 	m := &pb.Member{Id: x.WorkerConfig.RaftId, GroupId: x.WorkerConfig.ProposedGroupId,
-		Addr: x.WorkerConfig.MyAddr}
+		Addr: x.WorkerConfig.MyAddr, ForceGroupId: true}
 	var connState *pb.ConnectionState
 	var err error
 	for { // Keep on retrying. See: https://github.com/dgraph-io/dgraph/issues/2289
