04b9c8df243b0c45c2a53a7929449d3a66cc5644
Paras Shah <paras@dgraph.io>
2019-11-04 09:15:35 -0800

    address review comments

--- gql/fuzzit.sh
+++ gql/fuzzit.sh
@@ -1,33 +1,24 @@
 #!/bin/bash
 set -xe
 
-## Build fuzzing targets
-## go-fuzz doesn't support modules for now, so ensure we do everything
-## in the old style GOPATH way
-## Uncommenting line 8 has issues with go-fuzz-build below. Commenting for now.
-# export GO111MODULE="off"
-export GO111MODULE="on"
+## Step 1:  Build fuzzing targets
 
 ## Install go-fuzz
 go get -u github.com/dvyukov/go-fuzz/go-fuzz github.com/dvyukov/go-fuzz/go-fuzz-build
 
-# download dependencies into ${GOPATH}
-# -d : only download (don't install)f
-# -v : verbose
-# -u : use the latest version
-# will be different if you use vendoring or a dependency manager
-# like godep
-# go get -d -v -u ./...
-
+## Build a fuzz target which is later used for fuzzitdev for Continuous Fuzzing.
 go-fuzz-build -libfuzzer -o parser-fuzz-target.a $GOPATH/src/github.com/dgraph-io/dgraph/gql/
 docker run --rm -v $(pwd):/tmp rsmmr/clang clang -fsanitize=fuzzer /tmp/parser-fuzz-target.a -o /tmp/parser-fuzz-target
+
+## Step 2: Perform Fuzzing and local regression on the fuzz target using fuzzit CLI
 
 ## Install fuzzit latest version:
 wget -O fuzzit https://github.com/fuzzitdev/fuzzit/releases/latest/download/fuzzit_Linux_x86_64
 chmod a+x fuzzit
 
-## upload fuzz target for long fuzz testing on fuzzit.dev server or run locally for regression
+## Create a target on fuzzit servers
 ./fuzzit create target --skip-if-exists --seed ./gql/fuzz-data/corpus.tar.gz parser-fuzz-target
+## Start a job (${1} = [fuzzing][local-regression]). 
 ./fuzzit create job --type ${1} dgraph-io-gh/parser-fuzz-target parser-fuzz-target
 
 rm -f parser-fuzz-target parser-fuzz-target.a fuzzit
