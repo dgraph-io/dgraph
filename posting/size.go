e350c51d8e3a430db10c40a08b99ddf0b4d8a465
பாலாஜி <balaji@dgraph.io>
2019-11-13 19:48:27 +0530

    add helper method to calculate the size of List
    Signed-off-by: பாலாஜி <balaji@dgraph.io>

---
+++ posting/size.go
@@ -0,0 +1,204 @@
+/*
+ * Copyright 2019 Dgraph Labs, Inc. and Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package posting
+
+import (
+	"math"
+	"reflect"
+	"unsafe"
+
+	"github.com/dgraph-io/dgo/v2/protos/api"
+	"github.com/dgraph-io/dgraph/protos/pb"
+)
+
+const sizeOfBucket = 144
+
+// DeepSize computes the memory taken by a Posting List
+func (l *List) DeepSize() int {
+	var size int
+	if l == nil {
+		return size
+	}
+	// safe mutex is of 4 words.
+	size = 4 * 8
+	// plist pointer is of 1 word.
+	size += 1 * 8
+	// mutation map pointer is of 1 word.
+	size += 1 * 8
+	// minTs and maxTs takes 1 word each
+	size += 2 * 8
+	// key array
+	size += 3 * 8
+	// So far 11 words, in order to round the slab we're adding one
+	// more word
+	size += 1 * 8
+	// so far basic struct layout has been calculated.
+	// List.key
+	size += cap(l.key)
+
+	// List.PostingList
+	size += calculatePostingListSize(l.plist)
+	if l.mutationMap != nil {
+		// List.mutationMap
+		// map has maptype and hmap
+		// maptype is defined at compile time and is hardcoded in the compiled code.
+		// Hence, it doesn't consume any extra memory.
+		// Ref: https://dave.cheney.net/2018/05/29/how-the-go-runtime-implements-maps-efficiently-without-generics
+		// Now, let's look at hmap strcut.
+		// size of hmap struct
+		// Ref: https://golang.org/src/runtime/map.go?#L114
+		size += 6 * 8
+		// we'll calculate number of buckets based on pointer arithmetic in hmap struct.
+		// reflect value give us access to the hmap struct.
+		hmap := reflect.ValueOf(l.mutationMap)
+		nb := int(math.Pow(2, float64((*(*uint8)(unsafe.Pointer(hmap.Pointer() + uintptr(9)))))))
+		nob := (*(*uint16)(unsafe.Pointer(hmap.Pointer() + uintptr(10))))
+		size += int(nob * sizeOfBucket)
+		if len(l.mutationMap) > 0 || nb > 1 {
+			size += int(nb * sizeOfBucket)
+		}
+	}
+	// memory taken in PostingList in Map
+	for _, v := range l.mutationMap {
+		size += int(calculatePostingListSize(v))
+	}
+
+	return size
+}
+
+// calculatePostingListSize is used to calculate the size of posting list
+func calculatePostingListSize(list *pb.PostingList) int {
+	var size int
+
+	if list == nil {
+		return size
+	}
+
+	// 1+3+1+3+0+3+1
+	size += 12 * 8
+
+	// PostingList.Pack
+	size += calculatePackSize(list.Pack)
+
+	// PostingList.Postings
+	size += cap(list.Postings) * 8
+	for _, p := range list.Postings {
+		size += calculatePostingSize(p)
+	}
+
+	// PostingList.Splits
+	size += cap(list.Splits) * 8
+
+	// PostingList.XXX_unrecognized
+	size += cap(list.XXX_unrecognized)
+
+	return size
+}
+
+// calculatePostingSize is used to calculate the size of posting
+func calculatePostingSize(posting *pb.Posting) int {
+	var size int
+
+	if posting == nil {
+		return size
+	}
+
+	// 1 + 3 + 1 + 1 + 3 + 1 + 3 + 1 + 1 + 1 + 0 + 3 + 1
+	size += 20 * 8
+
+	// Posting.Value
+	size += cap(posting.Value)
+
+	// Posting.LangTag
+	size += cap(posting.LangTag)
+
+	// Posting.Label, strings are immutable, hence cap = len
+	size += len(posting.Label)
+
+	for _, f := range posting.Facets {
+		size += calcuateFacet(f)
+	}
+
+	// Posting.XXX_unrecognized
+	size += cap(posting.XXX_unrecognized)
+
+	return size
+}
+
+// calculatePackSize is used to calculate the size of uidpack
+func calculatePackSize(pack *pb.UidPack) int {
+	var size int
+
+	if pack == nil {
+		return size
+	}
+
+	// size of struct UidPack (1 + 3 + 0 + 3 + 1)
+	// UidPack.BlockSize consumes a full word
+	size += 8 * 8
+
+	// UidPack.Blocks, each pointer takes 1 word
+	size += cap(pack.Blocks) * 8
+	for _, block := range pack.Blocks {
+		size = calculateUIDBlock(block)
+	}
+	// UidPack.XXX_unrecognized
+	size += cap(pack.XXX_unrecognized)
+
+	return size
+}
+
+// calculateUIDBlock is used to calculate UidBlock
+func calculateUIDBlock(block *pb.UidBlock) int {
+	var size int
+
+	if block == nil {
+		return size
+	}
+
+	// size of struct UidBlock (1 + 3 + 1 + 0 + 3 + 1)
+	// Due to alignment, both NumUids and XXX_sizecache consume 8 bytes instead of 4 bytes
+	// to round the word making it 10
+	size += 10 * 8
+
+	// UidBlock.Deltas
+	size += cap(block.Deltas)
+
+	// UidBlock.XXX_unrecognized
+	size += cap(block.XXX_unrecognized)
+
+	return size
+}
+
+// calcuateFacet is used to calculate size of facet.
+func calcuateFacet(facet *api.Facet) int {
+	var size int
+	if facet == nil {
+		return size
+	}
+	// size of struct 1 + 3 + 1 + 3 + 1 + 0 + 3 + 1
+	// rounding to 16
+	size += 16 * 8
+	size += len(facet.Key)
+	size += cap(facet.Value)
+	for _, token := range facet.Tokens {
+		size += len(token)
+	}
+	size += len(facet.Alias)
+	size += len(facet.XXX_unrecognized)
+	return size
+}
---
+++ posting/size_test.go
@@ -0,0 +1,99 @@
+package posting
+
+import (
+	"testing"
+
+	"github.com/dgraph-io/dgo/v2/protos/api"
+	"github.com/dgraph-io/dgraph/protos/pb"
+	"github.com/stretchr/testify/require"
+)
+
+/*
+ * Copyright 2019 Dgraph Labs, Inc. and Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var list *List
+
+var plist *pb.PostingList
+
+var pack *pb.UidPack
+
+var block *pb.UidBlock
+
+var posting *pb.Posting
+
+var facet *api.Facet
+
+func BenchmarkPostingList(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		list = &List{}
+		list.mutationMap = make(map[uint64]*pb.PostingList)
+	}
+}
+
+func BenchmarkUidPack(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		pack = &pb.UidPack{}
+	}
+}
+
+func BenchmarkUidBlock(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		block = &pb.UidBlock{}
+	}
+}
+
+func BenchmarkPosting(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		posting = &pb.Posting{}
+	}
+}
+
+func BenchmarkFacet(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		facet = &api.Facet{}
+	}
+}
+
+func TestPostingListCalculation(t *testing.T) {
+	list = &List{}
+	list.mutationMap = make(map[uint64]*pb.PostingList)
+	// 144 is obtained from BenchmarkPostingList
+	require.Equal(t, int(144), list.DeepSize())
+}
+
+func TestUidPackCalculation(t *testing.T) {
+	pack = &pb.UidPack{}
+	// 64 is obtained from BenchmarkUidPack
+	require.Equal(t, int(64), calculatePackSize(pack))
+}
+
+func TestUidBlockCalculation(t *testing.T) {
+	block = &pb.UidBlock{}
+	// 80 is obtained from BenchmarkUidBlock
+	require.Equal(t, int(80), calculateUIDBlock(block))
+}
+
+func TestPostingCalculation(t *testing.T) {
+	posting = &pb.Posting{}
+	// 160 is obtained from BenchmarkPosting
+	require.Equal(t, int(160), calculatePostingSize(posting))
+}
+
+func TestFacetCalculation(t *testing.T) {
+	facet = &api.Facet{}
+	// 128 is obtained from BenchmarkFacet
+	require.Equal(t, int(128), calcuateFacet(facet))
+}
