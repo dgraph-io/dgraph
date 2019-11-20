759af1f5dd632334a1a6cfdc7001086a853942cf
பாலாஜி <balaji@dgraph.io>
2019-11-20 13:16:31 +0530

    typo
    Signed-off-by: பாலாஜி <balaji@dgraph.io>

--- posting/size_test.sh
+++ posting/size_test.sh
@@ -8,7 +8,7 @@ tar -xvf p.tar.gz
 # get the profiling and size
 go test -run Test21MillionDataSet$ -v -manual=true
 
-# compare oru calculation with the profile
+# compare our calculation with the profile
 go test -run Test21MillionDataSetSize$ -v -manual=true
 
 rm mem.out
