c7a59615873e2cd0ea284ac4a129ccfb8597660c
Ashish Goswami <ashish@dgraph.io>
2019-11-25 17:36:14 +0530

    Set Badger cache size to 1MB

--- dgraph/cmd/bulk/reduce.go
+++ dgraph/cmd/bulk/reduce.go
@@ -90,7 +90,7 @@ func (r *reducer) run() error {
 func (r *reducer) createBadger(i int) *badger.DB {
 	opt := badger.DefaultOptions(r.opt.shardOutputDirs[i]).WithSyncWrites(false).
 		WithTableLoadingMode(bo.MemoryMap).WithValueThreshold(1 << 10 /* 1 KB */).
-		WithLogger(nil)
+		WithLogger(nil).WithMaxCacheSize(1 << 20)
 	db, err := badger.OpenManaged(opt)
 	x.Check(err)
 	r.dbs = append(r.dbs, db)
