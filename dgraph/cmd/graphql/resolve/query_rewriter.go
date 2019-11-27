5e4c1eb475c2088fdaf5df411878117a2e9ca99b
Pawan Rawal <pawan0201@gmail.com>
2019-11-27 15:34:36 +1100

    Remove trimTypeName as its taken care of by mutatedType

--- dgraph/cmd/graphql/resolve/query_rewriter.go
+++ dgraph/cmd/graphql/resolve/query_rewriter.go
@@ -20,7 +20,6 @@ import (
 	"fmt"
 	"sort"
 	"strconv"
-	"strings"
 
 	"github.com/dgraph-io/dgraph/dgraph/cmd/graphql/schema"
 	"github.com/dgraph-io/dgraph/gql"
@@ -129,24 +128,11 @@ func rewriteAsQuery(field schema.Field) *gql.GraphQuery {
 	return dgQuery
 }
 
-// trimTypeName trims Delete from the beginning and Payload from the end of a type name.
-// It gets us the correct type to add to a filter in case of a deleteMutation.
-func trimTypeName(typName string) string {
-	const (
-		del     = "Delete"
-		payload = "Payload"
-	)
-	if strings.HasPrefix(typName, del) && strings.HasSuffix(typName, payload) {
-		typName = strings.TrimSuffix(strings.TrimPrefix(typName, del), payload)
-	}
-	return typName
-}
-
 func addTypeFilter(q *gql.GraphQuery, typ schema.Type) {
 	thisFilter := &gql.FilterTree{
 		Func: &gql.Function{
 			Name: "type",
-			Args: []gql.Arg{{Value: trimTypeName(typ.DgraphName())}},
+			Args: []gql.Arg{{Value: typ.DgraphName()}},
 		},
 	}
 
