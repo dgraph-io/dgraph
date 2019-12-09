fb512ce74382bca175771a86c4f1b9ee24b9fd6b
fristonio <deepshpathak@gmail.com>
2019-12-09 12:49:50 +0530

    contrib: add zone in gcp provider config
    Signed-off-by: fristonio <deepshpathak@gmail.com>

--- contrib/config/terraform/gcp/standalone/main.tf
+++ contrib/config/terraform/gcp/standalone/main.tf
@@ -5,6 +5,7 @@ provider "google" {
   credentials = file(var.credential_file)
   project     = var.project_name
   region      = var.region
+  zone        = var.zone
 }
 
 # --------------------------------------------------------------------------------
