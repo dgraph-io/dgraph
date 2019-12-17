9e51efdd2c27bc6d3e7eda0068f02cc4a1ea0846
Martin Martinez Rivera <martinmr@dgraph.io>
2019-12-17 10:11:16 -0800

    Ignore use of unsafe module in size.go

--- posting/size.go
+++ posting/size.go
@@ -62,8 +62,10 @@ func (l *List) DeepSize() uint64 {
 		// we'll calculate the number of buckets based on pointer arithmetic in hmap struct.
 		// reflect value give us access to the hmap struct.
 		hmap := reflect.ValueOf(l.mutationMap)
+		// skipcq: GSC-G103
 		numBuckets := int(math.Pow(
 			2, float64((*(*uint8)(unsafe.Pointer(hmap.Pointer() + uintptr(9)))))))
+		// skipcq: GSC-G103
 		numOldBuckets := (*(*uint16)(unsafe.Pointer(hmap.Pointer() + uintptr(10))))
 		size += uint64(numOldBuckets * sizeOfBucket)
 		if len(l.mutationMap) > 0 || numBuckets > 1 {
