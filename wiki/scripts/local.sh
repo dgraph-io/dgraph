c48ff6217ed9998c397cf35dec00b2ad85dd21e4
Prashant Shahi <prashant@dgraph.io>
2020-01-07 18:59:55 +0530

    updating local script to support preview

--- wiki/scripts/local.sh
+++ wiki/scripts/local.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+set -e
+
+GREEN='\033[32;1m'
+RESET='\033[0m'
+
 VERSIONS_ARRAY=(
-  'v0.9.0'
-  'master'
-  'v0.8.3'
+  'preview'
 )
 
 joinVersions() {
@@ -20,25 +23,34 @@ run() {
   export DGRAPH_ENDPOINT=${DGRAPH_ENDPOINT:-"https://play.dgraph.io/query?latency=true"}
 
 
-  HUGO_TITLE="Dgraph Doc - local" \
+  HUGO_TITLE="Dgraph Doc - Preview" \
   VERSIONS=${VERSION_STRING} \
   CURRENT_BRANCH="master" \
 
-  pushd $(dirname "$0")/.. > /dev/null
+  pushd "$(dirname "$0")/.." > /dev/null
   pushd themes > /dev/null
 
   if [ ! -d "hugo-docs" ]; then
+    echo -e "$(date) $GREEN  Hugo-docs repository not found. Cloning the repo. $RESET"
     git clone git@github.com:dgraph-io/hugo-docs.git
   else
+    echo -e "$(date) $GREEN  Hugo-docs repository found. Pulling the latest version from master. $RESET"
     pushd hugo-docs > /dev/null
     git pull
     popd > /dev/null
   fi
   popd > /dev/null
 
-
-  CURRENT_VERSION=${CURRENT_VERSION} hugo server -w
+  if [[ $1 == "-p" || $1 == "--preview" ]]; then
+      echo -e "$(date) $GREEN  Generating documentation static pages in the public folder. $RESET"
+      hugo \
+        --destination=public 1> /dev/null
+      echo -e "$(date) $GREEN  Done building. $RESET"
+  else
+    CURRENT_VERSION=${CURRENT_VERSION} hugo server -w \
+      --baseURL=http://localhost:1313
+  fi
   popd > /dev/null
 }
 
-run
+run $1
