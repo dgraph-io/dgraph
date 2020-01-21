021584f5ad853951a57582e21ad9e8291779f16b
Pawan Rawal <pawan0201@gmail.com>
2020-01-21 17:35:21 +0530

    GraphQL: Disallow reverse predicates in pred argument for dgraph directive on fields (#4598)
    Disallow reverse predicates in pred argument for dgraph directive.

--- graphql/schema/rules.go
+++ graphql/schema/rules.go
@@ -479,6 +479,13 @@ func dgraphDirectiveValidation(sch *ast.Schema, typ *ast.Definition, field *ast.
 			typ.Name, field.Name,
 		)
 	}
+	if strings.HasPrefix(predArg.Value.Raw, "~") || strings.HasPrefix(predArg.Value.Raw, "<~") {
+		return gqlerror.ErrorPosf(
+			dir.Position,
+			"Type %s; Field %s: reverse pred argument for @dgraph directive is not supported.",
+			typ.Name, field.Name,
+		)
+	}
 	return nil
 }
 
