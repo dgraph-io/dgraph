23ff99a82821727a6fc03fb0533a2e4827bf5fc5
Daniel Mai <daniel@dgraph.io>
2020-01-23 14:49:03 -0800

    Add Go test to run Jepsen tests.

---
+++ contrib/jepsen/jepsen_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestJepsen(t *testing.T) {
+	s := os.Getenv("HOME") + "/go/src/github.com/dgraph-io/jepsen"
+	jepsenRoot = &s
+	jepsenUp()
+	workloads := testAllWorkloads
+	nemeses := testAllNemeses
+	for _, w := range workloads {
+		for _, n := range nemeses {
+			t.Run(fmt.Sprintf("Workload=%v Nemesis=%v", w, n), func(t *testing.T) {
+				err := runJepsenTest(&jepsenTest{
+					workload:          w,
+					nemesis:           n,
+					timeLimit:         600,
+					concurrency:       "6n",
+					rebalanceInterval: "10h",
+					nemesisInterval:   "10",
+					localBinary:       "/gobin/dgraph",
+					nodes:             "n1,n2,n3,n4,n5",
+					skew:              "",
+					testCount:         1,
+				})
+				if err == errTestFail {
+					t.Error("Test failed.")
+				} else if err == errTestIncomplete {
+					t.Skip("Test incomplete.")
+				}
+			})
+		}
+	}
+	jepsenDown()
+}
