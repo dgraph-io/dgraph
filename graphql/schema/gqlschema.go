e445ea05a3dae2a82b0ca634b8a451a23a81cc95
harshil <harshil@dgraph.io>
2020-02-10 18:37:51 +0530

    Fixed a small bug

--- graphql/schema/gqlschema.go
+++ graphql/schema/gqlschema.go
@@ -507,7 +507,7 @@ func addPatchType(schema *ast.Schema, defn *ast.Definition) {
 	patchDefn := &ast.Definition{
 		Kind:   ast.InputObject,
 		Name:   defn.Name + "Patch",
-		Fields: append(nonIDFields, getPasswordField(defn)...),
+		Fields: append(nonIDFields),
 	}
 	schema.Types[defn.Name+"Patch"] = patchDefn
 
@@ -1134,7 +1134,8 @@ func getNonIDFields(schema *ast.Schema, defn *ast.Definition) ast.FieldList {
 
 		fldList = append(fldList, createField(schema, fld))
 	}
-	return fldList
+
+	return append(fldList, getPasswordField(defn)...)
 }
 
 func getFieldsWithoutIDType(schema *ast.Schema, defn *ast.Definition) ast.FieldList {
@@ -1163,6 +1164,7 @@ func getFieldsWithoutIDType(schema *ast.Schema, defn *ast.Definition) ast.FieldL
 
 		fldList = append(fldList, createField(schema, fld))
 	}
+
 	return fldList
 }
 
