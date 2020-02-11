c106576a67ae58351727f1662d1b0874a69671df
Pawan Rawal <pawan0201@gmail.com>
2020-02-11 21:18:11 +0530

    Fix bad merge

--- ee/acl/acl.go
+++ ee/acl/acl.go
@@ -14,6 +14,7 @@ package acl
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"strings"
 	"time"
@@ -415,7 +416,7 @@ func chMod(conf *viper.Viper) error {
 		return errors.Errorf("the group must not be empty")
 	case len(predicate) == 0:
 		return errors.Errorf("no predicates specified")
-	case perm > 7: // TODO(Animesh): also check if perm < 0
+	case perm > 7:
 		return errors.Errorf("the perm value must be less than or equal to 7, "+
 			"the provided value is %d", perm)
 	}
@@ -496,8 +497,23 @@ func chMod(conf *viper.Viper) error {
 		upsertRequest.Mutations = []*api.Mutation{deleteRule}
 	}
 	resp, err := txn.Do(ctx, upsertRequest)
-	fmt.Println(string(resp.GetJson()))
-	return err
+	if err != nil {
+		return err
+	}
+	var jsonResp map[string][]map[string]int
+	err = json.Unmarshal(resp.GetJson(), &jsonResp)
+	if err != nil {
+		return err
+	}
+
+	uidCount, ok := jsonResp["groupUIDCount"][0]["count"]
+	if !ok {
+		return errors.New("Malformed output of groupUIDCount")
+	} else if uidCount == 0 {
+		// We already have a check for multiple groups with same name at dgraph/ee/acl/utils.go:142
+		return errors.Errorf("Group <%s> doesn't exist", groupName)
+	}
+	return nil
 }
 
 func queryUser(ctx context.Context, txn *dgo.Txn, userid string) (user *User, err error) {
