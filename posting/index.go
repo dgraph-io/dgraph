8dca4dfe5b56450edf32764fe871eb3ca99f3bdb
Aman Mangal <aman@dgraph.io>
2020-02-13 09:53:08 +0530

    Use /tmp dir to store temporary index
    Fixes #4600
    While running dgraph as systemd service, we may not have permissions
    to create a folder in the current directory. Now, we instead use /tmp
    dir to store the temporary index solving the permission denied error.

--- posting/index.go
+++ posting/index.go
@@ -24,6 +24,7 @@ import (
 	"io/ioutil"
 	"math"
 	"os"
+	"path/filepath"
 	"sync/atomic"
 	"time"
 
@@ -511,7 +512,7 @@ func (r *rebuilder) Run(ctx context.Context) error {
 	// All the temp indexes go into the following directory. We delete the whole
 	// directory after the indexing step is complete. This deletes any other temp
 	// indexes that may have been left around in case defer wasn't executed.
-	tmpParentDir := "dgraph_index"
+	tmpParentDir := filepath.Join(os.TempDir(), "dgraph_index")
 
 	// We write the index in a temporary badger first and then,
 	// merge entries before writing them to p directory.
