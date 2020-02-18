5a8540dc689dee9b417ffcb1d56444c80033730d
Pawan Rawal <pawan0201@gmail.com>
2020-02-18 18:13:00 +0530

    Fix draining message in x/health.go

--- x/health.go
+++ x/health.go
@@ -32,7 +32,7 @@ var (
 	errHealth       = errors.New("Please retry again, server is not ready to accept requests")
 	errDrainingMode = errors.New("the server is in draining mode " +
 		"and client requests will only be allowed after exiting the mode " +
-		" by sending a POST request to /admin/draining?enable=false")
+		" by sending a GraphQL draining(enable: false) mutation to /admin")
 )
 
 // UpdateHealthStatus updates the server's health status so it can start accepting requests.
