e94a312e6e6e552a7a3f418e935566b029700eda
Aman Mangal <aman@dgraph.io>
2020-02-20 16:24:15 +0530

    Perform indexing in background

--- posting/list.go
+++ posting/list.go
@@ -24,6 +24,7 @@ import (
 	"sort"
 
 	"github.com/dgryski/go-farm"
+	"github.com/pkg/errors"
 
 	bpb "github.com/dgraph-io/badger/v2/pb"
 	"github.com/dgraph-io/dgraph/algo"
@@ -34,7 +35,6 @@ import (
 	"github.com/dgraph-io/dgraph/types"
 	"github.com/dgraph-io/dgraph/types/facets"
 	"github.com/dgraph-io/dgraph/x"
-	"github.com/pkg/errors"
 )
 
 var (
--- test.sh
+++ test.sh
@@ -256,6 +256,9 @@ if [[ :${TEST_SET}: == *:systest:* ]]; then
 
     Info "Running rebuilding index test"
     RunCmd ./systest/1million/test-reindex.sh || TestFailed
+
+    Info "Running background index test"
+    RunCmd ./systest/bgindex/test-bgindex.sh || TestFailed
 fi
 
 Info "Stopping cluster"
