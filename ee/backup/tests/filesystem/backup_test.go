46bba19f0084815928dfd99705ba0d5e4766fd56
Martin Martinez Rivera <martinmr@users.noreply.github.com>
2020-03-02 14:29:07 -0800

    Cherry-pick "Assign maxLeaseId during restore. (#4827)" (#4877)

--- ee/backup/tests/filesystem/backup_test.go
+++ ee/backup/tests/filesystem/backup_test.go
@@ -252,8 +252,8 @@ func runRestore(t *testing.T, backupLocation, lastDir string, commitTs uint64) m
 	require.NoError(t, os.RemoveAll(restoreDir))
 
 	t.Logf("--- Restoring from: %q", backupLocation)
-	_, err := backup.RunRestore("./data/restore", backupLocation, lastDir)
-	require.NoError(t, err)
+	result := backup.RunRestore("./data/restore", backupLocation, lastDir)
+	require.NoError(t, result.Err)
 
 	for i, pdir := range []string{"p1", "p2", "p3"} {
 		pdir = filepath.Join("./data/restore", pdir)
@@ -284,9 +284,9 @@ func runFailingRestore(t *testing.T, backupLocation, lastDir string, commitTs ui
 	// calling restore.
 	require.NoError(t, os.RemoveAll(restoreDir))
 
-	_, err := backup.RunRestore("./data/restore", backupLocation, lastDir)
-	require.Error(t, err)
-	require.Contains(t, err.Error(), "expected a BackupNum value of 1")
+	result := backup.RunRestore("./data/restore", backupLocation, lastDir)
+	require.Error(t, result.Err)
+	require.Contains(t, result.Err.Error(), "expected a BackupNum value of 1")
 }
 
 func dirSetup(t *testing.T) {
--- ee/backup/tests/minio-large/backup_test.go
+++ ee/backup/tests/minio-large/backup_test.go
@@ -159,8 +159,8 @@ func runRestore(t *testing.T, backupLocation, lastDir string, commitTs uint64) m
 	require.NoError(t, os.MkdirAll(restoreDir, os.ModePerm))
 
 	t.Logf("--- Restoring from: %q", backupLocation)
-	_, err := backup.RunRestore("./data/restore", backupLocation, lastDir)
-	require.NoError(t, err)
+	result := backup.RunRestore("./data/restore", backupLocation, lastDir)
+	require.NoError(t, result.Err)
 
 	restored1, err := testutil.GetPredicateValues("./data/restore/p1", "name1", commitTs)
 	require.NoError(t, err)
