6b684fd1278b45b0131598575fe75481aead85df
Aman Mangal <aman@dgraph.io>
2020-03-27 13:53:26 +0530

    more work

---
+++ contrib/aclupgrade/upgrade.sh
@@ -0,0 +1,38 @@
+# Setup cluster on old version
+OLD_VERSION=v1.2.2
+
+## Setup cluster
+
+## Setup ACL
+
+## Add data
+
+## Check whether ACL is working
+
+## Export data
+
+## Shutdown the cluster
+
+
+
+# Setup cluster on new version
+NEW_VERSION=v20.03.0
+
+## Setup cluster88
+
+## Upgrade ACL
+
+## Import data
+
+## Check whether ACL is working
+
+## Upgrade ACL again with deleteOld=true
+
+## Check whether ACL is working
+
+## Check that old rules are deleted
+
+## Upgrade ACL again, should fail now
+
+## Shutdown the cluster
+
