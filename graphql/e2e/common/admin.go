ab412c464c889a0f443f4d57816b7d723d653a72
Abhimanyu Singh Gaur <12651351+abhimanyusinghgaur@users.noreply.github.com>
2020-03-30 14:24:32 +0530

    fix test

--- graphql/e2e/common/admin.go
+++ graphql/e2e/common/admin.go
@@ -91,6 +91,15 @@ const (
             "type": "string"
         },
         {
+            "predicate": "dgraph.graphql.xid",
+            "type": "string",
+            "index": true,
+            "tokenizer": [
+                "exact"
+            ],
+            "upsert": true
+        },
+        {
             "predicate": "dgraph.type",
             "type": "string",
             "index": true,
@@ -113,6 +122,8 @@ const (
             "fields": [
                 {
                     "name": "dgraph.graphql.schema"
+                },{
+                    "name": "dgraph.graphql.xid"
                 }
             ],
             "name": "dgraph.graphql"
@@ -150,6 +161,15 @@ const (
             "type": "string"
         },
         {
+            "predicate": "dgraph.graphql.xid",
+            "type": "string",
+            "index": true,
+            "tokenizer": [
+                "exact"
+            ],
+            "upsert": true
+        },
+        {
             "predicate": "dgraph.type",
             "type": "string",
             "index": true,
@@ -175,6 +195,8 @@ const (
             "fields": [
                 {
                     "name": "dgraph.graphql.schema"
+                },{
+                    "name": "dgraph.graphql.xid"
                 }
             ],
             "name": "dgraph.graphql"
@@ -220,6 +242,15 @@ const (
             "type": "string"
         },
         {
+            "predicate": "dgraph.graphql.xid",
+            "type": "string",
+            "index": true,
+            "tokenizer": [
+                "exact"
+            ],
+            "upsert": true
+        },
+        {
             "predicate": "dgraph.type",
             "type": "string",
             "index": true,
@@ -248,6 +279,8 @@ const (
             "fields": [
                 {
                     "name": "dgraph.graphql.schema"
+                },{
+                    "name": "dgraph.graphql.xid"
                 }
             ],
             "name": "dgraph.graphql"
--- systest/backup/minio/backup_test.go
+++ systest/backup/minio/backup_test.go
@@ -289,7 +289,8 @@ func runRestore(t *testing.T, lastDir string, commitTs uint64) map[string]string
 
 	restoredPreds, err := testutil.GetPredicateNames(pdir, commitTs)
 	require.NoError(t, err)
-	require.ElementsMatch(t, []string{"dgraph.graphql.schema", "dgraph.type", "movie"}, restoredPreds)
+	require.ElementsMatch(t, []string{"dgraph.graphql.schema", "dgraph.graphql.xid", "dgraph.type",
+		"movie"}, restoredPreds)
 
 	restoredTypes, err := testutil.GetTypeNames(pdir, commitTs)
 	require.NoError(t, err)
