54d702ad16cd2b335fc8c19a89cf435dc5b6cf0d
Martin Martinez Rivera <martinmr@dgraph.io>
2020-04-03 11:40:39 -0700

    Update RAFT checkpoint when doing a clean shutdonw.

--- worker/draft.go
+++ worker/draft.go
@@ -851,6 +851,14 @@ func (n *node) updateRaftProgress() error {
 	return nil
 }
 
+func UpdateRaftProgress() error {
+	gr := groups()
+	if gr == nil || gr.Node == nil {
+		return nil
+	}
+	return gr.Node.updateRaftProgress()
+}
+
 func (n *node) checkpointAndClose(done chan struct{}) {
 	slowTicker := time.NewTicker(time.Minute)
 	defer slowTicker.Stop()
