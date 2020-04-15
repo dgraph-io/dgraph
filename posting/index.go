7397ac3f8852cc151dd8cbef90204f181ddd7b75
Ibrahim Jarif <jarifibrahim@gmail.com>
2020-04-15 23:10:46 +0530

    Update badger to commit 09dd2e1a4195

--- posting/index.go
+++ posting/index.go
@@ -555,7 +555,6 @@ func (r *rebuilder) Run(ctx context.Context) error {
 		WithNumVersionsToKeep(math.MaxInt32).
 		WithLogger(&x.ToGlog{}).
 		WithCompression(options.None).
-		WithEventLogging(false).
 		WithLogRotatesToFlush(10).
 		WithEncryptionKey(enc.ReadEncryptionKeyFile(x.WorkerConfig.BadgerKeyFile))
 	tmpDB, err := badger.OpenManaged(dbOpts)
