0dd252ac1d39ebfa9c89d6e6f739652ddd4c80ac
Michael Compton <michael@dgraph.io>
2020-04-20 18:34:50 +1000

    graphql: execution refactor (#5244)
    (cherry picked from commit 4742d4fe15e52e669f9bb1a3b1106d02612fd168)

--- graphql/resolve/resolver_error_test.go
+++ graphql/resolve/resolver_error_test.go
@@ -18,11 +18,11 @@ package resolve
 
 import (
 	"context"
+	"encoding/json"
 	"io/ioutil"
 	"testing"
 
 	dgoapi "github.com/dgraph-io/dgo/v2/protos/api"
-	"github.com/dgraph-io/dgraph/gql"
 	"github.com/dgraph-io/dgraph/graphql/schema"
 	"github.com/dgraph-io/dgraph/graphql/test"
 	"github.com/dgraph-io/dgraph/x"
@@ -45,6 +45,9 @@ type executor struct {
 	resp     string
 	assigned map[string]string
 	result   map[string]interface{}
+
+	queryTouched    uint64
+	mutationTouched uint64
 
 	// start reporting Dgraph fails at this point (0 = never fail, 1 = fail on
 	// first request, 2 = succeed once and then fail on 2nd request, etc.)
@@ -79,22 +82,38 @@ type Post {
 	author: Author!
 }`
 
-func (ex *executor) Query(ctx context.Context, query *gql.GraphQuery) ([]byte, error) {
-	ex.failQuery--
-	if ex.failQuery == 0 {
-		return nil, schema.GQLWrapf(errors.New("_bad stuff happend_"), "Dgraph query failed")
-	}
-	return []byte(ex.resp), nil
-}
+func (ex *executor) Execute(ctx context.Context, req *dgoapi.Request) (*dgoapi.Response, error) {
+	if len(req.Mutations) == 0 {
+		ex.failQuery--
+		if ex.failQuery == 0 {
+			return nil, schema.GQLWrapf(errors.New("_bad stuff happend_"), "Dgraph query failed")
+		}
 
-func (ex *executor) Mutate(ctx context.Context,
-	query *gql.GraphQuery,
-	mutations []*dgoapi.Mutation) (map[string]string, map[string]interface{}, error) {
+		return &dgoapi.Response{
+			Json: []byte(ex.resp),
+			Metrics: &dgoapi.Metrics{
+				NumUids: map[string]uint64{touchedUidsKey: ex.queryTouched}},
+		}, nil
+	}
+
 	ex.failMutation--
 	if ex.failMutation == 0 {
-		return nil, nil, schema.GQLWrapf(errors.New("_bad stuff happend_"), "Dgraph mutation failed")
+		return nil, schema.GQLWrapf(errors.New("_bad stuff happend_"),
+			"Dgraph mutation failed")
 	}
-	return ex.assigned, ex.result, nil
+
+	res, err := json.Marshal(ex.result)
+	if err != nil {
+		panic(err)
+	}
+
+	return &dgoapi.Response{
+		Json: []byte(res),
+		Uids: ex.assigned,
+		Metrics: &dgoapi.Metrics{
+			NumUids: map[string]uint64{touchedUidsKey: ex.mutationTouched}},
+	}, nil
+
 }
 
 // Tests in resolver_test.yaml are about what gets into a completed result (addition
@@ -450,8 +469,7 @@ func resolveWithClient(
 			Qrw: NewQueryRewriter(),
 			Arw: NewAddRewriter,
 			Urw: NewUpdateRewriter,
-			Qe:  ex,
-			Me:  ex,
+			Ex:  ex,
 		}))
 
 	return resolver.Resolve(context.Background(), &schema.Request{Query: gqlQuery, Variables: vars})
