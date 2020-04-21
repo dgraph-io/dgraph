5405082c722417a4ffd6396a4dd7c183007ab4d8
noot <36753753+noot@users.noreply.github.com>
2020-04-21 19:38:00 -0400

    lib/runtime/extrinsic: add extrinsic utils (#794)
    add lib/runtime/extrinsic package which can create, encode, decode various extrinsic types as defined in the runtime

---
+++ lib/runtime/extrinsic/extrinsic.go
@@ -0,0 +1,317 @@
+package extrinsic
+
+import (
+	"encoding/binary"
+	"errors"
+	"io"
+
+	"github.com/ChainSafe/gossamer/lib/common"
+	"github.com/ChainSafe/gossamer/lib/common/optional"
+	"github.com/ChainSafe/gossamer/lib/crypto/sr25519"
+	"github.com/ChainSafe/gossamer/lib/scale"
+)
+
+//nolint
+const (
+	AuthoritiesChangeType = 0
+	TransferType          = 1
+	IncludeDataType       = 2
+	StorageChangeType     = 3
+	// TODO: implement when storage changes trie is completed
+	//ChangesTrieConfigUpdateType = 4
+)
+
+// Extrinsic represents a runtime Extrinsic
+type Extrinsic interface {
+	Type() int
+	Encode() ([]byte, error)
+	Decode(r io.Reader) error
+}
+
+// DecodeExtrinsic decodes an Extrinsic from a Reader
+func DecodeExtrinsic(r io.Reader) (Extrinsic, error) {
+	typ, err := common.ReadByte(r)
+	if err != nil {
+		return nil, err
+	}
+
+	switch typ {
+	case AuthoritiesChangeType:
+		ext := new(AuthoritiesChangeExt)
+		return ext, ext.Decode(r)
+	case TransferType:
+		ext := new(TransferExt)
+		return ext, ext.Decode(r)
+	case IncludeDataType:
+		ext := new(IncludeDataExt)
+		return ext, ext.Decode(r)
+	case StorageChangeType:
+		ext := new(StorageChangeExt)
+		return ext, ext.Decode(r)
+	default:
+		return nil, errors.New("cannot decode invalid extrinsic type")
+	}
+}
+
+// AuthoritiesChangeExt represents an Extrinsic::AuthoritiesChange
+type AuthoritiesChangeExt struct {
+	authorityIDs [][32]byte
+}
+
+// NewAuthoritiesChangeExt returns an AuthoritiesChangeExt
+func NewAuthoritiesChangeExt(authorityIDs [][32]byte) *AuthoritiesChangeExt {
+	return &AuthoritiesChangeExt{
+		authorityIDs: authorityIDs,
+	}
+}
+
+// Type returns AuthoritiesChangeType
+func (e *AuthoritiesChangeExt) Type() int {
+	return AuthoritiesChangeType
+}
+
+// Encode returns the SCALE encoding of the AuthoritiesChangeExt
+func (e *AuthoritiesChangeExt) Encode() ([]byte, error) {
+	enc, err := scale.Encode(e.authorityIDs)
+	if err != nil {
+		return nil, err
+	}
+
+	return append([]byte{AuthoritiesChangeType}, enc...), nil
+}
+
+// Decode decodes the SCALE encoding into a AuthoritiesChangeExt
+func (e *AuthoritiesChangeExt) Decode(r io.Reader) error {
+	sd := &scale.Decoder{Reader: r}
+	d, err := sd.Decode(e.authorityIDs)
+	if err != nil {
+		return err
+	}
+
+	e.authorityIDs = d.([][32]byte)
+	return nil
+}
+
+// Transfer represents a runtime Transfer
+type Transfer struct {
+	from   [32]byte
+	to     [32]byte
+	amount uint64
+	nonce  uint64
+}
+
+// NewTransfer returns a Transfer
+func NewTransfer(from, to [32]byte, amount, nonce uint64) *Transfer {
+	return &Transfer{
+		from:   from,
+		to:     to,
+		amount: amount,
+		nonce:  nonce,
+	}
+}
+
+// Encode returns the SCALE encoding of the Transfer
+func (t *Transfer) Encode() ([]byte, error) {
+	enc := []byte{}
+
+	buf := make([]byte, 8)
+
+	enc = append(enc, t.from[:]...)
+	enc = append(enc, t.to[:]...)
+
+	binary.LittleEndian.PutUint64(buf, t.amount)
+	enc = append(enc, buf...)
+
+	binary.LittleEndian.PutUint64(buf, t.nonce)
+	enc = append(enc, buf...)
+
+	return enc, nil
+}
+
+// Decode decodes the SCALE encoding into a Transfer
+func (t *Transfer) Decode(r io.Reader) (err error) {
+	t.from, err = common.ReadHash(r)
+	if err != nil {
+		return err
+	}
+
+	t.to, err = common.ReadHash(r)
+	if err != nil {
+		return err
+	}
+
+	t.amount, err = common.ReadUint64(r)
+	if err != nil {
+		return err
+	}
+
+	t.nonce, err = common.ReadUint64(r)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// TransferExt represents an Extrinsic::Transfer
+type TransferExt struct {
+	transfer  *Transfer
+	signature [sr25519.SignatureLength]byte
+}
+
+// NewTransferExt returns a TransferExt
+func NewTransferExt(transfer *Transfer, signature [sr25519.SignatureLength]byte) *TransferExt {
+	return &TransferExt{
+		transfer:  transfer,
+		signature: signature,
+	}
+}
+
+// Type returns TransferType
+func (e *TransferExt) Type() int {
+	return TransferType
+}
+
+// Encode returns the SCALE encoding of the TransferExt
+func (e *TransferExt) Encode() ([]byte, error) {
+	enc := []byte{TransferType}
+
+	tenc, err := e.transfer.Encode()
+	if err != nil {
+		return nil, err
+	}
+
+	enc = append(enc, tenc...)
+	enc = append(enc, e.signature[:]...)
+
+	return enc, nil
+}
+
+// Decode decodes the SCALE encoding into a TransferExt
+func (e *TransferExt) Decode(r io.Reader) error {
+	e.transfer = new(Transfer)
+	err := e.transfer.Decode(r)
+	if err != nil {
+		return err
+	}
+
+	_, err = r.Read(e.signature[:])
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// IncludeDataExt represents an Extrinsic::IncludeData
+type IncludeDataExt struct {
+	data []byte
+}
+
+// NewIncludeDataExt returns a IncludeDataExt
+func NewIncludeDataExt(data []byte) *IncludeDataExt {
+	return &IncludeDataExt{
+		data: data,
+	}
+}
+
+// Type returns IncludeDataType
+func (e *IncludeDataExt) Type() int {
+	return IncludeDataType
+}
+
+// Encode returns the SCALE encoding of the IncludeDataExt
+func (e *IncludeDataExt) Encode() ([]byte, error) {
+	enc, err := scale.Encode(e.data)
+	if err != nil {
+		return nil, err
+	}
+
+	return append([]byte{IncludeDataType}, enc...), nil
+}
+
+// Decode decodes the SCALE encoding into a IncludeDataExt
+func (e *IncludeDataExt) Decode(r io.Reader) error {
+	sd := &scale.Decoder{Reader: r}
+	d, err := sd.Decode(e.data)
+	if err != nil {
+		return err
+	}
+
+	e.data = d.([]byte)
+	return nil
+}
+
+// StorageChangeExt represents an Extrinsic::StorageChange
+type StorageChangeExt struct {
+	key   []byte
+	value *optional.Bytes
+}
+
+// NewStorageChangeExt returns a StorageChangesExt
+func NewStorageChangeExt(key []byte, value *optional.Bytes) *StorageChangeExt {
+	return &StorageChangeExt{
+		key:   key,
+		value: value,
+	}
+}
+
+// Type returns StorageChangeType
+func (e *StorageChangeExt) Type() int {
+	return StorageChangeType
+}
+
+// Encode returns the SCALE encoding of the StorageChangeExt
+func (e *StorageChangeExt) Encode() ([]byte, error) {
+	enc := []byte{StorageChangeType}
+
+	d, err := scale.Encode(e.key)
+	if err != nil {
+		return nil, err
+	}
+
+	enc = append(enc, d...)
+
+	if e.value.Exists() {
+		enc = append(enc, 1)
+		d, err = scale.Encode(e.value.Value())
+		if err != nil {
+			return nil, err
+		}
+
+		enc = append(enc, d...)
+	} else {
+		enc = append(enc, 0)
+	}
+
+	return enc, nil
+}
+
+// Decode decodes the SCALE encoding into a StorageChangeExt
+func (e *StorageChangeExt) Decode(r io.Reader) error {
+	sd := &scale.Decoder{Reader: r}
+	d, err := sd.Decode([]byte{})
+	if err != nil {
+		return err
+	}
+
+	e.key = d.([]byte)
+
+	exists, err := common.ReadByte(r)
+	if err != nil {
+		return err
+	}
+
+	if exists == 1 {
+		d, err = sd.Decode([]byte{})
+		if err != nil {
+			return err
+		}
+
+		e.value = optional.NewBytes(true, d.([]byte))
+	} else {
+		e.value = optional.NewBytes(false, nil)
+	}
+
+	return nil
+}
---
+++ lib/runtime/extrinsic/extrinsic_test.go
@@ -0,0 +1,192 @@
+package extrinsic
+
+import (
+	"bytes"
+	"encoding/binary"
+	"reflect"
+	"testing"
+
+	"github.com/ChainSafe/gossamer/lib/common"
+	"github.com/ChainSafe/gossamer/lib/common/optional"
+	"github.com/ChainSafe/gossamer/lib/crypto/sr25519"
+)
+
+func TestAuthoritiesChangeExt_Encode(t *testing.T) {
+	t.Skip()
+
+	// TODO: scale isn't working for arrays of [32]byte
+	kp, err := sr25519.GenerateKeypair()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pub := kp.Public().Encode()
+	pubk := [32]byte{}
+	copy(pubk[:], pub)
+
+	authorities := [][32]byte{pubk}
+
+	ext := NewAuthoritiesChangeExt(authorities)
+
+	enc, err := ext.Encode()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(enc)
+
+	r := &bytes.Buffer{}
+	r.Write(enc)
+	res, err := DecodeExtrinsic(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !reflect.DeepEqual(res, ext) {
+		t.Fatalf("Fail: got %v expected %v", res, ext)
+	}
+}
+
+var alice, _ = common.HexToHash("0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d")
+var bob, _ = common.HexToHash("0x90b5ab205c6974c9ea841be688864633dc9ca8a357843eeacf2314649965fe22")
+
+func TestTransferExt_Encode(t *testing.T) {
+	transfer := NewTransfer(alice, bob, 1000, 1)
+	sig := [64]byte{}
+	ext := NewTransferExt(transfer, sig)
+
+	enc, err := ext.Encode()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	r := &bytes.Buffer{}
+	r.Write(enc)
+	res, err := DecodeExtrinsic(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !reflect.DeepEqual(res, ext) {
+		t.Fatalf("Fail: got %v expected %v", res, ext)
+	}
+}
+
+func TestTransferExt_Decode(t *testing.T) {
+	// from substrate test runtime
+	enc := []byte{1, 212, 53, 147, 199, 21, 253, 211, 28, 97, 20, 26, 189, 4, 169, 159, 214, 130, 44, 133, 88, 133, 76, 205, 227, 154, 86, 132, 231, 165, 109, 162, 125, 144, 181, 171, 32, 92, 105, 116, 201, 234, 132, 27, 230, 136, 134, 70, 51, 220, 156, 168, 163, 87, 132, 62, 234, 207, 35, 20, 100, 153, 101, 254, 34, 69, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 114, 13, 234, 84, 192, 135, 6, 254, 94, 142, 207, 135, 126, 177, 25, 27, 198, 159, 97, 165, 198, 228, 77, 117, 113, 253, 247, 97, 221, 110, 47, 9, 87, 209, 62, 254, 81, 200, 217, 45, 214, 53, 170, 217, 160, 137, 43, 78, 183, 89, 45, 2, 64, 120, 114, 100, 116, 148, 247, 92, 234, 57, 255, 139}
+	r := &bytes.Buffer{}
+	r.Write(enc)
+	res, err := DecodeExtrinsic(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	amount := binary.LittleEndian.Uint64([]byte{69, 0, 0, 0, 0, 0, 0, 0})
+	nonce := binary.LittleEndian.Uint64([]byte{1, 0, 0, 0, 0, 0, 0, 0})
+
+	sig, _ := common.HexToBytes("0x720dea54c08706fe5e8ecf877eb1191bc69f61a5c6e44d7571fdf761dd6e2f0957d13efe51c8d92dd635aad9a0892b4eb7592d02407872647494f75cea39ff8b")
+	sigb := [sr25519.SignatureLength]byte{}
+	copy(sigb[:], sig)
+
+	expected := &TransferExt{
+		transfer: &Transfer{
+			from:   alice,
+			to:     bob,
+			amount: amount,
+			nonce:  nonce,
+		},
+		signature: sigb,
+	}
+
+	var transfer *TransferExt
+	var ok bool
+
+	if transfer, ok = res.(*TransferExt); !ok {
+		t.Fatal("Fail: got wrong extrinsic type")
+	}
+
+	if !reflect.DeepEqual(transfer, expected) {
+		t.Fatalf("Fail: got %v expected %v", transfer, expected)
+	}
+}
+
+func TestIncludeDataExt_Encode(t *testing.T) {
+	data := []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
+	ext := NewIncludeDataExt(data)
+
+	enc, err := ext.Encode()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	r := &bytes.Buffer{}
+	r.Write(enc)
+	res, err := DecodeExtrinsic(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !reflect.DeepEqual(res, ext) {
+		t.Fatalf("Fail: got %v expected %v", res, ext)
+	}
+}
+
+func TestIncludeDataExt_Decode(t *testing.T) {
+	enc := []byte{2, 32, 111, 0, 0, 0, 0, 0, 0, 0}
+	r := &bytes.Buffer{}
+	r.Write(enc)
+	res, err := DecodeExtrinsic(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := &IncludeDataExt{
+		data: []byte{111, 0, 0, 0, 0, 0, 0, 0},
+	}
+
+	if !reflect.DeepEqual(res, expected) {
+		t.Fatalf("Fail: got %v expected %v", res, expected)
+	}
+}
+
+func TestStorageChangeExt_Encode(t *testing.T) {
+	key := []byte("noot")
+	value := optional.NewBytes(true, []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 0})
+	ext := NewStorageChangeExt(key, value)
+
+	enc, err := ext.Encode()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	r := &bytes.Buffer{}
+	r.Write(enc)
+	res, err := DecodeExtrinsic(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !reflect.DeepEqual(res, ext) {
+		t.Fatalf("Fail: got %v expected %v", res, ext)
+	}
+}
+
+func TestStorageChangeExt_Decode(t *testing.T) {
+	enc := []byte{3, 16, 77, 1, 2, 3, 1, 4, 99}
+	r := &bytes.Buffer{}
+	r.Write(enc)
+	res, err := DecodeExtrinsic(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := &StorageChangeExt{
+		key:   []byte{77, 1, 2, 3},
+		value: optional.NewBytes(true, []byte{99}),
+	}
+
+	if !reflect.DeepEqual(res, expected) {
+		t.Fatalf("Fail: got %v expected %v", res, expected)
+	}
+}
