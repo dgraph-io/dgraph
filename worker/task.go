6a4c079eab9a7093d9053b128bbe39e77aa3ee01
Anurag Sharma <anurags92@gmail.com>
2020-04-29 21:10:51 +0530

    Maintain col length in the code

--- worker/task.go
+++ worker/task.go
@@ -1832,7 +1832,9 @@ func parseSrcFn(ctx context.Context, q *pb.Query) (*functionContext, error) {
 			}
 			fc.uidsPresent = append(fc.uidsPresent, uidParsed)
 		}
-		sort.Slice(fc.uidsPresent, func(i, j int) bool { return fc.uidsPresent[i] < fc.uidsPresent[j] })
+		sort.Slice(fc.uidsPresent, func(i, j int) bool {
+			return fc.uidsPresent[i] < fc.uidsPresent[j]
+		})
 		checkRoot(q, fc)
 		if fc.isFuncAtRoot {
 			return nil, errors.Errorf("uid_in function not allowed at root")
