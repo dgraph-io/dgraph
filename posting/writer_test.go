baf5eaf7c72359b6103e166a1689e14a2ab407ad
Anurag Sharma <anurags92@gmail.com>
2020-05-04 13:57:33 +0530

    Modify writer_test.go to account for new API

--- posting/writer_test.go
+++ posting/writer_test.go
@@ -88,57 +88,12 @@ func BenchmarkWriter(b *testing.B) {
 		b.ResetTimer()
 
 		for i := 0; i < b.N; i++ {
-			// var wg sync.WaitGroup
-			// var entries []*badger.Entry
-			// // 	batch := db.NewWriteBatchAt(1)
-			// // 	for _, typ := range KVList {
-			// // 		k := typ.key
-			// // 		v := typ.value
-			// // 		batch.Set(k, v)
-			// // 	}
-			// // 	require.NoError(b, batch.Flush())
-			// for _, kv := range KVList {
-			// 	// var meta byte
-			// 	// var entry *badger.Entry
-			// 	// if len(kv.UserMeta) > 0 {
-			// 	// 	meta = kv.UserMeta[0]
-			// 	// }
-			// 	// switch meta {
-			// 	// case BitCompletePosting, BitEmptyPosting:
-			// 	// 	entry = (&badger.Entry{
-			// 	// 		Key:      kv.Key,
-			// 	// 		Value:    kv.Value,
-			// 	// 		UserMeta: meta,
-			// 	// 	}).WithDiscard()
-			// 	// default:
-			// 	k := kv.key
-			// 	v := kv.value
-			// 	var meta byte
-			// 	var entry *badger.Entry
-
-			// 	entry = &badger.Entry{
-			// 		Key:      k,
-			// 		Value:    v,
-			// 		UserMeta: meta,
-			// 	}
-			// 	// UserMeta: meta,
-			// 	// }
-			// 	entry.Key = y.KeyWithTs(entry.Key, 1) //Same version for each key
-			// 	entries = append(entries, entry)
-			// }
-
-			// wg.Add(1)
-			// err := db.BatchSetAsync(entries, func(err error) {
-			// 	defer wg.Done()
-			// 	if err != nil {
-			// 		// TODO: Handle error
-			// 		glog.Error(err)
-			// 		return
-			// 	}
-			// })
-			require.NoError(b, err)
-			// wg.Wait()
-
+			wb := db.NewManagedWriteBatch()
+			for _, typ := range KVList {
+				e := &badger.Entry{Key: typ.key, Value: typ.value}
+				wb.SetEntryAt(e, 1)
+			}
+			require.NoError(b, wb.Flush())
 		}
 	})
 }
