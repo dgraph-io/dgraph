94abc895969c10eb3226338bc63fbcb1e3ce96b1
Martin Martinez Rivera <martinmr@dgraph.io>
2020-05-12 08:32:39 -0700

    Change the default ratio of traces from 1 to 0.01. (#5405)
    Opencensus traces are taking a lot of memory.

--- dgraph/cmd/zero/run.go
+++ dgraph/cmd/zero/run.go
@@ -89,7 +89,7 @@ instances to achieve high-availability.
 	flag.Bool("telemetry", true, "Send anonymous telemetry data to Dgraph devs.")
 
 	// OpenCensus flags.
-	flag.Float64("trace", 1.0, "The ratio of queries to trace.")
+	flag.Float64("trace", 0.01, "The ratio of queries to trace.")
 	flag.String("jaeger.collector", "", "Send opencensus traces to Jaeger.")
 	// See https://github.com/DataDog/opencensus-go-exporter-datadog/issues/34
 	// about the status of supporting annotation logs through the datadog exporter
