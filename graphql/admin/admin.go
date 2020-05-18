a0992b12996c469125483ba6337ece8f85fd86aa
harshil <harshil@dgraph.io>
2020-05-19 04:22:57 +0530

    fixed some tests

--- graphql/admin/admin.go
+++ graphql/admin/admin.go
@@ -19,10 +19,11 @@ package admin
 import (
 	"context"
 	"fmt"
-	dgoapi "github.com/dgraph-io/dgo/v2/protos/api"
-	"github.com/dgraph-io/dgraph/gql"
 	"sync"
 	"time"
+
+	dgoapi "github.com/dgraph-io/dgo/v2/protos/api"
+	"github.com/dgraph-io/dgraph/gql"
 
 	"github.com/golang/glog"
 	"github.com/pkg/errors"
@@ -443,55 +444,81 @@ func newAdminResolverFactory() resolve.ResolverFactory {
 }
 
 func upsertEmptyGQLSchema() (*gqlSchema, error) {
-	varName := "GQLSchema"
+	existingSchemaVar := "ExistingGQLSchema"
+	xidInSchemaVar := "XidInSchema"
 	gqlType := "dgraph.graphql"
-
+	/*
+	   query {
+	     ExistingGQLSchema as ExistingGQLSchema(func: type(dgraph.graphql)) {
+	       uid
+	       dgraph.graphql.schema
+	       XidInSchema as dgraph.graphql.xid
+	     }
+	   }
+	*/
 	qry := &gql.GraphQuery{
-		Attr: varName,
-		Var:  varName,
+		Attr: existingSchemaVar,
+		Var:  existingSchemaVar,
 		Func: &gql.Function{
-			Name: "eq",
-			Args: []gql.Arg{
-				{Value: gqlSchemaXidKey},
-				{Value: fmt.Sprintf("%q", gqlSchemaXidVal)},
-			},
+			Name: "type",
+			Args: []gql.Arg{{Value: gqlType}},
 		},
-		Filter: &gql.FilterTree{
-			Func: &gql.Function{
-				Name: "type",
-				Args: []gql.Arg{{Value: gqlType}},
-			},
+		Children: []*gql.GraphQuery{
+			{Attr: "uid"},
+			{Attr: gqlSchemaPred},
+			{Attr: gqlSchemaXidKey, Var: xidInSchemaVar},
 		},
-		Children: []*gql.GraphQuery{{Attr: "uid"}, {Attr: gqlSchemaPred}},
 	}
-
+	/*
+	   mutation @if(eq(len(ExistingGQLSchema),1) AND eq(len(XidInSchema),0)) {
+	      set {
+	         "uid": "uid(ExistingGQLSchema)",
+	         "dgraph.graphql.xid": "dgraph.graphql.schema"
+	      }
+	   }
+	   mutation @if(eq(len(ExistingGQLSchema),0) AND eq(len(XidInSchema),0))
+	      set {
+	         "uid": "_:XidInSchema",
+	         "dgraph.type": ["dgraph.graphql"],
+	         "dgraph.graphql.xid": "dgraph.graphql.schema",
+	         "dgraph.graphql.schema": ""
+	      }
+	   }
+	*/
 	mutations := []*dgoapi.Mutation{
 		{
 			SetJson: []byte(fmt.Sprintf(`
-			{
-				"uid": "_:%s",
-				"dgraph.type": ["%s"],
-				"%s": "%s",
-				"%s": ""
-			}`, varName, gqlType, gqlSchemaXidKey, gqlSchemaXidVal, gqlSchemaPred)),
-			Cond: fmt.Sprintf(`@if(eq(len(%s),0))`, varName),
+         {
+            "uid": "uid(%s)",
+            "%s": "%s"
+         }`, existingSchemaVar, gqlSchemaXidKey, gqlSchemaXidVal)),
+			Cond: fmt.Sprintf(`@if(eq(len(%s),1) AND eq(len(%s),0))`, existingSchemaVar,
+				xidInSchemaVar),
+		},
+		{
+			SetJson: []byte(fmt.Sprintf(`
+         {
+            "uid": "_:%s",
+            "dgraph.type": ["%s"],
+            "%s": "%s",
+            "%s": ""
+         }`, xidInSchemaVar, gqlType, gqlSchemaXidKey, gqlSchemaXidVal, gqlSchemaPred)),
+			Cond: fmt.Sprintf(`@if(eq(len(%s),0) AND eq(len(%s),0))`, existingSchemaVar,
+				xidInSchemaVar),
 		},
 	}
-
 	assigned, result, err := resolve.AdminMutationExecutor().Mutate(context.Background(), qry,
 		mutations)
 	if err != nil {
 		return nil, err
 	}
-
-	// the Alpha which created the gql schema node will get the uid here
-	uid, ok := assigned[varName]
+	// the Alpha which created a new gql schema node with xid will get the uid here
+	uid, ok := assigned[xidInSchemaVar]
 	if ok {
 		return &gqlSchema{ID: uid}, nil
 	}
-
-	// the Alphas which didn't create the gql schema node, will get the uid here.
-	gqlSchemaNode := result[varName].([]interface{})[0].(map[string]interface{})
+	// the Alphas which didn't create a new gql schema node, will get the uid here.
+	gqlSchemaNode := result[existingSchemaVar].([]interface{})[0].(map[string]interface{})
 	return &gqlSchema{
 		ID:     gqlSchemaNode["uid"].(string),
 		Schema: gqlSchemaNode[gqlSchemaPred].(string),
--- graphql/admin/schema.go
+++ graphql/admin/schema.go
@@ -71,6 +71,12 @@ func (asr *updateSchemaResolver) Rewrite(
 	if err != nil {
 		return nil, nil, err
 	}
+
+	_, err = schema.FromString(schHandler.GQLSchema())
+	if err != nil {
+		return nil, nil, err
+	}
+
 	asr.newDgraphSchema = schHandler.DGSchema()
 
 	// There will always be a graphql schema node present in Dgraph cluster. So, we just need to
