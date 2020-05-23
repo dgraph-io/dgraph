efc8c91b7694d13dc790c67a691a8196965f6ca6
Michel Diz <michelconrado@outlook.com>
2020-05-22 23:27:28 -0300

    Fix URLs like...
    docs.dgraph.io to dgraph.io/docs/

--- graphql/admin/admin.go
+++ graphql/admin/admin.go
@@ -96,7 +96,7 @@ const (
 
 		"""
 		The group this node belongs to in the Dgraph cluster.
-		See : https://docs.dgraph.io/deploy/#cluster-setup.
+		See : https://dgraph.io/docs/deploy/#cluster-setup.
 		"""
 		group: Int
 
@@ -262,7 +262,7 @@ const (
 		"""
 		Starts an export of all data in the cluster.  Export format should be 'rdf' (the default
 		if no format is given), or 'json'.
-		See : https://docs.dgraph.io/deploy/#export-database
+		See : https://dgraph.io/docs/deploy/#export-database
 		"""
 		export(input: ExportInput!): ExportPayload
 
