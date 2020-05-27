386598b4e7b823e4e9866e3beb3d0663189f1623
Jatin Dev <jatin@dgraph.io>
2020-05-27 23:46:17 +0530

    Add Apollo Tracing Types

--- graphql/schema/response.go
+++ graphql/schema/response.go
@@ -21,7 +21,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-
+	"time"        
+	
 	"github.com/dgraph-io/dgraph/x"
 	"github.com/golang/glog"
 	"github.com/pkg/errors"
@@ -30,6 +31,7 @@ import (
 // Extensions represents GraphQL extensions
 type Extensions struct {
 	TouchedUids uint64 `json:"touched_uids,omitempty"`
+	Tracing   *Trace `json:"tracing,omitempty"`
 }
 
 // Merge merges ext with e
@@ -58,6 +60,87 @@ type Response struct {
 	Data       bytes.Buffer
 	Extensions *Extensions
 }
+
+
+
+
+// Trace : Apollo Tracing is a GraphQL extension for tracing resolver performance.
+// https://github.com/apollographql/apollo-tracing
+// Not part of the standard itself, it gets reported in GraphQL "extensions".
+// It's for reporting tracing data through all the resolvers in a GraphQL query.
+// Our results aren't as 'deep' as a traditional GraphQL server in that the Dgraph
+// layer resolves in a single step, rather than iteratively.  So we'll report on
+// all the top level queries/mutations.
+//
+// Currently, only reporting in the GraphQL result, but also planning to allow
+// exposing to Apollo Engine as per:
+// https://www.apollographql.com/docs/references/setup-analytics/#engine-reporting-endpoint
+type Trace struct {
+	// (comments from Apollo Tracing spec)
+
+	Version int64 `json:"version"`
+
+	// Timestamps in RFC 3339 format.
+	StartTime time.Time `json:"startTime"`
+	EndTime   time.Time `json:"endTime"`
+
+	// Duration in nanoseconds, relative to the request start, as an integer.
+	Duration int64 `json:"duration"`
+
+	Parsing    *OffsetDuration  `json:"parsing,omitempty"`
+	Validation *OffsetDuration  `json:"validation,omitempty"`
+	Execution  []*ResolverTrace `json:"execution,omitempty"`
+}
+
+// An OffsetDuration records the offset start and duration of GraphQL parsing/validation.
+type OffsetDuration struct {
+	// (comments from Apollo Tracing spec)
+
+	// Offset in nanoseconds, relative to the request start, as an integer
+	StartOffset int64 `json:"startOffset"`
+
+	// Duration in nanoseconds, relative to start of operation, as an integer.
+	Duration int64 `json:"duration"`
+}
+
+// LabeledOffsetDuration is an OffsetDuration with a string label.
+type LabeledOffsetDuration struct {
+	Label string `json:"label"`
+	OffsetDuration
+}
+
+// A ResolverTrace is a trace of one resolver.  In a traditional GraphQL server,
+// resolving say a query, would result in a ResolverTrace for the query itself
+// (with duration spanning the time to resolve the entire query) and traces for
+// every field in the query (with duration for just that field).
+//
+// Dgraph GraphQL layer resolves Queries in a single step, so each query has only
+// one associated ResolverTrace.  Mutations require two steps - the mutation itself
+// and the following query.  So mutations will have a ResolverTrace with duration
+// spanning the entire mutation (including the query part), and a trace of the query.
+// To give insight into what's actually happening there, the Dgraph time is also
+// recorded.  So for a mutation you can see total duration, mutation duration,
+// query duration and also amount of time spent by the API orchestrating the
+// mutation/query.
+type ResolverTrace struct {
+	// (comments from Apollo Tracing spec)
+
+	// the response path of the current resolver - same format as path in error
+	// result format specified in the GraphQL specification
+	Path       []interface{} `json:"path"`
+	ParentType string        `json:"parentType"`
+	FieldName  string        `json:"fieldName"`
+	ReturnType string        `json:"returnType"`
+
+	// Offset relative to request start and total duration or resolving
+	OffsetDuration
+
+	// Dgraph isn't in Apollo tracing.  It records the offsets and times
+	// of Dgraph operations for the query/mutation (including network latency)
+	// in nanoseconds.
+	Dgraph []*LabeledOffsetDuration `json:"dgraph"`
+}
+
 
 // ErrorResponse formats an error as a list of GraphQL errors and builds
 // a response with that error list and no data.  Because it doesn't add data, it
