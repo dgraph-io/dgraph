35dc57ae8693580239d42ad088a38344e1d214f7
Abhimanyu Singh Gaur <12651351+abhimanyusinghgaur@users.noreply.github.com>
2020-05-29 22:34:19 +0530

    graphql: make updateGQLSchema always return the new schema (#5540)
    This PR makes the updateGQLSchema mutation return the updated schema always, instead of relying on async badger update.
    
    Fixes #GRAPHQL-497.
    
    (cherry-picked from commit 265086f98d04e9a2bfddb5e8e40f677d2f426167)
    
    # Conflicts:
    #	graphql/admin/schema.go

--- graphql/admin/schema.go
+++ graphql/admin/schema.go
@@ -38,6 +38,10 @@ type updateSchemaResolver struct {
 
 	mutation schema.Mutation
 
+	// new GraphQL schema that is given as mutation input
+	newSchema string
+	// GraphQL schema that is generated from that input
+	generatedSchema string
 	// dgraph schema that is generated from the mutation input
 	newDgraphSchema string
 
@@ -73,11 +77,12 @@ func (asr *updateSchemaResolver) Rewrite(
 		return nil, err
 	}
 
-	_, err = schema.FromString(schHandler.GQLSchema())
+	asr.generatedSchema = schHandler.GQLSchema()
+	_, err = schema.FromString(asr.generatedSchema)
 	if err != nil {
 		return nil, err
 	}
-
+	asr.newSchema = input.Set.Schema
 	asr.newDgraphSchema = schHandler.DGSchema()
 
 	// There will always be a graphql schema node present in Dgraph cluster. So, we just need to
@@ -108,7 +113,11 @@ func (asr *updateSchemaResolver) Execute(
 	if req == nil || (req.Query == "" && len(req.Mutations) == 0) {
 		// For schema updates, Execute will get called twice.  Once for the
 		// mutation and once for the following query.  This is the query case.
-		b, err := doQuery(asr.admin.schema, asr.mutation.QueryField())
+		b, err := doQuery(&gqlSchema{
+			ID:              asr.admin.schema.ID,
+			Schema:          asr.newSchema,
+			GeneratedSchema: asr.generatedSchema,
+		}, asr.mutation.QueryField())
 		return &dgoapi.Response{Json: b}, err
 	}
 
@@ -118,7 +127,8 @@ func (asr *updateSchemaResolver) Execute(
 		return nil, err
 	}
 
-	_, err = (&edgraph.Server{}).Alter(ctx, &dgoapi.Operation{Schema: asr.newDgraphSchema})
+	_, err = (&edgraph.Server{}).Alter(ctx, &dgoapi.Operation{Schema: asr.newDgraphSchema,
+		RunInBackground: false})
 	if err != nil {
 		return nil, schema.GQLWrapf(err,
 			"succeeded in saving GraphQL schema but failed to alter Dgraph schema ")
