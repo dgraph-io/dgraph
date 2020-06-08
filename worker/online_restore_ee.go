4c35ebe705ebd5103219bf42ec09287988fcc941
Martin Martinez Rivera <martinmr@dgraph.io>
2020-06-08 14:37:25 -0700

    Change groupId from int to uint32.
    This change removes the need to convert between int and uint32.
    Group IDs are always uint32.

--- worker/online_restore_ee.go
+++ worker/online_restore_ee.go
@@ -230,7 +230,7 @@ func getEncConfig(req *pb.RestoreRequest) (*viper.Viper, error) {
 
 func writeBackup(ctx context.Context, req *pb.RestoreRequest) error {
 	res := LoadBackup(req.Location, req.BackupId,
-		func(r io.Reader, groupId int, preds predicateSet) (uint64, error) {
+		func(r io.Reader, groupId uint32, preds predicateSet) (uint64, error) {
 			cfg, err := getEncConfig(req)
 			if err != nil {
 				return 0, errors.Wrapf(err, "unable to get encryption config")
