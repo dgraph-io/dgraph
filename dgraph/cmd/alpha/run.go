a2a08a23ad5c9c22b50af1117b2d3b4de7d58365
parasssh <paras@dgraph.io>
2020-06-09 16:16:29 -0700

    Revert "sentry cid - take 1"
    This reverts commit e77fb8513643b81ce237d0294a390e7c30a7b41b.

--- dgraph/cmd/alpha/run.go
+++ dgraph/cmd/alpha/run.go
@@ -545,7 +545,6 @@ func setupServer(closer *y.Closer) {
 
 	glog.Infoln("gRPC server started.  Listening on port", grpcPort())
 	glog.Infoln("HTTP server started.  Listening on port", httpPort())
-	panic("pseudo-panic")
 	wg.Wait()
 }
 
