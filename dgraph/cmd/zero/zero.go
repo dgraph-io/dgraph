54b4ba0d479e1ffb8652c32f876cf53fa4c4260f
Martin Martinez Rivera <martinmr@dgraph.io>
2020-06-09 15:01:57 -0700

    Respect Force value in proposal.

--- dgraph/cmd/zero/zero.go
+++ dgraph/cmd/zero/zero.go
@@ -602,8 +602,13 @@ func (s *Server) ShouldServe(
 
 	// Set the tablet to be served by this server's group.
 	var proposal pb.ZeroProposal
-	// Multiple Groups might be assigned to same tablet, so during proposal we will check again.
-	tablet.Force = false
+
+	if !tablet.Force {
+		// Multiple Groups might be assigned to same tablet, so during proposal we will
+		// check again.
+		tablet.Force = false
+	}
+
 	if x.IsReservedPredicate(tablet.Predicate) {
 		// Force all the reserved predicates to be allocated to group 1.
 		// This is to make it easier to stream ACL updates to all alpha servers
