692fe4333f655ca7776e2b17c3ced097a5226676
Pawan Rawal <pawan0201@gmail.com>
2020-06-10 16:07:12 +0530

    Replace waitgroups and shutdownCh with a closer.

--- graphql/admin/shutdown.go
+++ graphql/admin/shutdown.go
@@ -19,23 +19,22 @@ package admin
 import (
 	"context"
 
+	"github.com/dgraph-io/badger/v2/y"
 	"github.com/dgraph-io/dgraph/graphql/resolve"
 	"github.com/dgraph-io/dgraph/graphql/schema"
 	"github.com/golang/glog"
 )
 
 var (
-	// ShutdownCh is used to pass the shutdown signal for the Alpha server between goroutines.
-	// When a shutdown command is issued by the user, the admin package closes this channel
-	// and as a response the setupServer() function in cmd/alpha/run.go gracefully stops the
-	// gRPC and HTTP server before stopping Alpha.
-	ShutdownCh = make(chan struct{})
+	// ServerCloser is used to signal and wait for other goroutines to return gracefully after user
+	// requests shutdown.
+	ServerCloser *y.Closer
 )
 
 func resolveShutdown(ctx context.Context, m schema.Mutation) (*resolve.Resolved, bool) {
 	glog.Info("Got shutdown request through GraphQL admin API")
 
-	close(ShutdownCh)
+	ServerCloser.Signal()
 
 	return &resolve.Resolved{
 		Data:  map[string]interface{}{m.Name(): response("Success", "Server is shutting down")},
