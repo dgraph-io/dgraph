80a65595428d9643a3b15191e5e1e9ab5ac53ea5
Manish R Jain <manish@dgraph.io>
2020-07-17 19:35:25 -0700

    Cancel the context when opening connection to leader for streaming snapshot.

--- worker/snapshot.go
+++ worker/snapshot.go
@@ -50,7 +50,9 @@ func (n *node) populateSnapshot(snap pb.Snapshot, pl *conn.Pool) (int, error) {
 	c := pb.NewWorkerClient(con)
 
 	// Set my RaftContext on the snapshot, so it's easier to locate me.
-	ctx := n.ctx
+	ctx, cancel := context.WithCancel(n.ctx)
+	defer cancel()
+
 	snap.Context = n.RaftContext
 	stream, err := c.StreamSnapshot(ctx)
 	if err != nil {
