8337137e16be637d98e9e16d0856a5b276f18569
பாலாஜி <balaji@dgraph.io>
2020-07-20 16:34:12 +0530

    add rdf output
    Signed-off-by: பாலாஜி <balaji@dgraph.io>

---
+++ query/outputrdf.go
@@ -0,0 +1,159 @@
+/*
+ * Copyright 2017-2020 Dgraph Labs, Inc. and Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package query
+
+import (
+	"fmt"
+
+	"github.com/dgraph-io/dgraph/algo"
+	"github.com/dgraph-io/dgraph/protos/pb"
+	"github.com/dgraph-io/dgraph/types"
+	"github.com/pkg/errors"
+)
+
+// ToRDF converts the given subgraph list into rdf format.
+func ToRDF(l *Latency, sgl []*SubGraph) ([]byte, error) {
+	output := ""
+	for _, sg := range sgl {
+		// Skip parent graph. we don't want parent values.
+		for _, child := range sg.Children {
+			if err := castToRDF(&output, child); err != nil {
+				return nil, err
+			}
+		}
+
+	}
+	return []byte(output), nil
+}
+
+// castToRDF converts the given subgraph to RDF and appends to the
+// output string.
+func castToRDF(output *string, sg *SubGraph) error {
+
+	uidCount := sg.Attr == "uid" && sg.Params.DoCount && sg.IsInternal()
+	if uidCount {
+		return errors.New("uid count is not supported in the rdf output format")
+	}
+
+	if sg.Params.Normalize {
+		return errors.New("normalize directive is not supported in the rdf output format")
+	}
+
+	if sg.Params.IgnoreReflex {
+		return errors.New("ignorereflex directive is not supported in the rdf output format")
+	}
+
+	if sg.SrcFunc != nil && sg.SrcFunc.Name == "checkpwd" {
+		return errors.New("chkpwd function is not supported in the rdf output format")
+	}
+
+	if sg.SrcUIDs != nil {
+		for i, uid := range sg.SrcUIDs.Uids {
+
+			if sg.Params.IgnoreResult {
+				// Skip ignored values.
+				continue
+			}
+
+			if sg.IsInternal() {
+				if sg.Params.Expand != "" {
+					continue
+				}
+				// Check if we have val for the given uid. If you got uid then populate
+				// the rdf.
+				val, ok := sg.Params.UidToVal[uid]
+				if !ok && val.Value == nil {
+					continue
+				}
+				outputval, err := valToBytes(val)
+				if err != nil {
+					return err
+				}
+				*output += fmt.Sprintf("<%#x> <%s> %s\n", uid, sg.aggWithVarFieldName(),
+					string(outputval))
+				continue
+			}
+
+			switch {
+			case len(sg.counts) > 0:
+				// Add count rdf.
+				rdfForCount(output, uid, sg.counts[i], sg)
+			case i < len(sg.uidMatrix) && len(sg.uidMatrix[i].Uids) != 0:
+				// Add posting list relation.
+				rdfForUIDList(output, uid, sg.uidMatrix[i], sg)
+			case i < len(sg.valueMatrix):
+				// TODO: add facet.
+				rdfForValueList(output, uid, sg.valueMatrix[i], sg.fieldName())
+			}
+		}
+	}
+
+	// Recursively cast the RDF of childrens.
+	for _, child := range sg.Children {
+		err := castToRDF(output, child)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// rdfForCount returns rdf for count fucntion.
+func rdfForCount(output *string, subject uint64, count uint32, sg *SubGraph) {
+	fieldName := sg.Params.Alias
+	if fieldName == "" {
+		fieldName = fmt.Sprintf("count(%s)", sg.Attr)
+	}
+	*output += fmt.Sprintf("<%#x> <%s> %d \n", subject, fieldName, count)
+}
+
+// rdfForUIDList returns rdf for uid list.
+func rdfForUIDList(output *string, subject uint64, list *pb.List, sg *SubGraph) {
+	for _, destUID := range list.Uids {
+		if algo.IndexOf(sg.DestUIDs, destUID) < 0 {
+			// This uid is filtered.
+			continue
+		}
+		// TODO: do the right RDF format.
+		*output += fmt.Sprintf("<%#x> <%s> <%#x> \n", subject, sg.fieldName(), destUID)
+	}
+}
+
+// rdfForValueList returns rdf for the value list.
+func rdfForValueList(output *string, subject uint64, valueList *pb.ValueList, attr string) error {
+	if attr == "uid" {
+		*output += fmt.Sprintf("<%#x> <%s> <%#x> \n", subject, attr, subject)
+		return nil
+	}
+	for _, destValue := range valueList.Values {
+		val, err := convertWithBestEffort(destValue, attr)
+		if err != nil {
+			return err
+		}
+		outputval, err := valToBytes(val)
+		if err != nil {
+			return err
+		}
+		switch val.Tid {
+		case types.UidID:
+			*output += fmt.Sprintf("<%#x> <%s> <%s> \n", subject, attr, string(outputval))
+		default:
+			*output += fmt.Sprintf("<%#x> <%s> %s \n", subject, attr, string(outputval))
+		}
+	}
+	return nil
+}
---
+++ query/rdf_result_test.go
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2020 Dgraph Labs, Inc. and Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package query
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestRDFResult(t *testing.T) {
+	query := `{
+		friends_15_and_19(func: uid(1)) {
+		  name
+		  friend @filter(ge(age, 15) AND lt(age, 19)) {
+			  name
+			  age
+		  }
+		}
+	  }`
+
+	rdf, err := processQueryRDF(context.Background(), t, query)
+	require.NoError(t, err)
+	require.Equal(t, string(rdf), `<0x1> <name> "Michonne" 
+<0x1> <friend> <0x17> 
+<0x1> <friend> <0x18> 
+<0x1> <friend> <0x19> 
+<0x17> <name> "Rick Grimes" 
+<0x18> <name> "Glenn Rhee" 
+<0x19> <name> "Daryl Dixon" 
+<0x17> <age> 15 
+<0x18> <age> 15 
+<0x19> <age> 17 
+`)
+}
