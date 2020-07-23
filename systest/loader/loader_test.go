fe9d5ece49a4910c34a949c81b990a34d83044b8
Anurag Sharma <anurags92@gmail.com>
2020-07-23 21:56:17 +0530

    Remove print statement and add python3 to goldendata-queries

--- systest/loader/loader_test.go
+++ systest/loader/loader_test.go
@@ -34,7 +34,6 @@ import (
 )
 
 func TestLoaderXidmap(t *testing.T) {
-	fmt.Println("Checking here for flakyness")
 	tmpDir, err := ioutil.TempDir("", "loader_test")
 	require.NoError(t, err)
 	defer os.RemoveAll(tmpDir)
