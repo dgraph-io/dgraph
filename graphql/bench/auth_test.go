b69836519a033b9c4efdc0bdc5fd402b1fcbe79e
Arijit Das <arijit@dgraph.io>
2020-07-28 09:13:57 +0530

    Add Mutation benchmark.

--- graphql/bench/auth_test.go
+++ graphql/bench/auth_test.go
@@ -21,6 +21,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"testing"
+	"time"
 
 	"github.com/dgraph-io/dgraph/graphql/authorization"
 	"github.com/dgraph-io/dgraph/graphql/e2e/common"
@@ -32,7 +33,7 @@ const (
 	graphqlURL = "http://100.83.146.128:8080/graphql"
 )
 
-func getJWT(b *testing.B, metaInfo *testutil.AuthMeta) http.Header {
+func getJWT(b require.TestingT, metaInfo *testutil.AuthMeta) http.Header {
 	jwtToken, err := metaInfo.GetSignedToken("")
 	require.NoError(b, err)
 
@@ -55,16 +56,17 @@ func getAuthMeta(schema string) *testutil.AuthMeta {
 	}
 }
 
-func clearAll(b *testing.B, metaInfo *testutil.AuthMeta) {
+func clearAll(b require.TestingT, metaInfo *testutil.AuthMeta) {
 	getParams := &common.GraphQLParams{
 		Headers: getJWT(b, metaInfo),
 		Query: `
-		mutation {
-  			deleteCuisine(filter: {}) {
+		mutation deleteCuisine($type: String) {
+  			deleteCuisine(filter: {type: { eq: $type}}) {
     			msg
   			}
 		}
 		`,
+		Variables: map[string]interface{}{"type": "TypeCuisineAuth"},
 	}
 	gqlResponse := getParams.ExecuteAsPost(b, graphqlURL)
 	require.Nil(b, gqlResponse.Errors)
@@ -72,12 +74,13 @@ func clearAll(b *testing.B, metaInfo *testutil.AuthMeta) {
 	getParams = &common.GraphQLParams{
 		Headers: getJWT(b, metaInfo),
 		Query: `
-		mutation {
-  			deleteRestaurant(filter: {}) {
+		mutation deleteRestaurant($name: String) {
+  			deleteRestaurant(filter: {name: { anyofterms: $name}}) {
     			msg
   			}
 		}
 		`,
+		Variables: map[string]interface{}{"name": "TypeRestaurantAuth"},
 	}
 	gqlResponse = getParams.ExecuteAsPost(b, graphqlURL)
 	require.Nil(b, gqlResponse.Errors)
@@ -85,12 +88,27 @@ func clearAll(b *testing.B, metaInfo *testutil.AuthMeta) {
 	getParams = &common.GraphQLParams{
 		Headers: getJWT(b, metaInfo),
 		Query: `
-		mutation {
-  			deleteDish(filter: {}) {
+		mutation deleteDish($type: String) {
+  			deleteDish(filter: {type: { eq: $type}}) {
     			msg
   			}
 		}
 		`,
+		Variables: map[string]interface{}{"type": "TypeDishAuth"},
+	}
+	gqlResponse = getParams.ExecuteAsPost(b, graphqlURL)
+	require.Nil(b, gqlResponse.Errors)
+
+	getParams = &common.GraphQLParams{
+		Headers: getJWT(b, metaInfo),
+		Query: `
+		mutation deleteOwner($name: String) {
+  			deleteOwner(filter: {name: { eq: $name}}) {
+    			msg
+  			}
+		}
+		`,
+		Variables: map[string]interface{}{"name": "TypeOwnerAuth"},
 	}
 	gqlResponse = getParams.ExecuteAsPost(b, graphqlURL)
 	require.Nil(b, gqlResponse.Errors)
@@ -265,6 +283,12 @@ type Dish struct {
 	Cuisines []Cuisine `json:"cuisines,omitempty"`
 }
 
+type Owner struct {
+	Username       string      `json:"username,omitempty"`
+	Name           string      `json:"name,omitempty"`
+	HasRestaurants Restaurants `json:"hasRestaurants,omitempty"`
+}
+
 type Cuisines []Cuisine
 
 func (c Cuisines) delete(b *testing.B, metaInfo *testutil.AuthMeta) {
@@ -301,7 +325,7 @@ func (c Cuisines) add(b *testing.B, metaInfo *testutil.AuthMeta) {
 
 type Restaurants []Restaurant
 
-func (r Restaurants) add(b *testing.B, metaInfo *testutil.AuthMeta) {
+func (r Restaurants) add(b require.TestingT, metaInfo *testutil.AuthMeta) {
 	getParams := &common.GraphQLParams{
 		Headers: getJWT(b, metaInfo),
 		Query: `
@@ -330,9 +354,9 @@ func BenchmarkOneLevelMutation(b *testing.B) {
 		"RCurr": "$",
 	}
 
-	items := 1000
+	items := 10000
 	var cusines Cuisines
-	for i := 0; i < items/2; i++ {
+	for i := 0; i < items; i++ {
 		r := Cuisine{
 			Name:   fmt.Sprintf("Test_Cuisine_%d", i),
 			Type:   "TypeCuisineAuth",
@@ -340,22 +364,14 @@ func BenchmarkOneLevelMutation(b *testing.B) {
 		}
 		cusines = append(cusines, r)
 	}
-
-	for i := items / 2; i < items; i++ {
-		r := Cuisine{
-			Name:   fmt.Sprintf("Test_Cuisine_%d", i),
-			Type:   "TypeCuisineAuth",
-			Public: false,
-		}
-		cusines = append(cusines, r)
-	}
-
 	mutations := []struct {
 		name      string
 		operation func(b *testing.B)
 	}{
 		{"add", func(b *testing.B) {
+			before := time.Now()
 			cusines.add(b, metaInfo)
+			fmt.Println("Add Time: ", time.Now().Sub(before))
 			b.StopTimer()
 			cusines.delete(b, metaInfo)
 		}},
@@ -363,7 +379,9 @@ func BenchmarkOneLevelMutation(b *testing.B) {
 			b.StopTimer()
 			cusines.add(b, metaInfo)
 			b.StartTimer()
+			before := time.Now()
 			cusines.delete(b, metaInfo)
+			fmt.Println("Delete Time: ", time.Now().Sub(before))
 		}},
 	}
 
@@ -374,6 +392,43 @@ func BenchmarkOneLevelMutation(b *testing.B) {
 			}
 		})
 	}
+}
+
+func generateMultiLevelMutationData(items int) Restaurants {
+	var restaurants Restaurants
+	ci := 1
+	di := 1
+	ri := 1
+	for ; ri <= items; ri++ {
+		r := Restaurant{
+			Xid:      fmt.Sprintf("Test_Restaurant_%d", ri),
+			Name:     "TypeRestaurantAuth",
+			Currency: "$",
+		}
+		var cuisines Cuisines
+		for ; ci%items != 0; ci++ {
+			c := Cuisine{
+				Name:   fmt.Sprintf("Test_Cuisine_%d", ci),
+				Type:   "TypeCuisineAuth",
+				Public: true,
+			}
+			var dishes []Dish
+			for ; di%items != 0; di++ {
+				d := Dish{
+					Name: fmt.Sprintf("Test_Dish_%d", di),
+					Type: "TypeDishAuth",
+				}
+				dishes = append(dishes, d)
+			}
+			di++
+			c.Dishes = dishes
+			cuisines = append(cuisines, c)
+		}
+		ci++
+		r.Cuisines = cuisines
+		restaurants = append(restaurants, r)
+	}
+	return restaurants
 }
 
 func BenchmarkMultiLevelMutation(b *testing.B) {
@@ -389,57 +444,108 @@ func BenchmarkMultiLevelMutation(b *testing.B) {
 		"RCurr": "$",
 	}
 
-	var restaurants Restaurants
-	items := 5
-	ci := 1
-	di := 1
-	for ri := 1; ri <= items; ri++ {
-		r := Restaurant{
-			Xid:      fmt.Sprintf("Test_Restaurant_%d", ri),
-			Name:     "TypeRestaurantAuth",
-			Currency: "$",
+	restaurants := generateMultiLevelMutationData(20)
+	var totalTime time.Duration
+	for i := 0; i < b.N; i++ {
+		before := time.Now()
+		restaurants.add(b, metaInfo)
+		reqTime := time.Now().Sub(before)
+		totalTime += reqTime
+		if i%10 == 0 {
+			avgTime := int64(totalTime) / int64(i+1)
+			fmt.Printf("Avg Time: %d Time: %d \n", avgTime, reqTime)
 		}
-		var cuisines Cuisines
-		for ; ci%items != 0; ci++ {
-			c := Cuisine{
-				Name:   fmt.Sprintf("Test_Cuisine_%d", ci),
-				Type:   "TypeCuisineAuth",
-				Public: true,
+		b.StopTimer()
+		clearAll(b, metaInfo)
+	}
+}
+
+func generateOwnerRestaurant(items int) Owners {
+	var owners Owners
+	ri := 1
+	oi := 1
+	for ; oi < items; oi++ {
+		var restaurants Restaurants
+		for ; ri%items != 0; ri++ {
+			r := Restaurant{
+				Xid:      fmt.Sprintf("Test_Restaurant_%d", ri),
+				Name:     "TypeRestaurantAuth",
+				Currency: "$",
 			}
-			ci++
-			var dishes []Dish
-			for ; di%items != 0; di++ {
-				d := Dish{
-					Name: fmt.Sprintf("Test_Dish_%d", di),
-					Type: "TypeDishAuth",
-				}
-				dishes = append(dishes, d)
-			}
-			di++
-			c.Dishes = dishes
-			cuisines = append(cuisines, c)
+			restaurants = append(restaurants, r)
 		}
-		r.Cuisines = cuisines
-		restaurants = append(restaurants, r)
+		ri++
+		o := Owner{
+			Username:       fmt.Sprintf("Test_User_%d", oi),
+			Name:           "TypeOwnerAuth",
+			HasRestaurants: restaurants,
+		}
+		owners = append(owners, o)
+	}
+	return owners
+}
+
+type Owners []Owner
+
+func (o Owners) add(b *testing.B, metaInfo *testutil.AuthMeta) {
+	getParams := &common.GraphQLParams{
+		Headers: getJWT(b, metaInfo),
+		Query: `
+		mutation addOwner($owners: [AddOwnerInput!]!) {
+			addOwner(input: $owners) {
+				numUids
+			}
+		}
+		`,
+		Variables: map[string]interface{}{"owners": o},
+	}
+	gqlResponse := getParams.ExecuteAsPost(b, graphqlURL)
+	require.Nil(b, gqlResponse.Errors)
+}
+
+func BenchmarkMutation(b *testing.B) {
+	schemaFile := "schema.graphql"
+	schema, err := ioutil.ReadFile(schemaFile)
+	require.NoError(b, err)
+
+	metaInfo := getAuthMeta(string(schema))
+	metaInfo.AuthVars = map[string]interface{}{
+		"Role":     "ADMIN",
+		"USERNAME": "$",
 	}
 
-	mutations := []struct {
-		name      string
-		operation func(b *testing.B)
-	}{
-		{"add", func(b *testing.B) {
-			restaurants.add(b, metaInfo)
-			b.StopTimer()
-			clearAll(b, metaInfo)
-		}},
+	owners := generateOwnerRestaurant(100)
+	owners.add(b, metaInfo)
+
+	query := `
+	query {
+		queryRestaurant (first: 300000) {
+	  		id 
+	  		name
+			owner {
+				username
+	  		}	
+		}
+	}
+	`
+
+	getUserParams := &common.GraphQLParams{
+		Headers: getJWT(b, metaInfo),
+		Query:   query,
 	}
 
-	for _, mutation := range mutations {
-		b.Run(mutation.name, func(b *testing.B) {
-			for i := 0; i < b.N; i++ {
-				mutation.operation(b)
-			}
-		})
+	var totalTime time.Duration
+	for i := 0; i < b.N; i++ {
+		before := time.Now()
+		gqlResponse := getUserParams.ExecuteAsPost(b, graphqlURL)
+		require.Nilf(b, gqlResponse.Errors, "%+v", gqlResponse.Errors)
+		reqTime := time.Now().Sub(before)
+		totalTime += reqTime
+		if i%10 == 0 {
+			avgTime := int64(totalTime) / (int64(i + 1))
+			fmt.Printf("Avg Time: %d Time: %d \n", avgTime, reqTime)
+		}
+		b.StopTimer()
+		clearAll(b, metaInfo)
 	}
-	clearAll(b, metaInfo)
 }
