28799d7cd61fa7b8dce730462fc88720757a5f5a
Anurag <anurag@dgraph.io>
2020-07-28 12:35:01 +0530

    Resolve conflicts during cherrypicking #6080

--- systest/loader/loader_test.go
+++ systest/loader/loader_test.go
@@ -17,23 +17,26 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
+	"context"
 	"io/ioutil"
-	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
+	"github.com/dgraph-io/dgo/v200/protos/api"
 	"github.com/dgraph-io/dgraph/testutil"
+	"github.com/dgraph-io/dgraph/x"
 )
 
+// TestLoaderXidmap checks that live loader re-uses xidmap on loading data from two different files
 func TestLoaderXidmap(t *testing.T) {
+	dg, err := testutil.DgraphClient(testutil.SockAddr)
+	require.NoError(t, err)
+	ctx := context.Background()
+	testutil.DropAll(t, dg)
 	tmpDir, err := ioutil.TempDir("", "loader_test")
 	require.NoError(t, err)
 	defer os.RemoveAll(tmpDir)
@@ -63,81 +66,27 @@ func TestLoaderXidmap(t *testing.T) {
 	liveCmd.Stderr = os.Stdout
 	require.NoError(t, liveCmd.Run())
 
-	exportRequest := `mutation {
-		export(input: {format: "rdf"}) {
-			response {
-				code
-				message
+	op := api.Operation{Schema: "name: string @index(exact) ."}
+	x.Check(dg.Alter(ctx, &op))
+
+	query := `
+	{
+		q(func: eq(name, "Alice")) {
+			age
+			location
+			friend{
+				name
+
+
+		"data": {
 			}
+		}
 		}
 	}`
+	expected := `{"q":[{"age":"13","location":"Wonderland","friend":[{"name":"Bob"}]}]}`
 
-	adminUrl := "http://" + testutil.SockAddrHttp + "/admin"
-	params := testutil.GraphQLParams{
-		Query:     exportRequest,
-		Variables: map[string]interface{}{"format": "json"},
-	}
-	b, err := json.Marshal(params)
+	resp, err := dg.NewReadOnlyTxn().Query(ctx, query)
 	require.NoError(t, err)
+	testutil.CompareJSON(t, expected, string(resp.GetJson()))
 
-	resp, err := http.Post(adminUrl, "application/json", bytes.NewBuffer(b))
-	require.NoError(t, err)
-	defer resp.Body.Close()
-
-	b, err = ioutil.ReadAll(resp.Body)
-	require.NoError(t, err)
-	expected := `{
-		"data": {
-		  "export": {
-			"response": {
-			  "code": "Success",
-			  "message": "Export completed."
-			}
-		  }
-		}
-	  }`
-	require.JSONEq(t, expected, string(b))
-
-	require.NoError(t, copyExportFiles(tmpDir))
-
-	dataFile, err := findFile(filepath.Join(tmpDir, "export"), ".rdf.gz")
-	require.NoError(t, err)
-
-	cmd := fmt.Sprintf("gunzip -c %s | sort", dataFile)
-	out, err := exec.Command("sh", "-c", cmd).Output()
-	require.NoError(t, err)
-
-	expected = `<0x2712> <name> "Bob" .
-<0x2> <age> "13" .
-<0x2> <friend> <0x2712> .
-<0x2> <location> "Wonderland" .
-<0x2> <name> "Alice" .
-`
-	require.Equal(t, expected, string(out))
-}
-
-func copyExportFiles(tmpDir string) error {
-	exportPath := filepath.Join(tmpDir, "export")
-	if err := os.MkdirAll(exportPath, 0755); err != nil {
-		return err
-	}
-
-	srcPath := "alpha1:/data/alpha1/export"
-	dstPath := filepath.Join(tmpDir, "export")
-	return testutil.DockerCp(srcPath, dstPath)
-}
-
-func findFile(dir string, ext string) (string, error) {
-	var fp string
-	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if strings.HasSuffix(path, ext) {
-			fp = path
-			return nil
-		}
-		return nil
-	})
-	return fp, err
 }
