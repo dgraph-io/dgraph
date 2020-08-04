cac185c93529c804c97c552f9dfa414df9eef3e7
Martin Martinez Rivera <martinmr@dgraph.io>
2020-08-04 14:51:36 -0700

    fix(Dgraph): Don't store start_ts in postings. (#6127)
    Clear out the start_ts field in the postings of deltas before they are
    marshalled to avoid storing that field in disk. This field is only meant
    to be used during in-memory processing.
    
    Related to https://discuss.dgraph.io/t/start-ts-not-being-cleared-before-postings-are-written-to-disk/9146
    
    (cherry picked from commit 9d7fc94c4c1e712cf6090581e7d35290c77b6d18)

--- posting/list.go
+++ posting/list.go
@@ -516,6 +516,9 @@ func (l *List) getMutation(startTs uint64) []byte {
 	l.RLock()
 	defer l.RUnlock()
 	if pl, ok := l.mutationMap[startTs]; ok {
+		for _, p := range pl.GetPostings() {
+			p.StartTs = 0
+		}
 		data, err := pl.Marshal()
 		x.Check(err)
 		return data
@@ -586,6 +589,7 @@ func (l *List) pickPostings(readTs uint64) (uint64, []*pb.Posting) {
 					deleteBelowTs = effectiveTs
 					continue
 				}
+				mpost.StartTs = startTs
 				posts = append(posts, mpost)
 			}
 		}
