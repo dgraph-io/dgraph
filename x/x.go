e23efa506b2d4f75a60658147a5f2722324adaf5
பாலாஜி <balaji@dgraph.io>
2020-08-07 14:17:22 +0530

    change header name
    Signed-off-by: பாலாஜி <balaji@dgraph.io>

--- x/x.go
+++ x/x.go
@@ -146,7 +146,7 @@ const (
 	AccessControlAllowedHeaders = "X-Dgraph-AccessToken, " +
 		"Content-Type, Content-Length, Accept-Encoding, Cache-Control, " +
 		"X-CSRF-Token, X-Auth-Token, X-Requested-With"
-	DgraphCostHeader = "X-Dgraph-Cost"
+	DgraphCostHeader = "numUids"
 
 	// GraphqlPredicates is the json representation of the predicate reserved for graphql system.
 	GraphqlPredicates = `
