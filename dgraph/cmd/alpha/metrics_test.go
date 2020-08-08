4cd3086a89900e48bac3b56b53e6028724edb668
Joaquin <joaquin.it@gmail.com>
2020-08-08 13:36:06 -0700

    WIP: placeholder - saving work

--- dgraph/cmd/alpha/metrics_test.go
+++ dgraph/cmd/alpha/metrics_test.go
@@ -17,6 +17,9 @@
 package alpha
 
 import (
+	"bytes"
+	"github.com/dgraph-io/dgo/v200/protos/api"
+	"github.com/dgraph-io/dgraph/x"
 	"net/http"
 	"regexp"
 	"strings"
@@ -24,6 +27,57 @@ import (
 
 	"github.com/stretchr/testify/require"
 )
+
+func TestMetricTxnAborts(t *testing.T) {
+	t.Log("a string")
+  	t.Fail()
+
+	// borrowed from integraiton/testxn/main_test.go
+	// 1. create two transactions
+	// 2. commit two transactions
+	// 3. test metric of any number, should exist
+	// 4. save result of metric
+	// 5. create 10 transactions
+	// 6. commit 10 transactions
+	// 7. test that metric is +10 from original amount
+
+	op := &api.Operation{}
+	op.DropAll = true
+	require.NoError(t, s.dg.Alter(context.Background(), op))
+
+	txn := s.dg.NewTxn()
+
+	mu := &api.Mutation{}
+	mu.SetJson = []byte(`{"name": "Manish"}`)
+	assigned, err := txn.Mutate(context.Background(), mu)
+	if err != nil {
+		log.Fatalf("Error while running mutation: %v\n", err)
+	}
+	if len(assigned.Uids) != 1 {
+		log.Fatalf("Error. Nothing assigned. %+v\n", assigned)
+	}
+	var uid string
+	for _, u := range assigned.Uids {
+		uid = u
+	}
+
+	txn2 := s.dg.NewTxn()
+	mu = &api.Mutation{}
+	mu.SetJson = []byte(fmt.Sprintf(`{"uid": "%s", "name": "Manish"}`, uid))
+	x.Check2(txn2.Mutate(context.Background(), mu))
+
+	require.NoError(t, txn.Commit(context.Background()))
+	err = txn2.Commit(context.Background())
+	x.AssertTrue(err != nil)
+
+	txn = s.dg.NewTxn()
+	q := fmt.Sprintf(`{ me(func: uid(%s)) { name }}`, uid)
+	resp, err := txn.Query(context.Background(), q)
+	if err != nil {
+		log.Fatalf("Error while running query: %v\n", err)
+	}
+	x.AssertTrue(bytes.Equal(resp.Json, []byte("{\"me\":[{\"name\":\"Manish\"}]}")))
+}
 
 func TestMetrics(t *testing.T) {
 	req, err := http.NewRequest("GET", addr+"/debug/prometheus_metrics", nil)
