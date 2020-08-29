4140cd9fde47817a6cd0487dc38d2cf140843401
Manish R Jain <manish@dgraph.io>
2020-08-28 18:53:16 -0700

    Fix(increment): Fix readTs less than minTs (#6317)
    Use the readTs for reads via LocalCache. This would ensure that users don't get a lot of ReadTs less than MinTs errors. This PR also fixes the flaky test in increment_test.go.
    
    This has a side-effect on posting list cache. That cache would need to understand versioning to be effective.
    
    Co-authored-by: Daniel Mai <daniel@dgraph.io>
    (cherry picked from commit 2de4675be0ba627e2b6475897b972ba2dfb4e1ba)

--- posting/lists.go
+++ posting/lists.go
@@ -20,7 +20,6 @@ import (
 	"context"
 	"fmt"
 	"io/ioutil"
-	"math"
 	"os"
 	"os/exec"
 	"runtime"
@@ -181,6 +180,12 @@ func NewLocalCache(startTs uint64) *LocalCache {
 	}
 }
 
+// NoCache returns a new LocalCache instance, which won't cache anything. Useful to pass startTs
+// around.
+func NoCache(startTs uint64) *LocalCache {
+	return &LocalCache{startTs: startTs}
+}
+
 func (lc *LocalCache) getNoStore(key string) *List {
 	lc.RLock()
 	defer lc.RUnlock()
@@ -205,8 +210,8 @@ func (lc *LocalCache) SetIfAbsent(key string, updated *List) *List {
 }
 
 func (lc *LocalCache) getInternal(key []byte, readFromDisk bool) (*List, error) {
-	if lc == nil {
-		return getNew(key, pstore, math.MaxUint64)
+	if lc.plists == nil {
+		return getNew(key, pstore, lc.startTs)
 	}
 	skey := string(key)
 	if pl := lc.getNoStore(skey); pl != nil {
