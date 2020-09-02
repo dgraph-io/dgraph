0d16e96966c308f59c66d0452d3ec46456db2e99
Daniel Mai <daniel@dgraph.io>
2020-09-02 08:35:15 -0700

    Don't set codename for non-v20.07 builds.

--- contrib/release.sh
+++ contrib/release.sh
@@ -117,7 +117,7 @@ pushd $basedir/dgraph
   git checkout $TAG
   # HEAD here points to whatever is checked out.
   lastCommitSHA1=$(git rev-parse --short HEAD)
-  codename="shuri"
+  codename="unnamed"
   gitBranch=$(git rev-parse --abbrev-ref HEAD)
   lastCommitTime=$(git log -1 --format=%ci)
   release_version=$(git describe --always --tags)
