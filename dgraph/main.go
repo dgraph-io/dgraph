31f95a1c823c1d4d3bcb53f9678460b98a53f831
Manish R Jain <manish@dgraph.io>
2020-09-15 06:54:24 -0700

    Pull in latest ristretto. Revert Dgraph Makefile.

--- dgraph/main.go
+++ dgraph/main.go
@@ -59,7 +59,7 @@ func main() {
 			// Read Jemalloc stats first. Print if there's a big difference.
 			z.ReadMemStats(&js)
 			if diff := absDiff(js.Active, lastJs.Active); diff > 256<<20 {
-				glog.V(2).Infof("Jemalloc: Active %s Allocated: %s Resident: %s Retained: %s\n",
+				glog.V(2).Infof("jemalloc: Active %s Allocated: %s Resident: %s Retained: %s\n",
 					humanize.IBytes(js.Active), humanize.IBytes(js.Allocated),
 					humanize.IBytes(js.Resident), humanize.IBytes(js.Retained))
 				lastJs = js
