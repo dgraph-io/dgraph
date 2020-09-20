8ebb4c7e51240b733abe2414becb4b79b08eefa5
Joaquin <joaquin.it@gmail.com>
2020-09-19 18:37:09 -0700

    wip: docker-compose and terraform

--- contrib/config/backups/azure/terraform/README.md
+++ contrib/config/backups/azure/terraform/README.md
@@ -1,1 +1,49 @@
-# Provisioning Azure Blob with Terraform
+# Azure Blob with Terraform
+
+## About
+
+This script will create required resources needed to create Azure Blog Storage using (`simple-azure-blob`)[https://github.com/darkn3rd/simple-azure-blob] module.
+
+## Prerequisites
+
+You need the following installed to use this automation:
+
+* [azure_cli](azure_cli/README.md) - Azure subscription setup
+* [terraform](terraform/README.md) - tool used to provision resources and create templates
+
+## Configuration
+
+You will need to define the following variables:
+
+* Required Variables:
+  * `resource_group_name` (required) - Azure resource group that contains the resources
+  * `storage_account_name` (required) - Azure storage account (unique global name) to contain storage
+  * `storage_container_name` (default = `dgraph-backups`) - Azure container to host the blob storage
+
+
+## Steps
+
+## Define Variables
+
+You can define these when prompted, or in `terrafrom.tfvars` file, or through command line variables, e.g. `TF_VAR_resource_group_name`, `TF_VAR_storage_account_name`.
+
+```terraform
+# terraform.tfvars
+resource_group_name  = "my-organization-resources"
+storage_account_name = "myorguniquestorage12345"
+```
+
+## Download Plugins and Modules
+
+```bash
+terraform init
+```
+
+## Prepare and Provision Resources
+
+```bash
+## get list of changes that will be made
+terraform plan
+## apply the changes
+terraform apply
+```
--- contrib/config/backups/azure/terraform/main.tf
+++ contrib/config/backups/azure/terraform/main.tf
@@ -1,13 +1,40 @@
 variable "resource_group_name" {}
 variable "storage_account_name" {}
-variable "storage_container_name" {}
+variable "storage_container_name" { default = "dgraph-backups" }
 
 ## Create Resource Group, Storage Account Name, and Container
-module "app_backups" {
-  source                  = "git::https://github.com/darkn3rd/simple-azure-blob.git?ref=v0.1"
-  resource_group_name     = var.resource_group_name
-  create_resource_group   = true
-  storage_account_name    = var.storage_account_name
-  create_storage_account  = true
-  storage_container_name  = "app-backups"
+module "dgraph_backups" {
+  source                 = "git::https://github.com/darkn3rd/simple-azure-blob.git?ref=v0.1"
+  resource_group_name    = var.resource_group_name
+  create_resource_group  = true
+  storage_account_name   = var.storage_account_name
+  create_storage_account = true
+  storage_container_name = var.storage_container_name
+}
+
+
+## Create Docker and Helm Chart Values
+
+#####################################################################
+# Locals
+#####################################################################
+
+locals {
+  minio_vars = {
+    accessKey  = module.dgraph_backups.AccountName
+    secretKey  = module.dgraph_backups.AccountKey
+  }
+
+  # minio_config  = templatefile("${path.module}/templates/values.minio_config.yaml.tmpl", local.minio_vars)
+  # minio_secrets = templatefile("${path.module}/templates/values.minio_secrets.yaml.tmpl", local.minio_vars)
+  minio_env     = templatefile("${path.module}/templates/minio.env.tmpl", local.minio_vars)
+}
+
+#####################################################################
+# File Resources
+#####################################################################
+resource "local_file" "minio_env" {
+  content         = local.minio_env
+  filename        = "${path.module}/../minio.env"
+  file_permission = "0644"
 }
