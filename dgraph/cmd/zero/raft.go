0a0c7407a8f2836244edf1a8d31754d9a7a093f0
Daniel Mai <daniel@dgraph.io>
2020-09-21 07:15:41 -0700

    Revert "Tick in a separate goroutine."
    This reverts commit c51270b4ab318b8061a065d39284830e8bdaa5d6.

--- dgraph/cmd/zero/raft.go
+++ dgraph/cmd/zero/raft.go
@@ -674,6 +674,8 @@ func (n *node) trySnapshot(skip uint64) {
 func (n *node) Run() {
 	var leader bool
 	licenseApplied := false
+	ticker := time.NewTicker(100 * time.Millisecond)
+	defer ticker.Stop()
 
 	// snapshot can cause select loop to block while deleting entries, so run
 	// it in goroutine
@@ -694,22 +696,6 @@ func (n *node) Run() {
 		closer.AddRunning(1)
 		go x.StoreSync(n.Store, closer)
 	}
-
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		ticker := time.NewTicker(100 * time.Millisecond)
-		defer ticker.Stop()
-		for {
-			select {
-			case <-ticker.C:
-				n.Raft().Tick()
-			case <-n.closer.HasBeenClosed():
-				return
-			}
-		}
-	}()
 
 	// var readNum uint64
 	// getRead := func() {
@@ -736,11 +722,10 @@ func (n *node) Run() {
 	for {
 		select {
 		case <-n.closer.HasBeenClosed():
-			wg.Wait() // Wait to stop ticking.
 			n.Raft().Stop()
 			return
-		// case <-ticker.C:
-		// 	n.Raft().Tick()
+		case <-ticker.C:
+			n.Raft().Tick()
 		case rd := <-n.Raft().Ready():
 			num++
 			if num%1000 == 0 {
