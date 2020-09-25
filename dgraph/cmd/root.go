0b4a98e5980f29bfb01ad00703c436ae2fdc16eb
Ibrahim Jarif <ibrahim@dgraph.io>
2020-09-25 20:03:31 +0530

    feat(raftmigrate): Add tool to migrate from old raft format to new one
    This PR also fixes the debug tool to support old and new raft format.

--- dgraph/cmd/root.go
+++ dgraph/cmd/root.go
@@ -30,6 +30,7 @@ import (
 	"github.com/dgraph-io/dgraph/dgraph/cmd/increment"
 	"github.com/dgraph-io/dgraph/dgraph/cmd/live"
 	"github.com/dgraph-io/dgraph/dgraph/cmd/migrate"
+	raftmigrate "github.com/dgraph-io/dgraph/dgraph/cmd/raft-migrate"
 	"github.com/dgraph-io/dgraph/dgraph/cmd/version"
 	"github.com/dgraph-io/dgraph/dgraph/cmd/zero"
 	"github.com/dgraph-io/dgraph/upgrade"
@@ -76,6 +77,7 @@ var rootConf = viper.New()
 var subcommands = []*x.SubCommand{
 	&bulk.Bulk, &cert.Cert, &conv.Conv, &live.Live, &alpha.Alpha, &zero.Zero, &version.Version,
 	&debug.Debug, &increment.Increment, &migrate.Migrate, &debuginfo.DebugInfo, &upgrade.Upgrade,
+	&raftmigrate.RaftMigrate,
 }
 
 func initCmds() {
