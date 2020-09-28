18a6c9a9eb4634bff5a5a363943e550789f7e0d9
Ibrahim Jarif <ibrahim@dgraph.io>
2020-09-28 23:56:46 +0530

    fix(bulkLoader): Use flags for cache (#6322) (#6465)
    Bulk loader uses caches in compression and this PR adds flags to make it
    configurable.
    
    Bulk loader was setting compressionLevel but not the compression option.
    As a result of this, badger wasn't compressing any data. This PR fixes
    this setting compression if compressionLevel is greater than 0.
    
    (cherry picked from commit 99341dc9f707e76b04006321acefaec4d869a000)

--- dgraph/cmd/bulk/loader.go
+++ dgraph/cmd/bulk/loader.go
@@ -69,6 +69,12 @@ type options struct {
 	ReduceShards int
 
 	shardOutputDirs []string
+
+	// ........... Badger options ..........
+	// BadgerCompressionlevel is the compression level to use while writing to badger.
+	BadgerCompressionLevel int
+	BlockCacheSize         int64
+	IndexCacheSize         int64
 }
 
 type state struct {
