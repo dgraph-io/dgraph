75586b752e47efb4a3af9b41de9dcf3b2b7f7225
Martin Martinez Rivera <martinmr@dgraph.io>
2020-09-28 16:09:50 -0700

    (release/v20.07): feat(Dgraph): add utility to export backup data. (#6550)
    This utility allows to take a single backup (full or incremental) and export the data
    and the schema inside it to RDF. Encrypted backups are supported.
    
    Fixes DGRAPH-2465
    
    (cherry picked from commit 369a5c11d5ce0d8e8ac4e29f085baf5f016c4993)

--- ee/backup/run.go
+++ ee/backup/run.go
@@ -33,15 +33,23 @@ var Restore x.SubCommand
 // LsBackup is the sub-command used to list the backups in a folder.
 var LsBackup x.SubCommand
 
+var ExportBackup x.SubCommand
+
 var opt struct {
-	backupId, location, pdir, zero string
-	key                            x.SensitiveByteSlice
-	forceZero                      bool
+	backupId    string
+	location    string
+	pdir        string
+	zero        string
+	key         x.SensitiveByteSlice
+	forceZero   bool
+	destination string
+	format      string
 }
 
 func init() {
 	initRestore()
 	initBackupLs()
+	initExportBackup()
 }
 
 func initRestore() {
@@ -246,4 +254,37 @@ func runLsbackupCmd() error {
 	}
 
 	return nil
+}
+
+func initExportBackup() {
+	ExportBackup.Cmd = &cobra.Command{
+		Use:   "export_backup",
+		Short: "Export data inside single full or incremental backup.",
+		Long:  ``,
+		Args:  cobra.NoArgs,
+		Run: func(cmd *cobra.Command, args []string) {
+			defer x.StartProfile(ExportBackup.Conf).Stop()
+			if err := runExportBackup(); err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+		},
+	}
+
+	flag := ExportBackup.Cmd.Flags()
+	flag.StringVarP(&opt.location, "location", "l", "",
+		"Sets the location of the backup. Only file URIs are supported for now.")
+	flag.StringVarP(&opt.format, "format", "f", "rdf",
+		"The format of the export output. Accepts a value of either rdf or json")
+	enc.RegisterFlags(flag)
+}
+
+func runExportBackup() error {
+	var err error
+	if opt.key, err = enc.ReadKey(ExportBackup.Conf); err != nil {
+		return err
+	}
+
+	exporter := worker.BackupExporter{}
+	return exporter.ExportBackup(opt.location, opt.destination, opt.format, opt.key)
 }
