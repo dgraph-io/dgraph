8f789c6417ee7c53b2cc35a1ce2db412535a0987
Daniel Mai <daniel@dgraph.io>
2020-09-29 09:46:15 -0700

    test: Fix ToHex test. (#6600)
    Second argument for ToHex was missing in the test.
    
        INFO: Running test for github.com/dgraph-io/dgraph/x
        ...
        # github.com/dgraph-io/dgraph/x [github.com/dgraph-io/dgraph/x.test]
        x/x_test.go:163:41: not enough arguments in call to ToHex
        	have (number)
        	want (uint64, bool)
        x/x_test.go:164:41: not enough arguments in call to ToHex
        	have (number)
        	want (uint64, bool)
        x/x_test.go:165:42: not enough arguments in call to ToHex
        	have (number)
        	want (uint64, bool)
        x/x_test.go:166:42: not enough arguments in call to ToHex
        FAIL	github.com/dgraph-io/dgraph/x [build failed]
        	have (number)
        	want (uint64, bool)
        x/x_test.go:167:56: not enough arguments in call to ToHex
        	have (number)
        	want (uint64, bool)

--- x/x_test.go
+++ x/x_test.go
@@ -160,9 +160,9 @@ func TestVersionString(t *testing.T) {
 }
 
 func TestToHex(t *testing.T) {
-	require.Equal(t, []byte(`"0x0"`), ToHex(0))
-	require.Equal(t, []byte(`"0xf"`), ToHex(15))
-	require.Equal(t, []byte(`"0x19"`), ToHex(25))
-	require.Equal(t, []byte(`"0xff"`), ToHex(255))
-	require.Equal(t, []byte(`"0xffffffffffffffff"`), ToHex(math.MaxUint64))
+	require.Equal(t, []byte(`"0x0"`), ToHex(0, false))
+	require.Equal(t, []byte(`"0xf"`), ToHex(15, false))
+	require.Equal(t, []byte(`"0x19"`), ToHex(25, false))
+	require.Equal(t, []byte(`"0xff"`), ToHex(255, false))
+	require.Equal(t, []byte(`"0xffffffffffffffff"`), ToHex(math.MaxUint64, false))
 }
