cec55674cb343c1c7f3e96dd9c5fe7867a42f5b8
Anurag <anurag@dgraph.io>
2020-09-30 12:43:46 +0530

    fix(GraphQL Query): Remove extra fields when querying interfaces (#6596)

--- graphql/resolve/resolver.go
+++ graphql/resolve/resolver.go
@@ -1268,6 +1268,10 @@ func completeObject(
 	var buf bytes.Buffer
 	comma := ""
 
+	// Below map keeps track of fields which have been seen as part of
+	// interface to avoid double entry in the resulting response
+	seenField := make(map[string]bool)
+
 	x.Check2(buf.WriteRune('{'))
 	dgraphTypes, ok := res["dgraph.type"].([]interface{})
 	for _, f := range fields {
@@ -1285,6 +1289,9 @@ func completeObject(
 		if len(dgraphTypes) > 0 {
 			includeField = f.IncludeInterfaceField(dgraphTypes)
 		}
+		if _, ok := seenField[f.ResponseName()]; ok {
+			includeField = false
+		}
 		if !includeField {
 			continue
 		}
@@ -1293,6 +1300,8 @@ func completeObject(
 		x.Check2(buf.WriteRune('"'))
 		x.Check2(buf.WriteString(f.ResponseName()))
 		x.Check2(buf.WriteString(`": `))
+
+		seenField[f.ResponseName()] = true
 
 		val := res[f.DgraphAlias()]
 		if f.Name() == schema.Typename {
