6c308bdfa49e3f471af845ac5477d55e84ed0a03
Joaquin <joaquin.it@gmail.com>
2020-10-19 16:49:00 -0700

    wip: finalized script

--- contrib/config/backups/client/README.md
+++ contrib/config/backups/client/README.md
@@ -1,1 +1,20 @@
-# Backup Client Script
+# Backup Script
+
+This backup script that supports many of the features in Dgraph, such as ACLs, MutualTLS, REST or GraphQL API.  See `./backup.sh --help` for all of the options.
+
+## Requirements
+
+This script requires GNU getopt for command line parameters. This was tested on macOS with Homebrew [gnu-getopt](https://formulae.brew.sh/formula/gnu-getopt) bottle, Ubuntu 20.04, and Windows with [MSYS2](https://www.msys2.org/).
+
+## Important Notes
+
+If you are using this script another system other than alpha, we'll call this *backup worksation*, you should be aware of the following:
+
+* General
+  * backup workstation will need access to alpha server
+* TLS
+  * when accessing alpha server secured by TLS, backup workstation will need access to `ca.crt` created with `dgraph cert`
+  * if MutualTLS is used, backup worksation will also need access to the client cert and key as well.
+* subpath option
+  * when specifying subpath that uses a datestamp, the backup workstation needs to have the same timestamp as the server.
+  * when backing to a filepath, such as NFS, the backup workstation will need access to the samefile path.
--- contrib/config/backups/client/backup.sh
+++ contrib/config/backups/client/backup.sh
@@ -1,9 +1,44 @@
 #!/usr/bin/env bash
 
+######
+# main - runs the script
+##########################
 main() {
-  parse_command
+  parse_command $@
+  run_backup
 }
 
+######
+# usage - print friendly usage statement
+##########################
+usage() {
+  cat <<-USAGE 1>&2
+Run Binary Backup
+
+Usage:
+  $0 [FLAGS] --location [LOCATION]
+
+Flags:
+ -a, --alpha string        Dgraph alpha HTTP/S server (default "127.0.0.1:8080")
+ -i, --api_type            API Type of REST or GraphQL (default "GraphQL")
+ -t, --auth_token string   The auth token passed to the server
+ -d, --debug               Enable debug in output
+ -f, --force_full          Force a full backup instead of an incremental backup.
+ -h, --help                Help for $0
+ -l, --location            Sets the source location URI (required).
+ -p, --password            Password of the user if login is required.
+     --subpath             Directory Path To Use to store backups, (default "dgraph_\$(date +%Y%m%d)")
+     --tls_cacert filepath The CA Cert file used to verify server certificates. Required for enabling TLS.
+     --tls_cert string     (optional) The Cert file provided by the client to the server.
+     --tls_key string      (optional) The private key file provided by the client to the server.
+ -u, --user                Username if login is required.
+
+USAGE
+}
+
+######
+# parse_command - parse command line options using GNU getopt
+##########################
 parse_command() {
   ## Check for GNU getopt
   if [[ "$(getopt --version)" =~ "--" ]]; then
@@ -16,54 +51,87 @@ parse_command() {
     exit 1
   fi
 
-  TEMP=$(
-    getopt -o dfma:l:t:b:u:p: --long debug,forcefull,miniosecure,alpha:,location:,subpath:,auth_token:,user:,password: \
-           -n 'backup.sh' -- "$@"
+  ## Parse Arguments with GNU getopt
+  PARSED_ARGUMENTS=$(
+    getopt -o a:i:t:dfhl:p:u: \
+    --long alpha:,api_type:,auth_token:,debug,force_full,help,location:,password:,subpath:,tls_cacert:,tls_cert:,tls_key:,user: \
+    -n 'backup.sh' -- "$@"
   )
+  if [ $? != 0 ] ; then usage; exit 1 ; fi
+  eval set -- "$PARSED_ARGUMENTS"
 
-  if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
-
-  # Note the quotes around `$TEMP': they are essential!
-  eval set -- "$TEMP"
-
-  DEBUG=false
-  ALPHA_HOST=localhost
+  ## Defaults
+  DEBUG="false"
+  ALPHA_HOST="http://localhost:8080"
   BACKUP_DESTINATION=""
-  SUB_PATH=""
-  API_TYPE=""
+  SUBPATH=dgraph_$(date +%Y%m%d)
+  API_TYPE="graphql"
   MINIO_SECURE=false
   AUTH_TOKEN=""
-  ACCESS_TOKEN=""
-  FORCE_FULL=false
+  FORCE_FULL="false"
 
+  ## Process Agurments
   while true; do
     case "$1" in
-      -d | --debug ) DEBUG=true; shift ;;
-      -f | --forcefull ) FORCE_FULL=true; shift ;;
-      -m | --miniosecure ) MINIO_SECURE=true; shift ;;
-      -a | --alpha ) ALPHA_HOST="$2"; shift 2 ;;
-      -l | --location ) BACKUP_DESTINATION="$2"; shift 2 ;;
-      -b | --subpath ) SUB_PATH="$2"; shift 2 ;;
-      -t | --auth_token ) AUTH_TOKEN="$2"; shift 2 ;;
-      -u | --user ) USER=$USER; shift 2;;
-      -p | --password ) PASSWORD=$PASSWORD; shift 2;;
-      -- ) shift; break ;;
-      * ) break ;;
+      -a | --alpha) ALPHA_HOST="$2"; shift 2 ;;
+      -i | --api_type) API_TYPE=${2,,}; shift 2;;
+      -t | --auth_token) AUTH_TOKEN="$2"; shift 2 ;;
+      -d | --debug) DEBUG=true; shift ;;
+      -f | --force_full) FORCE_FULL=true; shift ;;
+      -h | --help) usage; exit;;
+      -m | --minio_secure) MINIO_SECURE=true; shift ;;
+      -l | --location) BACKUP_DESTINATION="$2"; shift 2 ;;
+      -p | --password) PASSWORD=$PASSWORD; shift 2;;
+      --subpath) SUBPATH="$2"; shift 2 ;;
+      --tls_cacert) CACERT_PATH="$2"; shift 2 ;;
+      --tls_cert) CLIENT_CERT_PATH="$2"; shift 2;;
+      --tls_key) CLIENT_KEY_PATH="$2"; shift 2;;
+      -u | --user) USER=$USER; shift 2;;
+      --) shift; break ;;
+      *) break ;;
     esac
   done
 
-  cat <<TEST
-DEBUG=$DEBUG
-ALPHA_HOST=$ALPHA_HOST
-BACKUP_DESTINATION=$BACKUP_DESTINATION
-SUB_PATH=$SUB_PATH
-API_TYPE=$API_TYPE
-MINIO_SECURE=$MINIO_SECURE
-AUTH_TOKEN=$AUTH_TOKEN
-ACCESS_TOKEN=$ACCESS_TOKEN
-FORCE_FULL=$FORCE_FULL
-TEST
+  ## Check required variable was set
+  if [[ -z "$BACKUP_DESTINATION" ]]; then
+    printf "ERROR: location was not specified!!\n\n"
+    usage
+    exit 1
+  fi
+}
 
+######
+# run_backup - using user specified options, execute backup
+##########################
+run_backup() {
+  if [[ $DEBUG == "true" ]]; then
+    set -ex
+  else
+    set -e
+  fi
+
+  [[ -f ./backup_lib.sh ]] || { echo "ERROR: Backup Script library (./backup_lib.sh) missing" 1>&2; exit 1; }
+  source ./backup_lib.sh
+
+  echo "ALPHA_HOST=$ALPH_HOST"
+  echo "BACKUP_DESTINATION=$BACKUP_DESTINATION"
+  echo "SUBPATH=$SUBPATH"
+  echo "API_TYPE=$API_TYPE"
+  echo "MINIO_SECURE=$MINIO_SECURE"
+  echo "AUTH_TOKEN=$AUTH_TOKEN"
+
+  echo "CACERT_PATH=$CACERT_PATH"
+  echo "CLIENT_CERT_PATH=$CLIENT_CERT_PATH"
+  echo "CLIENT_KEY_PATH=$CLIENT_KEY_PATH"
+
+  ## login if user was specified
+  if ! [[ -z $USER ]]; then
+    ACCESS_TOKEN=$(get_token $USER $PASSWORD $AUTH_TOKEN )
+  fi
+
+  echo "FORCE_FULL=$FORCE_FULL"
+  ## perform backup with valid options set
+  backup "$ACCESS_TOKEN" "$AUTH_TOKEN"
 }
 
 main $@
