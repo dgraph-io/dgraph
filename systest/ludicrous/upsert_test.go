b7ca70203dd669e585bc4e29e47a788602b3deb0
Rahul Gurnani <rahulgurnani09@gmail.com>
2020-10-21 11:30:19 +0530

    fix(test): Add sleep before running ludicrous mode tests (#6766)
    * Add sleep before running the tests
    
    * Fix the assert in tests
    
    * Wait for eventual consistency

--- systest/ludicrous/upsert_test.go
+++ systest/ludicrous/upsert_test.go
@@ -23,6 +23,8 @@ import (
 	"sync"
 	"testing"
 
+	"time"
+
 	"github.com/dgraph-io/dgo/v200/protos/api"
 	"github.com/dgraph-io/dgraph/testutil"
 	"github.com/stretchr/testify/require"
@@ -73,6 +75,8 @@ func InitData(t *testing.T) {
 }
 
 func TestConcurrentUpdate(t *testing.T) {
+	// wait for server to be ready
+	time.Sleep(2 * time.Second)
 	InitData(t)
 	ctx := context.Background()
 	dg, err := testutil.DgraphClient(testutil.SockAddr)
@@ -101,6 +105,8 @@ func TestConcurrentUpdate(t *testing.T) {
 		go mutation(i)
 	}
 	wg.Wait()
+	// eventual consistency
+	time.Sleep(2 * time.Second)
 
 	q := `query all($a: string) {
 			all(func: eq(name, $a)) {
@@ -116,10 +122,12 @@ func TestConcurrentUpdate(t *testing.T) {
 	err = json.Unmarshal(res.Json, &dat)
 	require.NoError(t, err)
 
-	require.Equal(t, len(dat.All[0].Counts), 10)
+	require.Equal(t, 10, len(dat.All[0].Counts))
 }
 
 func TestSequentialUpdate(t *testing.T) {
+	// wait for server to be ready
+	time.Sleep(2 * time.Second)
 	InitData(t)
 	ctx := context.Background()
 	dg, err := testutil.DgraphClient(testutil.SockAddr)
@@ -146,6 +154,9 @@ func TestSequentialUpdate(t *testing.T) {
 		mutation(i)
 	}
 
+	// eventual consistency
+	time.Sleep(1 * time.Second)
+
 	q := `query all($a: string) {
 			all(func: eq(name, $a)) {
 			  name
@@ -160,5 +171,5 @@ func TestSequentialUpdate(t *testing.T) {
 	err = json.Unmarshal(res.Json, &dat)
 	require.NoError(t, err)
 
-	require.Equal(t, len(dat.All[0].Counts), 10)
+	require.Equal(t, 10, len(dat.All[0].Counts))
 }
