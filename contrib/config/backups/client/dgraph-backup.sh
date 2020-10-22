6dbd11634aa52581de1e01599173afc436e184c5
Joaquin <joaquin.it@gmail.com>
2020-10-21 23:17:38 -0700

    wip: comments, usage statement

--- contrib/config/backups/client/dgraph-backup.sh
+++ contrib/config/backups/client/dgraph-backup.sh
@@ -19,7 +19,7 @@ Usage:
   $0 [FLAGS] --location [LOCATION]
 
 Flags:
- -a, --alpha string        Dgraph alpha HTTP/S server (default "127.0.0.1")
+ -a, --alpha string        Dgraph alpha HTTP/S server (default "localhost")
  -i, --api_type            API Type of REST or GraphQL (default "GraphQL")
  -t, --auth_token string   The auth token passed to the server
  -d, --debug               Enable debug in output
