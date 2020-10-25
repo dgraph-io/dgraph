c242a49f3c5ee57a3fa5d41f20517cf3000a83e6
Joaquin <joaquin.it@gmail.com>
2020-10-25 03:22:42 -0700

    wip: rook update

---
+++ contrib/config/backups/nfs/charts/env.rook.sh
@@ -0,0 +1,9 @@
+## global
+export NFS_STRATEGY="rook"
+## values for rook
+export NFS_SERVER="rook-nfs"
+export NFS_PATH="share1"
+export NFS_SIZE="32Gi"
+export NFS_CLAIM="rook-nfs-pv-claim"
+## values for dgraph
+export NFS_TYPE="dynamic"
