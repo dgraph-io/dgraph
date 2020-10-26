84d7dec5e77ced6828c57859de2e128cda66c907
Joaquin <joaquin.it@gmail.com>
2020-10-26 00:19:09 -0700

    wip: finalize scheme for external and internal nfs

--- contrib/config/backups/nfs/charts/env.rook.sh
+++ contrib/config/backups/nfs/charts/env.rook.sh
@@ -1,9 +1,14 @@
 ## global
 export NFS_STRATEGY="rook"
+
 ## values for rook
 export NFS_SERVER="rook-nfs"
 export NFS_PATH="share1"
-export NFS_SIZE="32Gi"
+## storage to use by NFS server
+export NFS_DISK_SIZE="32Gi"
+## storage to use from NFS server
+export NFS_ALLOC_SIZE="32Gi"
 export NFS_CLAIM="rook-nfs-pv-claim"
-## values for dgraph
-export NFS_TYPE="dynamic"
+
+## values for dgraph (dynamic = will supply PVC claim to Dgraph)
+export VOL_TYPE="volume"
