15800c3eb1e8c78131828139196ae3de3cc3c04b
joaquin <joaquin@dgraph.io>
2020-10-26 12:36:39 -0700

    wip: update env var name for consistency of interface

--- contrib/config/backups/nfs/charts/env.rook.sh
+++ contrib/config/backups/nfs/charts/env.rook.sh
@@ -7,8 +7,8 @@ export NFS_PATH="share1"
 ## storage to use by NFS server
 export NFS_DISK_SIZE="32Gi"
 ## storage to use from NFS server
-export NFS_ALLOC_SIZE="32Gi"
-export NFS_CLAIM="rook-nfs-pv-claim"
+export NFS_CLIAM_SIZE="32Gi"
+export NFS_CLAIM_NAME="rook-nfs-pv-claim"
 
 ## values for dgraph (dynamic = will supply PVC claim to Dgraph)
 export VOL_TYPE="volume"
