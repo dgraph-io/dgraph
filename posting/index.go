e024f2088cb6738c4e6589a91c5698ba95f38104
NamanJain8 <jnaman806@gmail.com>
2020-11-03 11:43:52 +0530

    use encrypted tmpDBs for index building if encryption is enabled

--- posting/index.go
+++ posting/index.go
@@ -613,6 +613,7 @@ func (r *rebuilder) Run(ctx context.Context) error {
 		WithNumVersionsToKeep(math.MaxInt32).
 		WithLogger(&x.ToGlog{}).
 		WithCompression(options.None).
+		WithEncryptionKey(x.WorkerConfig.EncryptionKey).
 		WithLogRotatesToFlush(10)
 
 	// Set cache if we have encryption.
