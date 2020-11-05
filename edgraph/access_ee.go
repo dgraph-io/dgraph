8b392dabdd9e832d89430bfca2df2bcd6652f825
OmarAyo <omar@dgraph.io>
2020-11-05 00:22:59 +0000

    Make ACL login error message more generic (#6716) (#6842)
    (cherry picked from commit c269de85e0c5351290b9af5e1a1d3a6ede94bfd5)
    
    Co-authored-by: Animesh Chandra Pathak <animesh@dgraph.io>

--- edgraph/access_ee.go
+++ edgraph/access_ee.go
@@ -73,9 +73,8 @@ func (s *Server) Login(ctx context.Context,
 
 	user, err := s.authenticateLogin(ctx, request)
 	if err != nil {
-		errMsg := fmt.Sprintf("Authentication from address %s failed: %v", addr, err)
-		glog.Errorf(errMsg)
-		return nil, errors.Errorf(errMsg)
+		glog.Errorf("Authentication from address %s failed: %v", addr, err)
+		return nil, x.ErrorInvalidLogin
 	}
 	glog.Infof("%s logged in successfully", user.UserID)
 
