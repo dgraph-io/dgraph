d76aae9629263eb8c505b2df5376677ec09772b6
NamanJain8 <jnaman806@gmail.com>
2020-11-06 21:46:36 +0530

    free up allocators from allocatorPool

--- dgraph/main.go
+++ dgraph/main.go
@@ -99,6 +99,8 @@ func main() {
 
 	// Run the program.
 	cmd.Execute()
+	// Free up allocators from alloctors from allocatorPool.
+	z.Done()
 
 	ticker.Stop()
 	fmt.Printf("Allocated Bytes at program end: %s\n", humanize.Bytes(uint64(z.NumAllocBytes())))
