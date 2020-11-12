356f246422f261d3d3b7bda54a74ebd3adc83025
Manish R Jain <manish@dgraph.io>
2020-11-12 15:05:42 -0800

    Decrease size of key commit map

--- dgraph/cmd/zero/oracle.go
+++ dgraph/cmd/zero/oracle.go
@@ -59,6 +59,8 @@ type Oracle struct {
 	syncMarks   []syncMark
 }
 
+const keyCommitMapSz = 64 << 20
+
 // Init initializes the oracle.
 func (o *Oracle) Init() {
 	o.commits = make(map[uint64]uint64)
@@ -66,7 +68,7 @@ func (o *Oracle) Init() {
 	// to wrong results.
 	fname := filepath.Join(opts.w, "keyCommit.map")
 	os.RemoveAll(fname)
-	o.keyCommit = z.NewTree(fname, 64<<20)
+	o.keyCommit = z.NewTree(fname, keyCommitMapSz)
 	o.subscribers = make(map[int]chan pb.OracleDelta)
 	o.updates = make(chan *pb.OracleDelta, 100000) // Keeping 1 second worth of updates.
 	o.doneUntil.Init(nil)
@@ -82,7 +84,7 @@ func (o *Oracle) updateStartTxnTs(ts uint64) {
 	o.Lock()
 	defer o.Unlock()
 	o.startTxnTs = ts
-	o.keyCommit.Reset(1 << 30)
+	o.keyCommit.Reset(keyCommitMapSz)
 }
 
 // TODO: This should be done during proposal application for Txn status.
