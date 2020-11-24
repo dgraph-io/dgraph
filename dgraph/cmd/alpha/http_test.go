4bdf25d0cc8bc681bed02677bee05917bb347059
Manish R Jain <manish@dgraph.io>
2020-11-23 22:30:04 -0800

    Make TestHttpCompressionSupport run correctly (#6960)
    * Deal with context deadline correctly

--- dgraph/cmd/alpha/http_test.go
+++ dgraph/cmd/alpha/http_test.go
@@ -720,8 +720,11 @@ func TestHttpCompressionSupport(t *testing.T) {
 	require.Equal(t, "gzip", resp.Header.Get("Content-Encoding"))
 
 	// query with timeout
-	data, _, err = queryWithGz(q1, "application/dql", "", "1ms", false, false)
-	require.EqualError(t, err, ": context deadline exceeded")
+	data, _, err = queryWithGz(q1, "application/dql", "", "100us", false, false)
+	if !strings.Contains(err.Error(), "context deadline exceeded") {
+		t.Logf("Got error: %v when expecting context deadline exceeded", err)
+		t.Fail()
+	}
 	require.Equal(t, "", data)
 
 	data, resp, err = queryWithGz(q1, "application/dql", "", "1s", false, false)
