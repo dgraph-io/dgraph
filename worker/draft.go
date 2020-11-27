1c9fd5074ac4e2acdecc2605fc9c1306a413b851
Daniel Mai <daniel@dgraph.io>
2020-11-26 23:23:52 -0800

    Rename func to monitor.

--- worker/draft.go
+++ worker/draft.go
@@ -1711,7 +1711,7 @@ func (n *node) InitAndStartNode() {
 	go n.processTabletSizes()
 	go n.processApplyCh()
 	go n.BatchAndSendMessages()
-	go n.updateRaftMetrics()
+	go n.monitorRaftMetrics()
 	// Ignoring the error since InitAndStartNode does not return an error and using x.Check would
 	// not be the right thing to do.
 	_, _ = n.startTask(opRollup)
@@ -1727,10 +1727,9 @@ func (n *node) AmLeader() bool {
 	return r.Status().Lead == r.Status().ID
 }
 
-func (n *node) updateRaftMetrics() {
+func (n *node) monitorRaftMetrics() {
 	ticker := time.NewTicker(5 * time.Second)
 	defer ticker.Stop()
-
 	for range ticker.C {
 		curPendingSize := atomic.LoadInt64(&n.pendingSize)
 		ostats.Record(n.ctx, x.RaftPendingSize.M(curPendingSize))
