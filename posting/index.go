e8f8106b285976d57d5ca8a66de607de2281dc73
NamanJain8 <jnaman806@gmail.com>
2020-12-10 20:32:31 +0530

    do not try building indices when inserting a new predicate
    (cherry picked from commit 01923bf479a094f4d3e55d714fbe1a72d904734d)

--- posting/index.go
+++ posting/index.go
@@ -799,10 +799,13 @@ func (rb *IndexRebuild) DropIndexes(ctx context.Context) error {
 	if err := dropTokIndexes(ctx, rb); err != nil {
 		return err
 	}
-	if err := dropReverseEdges(ctx, rb); err != nil {
-		return err
+	prefixes = append(prefixes, prefixesToDropReverseEdges(ctx, rb)...)
+	prefixes = append(prefixes, prefixesToDropCountIndex(ctx, rb)...)
+	if len(prefixes) == 0 {
+		return nil
 	}
-	return dropCountIndex(ctx, rb)
+	glog.Infof("Deleting indexes for %s", rb.Attr)
+	return pstore.DropPrefix(prefixes...)
 }
 
 // BuildData updates data.
@@ -841,8 +844,10 @@ func (rb *IndexRebuild) needsTokIndexRebuild() indexRebuildInfo {
 	// If the old schema is nil, we can treat it as an empty schema. Copy it
 	// first to avoid overwriting it in rb.
 	old := rb.OldSchema
-	if old == nil {
-		old = &pb.SchemaUpdate{}
+	if old.Predicate == "" {
+		return indexRebuildInfo{
+			op: indexNoop,
+		}
 	}
 
 	currIndex := rb.CurrentSchema.Directive == pb.SchemaUpdate_INDEX
@@ -948,7 +953,6 @@ func rebuildTokIndex(ctx context.Context, rb *IndexRebuild) error {
 	if rebuildInfo.op != indexRebuild {
 		return nil
 	}
-
 	// Exit early if there are no tokenizers to rebuild.
 	if len(rebuildInfo.tokenizersToRebuild) == 0 {
 		return nil
@@ -998,8 +1002,8 @@ func (rb *IndexRebuild) needsCountIndexRebuild() indexOp {
 	// If the old schema is nil, treat it as an empty schema. Copy it to avoid
 	// overwriting it in rb.
 	old := rb.OldSchema
-	if old == nil {
-		old = &pb.SchemaUpdate{}
+	if old.Predicate == "" {
+		return indexNoop
 	}
 
 	// Do nothing if the schema directive did not change.
@@ -1086,8 +1090,8 @@ func (rb *IndexRebuild) needsReverseEdgesRebuild() indexOp {
 	// If old schema is nil, treat it as an empty schema. Copy it to avoid
 	// overwriting it in rb.
 	old := rb.OldSchema
-	if old == nil {
-		old = &pb.SchemaUpdate{}
+	if old.Predicate == "" {
+		return indexNoop
 	}
 
 	currIndex := rb.CurrentSchema.Directive == pb.SchemaUpdate_REVERSE
@@ -1157,7 +1161,7 @@ func rebuildReverseEdges(ctx context.Context, rb *IndexRebuild) error {
 func (rb *IndexRebuild) needsListTypeRebuild() (bool, error) {
 	x.AssertTruef(rb.CurrentSchema != nil, "Current schema cannot be nil.")
 
-	if rb.OldSchema == nil {
+	if rb.OldSchema.Predicate == "" {
 		return false, nil
 	}
 	if rb.CurrentSchema.List && !rb.OldSchema.List {
