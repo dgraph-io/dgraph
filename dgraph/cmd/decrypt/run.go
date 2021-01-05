db1c42444ee1f88547d4f71b9ae4ca71e0e7346e
OmarAyo <omar@dgraph.io>
2021-01-05 16:43:29 +0000

    additional imports

--- dgraph/cmd/decrypt/run.go
+++ dgraph/cmd/decrypt/run.go
@@ -18,7 +18,6 @@ package decrypt
 
 import (
 	"compress/gzip"
-	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -27,6 +26,7 @@ import (
 
 	"github.com/dgraph-io/dgraph/ee/enc"
 	"github.com/dgraph-io/dgraph/x"
+  "github.com/spf13/cobra"
 )
 
 type options struct {
@@ -36,6 +36,7 @@ type options struct {
 }
 
 var opts options
+var Decrypt x.SubCommand
 
 func init() {
 	Decrypt.Cmd = &cobra.Command{
@@ -46,39 +47,39 @@ A Decrypt tool that can be used to decrypt an export that has been taken from
 an encrypted Dgraph Cluster
 `,
 Run: func(cmd *cobra.Command, args []string) {
-  run()
-},
-}
-Decrypt.EnvPrefix = "DGRAPH_DECRYPT"
-flag := Decrypt.Cmd.Flags()
-flag.StringVar(&opts.keyfile, "keyfile", "", "Location of the key file to decrypt the schema "+
-"and data files")
-flag.StringVar(&opts.file, "file", "", "Path to file to decrypt")
-flag.StringVar(&opts.output, "output", "", "Path to output.")
+			run()
+		},
+	}
+	Decrypt.EnvPrefix = "DGRAPH_DECRYPT"
+	flag := Decrypt.Cmd.Flags()
+	flag.StringVar(&opts.keyfile, "keyfile", "", "Location of the key file to decrypt the schema "+
+		"and data files")
+	flag.StringVar(&opts.file, "file", "", "Path to file to decrypt")
+	flag.StringVar(&opts.output, "output", "", "Path to output.")
 }
 func run() {
-f, err := os.Open(opts.file)
-x.CheckfNoTrace(err)
-defer f.Close()
-var sensitiveKey x.SensitiveByteSlice
-sensitiveKey, err = ioutil.ReadFile(opts.keyfile)
-x.Check(err)
-reader, err := enc.GetReader(sensitiveKey, f)
-x.Check(err)
-if strings.HasSuffix(strings.ToLower(opts.file), ".gz") {
-reader, err = gzip.NewReader(reader)
-x.Check(err)
-}
-outf, err := os.OpenFile(opts.output, os.O_WRONLY|os.O_CREATE, 0644)
-x.CheckfNoTrace(err)
-w := gzip.NewWriter(outf)
-_, err = io.Copy(w, reader)
-x.Check(err)
-err = w.Flush()
-x.Check(err)
-err = w.Close()
-x.Check(err)
-err = outf.Close()
-x.Check(err)
-fmt.Printf("Done. Outputted to %v\n", opts.output)
-}
+  f, err := os.Open(opts.file)
+  	x.CheckfNoTrace(err)
+  	defer f.Close()
+  	var sensitiveKey x.SensitiveByteSlice
+  	sensitiveKey, err = ioutil.ReadFile(opts.keyfile)
+  	x.Check(err)
+  	reader, err := enc.GetReader(sensitiveKey, f)
+  	x.Check(err)
+  	if strings.HasSuffix(strings.ToLower(opts.file), ".gz") {
+  		reader, err = gzip.NewReader(reader)
+  		x.Check(err)
+  	}
+  	outf, err := os.OpenFile(opts.output, os.O_WRONLY|os.O_CREATE, 0644)
+  	x.CheckfNoTrace(err)
+  	w := gzip.NewWriter(outf)
+  	_, err = io.Copy(w, reader)
+  	x.Check(err)
+  	err = w.Flush()
+  	x.Check(err)
+  	err = w.Close()
+  	x.Check(err)
+  	err = outf.Close()
+  	x.Check(err)
+  fmt.Printf("Done. Outputted to %v\n", opts.output)
+   }
