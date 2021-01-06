dgraph-io/dgraph

commit 6884b151c355669d1387f30696566f458d0a905f

[BREAKING] opt(raftwal):  store only membership state to disk in the zero snapshot (#7143)
In zero's raftwal meta, we used to store MembershipState only. The changes in #7096 made it store ZeroSnapshot. Index and CheckpointTs actually need not be stored.
This PR makes raftwal meta store only MembershipState.

Co-authored-by: Manish R Jain <manish@dgraph.io>

dgraph/cmd/debug/wal.go
@@ -77,11 +77,11 @@ func printBasic(store RaftStore) (uint64, uint64) {
 	} else {
 		fmt.Printf("Snapshot Metadata: %+v\n", snap.Metadata)
 		var ds pb.Snapshot
-		var zs pb.ZeroSnapshot
+		var ms pb.MembershipState
 		if err := ds.Unmarshal(snap.Data); err == nil {
 			fmt.Printf("Snapshot Alpha: %+v\n", ds)
-		} else if err := zs.Unmarshal(snap.Data); err == nil {
-			for gid, group := range zs.State.GetGroups() {
+		} else if err := ms.Unmarshal(snap.Data); err == nil {
+			for gid, group := range ms.GetGroups() {
 				fmt.Printf("\nGROUP: %d\n", gid)
 				for _, member := range group.GetMembers() {
 					fmt.Printf("Member: %+v .\n", member)
@@ -93,8 +93,8 @@ func printBasic(store RaftStore) (uint64, uint64) {
 				group.Tablets = nil
 				fmt.Printf("Group: %d %+v .\n", gid, group)
 			}
-			zs.State.Groups = nil
-			fmt.Printf("\nSnapshot Zero: %+v\n", zs)
+			ms.Groups = nil
+			fmt.Printf("\nSnapshot Zero: %+v\n", ms)
 		} else {
 			fmt.Printf("Unable to unmarshal Dgraph snapshot: %v", err)
 		}

dgraph/cmd/zero/raft.go
@@ -311,7 +311,9 @@ func (n *node) applySnapshot(snap *pb.ZeroSnapshot) error {
 	}
 	n.server.orc.purgeBelow(snap.CheckpointTs)
 
-	data, err := snap.Marshal()
+	// We are storing only the MembershipState in the meta file. The other 2 fields of ZeroSnapshot;
+	// Index and CheckpointTs need not be persisted as they are used only for in-memory operations.
+	data, err := snap.GetState().Marshal()
 	x.Check(err)
 
 	for {
@@ -543,11 +545,11 @@ func (n *node) initAndStartNode() error {
 			// It is important that we pick up the conf state here.
 			n.SetConfState(&sp.Metadata.ConfState)
 
-			var zs pb.ZeroSnapshot
-			x.Check(zs.Unmarshal(sp.Data))
-			n.server.SetMembershipState(zs.State)
+			var ms pb.MembershipState
+			x.Check(ms.Unmarshal(sp.Data))
+			n.server.SetMembershipState(&ms)
 			for _, id := range sp.Metadata.ConfState.Nodes {
-				n.Connect(id, zs.State.Zeros[id].Addr)
+				n.Connect(id, ms.Zeros[id].Addr)
 			}
 		}
 

commit 0e4f76b2a27b3da974f5623fe4998cccc76ef3b1

[UNDO-BREAKING] fix(zero-snapshot): Do not break the raftwal format due to ZeroSnapshot (#7249)
We introduced a change in #7143 that would have been breaking for the next release v21.03. That change should have been a part of release/v20.11 but was mistakenly missed. We are doing a revert so that release/v21.03 is not breaking due to that change.

Co-authored-by: Ibrahim Jarif <ibrahim@dgraph.io>

dgraph/cmd/debug/wal.go
@@ -74,11 +74,11 @@ func printBasic(store RaftStore) (uint64, uint64) {
 	} else {
 		fmt.Printf("Snapshot Metadata: %+v\n", snap.Metadata)
 		var ds pb.Snapshot
-		var ms pb.MembershipState
+		var zs pb.ZeroSnapshot
 		if err := ds.Unmarshal(snap.Data); err == nil {
 			fmt.Printf("Snapshot Alpha: %+v\n", ds)
-		} else if err := ms.Unmarshal(snap.Data); err == nil {
-			for gid, group := range ms.GetGroups() {
+		} else if err := zs.Unmarshal(snap.Data); err == nil {
+			for gid, group := range zs.State.GetGroups() {
 				fmt.Printf("\nGROUP: %d\n", gid)
 				for _, member := range group.GetMembers() {
 					fmt.Printf("Member: %+v .\n", member)
@@ -90,8 +90,8 @@ func printBasic(store RaftStore) (uint64, uint64) {
 				group.Tablets = nil
 				fmt.Printf("Group: %d %+v .\n", gid, group)
 			}
-			ms.Groups = nil
-			fmt.Printf("\nSnapshot Zero: %+v\n", ms)
+			zs.State.Groups = nil
+			fmt.Printf("\nSnapshot Zero: %+v\n", zs)
 		} else {
 			fmt.Printf("Unable to unmarshal Dgraph snapshot: %v", err)
 		}

dgraph/cmd/zero/raft.go
@@ -316,9 +316,7 @@ func (n *node) applySnapshot(snap *pb.ZeroSnapshot) error {
 	}
 	n.server.orc.purgeBelow(snap.CheckpointTs)
 
-	// We are storing only the MembershipState in the meta file. The other 2 fields of ZeroSnapshot;
-	// Index and CheckpointTs need not be persisted as they are used only for in-memory operations.
-	data, err := snap.GetState().Marshal()
+	data, err := snap.Marshal()
 	x.Check(err)
 
 	for {
@@ -550,11 +548,11 @@ func (n *node) initAndStartNode() error {
 			// It is important that we pick up the conf state here.
 			n.SetConfState(&sp.Metadata.ConfState)
 
-			var ms pb.MembershipState
-			x.Check(ms.Unmarshal(sp.Data))
-			n.server.SetMembershipState(&ms)
+			var zs pb.ZeroSnapshot
+			x.Check(zs.Unmarshal(sp.Data))
+			n.server.SetMembershipState(zs.State)
 			for _, id := range sp.Metadata.ConfState.Nodes {
-				n.Connect(id, ms.Zeros[id].Addr)
+				n.Connect(id, zs.State.Zeros[id].Addr)
 			}
 		}
 
@@ -869,9 +867,9 @@ func (n *node) Run() {
 			}
 
 			if !raft.IsEmptySnap(rd.Snapshot) {
-				var state pb.MembershipState
-				x.Check(state.Unmarshal(rd.Snapshot.Data))
-				n.server.SetMembershipState(&state)
+				var zs pb.ZeroSnapshot
+				x.Check(zs.Unmarshal(rd.Snapshot.Data))
+				n.server.SetMembershipState(zs.State)
 			}
 
 			for _, entry := range rd.CommittedEntries {