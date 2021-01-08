79811bd1b1db7960031dcaed2ce58965d69c49e8
OmarAyo <omar@dgraph.io>
2021-01-08 17:26:57 +0000

    feat(tool/decrypt): Add dgraph decrypt tool (#7248) (#7255)
    Create a tool `dgraph tool decrypt` to decrypt encrypted export data.
    
    e.g.
    ```
    $ dgraph tool -h
    [Decoder]: Using assembly version of decoder
    A suite of Dgraph tools
    
    Usage:
      dgraph tool [command]
    
    Available Commands:
      decrypt     Run Decrypt tool
    ```
    
    ```
    $ dgraph tool decrypt -h
    [Decoder]: Using assembly version of decoder
    A tool to decrypt an export file created by an encrypted Dgraph cluster
    
    Usage:
      dgraph tool decrypt [flags]
    
    Flags:
          --encryption_key_file string   The file that stores the symmetric key of length 16, 24, or 32 bytes. The key size determines the chosen AES cipher (AES-128, AES-192, and AES-256 respectively). Enterprise feature.
      -f, --file string                  Path to file to decrypt.
      -h, --help                         help for decrypt
      -o, --out string                   Path to the decrypted file.
          --vault_addr string            Vault server's address in the form http://ip:port. (default "http://localhost:8200")
          --vault_field string           Vault kv store field whose value is the Base64 encoded encryption key. (default "enc_key")
          --vault_format string          Vault field format. raw or base64 (default "base64")
          --vault_path string            Vault kv store path. e.g. secret/data/dgraph for kv-v2, kv/dgraph for kv-v1. (default "secret/data/dgraph")
          --vault_roleid_file string     File containing Vault role-id used for approle auth.
          --vault_secretid_file string   File containing Vault secret-id used for approle auth.
    ```
    
    Changes:
    * feat: Add `dgraph tool decrypt` command
    * chore(ee): Remove glog when reading encryption key file.
    
    (cherry picked from commit f53e6c0af9f53b78dafc80bdef76532191f71d2f)
    (cherry picked from commit b33b93f7090c416d3a7d20987e34d3263624e682)

--- dgraph/cmd/root.go
+++ dgraph/cmd/root.go
@@ -32,6 +32,7 @@ import (
 	"github.com/dgraph-io/dgraph/dgraph/cmd/migrate"
 	"github.com/dgraph-io/dgraph/dgraph/cmd/version"
 	"github.com/dgraph-io/dgraph/dgraph/cmd/zero"
+	"github.com/dgraph-io/dgraph/dgraph/cmd/tool"
 	"github.com/dgraph-io/dgraph/upgrade"
 	"github.com/dgraph-io/dgraph/x"
 
@@ -75,7 +76,7 @@ var rootConf = viper.New()
 // subcommands initially contains all default sub-commands.
 var subcommands = []*x.SubCommand{
 	&bulk.Bulk, &cert.Cert, &conv.Conv, &live.Live, &alpha.Alpha, &zero.Zero, &version.Version,
-	&debug.Debug, &increment.Increment, &migrate.Migrate, &debuginfo.DebugInfo, &upgrade.Upgrade,
+	&debug.Debug, &increment.Increment, &migrate.Migrate, &debuginfo.DebugInfo, &upgrade.Upgrade, tool.Tool
 }
 
 func initCmds() {
