8686dce4bf5ec22f559e16844b512e2e2ab641a8
NamanJain8 <jnaman806@gmail.com>
2021-01-11 16:03:25 +0530

    remove debug func

--- x/x.go
+++ x/x.go
@@ -31,7 +31,6 @@ import (
 	"net/http"
 	"os"
 	"regexp"
-	"runtime"
 	"sort"
 	"strconv"
 	"strings"
@@ -1202,37 +1201,4 @@ func ToHex(i uint64, rdf bool) []byte {
 	}
 
 	return out
-}
-
-func GetCurrentFunctionName() string {
-	// Skip GetCurrentFunctionName
-	return getFrame(1).Function
-}
-
-func GetCallerFunctionName() string {
-	// Skip GetCallerFunctionName and the function to get the caller of
-	return getFrame(2).Function
-}
-
-func getFrame(skipFrames int) runtime.Frame {
-	// We need the frame at index skipFrames+2, since we never want runtime.Callers and getFrame
-	targetFrameIndex := skipFrames + 2
-
-	// Set size to targetFrameIndex+2 to ensure we have room for one more caller than we need
-	programCounters := make([]uintptr, targetFrameIndex+2)
-	n := runtime.Callers(0, programCounters)
-
-	frame := runtime.Frame{Function: "unknown"}
-	if n > 0 {
-		frames := runtime.CallersFrames(programCounters[:n])
-		for more, frameIndex := true, 0; more && frameIndex <= targetFrameIndex; frameIndex++ {
-			var frameCandidate runtime.Frame
-			frameCandidate, more = frames.Next()
-			if frameIndex == targetFrameIndex {
-				frame = frameCandidate
-			}
-		}
-	}
-
-	return frame
 }
