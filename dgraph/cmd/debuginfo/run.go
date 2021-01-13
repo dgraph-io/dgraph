2a962d71a21fc45fc3058ffa3f0757c67025fef5
OmarAyo <omar@dgraph.io>
2021-01-13 10:48:02 +0000

    updated copyright

--- dgraph/cmd/debuginfo/run.go
+++ dgraph/cmd/debuginfo/run.go
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2020 Dgraph Labs, Inc. and Contributors
+ * Copyright 2019-2021 Dgraph Labs, Inc. and Contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
