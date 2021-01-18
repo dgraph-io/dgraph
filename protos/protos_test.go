afa02c5e77b429bd7315f07d363b0eca927e932d
Daniel Mai <daniel@dgraph.io>
2021-01-18 10:32:03 -0800

    test: Add check if protos changed.

--- protos/protos_test.go
+++ protos/protos_test.go
@@ -1,6 +1,7 @@
 package protos
 
 import (
+	"path/filepath"
 	"testing"
 
 	"github.com/dgraph-io/dgraph/testutil"
@@ -10,4 +11,8 @@ import (
 func TestProtosRegenerate(t *testing.T) {
 	err := testutil.Exec("make", "regenerate")
 	require.NoError(t, err, "Got error while regenerating protos: %v\n", err)
+
+	generatedProtos := filepath.Join("pb", "pb.pb.go")
+	err = testutil.Exec("git", "diff", "--quiet", "--", generatedProtos)
+	require.NoError(t, err, "pb.pb.go changed after regenerating")
 }
