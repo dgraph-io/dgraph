764803c179359585425c7e2ae245e64f000410fb
Tejas Dinkar <tejas@gja.in>
2021-01-26 00:56:51 +0530

    feat(Bulk Loader + Live Loader): Supporting Loading files via s3/minio (#7359)
    It is now possible to pass an s3 / minio path to bulk and live loader. -f s3:///<bucket>/path-to-rdf

--- testutil/exec.go
+++ testutil/exec.go
@@ -127,6 +127,11 @@ func pipelineInternal(cmds [][]string, opts []CmdOpts) (string, error) {
 }
 
 func DgraphBinaryPath() string {
+	// Useful for OSX, as $GOPATH/bin/dgraph is set to the linux binary for docker
+	if dgraphBinary := os.Getenv("DGRAPH_BINARY"); dgraphBinary != "" {
+		return dgraphBinary
+	}
+
 	gopath := os.Getenv("GOPATH")
 
 	if gopath == "" {
