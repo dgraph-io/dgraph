5fc028cb2ff2f6279580635c625388945a9835a1
Karl McGuire <karl@xba.io>
2021-01-28 16:32:59 -0500

    grabbing heap profiles during backup

--- graphql/admin/backup.go
+++ graphql/admin/backup.go
@@ -19,6 +19,10 @@ package admin
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"os"
+	"runtime/pprof"
+	"time"
 
 	"github.com/dgraph-io/dgraph/graphql/resolve"
 	"github.com/dgraph-io/dgraph/graphql/schema"
@@ -33,6 +37,18 @@ type backupInput struct {
 }
 
 func resolveBackup(ctx context.Context, m schema.Mutation) (*resolve.Resolved, bool) {
+	go func() {
+		for i := 0; ; i++ {
+			<-time.After(time.Second * 1)
+			f, err := os.OpenFile(fmt.Sprintf("/data/heap_%d", i), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("-----------------------------------------: %d\n", i)
+			pprof.Lookup("heap").WriteTo(f, 0)
+		}
+	}()
+
 	glog.Info("Got backup request")
 
 	input, err := getBackupInput(m)
