b9cee9bd0bf3be458fed09ace4b34ea607a0af08
Tejas Dinkar <tejas@gja.in>
2021-01-28 23:22:33 +0530

    fix: Fixing the content type for /probe/graphql

--- dgraph/cmd/alpha/run.go
+++ dgraph/cmd/alpha/run.go
@@ -475,9 +475,9 @@ func setupServer(closer *z.Closer) {
 		if !healthStatus.Healthy {
 			httpStatusCode = http.StatusServiceUnavailable
 		}
-		w.WriteHeader(httpStatusCode)
 		w.Header().Set("Content-Type", "application/json")
 		x.AddCorsHeaders(w)
+		w.WriteHeader(httpStatusCode)
 		x.Check2(w.Write([]byte(fmt.Sprintf(`{"status":"%s","schemaUpdateCounter":%d}`,
 			healthStatus.StatusMsg, atomic.LoadUint64(&globalEpoch)))))
 	})
