959a03a7974fae90f162b83acd7fd3f2da693a08
minhaj-shakeel <minhaj@dgraph.io>
2021-02-02 13:10:04 +0530

    remove issue test

--- graphql/e2e/auth/auth_test.go
+++ graphql/e2e/auth/auth_test.go
@@ -496,46 +496,27 @@ func TestAuthOnInterfaces(t *testing.T) {
 }
 
 func TestAuthRulesWithNullValuesInJWT(t *testing.T) {
-	testCases := []TestCase{
-		{
-			name: "Query with null value in jwt: top level",
-			query: `
+	tcase := TestCase{
+		name: "Query with null value in jwt",
+		query: `
 			query {
 				queryProject {
 					name
 				}
 			}
 			`,
-			result: `{"queryProject":[]}`,
-		},
-		{
-			name: "Query with null value in jwt: deep level",
-			query: `
-			query {
-				queryUser(order: {asc: username}, first: 1) {
-					username
-					issues {
-						msg
-					}
-				}
-			}
-			`,
-			role:   "ADMIN",
-			result: `{"queryUser":[{"username":"user1","issues":[]}]}`,
-		},
+		result: `{"queryProject":[]}`,
 	}
 
-	for _, tcase := range testCases {
-		queryParams := &common.GraphQLParams{
-			Headers: common.GetJWTWithNullUser(t, tcase.role, metaInfo),
-			Query:   tcase.query,
-		}
-		gqlResponse := queryParams.ExecuteAsPost(t, common.GraphqlURL)
-		common.RequireNoGQLErrors(t, gqlResponse)
+	queryParams := &common.GraphQLParams{
+		Headers: common.GetJWTWithNullUser(t, tcase.role, metaInfo),
+		Query:   tcase.query,
+	}
+	gqlResponse := queryParams.ExecuteAsPost(t, common.GraphqlURL)
+	common.RequireNoGQLErrors(t, gqlResponse)
 
-		if diff := cmp.Diff(tcase.result, string(gqlResponse.Data)); diff != "" {
-			t.Errorf("Test: %s result mismatch (-want +got):\n%s", tcase.name, diff)
-		}
+	if diff := cmp.Diff(tcase.result, string(gqlResponse.Data)); diff != "" {
+		t.Errorf("Test: %s result mismatch (-want +got):\n%s", tcase.name, diff)
 	}
 }
 func TestAuthRulesWithMissingJWT(t *testing.T) {
