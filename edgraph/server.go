6130bfab1cb181f5b570066dbbf9dc2985c9f85a
NamanJain8 <jnaman806@gmail.com>
2021-02-17 20:20:08 +0530

    clean up

--- edgraph/server.go
+++ edgraph/server.go
@@ -1018,18 +1018,21 @@ func (s *Server) Health(ctx context.Context, all bool) (*api.Response, error) {
 
 // Filter out the tablets that do not belong to the requestor's namespace.
 func filterTablets(ctx context.Context, ms *pb.MembershipState) error {
+	if !x.WorkerConfig.AclEnabled {
+		return nil
+	}
 	namespace, err := x.ExtractJWTNamespace(ctx)
 	if err != nil {
 		return errors.Errorf("Namespace not found in JWT.")
 	}
 	if namespace == x.GalaxyNamespace {
+		// For galaxy namespace, we don't want to filter out the predicates.
 		return nil
 	}
 	for _, group := range ms.GetGroups() {
 		tablets := make(map[string]*pb.Tablet)
 		for pred, tablet := range group.GetTablets() {
-			ns, attr := x.ParseNamespaceAttr(pred)
-			if namespace == ns {
+			if ns, attr := x.ParseNamespaceAttr(pred); namespace == ns {
 				tablets[attr] = tablet
 				tablets[attr].Predicate = attr
 			}
