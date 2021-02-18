69a0a9ebb21453041c4f3c1de00e1c6ebbb30677
Rajas Vanjape <rajas@dgraph.io>
2021-02-18 11:08:15 +0530

    Make XID node referencing invariant of order

--- graphql/resolve/mutation_rewriter.go
+++ graphql/resolve/mutation_rewriter.go
@@ -205,7 +205,12 @@ func (xidMetadata *xidMetadata) isDuplicateXid(atTopLevel bool, xidVar string,
 		}
 	}
 
-	if len(newXidObj) > 1 && !reflect.DeepEqual(xidMetadata.variableObjMap[xidVar], newXidObj) {
+	// We return an error if both occurrences of xid contain more than one values
+	// and are not equal.
+	// XID should be defined with all its values at one of the places and references with its
+	// XID from other places.
+	if len(newXidObj) > 1 && len(xidMetadata.variableObjMap[xidVar]) > 1 &&
+		!reflect.DeepEqual(xidMetadata.variableObjMap[xidVar], newXidObj) {
 		return true
 	}
 
@@ -1315,6 +1320,10 @@ func rewriteObject(
 						exclude = invField.Name()
 					}
 				}
+				// We replace obj with xidMetadata.variableObjMap[variable] in this case.
+				// This is done to ensure that the first time we encounter an XID node, we use
+				// its definition and later times, we just use its reference.
+				obj = xidMetadata.variableObjMap[variable]
 				if err := typ.EnsureNonNulls(obj, exclude); err != nil {
 					// This object does not contain XID. This is an error.
 					retErrors = append(retErrors, err)
@@ -1571,15 +1580,29 @@ func existenceQueries(
 			variable := varGen.Next(typ, xid.Name(), xidString, false)
 
 			if xidMetadata.variableObjMap[variable] != nil {
-				// if we already encountered an object with same xid earlier, and this object is
-				// considered a duplicate of the existing object, then return error.
+				// There are two cases:
+				// Case 1: We are at top level:
+				// 	       We return an error if the same node is referenced twice at top level.
+				// Case 2: We are not at top level:
+				//         We don't return an error if one of the occurrences of XID is a reference
+				//         and other is definition.
+				//         We return an error if both occurrences contain values other than XID and are
+				//         not equal.
 				if xidMetadata.isDuplicateXid(atTopLevel, variable, obj, srcField) {
 					err := errors.Errorf("duplicate XID found: %s", xidString)
 					retErrors = append(retErrors, err)
 					return nil, retErrors
 				}
-				// In the other case it is not duplicate. In this case we don't move ahead and
-				// stop processing.
+				// In the other case it is not duplicate, we update variableObjMap in case the new
+				// occurrence of XID is its description and the old occurrence was a reference.
+				// Example:
+				// obj = { "id": "1", "name": "name1"}
+				// xidMetadata.variableObjMap[variable] = { "id": "1" }
+				// In this case, as obj is the correct definition of the object, we update variableObjMap
+				oldObj := xidMetadata.variableObjMap[variable]
+				if len(oldObj) == 1 && len(obj) > 1 {
+					xidMetadata.variableObjMap[variable] = obj
+				}
 				return ret, retErrors
 			}
 
