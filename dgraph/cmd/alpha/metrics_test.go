f21ee3672c452aead4c337efdefecf48b75a822f
Daniel Mai <daniel@dgraph.io>
2021-03-02 13:18:11 -0800

    fix(metrics): Expose Badger LSM and vlog size bytes. (#7488)
    Expose the Badger metrics for the lsm tree size and vlog size.
    
    * badger_v3_lsm_size_bytes
    * badger_v3_vlog_size_bytes

--- dgraph/cmd/alpha/metrics_test.go
+++ dgraph/cmd/alpha/metrics_test.go
@@ -183,6 +183,9 @@ func TestMetrics(t *testing.T) {
 		"badger_v3_disk_reads_total", "badger_v3_disk_writes_total", "badger_v3_gets_total",
 		"badger_v3_memtable_gets_total", "badger_v3_puts_total", "badger_v3_read_bytes",
 		"badger_v3_written_bytes",
+		// The following metrics get exposed after 1 minute from Badger, so
+		// they're not available in time for this test
+		// "badger_v3_lsm_size_bytes", "badger_v3_vlog_size_bytes",
 
 		// Transaction Metrics
 		"dgraph_txn_aborts_total", "dgraph_txn_commits_total", "dgraph_txn_discards_total",
