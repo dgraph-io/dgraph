fd797fdf83fd61dfd840e4b61659067e3f30c22a
Ajeet D'Souza <98ajeet@gmail.com>
2021-03-08 11:48:28 +0530

    Remove vault test due to dependency conflict

--- ee/vault/vault_test.go
+++ ee/vault/vault_test.go
@@ -12,139 +12,141 @@
 
 package vault
 
-import (
-	"encoding/base64"
-	"fmt"
-	"io/ioutil"
-	"os"
-	"testing"
+// NOTE: Vault causes dependency issues, so this test has been commented out.
 
-	"github.com/hashicorp/vault/api"
-	"github.com/hashicorp/vault/builtin/credential/approle"
-	"github.com/hashicorp/vault/http"
-	"github.com/hashicorp/vault/vault"
-	"github.com/spf13/viper"
-	"github.com/stretchr/testify/require"
-)
+// import (
+// 	"encoding/base64"
+// 	"fmt"
+// 	"io/ioutil"
+// 	"os"
+// 	"testing"
 
-const (
-	path = "secret/data/dgraph"
-)
+// 	"github.com/hashicorp/vault/api"
+// 	"github.com/hashicorp/vault/builtin/credential/approle"
+// 	"github.com/hashicorp/vault/http"
+// 	"github.com/hashicorp/vault/vault"
+// 	"github.com/spf13/viper"
+// 	"github.com/stretchr/testify/require"
+// )
 
-func TestGetKeys(t *testing.T) {
-	const (
-		aclField = "acl-field"
-		encField = "enc-field"
+// const (
+// 	path = "secret/data/dgraph"
+// )
 
-		aclKey = "01234567890123456789012345678901"
-		encKey = "0123456789012345"
-	)
+// func TestGetKeys(t *testing.T) {
+// 	const (
+// 		aclField = "acl-field"
+// 		encField = "enc-field"
 
-	// Set up Vault server.
-	client := newTestServer(t)
-	roleIdFile, secretIdFile := addTestRole(t, client)
+// 		aclKey = "01234567890123456789012345678901"
+// 		encKey = "0123456789012345"
+// 	)
 
-	// Add ACL and encryption keys to Vault.
-	_, err := client.Logical().Write(path, map[string]interface{}{
-		aclField: base64.StdEncoding.EncodeToString([]byte(aclKey)),
-		encField: base64.StdEncoding.EncodeToString([]byte(encKey)),
-	})
-	require.NoError(t, err)
+// 	// Set up Vault server.
+// 	client := newTestServer(t)
+// 	roleIdFile, secretIdFile := addTestRole(t, client)
 
-	// Configure Vault in Dgraph.
-	config := viper.New()
-	config.Set(flagVault, fmt.Sprintf(
-		`%s=%s;%s=%s;%s=%s;%s=%s;%s=%s;%s=%s;%s=%s;%s=%s;`,
-		flagAddr, client.Address(),
-		flagRoleIdFile, roleIdFile,
-		flagSecretIdFile, secretIdFile,
-		flagPath, path,
-		flagAclField, aclField,
-		flagAclFormat, "base64",
-		flagEncField, encField,
-		flagEncFormat, "base64"))
+// 	// Add ACL and encryption keys to Vault.
+// 	_, err := client.Logical().Write(path, map[string]interface{}{
+// 		aclField: base64.StdEncoding.EncodeToString([]byte(aclKey)),
+// 		encField: base64.StdEncoding.EncodeToString([]byte(encKey)),
+// 	})
+// 	require.NoError(t, err)
 
-	// Retrieve keys using Dgraph and compare.
-	aclKeyGot, encKeyGot := GetKeys(config)
-	require.Equal(t, aclKey, string(aclKeyGot))
-	require.Equal(t, encKey, string(encKeyGot))
-}
+// 	// Configure Vault in Dgraph.
+// 	config := viper.New()
+// 	config.Set(flagVault, fmt.Sprintf(
+// 		`%s=%s;%s=%s;%s=%s;%s=%s;%s=%s;%s=%s;%s=%s;%s=%s;`,
+// 		flagAddr, client.Address(),
+// 		flagRoleIdFile, roleIdFile,
+// 		flagSecretIdFile, secretIdFile,
+// 		flagPath, path,
+// 		flagAclField, aclField,
+// 		flagAclFormat, "base64",
+// 		flagEncField, encField,
+// 		flagEncFormat, "base64"))
 
-// newServer starts an AppRole-enabled Vault server and returns a client for it.
-func newTestServer(t *testing.T) *api.Client {
-	// Start up Vault server.
-	vault.AddTestCredentialBackend("approle", approle.Factory)
-	core, _, rootToken := vault.TestCoreUnsealed(t)
-	_, address := http.TestServer(t, core)
+// 	// Retrieve keys using Dgraph and compare.
+// 	aclKeyGot, encKeyGot := GetKeys(config)
+// 	require.Equal(t, aclKey, string(aclKeyGot))
+// 	require.Equal(t, encKey, string(encKeyGot))
+// }
 
-	// Create configuration.
-	config := api.DefaultConfig()
-	config.Address = address
+// // newServer starts an AppRole-enabled Vault server and returns a client for it.
+// func newTestServer(t *testing.T) *api.Client {
+// 	// Start up Vault server.
+// 	vault.AddTestCredentialBackend("approle", approle.Factory)
+// 	core, _, rootToken := vault.TestCoreUnsealed(t)
+// 	_, address := http.TestServer(t, core)
 
-	// Create Vault client.
-	client, err := api.NewClient(config)
-	if err != nil {
-		t.Fatal(err)
-	}
-	client.SetToken(rootToken)
-	client.Sys().EnableAuthWithOptions("approle/", &api.EnableAuthOptions{Type: "approle"})
+// 	// Create configuration.
+// 	config := api.DefaultConfig()
+// 	config.Address = address
 
-	return client
-}
+// 	// Create Vault client.
+// 	client, err := api.NewClient(config)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	client.SetToken(rootToken)
+// 	client.Sys().EnableAuthWithOptions("approle/", &api.EnableAuthOptions{Type: "approle"})
 
-// addTestRole adds a new role to Vault and returns its credentials.
-func addTestRole(t *testing.T, client *api.Client) (roleIdFile, secretIdFile string) {
-	const roleName = "test-role"
-	const policyName = "test-policy"
+// 	return client
+// }
 
-	// Add roleName to Vault.
-	_, err := client.Logical().Write(fmt.Sprintf("auth/approle/role/%s", roleName), map[string]interface{}{
-		"secret_id_ttl":      "10m",
-		"token_num_uses":     10,
-		"token_ttl":          "20m",
-		"token_max_ttl":      "30m",
-		"secret_id_num_uses": 40,
-	})
-	require.NoError(t, err)
+// // addTestRole adds a new role to Vault and returns its credentials.
+// func addTestRole(t *testing.T, client *api.Client) (roleIdFile, secretIdFile string) {
+// 	const roleName = "test-role"
+// 	const policyName = "test-policy"
 
-	// Get the role ID of roleName.
-	secret, err := client.Logical().Read(fmt.Sprintf("auth/approle/role/%s/role-id", roleName))
-	require.NoError(t, err)
-	roleId := secret.Data["role_id"].(string)
-	roleIdFile = writeTempFile(t, roleId)
+// 	// Add roleName to Vault.
+// 	_, err := client.Logical().Write(fmt.Sprintf("auth/approle/role/%s", roleName), map[string]interface{}{
+// 		"secret_id_ttl":      "10m",
+// 		"token_num_uses":     10,
+// 		"token_ttl":          "20m",
+// 		"token_max_ttl":      "30m",
+// 		"secret_id_num_uses": 40,
+// 	})
+// 	require.NoError(t, err)
 
-	// Generate a secret ID for roleName.
-	secret, err = client.Logical().Write(fmt.Sprintf("auth/approle/role/%s/secret-id", roleName), nil)
-	require.NoError(t, err)
-	secretId := secret.Data["secret_id"].(string)
-	secretIdFile = writeTempFile(t, secretId)
+// 	// Get the role ID of roleName.
+// 	secret, err := client.Logical().Read(fmt.Sprintf("auth/approle/role/%s/role-id", roleName))
+// 	require.NoError(t, err)
+// 	roleId := secret.Data["role_id"].(string)
+// 	roleIdFile = writeTempFile(t, roleId)
 
-	// Create a new policy for this role to read from `path`.
-	_, err = client.Logical().Write(fmt.Sprintf("sys/policies/acl/%s", policyName), map[string]interface{}{
-		"policy": fmt.Sprintf(`
-		path "%s"
-		{
-			capabilities = ["read"]
-		}`, path),
-	})
-	require.NoError(t, err)
-	secret, err = client.Logical().Write(fmt.Sprintf("auth/approle/role/%s/policies", roleName), map[string]interface{}{
-		"token_policies": []string{policyName},
-	})
-	require.NoError(t, err)
+// 	// Generate a secret ID for roleName.
+// 	secret, err = client.Logical().Write(fmt.Sprintf("auth/approle/role/%s/secret-id", roleName), nil)
+// 	require.NoError(t, err)
+// 	secretId := secret.Data["secret_id"].(string)
+// 	secretIdFile = writeTempFile(t, secretId)
 
-	return
-}
+// 	// Create a new policy for this role to read from `path`.
+// 	_, err = client.Logical().Write(fmt.Sprintf("sys/policies/acl/%s", policyName), map[string]interface{}{
+// 		"policy": fmt.Sprintf(`
+// 		path "%s"
+// 		{
+// 			capabilities = ["read"]
+// 		}`, path),
+// 	})
+// 	require.NoError(t, err)
+// 	secret, err = client.Logical().Write(fmt.Sprintf("auth/approle/role/%s/policies", roleName), map[string]interface{}{
+// 		"token_policies": []string{policyName},
+// 	})
+// 	require.NoError(t, err)
 
-// writeTempFile creates a tempfile with the required data and returns the filename.
-func writeTempFile(t *testing.T, data string) string {
-	file, err := ioutil.TempFile("", "")
-	require.NoError(t, err)
-	defer file.Close()
-	t.Cleanup(func() { os.Remove(file.Name()) })
+// 	return
+// }
 
-	_, err = file.WriteString(data)
-	require.NoError(t, err)
+// // writeTempFile creates a tempfile with the required data and returns the filename.
+// func writeTempFile(t *testing.T, data string) string {
+// 	file, err := ioutil.TempFile("", "")
+// 	require.NoError(t, err)
+// 	defer file.Close()
+// 	t.Cleanup(func() { os.Remove(file.Name()) })
 
-	return file.Name()
-}
+// 	_, err = file.WriteString(data)
+// 	require.NoError(t, err)
+
+// 	return file.Name()
+// }
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ go 1.12
 require (
 	contrib.go.opencensus.io/exporter/jaeger v0.1.0
 	contrib.go.opencensus.io/exporter/prometheus v0.1.0
+	github.com/DataDog/datadog-go v0.0.0-20190425163447-40bafcb5f6c1 // indirect
 	github.com/DataDog/opencensus-go-exporter-datadog v0.0.0-20190503082300-0f32ad59ab08
 	github.com/DataDog/zstd v1.4.5 // indirect
 	github.com/Masterminds/semver/v3 v3.1.0
@@ -28,49 +29,51 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/dgrijalva/jwt-go/v4 v4.0.0-preview1
 	github.com/dgryski/go-farm v0.0.0-20200201041132-a6ae2369ad13
-	github.com/docker/docker v17.12.0-ce-rc1.0.20200309214505-aa6a9891b09c+incompatible
+	github.com/docker/distribution v2.7.1+incompatible // indirect
+	github.com/docker/docker v1.13.1
 	github.com/dustin/go-humanize v1.0.0
 	github.com/getsentry/sentry-go v0.6.0
-	github.com/go-sql-driver/mysql v1.5.0
+	github.com/go-sql-driver/mysql v0.0.0-20190330032241-c0f6b444ad8f
 	github.com/gogo/protobuf v1.3.2
 	github.com/golang/geo v0.0.0-20170810003146-31fb0106dc4a
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
-	github.com/golang/protobuf v1.4.2
+	github.com/golang/protobuf v1.4.1
 	github.com/golang/snappy v0.0.2
 	github.com/google/codesearch v1.0.0
 	github.com/google/go-cmp v0.5.4
-	github.com/google/uuid v1.1.1
+	github.com/google/uuid v1.0.0
 	github.com/gorilla/websocket v1.4.2
 	github.com/graph-gophers/graphql-go v0.0.0-20200309224638-dae41bde9ef9
 	github.com/graph-gophers/graphql-transport-ws v0.0.0-20190611222414-40c048432299 // indirect
-	github.com/hashicorp/vault v1.6.3
-	github.com/hashicorp/vault/api v1.0.5-0.20201001211907-38d91b749c77
+	github.com/hashicorp/vault/api v1.0.4
 	github.com/minio/minio-go/v6 v6.0.55
 	github.com/mitchellh/panicwrap v1.0.0
 	github.com/paulmach/go.geojson v0.0.0-20170327170536-40612a87147b
 	github.com/pierrec/lz4 v2.6.0+incompatible // indirect
 	github.com/pkg/errors v0.9.1
 	github.com/pkg/profile v1.2.1
-	github.com/prometheus/client_golang v1.7.1
+	github.com/prometheus/client_golang v0.9.3
+	github.com/prometheus/common v0.4.1 // indirect
+	github.com/prometheus/procfs v0.0.0-20190517135640-51af30a78b0e // indirect
 	github.com/soheilhy/cmux v0.1.4
 	github.com/spf13/cast v1.3.0
 	github.com/spf13/cobra v0.0.5
-	github.com/spf13/pflag v1.0.5
+	github.com/spf13/pflag v1.0.3
 	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.6.1
 	github.com/tinylib/msgp v1.1.5 // indirect
 	github.com/twpayne/go-geom v1.0.5
-	go.etcd.io/etcd v0.5.0-alpha.5.0.20200425165423-262c93980547
+	go.etcd.io/etcd v0.0.0-20190228193606-a943ad0ee4c9
 	go.opencensus.io v0.22.5
 	go.uber.org/zap v1.16.0
-	golang.org/x/crypto v0.0.0-20201002170205-7f63de1d35b0
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
 	golang.org/x/net v0.0.0-20201021035429-f5854403a974
 	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9
 	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c
 	golang.org/x/text v0.3.3
 	golang.org/x/tools v0.0.0-20210106214847-113979e3529a
-	google.golang.org/grpc v1.29.1
+	google.golang.org/grpc v1.27.0
 	gopkg.in/DataDog/dd-trace-go.v1 v1.13.1 // indirect
-	gopkg.in/square/go-jose.v2 v2.5.1
-	gopkg.in/yaml.v2 v2.3.0
+	gopkg.in/square/go-jose.v2 v2.3.1
+	gopkg.in/yaml.v2 v2.2.4
 )
