6ee053c38238755a68f10a1fbe711137dd74d485
Kaustubh J <79963138+thsubaku9-dgraph@users.noreply.github.com>
2021-03-16 19:17:24 +0530

    cherry-picking off e8b78b7aa70357e6f8be4d18049071edebf0e865

--- dgraph/cmd/zero/run.go
+++ dgraph/cmd/zero/run.go
@@ -89,6 +89,7 @@ instances to achieve high-availability.
 	flag.StringP("wal", "w", "zw", "Directory storing WAL.")
 	flag.Duration("rebalance_interval", 8*time.Minute, "Interval for trying a predicate move.")
 	flag.String("enterprise_license", "", "Path to the enterprise license file.")
+	flag.String("cid", "", "Cluster ID")
 	// TLS configurations
 	x.RegisterServerTLSFlags(flag)
 }
