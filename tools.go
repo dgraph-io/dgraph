d724116175fd0546e55f93cc31fbfc25aa1ec15a
Joaquin Menchaca <joaquin@dgraph.io>
2021-03-15 19:31:47 -0700

    update year in license

--- tools.go
+++ tools.go
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2018 Dgraph Labs, Inc. and Contributors
+ * Copyright 2021 Dgraph Labs, Inc. and Contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
