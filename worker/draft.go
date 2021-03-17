e489fd2e8bf1b0f037c510628e2bef9d5e345299
Ahsan Barkati <ahsanbarkati@gmail.com>
2021-03-18 03:45:25 +0530

    Fix(learner): Don't start a learner node with no peers (#7582)
    When a learner node starts and there is no alpha peer then it 
    behaves like a normal mode. This change causes it to crash 
    itself if it has no peer alpha nodes.

--- worker/draft.go
+++ worker/draft.go
@@ -1729,7 +1729,8 @@ func (n *node) InitAndStartNode() {
 	_, restart, err := n.PastLife()
 	x.Check(err)
 
-	if _, hasPeer := groups().MyPeer(); !restart && hasPeer {
+	_, hasPeer := groups().MyPeer()
+	if !restart && hasPeer {
 		// The node has other peers, it might have crashed after joining the cluster and before
 		// writing a snapshot. Check from leader, if it is part of the cluster. Consider this a
 		// restart if it is part of the cluster, else start a new node.
@@ -1740,6 +1741,10 @@ func (n *node) InitAndStartNode() {
 			glog.Errorf("Error while calling hasPeer: %v. Retrying...\n", err)
 			time.Sleep(time.Second)
 		}
+	}
+
+	if n.RaftContext.IsLearner && !hasPeer {
+		glog.Fatal("Cannot start a learner node without peer alpha nodes")
 	}
 
 	if restart {
@@ -1754,6 +1759,7 @@ func (n *node) InitAndStartNode() {
 			// zero-member Raft group.
 			n.SetConfState(&sp.Metadata.ConfState)
 
+			// TODO: Making connections here seems unnecessary, evaluate.
 			members := groups().members(n.gid)
 			for _, id := range sp.Metadata.ConfState.Nodes {
 				m, ok := members[id]
