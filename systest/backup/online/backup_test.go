6aee265c2825dd2c248124c54d012548c77d8e8a
Ahsan Barkati <ahsanbarkati@gmail.com>
2021-03-23 22:52:13 +0530

    Add one more test

--- systest/backup/online/backup_test.go
+++ systest/backup/online/backup_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -10,6 +11,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/dgraph-io/dgo/v200/protos/api"
 	"github.com/dgraph-io/dgraph/systest/backup/common"
 	"github.com/dgraph-io/dgraph/testutil"
 	"github.com/dgraph-io/dgraph/x"
@@ -23,6 +25,7 @@ var (
 func TestBackupWithMoveTablet(t *testing.T) {
 	common.DirSetup(t)
 
+	// Call increment on predicate p1 and p2.
 	cmd := exec.Command("dgraph", "increment", "--num", "10",
 		"--alpha", testutil.SockAddr, "--pred", "p1")
 	if out, err := cmd.CombinedOutput(); err != nil {
@@ -45,22 +48,26 @@ func TestBackupWithMoveTablet(t *testing.T) {
 	require.NoError(t, err)
 
 	t.Log("Pausing to let zero move tablets...")
-	moveOk := true
-	for retry := 60; retry > 0; retry-- {
-		time.Sleep(1 * time.Second)
-		state, err := testutil.GetState()
-		require.NoError(t, err)
-		if _, ok := state.Groups["1"].Tablets[x.NamespaceAttr(x.GalaxyNamespace, "p1")]; !ok {
-			moveOk = false
+
+	checkTablets := func() bool {
+		moveOk := true
+		for retry := 60; retry > 0; retry-- {
+			time.Sleep(1 * time.Second)
+			state, err := testutil.GetState()
+			require.NoError(t, err)
+			if _, ok := state.Groups["1"].Tablets[x.NamespaceAttr(x.GalaxyNamespace, "p1")]; !ok {
+				moveOk = false
+			}
+			if _, ok := state.Groups["2"].Tablets[x.NamespaceAttr(x.GalaxyNamespace, "p2")]; !ok {
+				moveOk = false
+			}
+			if moveOk {
+				break
+			}
 		}
-		if _, ok := state.Groups["2"].Tablets[x.NamespaceAttr(x.GalaxyNamespace, "p2")]; !ok {
-			moveOk = false
-		}
-		if moveOk {
-			break
-		}
+		return moveOk
 	}
-	require.True(t, moveOk)
+	require.True(t, checkTablets())
 
 	// Take an incremental backup
 	runBackup(t)
@@ -71,13 +78,8 @@ func TestBackupWithMoveTablet(t *testing.T) {
 	testutil.DropAll(t, dg)
 	runRestore(t)
 
-	time.Sleep(5 * time.Second)
 	// Get the membership state after restore and verify that p1 and p2 belongs to expected groups.
-	state, err := testutil.GetState()
-	_, ok := state.Groups["1"].Tablets[x.NamespaceAttr(x.GalaxyNamespace, "p1")]
-	require.True(t, ok)
-	_, ok = state.Groups["2"].Tablets[x.NamespaceAttr(x.GalaxyNamespace, "p2")]
-	require.True(t, ok)
+	require.True(t, checkTablets())
 
 	// Verify the count of p1 and p2
 	q1 := `{q(func: has(p1)){ p1 }}`
@@ -89,6 +91,46 @@ func TestBackupWithMoveTablet(t *testing.T) {
 	res = testutil.QueryData(t, dg, q2)
 	require.NoError(t, err)
 	require.JSONEq(t, `{"q":[{"p2":10}]}`, string(res))
+	common.DirCleanup(t)
+}
+
+func TestBackupSchema(t *testing.T) {
+	common.DirSetup(t)
+	dg, err := testutil.DgraphClient(testutil.SockAddr)
+	require.NoError(t, err)
+	testutil.DropAll(t, dg)
+
+	ctx := context.Background()
+
+	_, err = dg.NewTxn().Mutate(ctx, &api.Mutation{
+		CommitNow: true,
+		SetNquads: []byte(`
+			<_:a> <name> "alice" .
+			<_:a> <pet> "dog" .
+		`),
+	})
+	require.NoError(t, err)
+
+	runBackup(t)
+	testutil.DropAll(t, dg)
+
+	_, err = dg.NewTxn().Mutate(ctx, &api.Mutation{
+		CommitNow: true,
+		SetNquads: []byte(`
+			<_:a> <name> "bob" .
+		`),
+	})
+	runBackup(t)
+
+	testutil.DropAll(t, dg)
+	runRestore(t)
+	time.Sleep(5 * time.Second)
+
+	q := `{q(func: has(name)){ name }}`
+	res := testutil.QueryData(t, dg, q)
+	require.NoError(t, err)
+	require.JSONEq(t, `{"q":[{"name": "bob"}]}`, string(res))
+
 	common.DirCleanup(t)
 }
 
