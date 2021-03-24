21670c5049f3741c40c927f3e9caea49ffc49b8f
Ahsan Barkati <ahsanbarkati@gmail.com>
2021-03-24 12:39:54 +0530

    Move z and jemalloc to /debug

--- dgraph/cmd/alpha/run.go
+++ dgraph/cmd/alpha/run.go
@@ -501,7 +501,8 @@ func setupServer(closer *z.Closer) {
 	baseMux.HandleFunc("/alter", alterHandler)
 	baseMux.HandleFunc("/health", healthCheck)
 	baseMux.HandleFunc("/state", stateHandler)
-	baseMux.HandleFunc("/jemalloc", x.JemallocHandler)
+	baseMux.HandleFunc("/debug/jemalloc", x.JemallocHandler)
+	zpages.Handle(baseMux, "/debug/z")
 
 	// TODO: Figure out what this is for?
 	http.HandleFunc("/debug/store", storeStatsHandler)
@@ -556,9 +557,6 @@ func setupServer(closer *z.Closer) {
 	addr := fmt.Sprintf("%s:%d", laddr, httpPort())
 	glog.Infof("Bringing up GraphQL HTTP API at %s/graphql", addr)
 	glog.Infof("Bringing up GraphQL HTTP admin API at %s/admin", addr)
-
-	// Add OpenCensus z-pages.
-	zpages.Handle(baseMux, "/z")
 
 	baseMux.Handle("/", http.HandlerFunc(homeHandler))
 	baseMux.Handle("/ui/keywords", http.HandlerFunc(keywordHandler))
