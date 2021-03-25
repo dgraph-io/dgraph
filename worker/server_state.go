7e7a2cd2a266157fe05cd8169047a5a65f698ce2
Rohan Prasad <prasad.rohan93@gmail.com>
2021-03-25 22:11:44 +0530

    Move cache options to superflag for alpha

--- worker/server_state.go
+++ worker/server_state.go
@@ -50,6 +50,7 @@ const (
 		`mutations-nquad=1000000; disallow-drop=false; query-timeout=0ms;`
 	GraphQLDefaults = `introspection=true; debug=false; extensions=true; poll-interval=1s; ` +
 		`lambda-url=;`
+	CacheDefaults = `size-mb=1024; percentage=0,65,35,0;`
 )
 
 // ServerState holds the state of the Dgraph server.
