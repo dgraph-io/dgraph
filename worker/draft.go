6e3db9f233598d0ef42d12c4eced839c0ba28b8b
Rohan Prasad <prasad.rohan93@gmail.com>
2021-04-14 11:18:23 +0530

    Move sanitization to a function

--- worker/draft.go
+++ worker/draft.go
@@ -24,6 +24,7 @@ import (
 	"fmt"
 	"math"
 	"sort"
+	"strings"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -49,6 +50,10 @@ import (
 	"github.com/dgraph-io/dgraph/types"
 	"github.com/dgraph-io/dgraph/x"
 	"github.com/dgraph-io/ristretto/z"
+)
+
+const (
+	sensitiveString = "******"
 )
 
 type node struct {
@@ -849,7 +854,8 @@ func (n *node) processApplyCh() {
 				previous[prop.Key] = p
 			}
 			if perr != nil {
-				glog.Errorf("Applying proposal. Error: %v. Proposal: %q.", perr, prop)
+				glog.Errorf("Applying proposal. Error: %v. Proposal: %q.", perr,
+					getSanitizedString(&prop))
 			}
 			n.elog.Printf("Applied proposal with key: %d, index: %d. Err: %v",
 				prop.Key, prop.Index, perr)
@@ -2058,4 +2064,19 @@ func (n *node) monitorRaftMetrics() {
 		ostats.Record(n.ctx, x.RaftPendingSize.M(curPendingSize))
 		ostats.Record(n.ctx, x.RaftApplyCh.M(int64(len(n.applyCh))))
 	}
+}
+
+func getSanitizedString(proposal *pb.Proposal) string {
+	ps := proposal.String()
+	if proposal.GetRestore() != nil {
+		if len(proposal.GetRestore().GetAccessKey()) != 0 {
+			ps = strings.Replace(ps, proposal.GetRestore().GetAccessKey(),
+				sensitiveString, 1)
+		}
+		if len(proposal.GetRestore().GetSecretKey()) != 0 {
+			ps = strings.Replace(ps, proposal.GetRestore().GetSecretKey(),
+				sensitiveString, 1)
+		}
+	}
+	return ps
 }
