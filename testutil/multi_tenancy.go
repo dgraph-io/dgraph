ed657d51e1e9fd9dd45dc7e29073a69dcd406359
Naman Jain <naman@dgraph.io>
2021-05-05 20:53:38 +0530

    feat(cloud): add shared-instance flag in limit superflag in alpha (#7770) (#7778)
    This PR adds a shared-instance flag to --limit superflag.
    When set to true (false by default), it will:
    
    - Restrict access to any of the ACL operations like Login, add/remove/update user from non-galaxy namespaces.
    - Prevent the leaking of environment variables for minio and aws.
    
    (cherry picked from commit 5f3cece75da375077c45ab64228cb7053cf03d02)
    (cherry picked from commit eeb7bea1a7cb49636d38c14994cf3cf10bf857d2)

--- testutil/multi_tenancy.go
+++ testutil/multi_tenancy.go
@@ -177,7 +177,7 @@ func DeleteNamespace(t *testing.T, token *HttpToken, nsID uint64) error {
 }
 
 func CreateUser(t *testing.T, token *HttpToken, username,
-	password string) {
+	password string) *GraphQLResponse {
 	addUser := `
 	mutation addUser($name: String!, $pass: String!) {
 		addUser(input: [{name: $name, password: $pass}]) {
@@ -205,6 +205,7 @@ func CreateUser(t *testing.T, token *HttpToken, username,
 	var r Response
 	err := json.Unmarshal(resp.Data, &r)
 	require.NoError(t, err)
+	return resp
 }
 
 func CreateGroup(t *testing.T, token *HttpToken, name string) {
@@ -377,4 +378,37 @@ func QueryData(t *testing.T, dg *dgo.Dgraph, query string) []byte {
 	resp, err := dg.NewReadOnlyTxn().Query(context.Background(), query)
 	require.NoError(t, err)
 	return resp.GetJson()
+}
+
+func Export(t *testing.T, token *HttpToken, dest, accessKey, secretKey string) *GraphQLResponse {
+	exportRequest := `mutation export($dst: String!, $f: String!, $acc: String!, $sec: String!){
+export(input: {destination: $dst, format: $f, accessKey: $acc, secretKey: $sec}) {
+			response {
+				message
+			}
+		}
+	}`
+
+	params := GraphQLParams{
+		Query: exportRequest,
+		Variables: map[string]interface{}{
+			"dst": dest,
+			"f":   "rdf",
+			"acc": accessKey,
+			"sec": secretKey,
+		},
+	}
+
+	resp := MakeRequest(t, token, params)
+	type Response struct {
+		Export struct {
+			Response struct {
+				Message string
+			}
+		}
+	}
+	var r Response
+	err := json.Unmarshal(resp.Data, &r)
+	require.NoError(t, err)
+	return resp
 }
