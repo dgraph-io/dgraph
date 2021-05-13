546a5aa2dcbc8ba93716f505bcc9c5632a4d965e
tstreamDOTh <tstream.h@gmail.com>
2021-05-14 00:36:50 +0530

    remove non nullable condition from sort

--- gql/parser_test.go
+++ gql/parser_test.go
@@ -5334,28 +5334,43 @@ func TestFilterWithDollarError(t *testing.T) {
 	require.Error(t, err)
 }
 
-func TestLexQuery(t *testing.T) {
+func TestLexQueryWithValidQuery(t *testing.T) {
 	query := `{
-	q(func: allofterms(<name:is>, "hey you there"), first:20, offset:0, orderasc:Pokemon.id){
-		uid
-		expand(_all_)(first:1){
+		q(func: allofterms(<name:is>, "hey you there"), first:20, offset:0, orderasc:Pokemon.id){
 			uid
-			name@en:ru:.
-			Pokemon.name
-			Pokemon.type
-			expand(_all_)(first:1)
+			expand(_all_)(first:1){
+				uid
+				Pokemon.name
+				expand(_all_)(first:1)
+			}
 		}
-	}
-	n(func:type(Pokemon)){
-		count:count(uid
-		)
-	}
-}`
+		n(func:type(Pokemon)){
+			count:count(uid)
+		}
+	}`
 
 	items := LexQuery(query)
 	for i, item := range items {
 		t.Logf("[%d] item: %+v\n", i, item)
 	}
+	require.Equal(t, 68, len(items))
+}
+
+func TestLexQueryWithInvalidQuery(t *testing.T) {
+	query := `{
+		q(func: allofterms(<name:is>, "hey you there"), first: 20, offset:0, orderasc:Pokemon.id){
+			uid
+		}
+		n(func:type(Pokemon)){
+			count:count(uid)
+	}`
+
+	items := LexQuery(query)
+	for i, item := range items {
+		t.Logf("[%d] item: %+v\n", i, item.Typ)
+	}
+	require.Equal(t, 45, len(items))
+	require.Equal(t, lex.ItemError, items[44].Typ)
 }
 
 func TestFilterWithVar(t *testing.T) {
