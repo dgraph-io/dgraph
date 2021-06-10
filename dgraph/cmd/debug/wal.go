17da87b188d2c9f03943cb3fdfb57729abbaf0a3
Daniel Mai <daniel@dgraph.io>
2021-06-10 16:03:21 -0700

    fix(cmd/debug): Don't print proposal key.
    This can panic.
    
        18 . 274207 . EntryNormal . 38 B   . 889096048116477 . Delta . Max: 292322 . Pending txns: 0 .
        18 . 274208 . EntryNormal . 38 B   . 889096048116478 . Delta . Max: 292323 . Pending txns: 0 .
        18 . 274209 . EntryNormal . 26 B   . 889096048116479 .
    
        panic: runtime error: slice bounds out of range [:8] with capacity 0
    
        goroutine 1 [running]:
        github.com/dgraph-io/dgraph/dgraph/cmd/debug.printEntry(0x13, 0x42f22, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xc000694e80, ...)
        	/ext-go/1/src/github.com/dgraph-io/dgraph/dgraph/cmd/debug/wal.go:40 +0x625
        github.com/dgraph-io/dgraph/dgraph/cmd/debug.printRaft(0xc000298780)
        	/ext-go/1/src/github.com/dgraph-io/dgraph/dgraph/cmd/debug/wal.go:133 +0x26b
        github.com/dgraph-io/dgraph/dgraph/cmd/debug.handleWal(0xc000298780, 0x0, 0x0)
        	/ext-go/1/src/github.com/dgraph-io/dgraph/dgraph/cmd/debug/wal.go:205 +0x185
        github.com/dgraph-io/dgraph/dgraph/cmd/debug.run()
        	/ext-go/1/src/github.com/dgraph-io/dgraph/dgraph/cmd/debug/run.go:901 +0x17b
        github.com/dgraph-io/dgraph/dgraph/cmd/debug.init.0.func1(0xc00035e280, 0xc000293020, 0x0, 0x2)
        	/ext-go/1/src/github.com/dgraph-io/dgraph/dgraph/cmd/debug/run.go:83 +0x25
        github.com/spf13/cobra.(*Command).execute(0xc00035e280, 0xc000292fe0, 0x2, 0x2, 0xc00035e280, 0xc000292fe0)
        	/go/pkg/mod/github.com/spf13/cobra@v0.0.5/command.go:830 +0x2c2
        github.com/spf13/cobra.(*Command).ExecuteC(0x2f92d60, 0x0, 0x0, 0x0)
        	/go/pkg/mod/github.com/spf13/cobra@v0.0.5/command.go:914 +0x30b
        github.com/spf13/cobra.(*Command).Execute(...)
        	/go/pkg/mod/github.com/spf13/cobra@v0.0.5/command.go:864
        github.com/dgraph-io/dgraph/dgraph/cmd.Execute()
        	/ext-go/1/src/github.com/dgraph-io/dgraph/dgraph/cmd/root.go:78 +0x85
        main.main()
        	/ext-go/1/src/github.com/dgraph-io/dgraph/dgraph/main.go:99 +0xf0

--- dgraph/cmd/debug/wal.go
+++ dgraph/cmd/debug/wal.go
@@ -18,7 +18,6 @@ package debug
 
 import (
 	"bytes"
-	"encoding/binary"
 	"fmt"
 	"strconv"
 	"strings"
@@ -36,8 +35,8 @@ func printEntry(es raftpb.Entry, pending map[uint64]bool, isZero bool) {
 	defer func() {
 		fmt.Printf("%s\n", buf.Bytes())
 	}()
-	fmt.Fprintf(&buf, "%d . %d . %v . %-6s . %8d .", es.Term, es.Index, es.Type,
-		humanize.Bytes(uint64(es.Size())), binary.BigEndian.Uint64(es.Data[:8]))
+	fmt.Fprintf(&buf, "%d . %d . %v . %-6s .", es.Term, es.Index, es.Type,
+		humanize.Bytes(uint64(es.Size())))
 	if es.Type == raftpb.EntryConfChange {
 		return
 	}
