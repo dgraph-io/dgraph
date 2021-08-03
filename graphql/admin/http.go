77a152f6bca106490b967d75ee7fd6e6c01a3336
Manish R Jain <manish@dgraph.io>
2021-08-03 11:30:56 -0700

    Manish's changes
    (cherry picked from commit fef72aae955790185b449b26f4c4190fcef4aa5d)

--- graphql/admin/http.go
+++ graphql/admin/http.go
@@ -275,6 +275,7 @@ func (gh *graphqlHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	res = resolver.Resolve(ctx, gqlReq)
 	write(w, res, strings.Contains(r.Header.Get("Accept-Encoding"), "gzip"))
+	// write(w, res, false)
 }
 
 func (gh *graphqlHandler) isValid(namespace uint64) error {
