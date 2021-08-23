94039adc23376d667a39a670e2e7a33810304914
Ahsan Barkati <ahsanbarkati@gmail.com>
2021-08-23 20:03:47 +0530

    Clear cache before delete operations

--- worker/draft.go
+++ worker/draft.go
@@ -559,9 +559,6 @@ func (n *node) applyMutations(ctx context.Context, proposal *pb.Proposal) (rerr 
 			return err
 		}
 
-		// TODO: Revisit this when we work on posting cache. Clear entire cache.
-		// We don't want to drop entire cache, just due to one namespace.
-		// posting.ResetCache()
 		return nil
 	}
 
