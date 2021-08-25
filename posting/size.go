b43c715a8a20c6989d8b6aaada73b97085f5a9e9
Manish R Jain <manish@dgraph.io>
2021-08-24 22:49:20 -0700

    Avoid changing size.go

--- posting/size.go
+++ posting/size.go
@@ -36,13 +36,12 @@ func (l *List) DeepSize() uint64 {
 	l.RLock()
 	defer l.RUnlock()
 
-	var size uint64 = uint64(unsafe.Sizeof(&List{}))
-	// var size uint64 = 4*8 + // safe mutex consists of 4 words.
-	// 	1*8 + // plist pointer consists of 1 word.
-	// 	1*8 + // mutation map pointer  consists of 1 word.
-	// 	2*8 + // minTs and maxTs take 1 word each.
-	// 	3*8 + // array take 3 words. so key array is 3 words.
-	// 	1*8 // So far 11 words, in order to round the slab we're adding one more word.
+	var size uint64 = 4*8 + // safe mutex consists of 4 words.
+		1*8 + // plist pointer consists of 1 word.
+		1*8 + // mutation map pointer  consists of 1 word.
+		2*8 + // minTs and maxTs take 1 word each.
+		3*8 + // array take 3 words. so key array is 3 words.
+		1*8 // So far 11 words, in order to round the slab we're adding one more word.
 	// // so far basic struct layout has been calculated.
 
 	// Add each entry size of key array.
