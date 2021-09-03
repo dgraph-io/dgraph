8f17dda944f1c52921be34ebc4c397b2afce10d3
NamanJain8 <jnaman806@gmail.com>
2021-09-03 18:16:29 +0530

    fix(lambda): fix race condition in lambda server spin up

--- dgraph/cmd/alpha/run.go
+++ dgraph/cmd/alpha/run.go
@@ -34,6 +34,7 @@ import (
 	"path/filepath"
 	"strconv"
 	"strings"
+	"sync"
 	"sync/atomic"
 	"syscall"
 	"time"
@@ -487,11 +488,13 @@ func setupLambdaServer(closer *z.Closer) {
 	}
 
 	type lambda struct {
+		sync.Mutex
 		cmd        *exec.Cmd
 		active     bool
 		lastActive int64
-		health     string
-		port       int
+
+		health string
+		port   int
 	}
 
 	lambdas := make([]*lambda, 0, num)
@@ -519,9 +522,11 @@ func setupLambdaServer(closer *z.Closer) {
 					cmd.Env = append(cmd.Env, fmt.Sprintf("DGRAPH_URL="+dgraphUrl))
 					cmd.Stdout = os.Stdout
 					cmd.Stderr = os.Stderr
+					lambdas[i].Lock()
 					lambdas[i].cmd = cmd
 					lambdas[i].lastActive = time.Now().UnixNano()
 					lambdas[i].active = true
+					lambdas[i].Unlock()
 					glog.Infof("Running node command: %+v\n", cmd)
 					if err := cmd.Run(); err != nil {
 						glog.Errorf("Lambda server at port: %d stopped with error: %v",
@@ -544,11 +549,15 @@ func setupLambdaServer(closer *z.Closer) {
 			case <-closer.HasBeenClosed():
 				return
 			case <-ticker.C:
-				timestamp := time.Now().UnixNano()
-				for _, l := range lambdas {
+				healthCheck := func(l *lambda) {
+					l.Lock()
+					defer l.Unlock()
+
 					if !l.active {
-						continue
+						return
 					}
+
+					timestamp := time.Now().UnixNano()
 					resp, err := client.Get(l.health)
 					if err != nil || resp.StatusCode != 200 {
 						if time.Duration(timestamp-l.lastActive) > x.Config.Lambda.RestartAfter {
@@ -556,10 +565,15 @@ func setupLambdaServer(closer *z.Closer) {
 								" Killed it with err: %v", l.port, l.cmd.Process.Kill())
 							l.active = false
 						}
-						continue
+						return
 					}
+
 					resp.Body.Close()
 					l.lastActive = timestamp
+				}
+
+				for _, l := range lambdas {
+					healthCheck(l)
 				}
 			}
 		}
