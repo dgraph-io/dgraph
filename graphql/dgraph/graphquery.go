8f2c385a846ae8fe9d644f2606dc8496838fbf2e
Ronald Schartmüller <ronald.schartmueller@gmail.com>
2021-12-02 22:40:32 +0100

    fix linter error

--- graphql/dgraph/graphquery.go
+++ graphql/dgraph/graphquery.go
@@ -204,7 +204,8 @@ func writeUIDFunc(b *strings.Builder, uids []uint64, args []gql.Arg, needVar []g
 // writeRoot writes the root function as well as any ordering and paging
 // specified in q.
 //
-// Only uid(0x123, 0x124), type(...), eq(Type.Predicate, ...) and eq(val(var), ...) functions are supported at root.
+// Only uid(0x123, 0x124), type(...), eq(Type.Predicate, ...) and
+// eq(val(var), ...) functions are supported at root.
 // Multiple arguments for `eq` filter will be required in case of resolving `entities` query.
 func writeRoot(b *strings.Builder, q *gql.GraphQuery) {
 	if q.Func == nil {
