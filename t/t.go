47fa7494a0029ab0682301e5f7164c49e0fd108e
dshekhar95 <dilip@dgraph.io>
2022-09-12 15:19:51 -0400

    testing flag tlsverify=1

--- t/t.go
+++ t/t.go
@@ -100,16 +100,19 @@ func commandWithContext(ctx context.Context, args ...string) *exec.Cmd {
 
 // command takes a list of args and executes them as a program.
 // Example:
-//   docker-compose up -f "./my docker compose.yml"
+//
+//	docker-compose up -f "./my docker compose.yml"
+//
 // would become:
-//   command("docker-compose", "up", "-f", "./my docker compose.yml")
+//
+//	command("docker-compose", "up", "-f", "./my docker compose.yml")
 func command(args ...string) *exec.Cmd {
 	return commandWithContext(ctxb, args...)
 }
 
 func startCluster(composeFile, prefix string) error {
 	cmd := command(
-		"docker-compose", "-f", composeFile, "-p", prefix,
+		"docker-compose", "DOCKER_TLS_VERIFY=1", "-f", composeFile, "-p", prefix,
 		"up", "--force-recreate", "--build", "--remove-orphans", "--detach")
 	cmd.Stderr = nil
 
