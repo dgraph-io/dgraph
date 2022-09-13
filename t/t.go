52f13654f790e3d81eae0575ad9edbf97c3f9ed7
dshekhar95 <111321288+dshekhar95@users.noreply.github.com>
2022-09-13 13:36:26 -0400

    fix(test) t.go add --compatibility flag (#8269)
    * add --compatibility flag
    
    * add --compability flag to stopCluster

--- t/t.go
+++ t/t.go
@@ -112,7 +112,7 @@ func command(args ...string) *exec.Cmd {
 
 func startCluster(composeFile, prefix string) error {
 	cmd := command(
-		"docker-compose", "-f", composeFile, "-p", prefix,
+		"docker-compose", "--compatibility", "-f", composeFile, "-p", prefix,
 		"up", "--force-recreate", "--build", "--remove-orphans", "--detach")
 	cmd.Stderr = nil
 
@@ -175,7 +175,7 @@ func stopCluster(composeFile, prefix string, wg *sync.WaitGroup, err error) {
 		if err != nil {
 			outputLogs(prefix)
 		}
-		cmd := command("docker-compose", "-f", composeFile, "-p", prefix, "down", "-v")
+		cmd := command("docker-compose", "--compatibility", "-f", composeFile, "-p", prefix, "down", "-v")
 		cmd.Stderr = nil
 		if err := cmd.Run(); err != nil {
 			fmt.Printf("Error while bringing down cluster. Prefix: %s. Error: %v\n",
