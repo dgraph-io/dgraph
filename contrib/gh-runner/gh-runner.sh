a0eb9a28a278e6cd2c88f439dc0bb4f354599661
Sudhish <108091997+skrdgraph@users.noreply.github.com>
2022-09-14 19:17:15 +0000

    Update gh-runner.sh

--- contrib/gh-runner/gh-runner.sh
+++ contrib/gh-runner/gh-runner.sh
@@ -26,7 +26,7 @@ mkdir actions-runner && cd actions-runner
 curl -o actions-runner-linux-x64-2.296.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.296.2/actions-runner-linux-x64-2.296.2.tar.gz
 echo "34a8f34956cdacd2156d4c658cce8dd54c5aef316a16bbbc95eb3ca4fd76429a  actions-runner-linux-x64-2.296.2.tar.gz" | shasum -a 256 -c
 tar xzf ./actions-runner-linux-x64-2.296.2.tar.gz
-./config.sh --url https://github.com/dgraph-io/dgraph --token AZYVUXLIV5P5N6GAZTBG2KLDEDR76
+./config.sh --url https://github.com/dgraph-io/dgraph --token $TOKEN
 ./run.sh >/dev/null 2>&1 &
 
 
