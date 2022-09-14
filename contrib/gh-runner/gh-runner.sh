72cd9c4d64fb5000b5ae7d40c566e09ed501fabf
dshekhar95 <dilip@dgraph.io>
2022-09-14 14:39:41 -0400

    added gh runner shell script

---
+++ contrib/gh-runner/gh-runner.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#Machine Setup
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release \
+    build-essential #Libc Packages
+#Docker Setup
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+ echo \
+   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get install docker-compose
+#Add Docker to sudoers group
+sudo groupadd docker
+sudo usermod -aG docker ${USER}
+su -s ${USER}
+#Hook up to GH Actions
+mkdir actions-runner && cd actions-runner
+curl -o actions-runner-linux-x64-2.296.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.296.2/actions-runner-linux-x64-2.296.2.tar.gz
+echo "34a8f34956cdacd2156d4c658cce8dd54c5aef316a16bbbc95eb3ca4fd76429a  actions-runner-linux-x64-2.296.2.tar.gz" | shasum -a 256 -c
+tar xzf ./actions-runner-linux-x64-2.296.2.tar.gz
+./config.sh --url https://github.com/dgraph-io/dgraph --token AZYVUXLIV5P5N6GAZTBG2KLDEDR76
+./run.sh >/dev/null 2>&1 &
+
+
+
