eb735eda042d832a1b14f11edfa1e840e59d783f
Sudhish <108091997+skrdgraph@users.noreply.github.com>
2022-09-14 15:40:47 -0700

    fix(test): runner reboot issue (#8282)
    * Update gh-runner.sh
    
    * Update gh-runner.sh
    
    * Update gh-runner.sh
    
    * Update README.md
    
    * Update gh-runner.sh
    
    * Update README.md

--- contrib/gh-runner/gh-runner.sh
+++ contrib/gh-runner/gh-runner.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Machine Setup
+# Machine Setup
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install \
@@ -8,7 +8,7 @@ sudo apt-get -y install \
     gnupg \
     lsb-release \
     build-essential #Libc Packages
-#Docker Setup
+# Docker Setup
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
  echo \
@@ -20,10 +20,12 @@ sudo apt-get -y install docker-compose
 #Add Docker to sudoers group
 sudo usermod -aG docker ${USER}
 newgrp docker &
-#Hook up to GH Actions
+# Hook up to GH Actions
 mkdir actions-runner && cd actions-runner
 curl -o actions-runner-linux-x64-2.296.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.296.2/actions-runner-linux-x64-2.296.2.tar.gz
 echo "34a8f34956cdacd2156d4c658cce8dd54c5aef316a16bbbc95eb3ca4fd76429a  actions-runner-linux-x64-2.296.2.tar.gz" | shasum -a 256 -c
 tar xzf ./actions-runner-linux-x64-2.296.2.tar.gz
 ./config.sh --url https://github.com/dgraph-io/dgraph --token $TOKEN
 ./run.sh & # TODO move to /dev/null
+# Reboot Machine
+sudo reboot
