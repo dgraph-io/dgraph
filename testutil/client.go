80fe7039f572faa6037afab72d3fc3cdddbfccd5
Joshua Goldstein <jgoldstein345@gmail.com>
2022-10-17 14:28:10 -0500

    add changes

--- testutil/client.go
+++ testutil/client.go
@@ -258,16 +258,13 @@ type LoginParams struct {
 // and returns the access JWT and refresh JWT extracted from
 // the HTTP response
 func HttpLogin(params *LoginParams) (*HttpToken, error) {
-	fmt.Println("HttpLogin: params data ", params)
 	loginPayload := api.LoginRequest{}
-	fmt.Println("HttpLogin: login payload at start", loginPayload)
 	if len(params.RefreshJwt) > 0 {
 		loginPayload.RefreshToken = params.RefreshJwt
 	} else {
 		loginPayload.Userid = params.UserID
 		loginPayload.Password = params.Passwd
 	}
-	fmt.Println("HttpLogin: login payload at end", loginPayload)
 
 	login := `mutation login($userId: String, $password: String, $namespace: Int, $refreshToken: String) {
 		login(userId: $userId, password: $password, namespace: $namespace, refreshToken: $refreshToken) {
@@ -291,28 +288,24 @@ func HttpLogin(params *LoginParams) (*HttpToken, error) {
 	if err != nil {
 		return nil, errors.Wrapf(err, "unable to marshal body")
 	}
-	fmt.Println("HttpLogin: body", body)
 
 	req, err := http.NewRequest("POST", params.Endpoint, bytes.NewBuffer(body))
 	if err != nil {
 		return nil, errors.Wrapf(err, "unable to create request")
 	}
 	req.Header.Set("Content-Type", "application/json")
-	fmt.Println("HttpLogin: req", req)
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
 		return nil, errors.Wrapf(err, "login through curl failed")
 	}
-	fmt.Println("HttpLogin: resp", resp)
 	defer resp.Body.Close()
 
 	respBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, errors.Wrapf(err, "unable to read from response")
 	}
-	fmt.Println("HttpLogin: respBody", respBody)
 	if resp.StatusCode != http.StatusOK {
 		return nil, errors.New(fmt.Sprintf("got non 200 response from the server with %s ",
 			string(respBody)))
@@ -327,7 +320,6 @@ func HttpLogin(params *LoginParams) (*HttpToken, error) {
 		}
 		return nil, errors.Wrapf(err, "unable to unmarshal the output to get JWTs")
 	}
-	fmt.Println("HttpLogin: outputJson", outputJson)
 
 	data, found := outputJson["data"].(map[string]interface{})
 	if !found {
@@ -352,11 +344,7 @@ func HttpLogin(params *LoginParams) (*HttpToken, error) {
 	if !found || newRefreshJwt == "" {
 		return nil, errors.Errorf("no refresh JWT found in the output")
 	}
-	fmt.Println("HttpLogin: http token ", &HttpToken{UserId: params.UserID,
-		Password:     params.Passwd,
-		AccessJwt:    newAccessJwt,
-		RefreshToken: newRefreshJwt,
-	})
+
 	return &HttpToken{
 		UserId:       params.UserID,
 		Password:     params.Passwd,
