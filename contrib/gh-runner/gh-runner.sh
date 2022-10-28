5d04f4622b8c9170c95f8217278a6e8b5a401287
Aditya Sadalage <aditya.sadalage@gmail.com>
2022-10-28 11:21:27 -0700

    Updated curl commands

--- contrib/gh-runner/gh-runner.sh
+++ contrib/gh-runner/gh-runner.sh
@@ -38,10 +38,10 @@ elif [ "$(uname -m)" = "x86_64" ]; then
     echo "Detected amd64 architecture"
 
     # Download the latest runner package
-    curl -o actions-runner-linux-x64-2.296.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.296.2/actions-runner-linux-x64-2.296.2.tar.gz
+    curl -o actions-runner-linux-x64-2.298.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.298.2/actions-runner-linux-x64-2.298.2.tar.gz
 
     # Optional: Validate the hash
-    echo "34a8f34956cdacd2156d4c658cce8dd54c5aef316a16bbbc95eb3ca4fd76429a  actions-runner-linux-x64-2.296.2.tar.gz" | shasum -a 256 -c 
+    echo "0bfd792196ce0ec6f1c65d2a9ad00215b2926ef2c416b8d97615265194477117  actions-runner-linux-x64-2.298.2.tar.gz" | shasum -a 256 -c 
 
     # Extract the installer
     tar xzf ./actions-runner-linux-x64-2.298.2.tar.gz
