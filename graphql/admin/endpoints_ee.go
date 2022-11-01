9e1ab9c29c2e3b569621e89db749f1d97f7285bc
Michel Diz <MichelDiz@users.noreply.github.com>
2022-11-01 17:05:14 -0300

    chore(docs): Add contextual information about ACL into GraphQL Doc. (#8393)
    ## Problem
    
    The user is not always aware that he can change the default password of
    a newly created namespace. Or sometimes he does, but doesn't realize
    he's entered a different password. This GraphQL documentation makes him
    aware.
    
    ## Solution
    
    Just adding more information in GraphQL Docs about groot password in the
    new namespace created.

--- graphql/admin/endpoints_ee.go
+++ graphql/admin/endpoints_ee.go
@@ -410,6 +410,9 @@ const adminTypes = `
 	}
 
 	input AddNamespaceInput {
+		"""
+		Enter a new password for groot in that namespace. If you leave it blank, the password will be the default.
+		"""
 		password: String
 	}
 
