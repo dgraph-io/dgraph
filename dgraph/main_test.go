e6a1d4bb78667f8a2a3895c5cf123fc743a31f69
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2022-11-18 15:17:51 -0800

    Create main_test.go as entrypoint for the test binary

---
+++ dgraph/main_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"flag"
+	"testing"
+)
+var systemTest *bool
+func init() {
+  systemTest = flag.Bool("systemTest", false, "Set to true when running system tests")
+}
+
+func TestSystem(t *testing.T) {
+  if *systemTest {
+     main()
+  }
+}
