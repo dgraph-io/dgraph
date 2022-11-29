875e2ac3c13b99111ffb58e8927c527c79a35a89
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2022-11-29 15:48:58 -0800

    Remove systemTest flag from main_test

--- dgraph/main_test.go
+++ dgraph/main_test.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"flag"
 	"testing"
 )
-var systemTest *bool
-func init() {
-  systemTest = flag.Bool("systemTest", false, "Set to true when running system tests")
-}
 
 func TestSystem(t *testing.T) {
-  if *systemTest {
-     main()
-  }
+  main()
 }
