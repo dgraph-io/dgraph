b7936a51954149bf9377fe547ccfbdc83f901c83
Joshua Goldstein <jgoldstein345@gmail.com>
2022-11-30 00:40:31 -0600

    fix query

--- query/query0_test.go
+++ query/query0_test.go
@@ -543,7 +543,7 @@ func TestCascadeWithSort(t *testing.T) {
 	}
 	`
 	js := processQueryNoErr(t, query)
-	require.JSONEq(t, `{"data":{"me":[{"name":"Andrea"},{"name":"Daryl Dixon"},{"name":"Glenn Rhee"},{"name":"King Lear"},{"name":"Leonard"},{"name":"Margaret"},{"name":"Rick Grimes"},{"alive":true}]}}`, js)
+	require.JSONEq(t, `{"data":{"me":[{"name": "Daryl Dixon","alive": false},{"name": "Rick Grimes","alive": true}]}}`, js)
 }
 
 // Resolved in PR #8441
@@ -557,7 +557,7 @@ func TestNegativeOffset(t *testing.T) {
 	}
 	`
 	js := processQueryNoErr(t, query)
-	require.JSONEq(t, `{"data":{"me":[{"name":"King Lear"},{"name":"Leonard"},{"name":"Margaret"},{"alive":true}]}}`, js)
+	require.JSONEq(t, `{"data":{"me":[{"name":"Andrea"},{"name":"Daryl Dixon"},{"name":"Glenn Rhee"},{"name":"King Lear"},{"name":"Leonard"},{"name":"Margaret"},{"name":"Rick Grimes"},{"alive":true}]}}`, js)
 }
 
 func TestLevelBasedFacetVarAggSum(t *testing.T) {
