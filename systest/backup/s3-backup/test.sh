5e0e5b4bb0ef65b17a84a4d7b0e23b35ae3012d7
SiddeshUbale <95419553+SiddeshUbale@users.noreply.github.com>
2022-12-06 20:09:26 +0530

    Update test.sh

--- systest/backup/s3-backup/test.sh
+++ systest/backup/s3-backup/test.sh
@@ -2,11 +2,11 @@
 #!/bin/sh
 pwd
 docker-compose --compatibility -f docker-compose.yml  up --force-recreate --build --remove-orphans --detach
-healthcheck.sh zero1_1 120
-healthcheck.sh zero2_1 120
-healthcheck.sh alpha1_1 120
-healthcheck.sh alpha2_1 120
-healthcheck.sh alpha3_1 120
-healthcheck.sh alpha4_1 120
-healthcheck.sh alpha5_1 120
-healthcheck.sh alpha6_1 120
+./healthCheck.sh zero1_1 120
+./healthCheck.sh zero2_1 120
+./healthCheck.sh alpha1_1 120
+./healthCheck.sh alpha2_1 120
+.healthCheck.sh alpha3_1 120
+./healthCheck.sh alpha4_1 120
+./healthCheck.sh alpha5_1 120
+./healthCheck.sh alpha6_1 120
