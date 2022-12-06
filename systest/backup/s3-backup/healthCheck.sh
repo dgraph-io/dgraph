bf1e1eaf406a9e5d282d22934e87788fe5af7cf1
SiddeshUbale <95419553+SiddeshUbale@users.noreply.github.com>
2022-12-06 15:13:13 +0530

    Added test scripts for debugging Github Actions

---
+++ systest/backup/s3-backup/healthCheck.sh
@@ -0,0 +1,28 @@
+container_name="$1"
+# Timeout in seconds. Default: 60
+timeout=$((${2:-60}));
+
+if [ -z $container_name ]; then
+  echo "No container name specified";
+  exit 1;
+fi
+
+echo "Container: $container_name";
+echo "Timeout: $timeout sec";
+
+try=0;
+is_healthy="false";
+while [ $is_healthy != "true" ];
+do
+  try=$(($try + 1));
+  printf "■";
+  is_healthy=$(docker inspect --format='{{json .State.Health}}' $container_name | jq '.Status == "healthy"');
+  echo " Container status "
+  echo "Container $container_name: $is_healthy"
+  sleep 1;
+  if [[ $try -eq $timeout ]]; then
+    echo " Container did not boot within timeout";
+    exit 1;
+  fi
+done
+
---
+++ systest/backup/s3-backup/test.sh
@@ -0,0 +1,10 @@
+# Create containers
+docker-compose --compatibility -f docker-compose.yml  up --force-recreate --build --remove-orphans --detach
+healthcheck.sh zero1_1 120
+healthcheck.sh zero2_1 120
+healthcheck.sh alpha1_1 120
+healthcheck.sh alpha2_1 120
+healthcheck.sh alpha3_1 120
+healthcheck.sh alpha4_1 120
+healthcheck.sh alpha5_1 120
+healthcheck.sh alpha6_1 120
