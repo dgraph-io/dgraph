27a159533415b377eaed4f79b214fe5e8e0c7c4f
Jaswinder Bhamra <jbhamra@dgraph.io>
2022-12-13 10:35:52 +0000

    Changes done for Aman and Bill's review comments.

--- edgraph/access_ee_test.go
+++ edgraph/access_ee_test.go
@@ -1,48 +1,26 @@
 package edgraph
 
 import (
-	"fmt"
 	"strings"
 	"testing"
 	"time"
 
 	"github.com/dgraph-io/dgraph/ee/acl"
 	"github.com/dgraph-io/dgraph/worker"
+
 	jwt "github.com/dgrijalva/jwt-go"
 )
 
-var (
-	userDataList = []userData{
-		{1234567890, "user1", []string{"701", "702"}},
-		{2345678901, "user2", []string{"703", "701"}},
-		{3456789012, "user3", []string{"702", "703"}},
-	}
-)
-
-func generateJWT(namespace uint64, userId string, groupIds []string, exp int64) (string, error) {
-	var token *jwt.Token
-
-	e1 := exp
-	if e1 == 0 {
-		e1 = time.Now().Add(time.Minute * 30).Unix()
-	}
-
-	claims := jwt.MapClaims{
-		"namespace": namespace,
-		"userid":    userId,
-		"exp":       e1,
-	}
+func generateJWT(namespace uint64, userId string, groupIds []string, expiry int64) string {
+	claims := jwt.MapClaims{"namespace": namespace, "userid": userId, "exp": expiry}
 	if groupIds != nil {
 			claims["groups"] = groupIds
 	}
-	token = jwt.NewWithClaims(jwt.SigningMethodHS256, &claims)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &claims)
 
-	tokenString, err := token.SignedString([]byte(worker.Config.HmacSecret))
-	if err != nil {
-		_ = fmt.Errorf("Something Went Wrong: %s", err.Error())
-		return "", err
-	}
-	return tokenString, nil
+	tokenString, _ := token.SignedString([]byte(worker.Config.HmacSecret))
+
+	return tokenString
 }
 
 func sliceCompare(x, y []string) bool {
@@ -50,8 +28,8 @@ func sliceCompare(x, y []string) bool {
 		return false
 	}
 
-	for i, s := range x {
-		if s != y[i] {
+	for i := range x {
+		if x[i] != y[i] {
 			return false
 		}
 	}
@@ -59,43 +37,35 @@ func sliceCompare(x, y []string) bool {
 	return true
 }
 
-func compareTokenParts(tok1 string, tok2 string, part ...int) bool {
-	var (
-		match bool = true
-	)
-
+func compareTokenParts(tok1 string, tok2 string, part []bool) bool {
 	t1 := strings.Split(tok1, ".")
 	t2 := strings.Split(tok2, ".")
 
-	for _, p := range part {
-		if p <= 0 {
+	for i := range part {
+		if !part[i] {
 			continue
 		}
 
-		if t1[p-1] != t2[p-1] {
-			match = false
-			break
+		if t1[i] != t2[i] {
+			return false
 		}
 	}
 
-	return match
+	return true
 }
 
 func TestValidateToken(t *testing.T) {
-	var (
-		err         error
-		tokenString string
-	)
+	expiry := time.Now().Add(time.Minute * 30).Unix()
+	userDataList := []userData{
+		{1234567890, "user1", []string{"701", "702"}},
+		{2345678901, "user2", []string{"703", "701"}},
+		{3456789012, "user3", []string{"702", "703"}},
+	}
 
 	for _, userdata := range userDataList {
-		if tokenString, err = generateJWT(userdata.namespace, userdata.userId, userdata.groupIds, 0); err != nil {
-			t.Errorf("JWT token string generation error: %s", err)
-		}
+		tokenString := generateJWT(userdata.namespace, userdata.userId, userdata.groupIds, expiry)
 
-		ud, err := validateToken(tokenString)
-		if err != nil {
-			t.Errorf("validateToken() error: %s", err)
-		}
+		ud, _ := validateToken(tokenString)
 		if ud.namespace != userdata.namespace || ud.userId != userdata.userId || !sliceCompare(ud.groupIds, userdata.groupIds) {
 			t.Errorf("Actual output %+v is not equal to the expected output %+v", userdata, ud)
 		}
@@ -103,11 +73,6 @@ func TestValidateToken(t *testing.T) {
 }
 
 func TestGetAccessJwt(t *testing.T) {
-	var (
-		tokstr string
-		jwtstr string
-		exp    int64 = time.Now().Add(worker.Config.AccessJwtTtl).Unix()
-	)
 
 	grpLst := []acl.Group{
 		{
@@ -130,41 +95,53 @@ func TestGetAccessJwt(t *testing.T) {
 		},
 	}
 
+	expiry := time.Now().Add(worker.Config.AccessJwtTtl).Unix()
+	userDataList := []userData{
+		{1234567890, "user1", []string{"701", "702"}},
+		{2345678901, "user2", []string{"703", "701"}},
+		{3456789012, "user3", []string{"702", "703"}},
+	}
+
 	for _, userdata := range userDataList {
 		g := acl.GetGroupIDs(grpLst)
-		tokstr, _ = generateJWT(userdata.namespace, userdata.userId, g, exp)
-		jwtstr, _ = getAccessJwt(userdata.userId, grpLst, userdata.namespace)
+		tokstr := generateJWT(userdata.namespace, userdata.userId, g, expiry)
+		jwtstr, _ := getAccessJwt(userdata.userId, grpLst, userdata.namespace)
 
 		/*
 		 * JWT token format = [header].[payload].[signature].
 		 * One of the factor [signature] would differ is based on the expiry time.
 		 * This will differ for tokstr and jwtstr and hence ignoring it.
 		 */
-		match := compareTokenParts(tokstr, jwtstr, 1, 2, 0) // compare 1st and 2nd part, ignore the 3rd.
-		if match == false {
+
+		// compare 1st and 2nd part, ignore the 3rd.
+		testTokParts := []bool{true, true, false}
+		if !compareTokenParts(tokstr, jwtstr, testTokParts) {
 			t.Errorf("Actual output is not equal to the expected output")
 		}
 	}
 }
 
 func TestGetRefreshJwt(t *testing.T) {
-	var (
-		tokstr string
-		jwtstr string
-		exp    int64 = time.Now().Add(worker.Config.RefreshJwtTtl).Unix()
-	)
+	expiry := time.Now().Add(worker.Config.RefreshJwtTtl).Unix()
+	userDataList := []userData{
+		{1234567890, "user1", []string{"701", "702"}},
+		{2345678901, "user2", []string{"703", "701"}},
+		{3456789012, "user3", []string{"702", "703"}},
+	}
 
 	for _, userdata := range userDataList {
-		tokstr, _ = generateJWT(userdata.namespace, userdata.userId, nil, exp)
-		jwtstr, _ = getRefreshJwt(userdata.userId, userdata.namespace)
+		tokstr := generateJWT(userdata.namespace, userdata.userId, nil, expiry)
+		jwtstr, _ := getRefreshJwt(userdata.userId, userdata.namespace)
 
 		/*
 		 * JWT token format = [header].[payload].[signature]
 		 * One of the factor [signature] would differ is based on the expiry time.
 		 * This will differ for tokstr and jwtstr and hence ignoring it.
 		 */
-		match := compareTokenParts(tokstr, jwtstr, 1, 2, 0) // compare 1st and 2nd part, ignore the 3rd.
-		if match == false {
+
+		// compare 1st and 2nd part, ignore the 3rd.
+		testTokParts := []bool{true, true, false}
+		if !compareTokenParts(tokstr, jwtstr, testTokParts) {
 			t.Errorf("Actual output is not equal to the expected output")
 		}
 	}
