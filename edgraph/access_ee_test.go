e422fca87a8c76b1e3aaf9a75116b2b2aef5d1f8
Jaswinder Bhamra <jbhamra@dgraph.io>
2022-12-13 17:00:07 +0000

    Changes added for Aman's comment.

--- edgraph/access_ee_test.go
+++ edgraph/access_ee_test.go
@@ -78,7 +78,6 @@ func TestGetAccessJwt(t *testing.T) {
 	}
 
 	g := acl.GetGroupIDs(grpLst)
-	expiry := time.Now().Add(worker.Config.AccessJwtTtl).Unix()
 	userDataList := []userData{
 		{1234567890, "user1", []string{"701", "702"}},
 		{2345678901, "user2", []string{"703", "701"}},
@@ -86,19 +85,16 @@ func TestGetAccessJwt(t *testing.T) {
 	}
 
 	for _, userdata := range userDataList {
-		tokstr := generateJWT(userdata.namespace, userdata.userId, g, expiry)
-		ud1, _ := validateToken (tokstr)
 		jwtstr, _ := getAccessJwt(userdata.userId, grpLst, userdata.namespace)
-		ud2, _ := validateToken (jwtstr)
+		ud, _ := validateToken (jwtstr)
 
-		if ud1.namespace != ud2.namespace || ud1.userId != ud2.userId || !sliceCompare(ud1.groupIds, ud2.groupIds) {
-			t.Errorf("Generated jwt output %+v is not equal to the access jwt output %+v", ud1, ud2)
+		if ud.namespace != userdata.namespace || ud.userId != userdata.userId || !sliceCompare(ud.groupIds, g) {
+			t.Errorf("Actual output {%v %v %v} is not equal to the output %v generated from getAccessJwt() token", userdata.namespace, userdata.userId, grpLst, ud)
 		}
 	}
 }
 
 func TestGetRefreshJwt(t *testing.T) {
-	expiry := time.Now().Add(worker.Config.RefreshJwtTtl).Unix()
 	userDataList := []userData{
 		{1234567890, "user1", []string{"701", "702"}},
 		{2345678901, "user2", []string{"703", "701"}},
@@ -106,13 +102,11 @@ func TestGetRefreshJwt(t *testing.T) {
 	}
 
 	for _, userdata := range userDataList {
-		tokstr := generateJWT(userdata.namespace, userdata.userId, nil, expiry)
-		ud1, _ := validateToken (tokstr)
 		jwtstr, _ := getRefreshJwt(userdata.userId, userdata.namespace)
-		ud2, _ := validateToken (jwtstr)
+		ud, _ := validateToken (jwtstr)
 
-		if ud1.namespace != ud2.namespace || ud1.userId != ud2.userId || !sliceCompare(ud1.groupIds, ud2.groupIds) {
-			t.Errorf("Generated jwt output %+v is not equal to the refresh jwt output %+v", ud1, ud2)
+		if ud.namespace != userdata.namespace || ud.userId != userdata.userId {
+			t.Errorf("Actual output {%v %v} is not equal to the output {%v %v} generated from getRefreshJwt() token", userdata.namespace, userdata.userId, ud.namespace, ud.userId)
 		}
 	}
 }
