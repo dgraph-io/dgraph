4c8752344fd0f9878d74e38d618d2673f2ca25c3
Anurag <anurags92@gmail.com>
2023-02-15 22:04:16 +0530

    copyright

--- contrib/scripts/license.sh
+++ contrib/scripts/license.sh
@@ -7,7 +7,7 @@ for f in $files; do
   # Start from year.
   year=$(git log --format=%aD $f | tail -1 | awk '{print $4}')
   if [ "$year" != "2018" ]; then
-    sed -i "s/Copyright 2023 Dgraph/Copyright $year-2022 Dgraph/g" $f
+    sed -i "s/Copyright 2023 Dgraph/Copyright $year-2023 Dgraph/g" $f
   fi
 
   # Format it.
