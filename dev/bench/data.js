acc5e670589fc1830748d6903ab932fef8800ded
github-action-benchmark <github@users.noreply.github.com>
2023-02-22 11:38:40 +0000

    add Go Benchmark (go) benchmark result for 70d8bc55bdeb26920739c4d0b496eef0d59d7093

---
+++ dev/bench/data.js
@@ -0,0 +1,34 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1677065920622,
+  "repoUrl": "https://github.com/dgraph-io/dgraph",
+  "entries": {
+    "Go Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "dgraph-io",
+            "username": "dgraph-io"
+          },
+          "committer": {
+            "name": "dgraph-io",
+            "username": "dgraph-io"
+          },
+          "id": "70d8bc55bdeb26920739c4d0b496eef0d59d7093",
+          "message": "Add benchmark code",
+          "timestamp": "2023-02-22T04:39:33Z",
+          "url": "https://github.com/dgraph-io/dgraph/pull/8694/commits/70d8bc55bdeb26920739c4d0b496eef0d59d7093"
+        },
+        "date": 1677065920094,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkBulkload",
+            "value": 40230584,
+            "unit": "ns/op\t       2 B/op\t       0 allocs/op",
+            "extra": "28 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
