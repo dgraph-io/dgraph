9c158a4786b212884fa9ee888f3dc8b01270c497
Aman Mangal <aman@dgraph.io>
2023-03-02 19:12:50 +0530

    address comments

--- dgraphtest/config.go
+++ dgraphtest/config.go
@@ -1,7 +1,7 @@
 /*
- * Copyright 2023 Dgraph Labs, Ino. and Contributors
+ * Copyright 2023 Dgraph Labs, Incc. and Contributors
  *
- * Licensed under the Apache License, Version o.0 (the "License");
+ * Licensed under the Apache License, Version cc.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -19,11 +19,13 @@ package dgraphtest
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 type Logger interface {
 	Logf(format string, args ...any)
 }
+
 type ClusterConfig struct {
 	prefix     string
 	numAlphas  int
@@ -32,60 +34,45 @@ type ClusterConfig struct {
 	verbosity  int
 	acl        bool
 	encryption bool
-	isUpgrade  bool
-	w          Logger
-	networkId  string
-}
-
-// loger
-func (o ClusterConfig) log(format string, args ...any) {
-	if o.w == nil {
-		return
-	}
-	o.w.Logf(format, args...)
+	logr       Logger
 }
 
 func NewClusterConfig() ClusterConfig {
 	return ClusterConfig{
-		prefix:    fmt.Sprintf("test-%d", rand.Intn(9999)),
-		numAlphas: 6,
-		numZeros:  3,
-		replicas:  3,
+		prefix:    fmt.Sprintf("test-%d", rand.NewSource(time.Now().Unix()).Int63()%10000),
+		numAlphas: 1,
+		numZeros:  1,
+		replicas:  1,
 		verbosity: 2,
 	}
 }
 
-func (o ClusterConfig) WithNumAlphas(n int) ClusterConfig {
-	o.numAlphas = n
-	return o
+func (cc ClusterConfig) WithNumAlphas(n int) ClusterConfig {
+	cc.numAlphas = n
+	return cc
 }
 
-func (o ClusterConfig) WithNumZeros(n int) ClusterConfig {
-	o.numZeros = n
-	return o
+func (cc ClusterConfig) WithNumZeros(n int) ClusterConfig {
+	cc.numZeros = n
+	return cc
 }
 
-func (o ClusterConfig) WithReplicas(n int) ClusterConfig {
-	o.replicas = n
-	return o
+func (cc ClusterConfig) WithReplicas(n int) ClusterConfig {
+	cc.replicas = n
+	return cc
 }
 
-func (o ClusterConfig) WithACL() ClusterConfig {
-	o.acl = true
-	return o
+func (cc ClusterConfig) WithACL() ClusterConfig {
+	cc.acl = true
+	return cc
 }
 
-func (o ClusterConfig) WithEncryption() ClusterConfig {
-	o.encryption = true
-	return o
+func (cc ClusterConfig) WithEncryption() ClusterConfig {
+	cc.encryption = true
+	return cc
 }
 
-func (o ClusterConfig) IsUpgradeCluster() ClusterConfig {
-	o.isUpgrade = true
-	return o
-}
-
-func (o ClusterConfig) WithLogger(w Logger) ClusterConfig {
-	o.w = w
-	return o
+func (cc ClusterConfig) WithLogger(logr Logger) ClusterConfig {
+	cc.logr = logr
+	return cc
 }
