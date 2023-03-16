d3bab0507a042c0cf6f94c39e64516ae82d94ab0
Anurag <anurags92@gmail.com>
2023-03-16 14:29:36 +0530

    Add a sample benchmark

--- dgraphtest/bench_test.go
+++ dgraphtest/bench_test.go
@@ -1,11 +1,52 @@
 package dgraphtest
 
 import (
+	"fmt"
 	"os"
+	"os/exec"
+	"strconv"
 	"testing"
+
+	"github.com/dgraph-io/dgraph/testutil"
 )
 
 var perfTests map[string]DgraphPerf
+
+func BenchmarkBulkoad(b *testing.B) {
+	var args []string
+	var schemaFile string
+	var rdfFile string
+	var res []byte
+	args = append(args, "bulk",
+		"-f", rdfFile,
+		"-s", schemaFile,
+		"--http", "localhost:8000",
+		"--reduce_shards=1",
+		"--map_shards=1",
+		"--store_xids=true",
+		"--zero", testutil.SockAddrZero,
+		"--force-namespace", strconv.FormatUint(0, 10))
+
+	bulkCmd := exec.Command(testutil.DgraphBinaryPath(), args...)
+	fmt.Printf("Running %s\n", bulkCmd)
+
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		if err := testutil.MakeDirEmpty([]string{"out/0"}); err != nil {
+			os.Exit(1)
+		}
+		bulkCmd = exec.Command(testutil.DgraphBinaryPath(), args...)
+		b.StartTimer()
+		out, err := bulkCmd.CombinedOutput()
+		b.StopTimer()
+		if err != nil {
+			b.Fatal(err)
+		}
+		res = out
+	}
+	fmt.Printf("Finished bulk load. Output \n%s\n", res)
+
+}
 
 func BenchmarkXYZ(b *testing.B) {
 	for k, v := range PerfTests {
@@ -15,6 +56,7 @@ func BenchmarkXYZ(b *testing.B) {
 }
 
 func TestMain(m *testing.M) {
+	perfTests["bulkload"] = DgraphPerf{"bulkload", ClusterConfig{}, MetricConfig{}, ResourceConfig{}, BenchmarkBulkoad}
 
 	// check env variable and run only that benchmark
 	if bench := os.Getenv("TEST_TO_RUN"); bench == "" {
