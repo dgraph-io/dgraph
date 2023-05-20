e68950e30d3cc5c44350640efadfae08c2da7f06
rderbier <rderbier@gmail.com>
2023-05-19 17:03:53 -0700

    Add a lambda-acl target
    Create a docker image with lambda server and acl enabled.
    Add a sample lambda files with test functions.

--- contrib/local-test/scripts/script.js
+++ contrib/local-test/scripts/script.js
@@ -1,1 +1,29 @@
-// Add lambda scripts here
+ /*
+ Test functions to use with following Schema
+ type Query {
+  dqlquery(query: String): String @lambda
+  gqlquery(query: String): String @lambda
+  dqlmutate(query: String): String @lambda
+}
+
+*/
+ const dqlquery = async ({args, authHeader, graphql, dql}) => {
+  
+    const dqlQ = await dql.query(`${args.query}`)
+    return  JSON.stringify(dqlQ)
+  }
+  const gqlquery = async ({args, authHeader, graphql, dql}) => {
+    const gqlQ = await graphql(`${args.query}`)
+    return  JSON.stringify(gqlQ)
+  }
+  const dqlmutation = async ({args, authHeader, graphql, dql}) => {
+     // Mutate User with DQL
+    const dqlM = await dql.mutate(`${args.query}`)
+    return  JSON.stringify(dqlM)
+  }
+  
+  self.addGraphQLResolvers({
+    "Query.dqlquery": dqlquery,
+    "Query.gqlquery": gqlquery,
+    "Query.dqlmutate": dqlmutation,
+  });
