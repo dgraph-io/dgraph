b048a52450afd98b9b1b529dbb4032d48572ac26
Aman Mangal <aman@dgraph.io>
2023-06-14 08:09:28 +0530

    ci(dql): add workflow for fuzz testing

---
+++ dql/parser_fuzz_test.go
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2017-2023 Dgraph Labs, Inc. and Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dql
+
+import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
+	"io"
+	"os"
+	"testing"
+)
+
+const (
+	corpusTarFile = "fuzz-data/corpus.tar.gz"
+)
+
+func FuzzTestParser(f *testing.F) {
+	// add the corpus data to the test
+	fd, err := os.Open(corpusTarFile)
+	if err != nil {
+		f.Fatalf("error opening corpus tar file: %v", err)
+	}
+	defer func() {
+		if err := fd.Close(); err != nil {
+			f.Logf("error closing corpus tar file: %v", err)
+		}
+	}()
+
+	gzr, err := gzip.NewReader(fd)
+	if err != nil {
+		f.Fatalf("error reading corpus tar file: %v", err)
+	}
+	defer func() {
+		if err := gzr.Close(); err != nil {
+			f.Logf("error closing corpus tar file: %v", err)
+		}
+	}()
+	tr := tar.NewReader(gzr)
+
+	for {
+		header, err := tr.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			f.Fatalf("error while reading corpus tar file: %v", err)
+		}
+
+		switch header.Typeflag {
+		case tar.TypeReg:
+			buf := new(bytes.Buffer)
+			if _, err := io.Copy(buf, tr); err != nil {
+				f.Fatalf("error while copying file [%v]: %v", header.Name, err)
+			}
+			f.Add(buf.Bytes())
+			f.Logf("adding input: %v", buf.String())
+		}
+	}
+
+	f.Fuzz(func(t *testing.T, in []byte) {
+		defer func() {
+			if err := recover(); err != nil {
+				t.Errorf("DQL parsing failed for input [%x] with error: [%v]", in, err)
+			}
+		}()
+
+		_, _ = Parse(Request{Str: string(in)})
+	})
+}
--- go.mod
+++ go.mod
@@ -56,11 +56,11 @@ require (
 	go.uber.org/zap v1.16.0
 	golang.org/x/crypto v0.9.0
 	golang.org/x/net v0.10.0
-	golang.org/x/sync v0.1.0
-	golang.org/x/sys v0.8.0
+	golang.org/x/sync v0.2.0
+	golang.org/x/sys v0.9.0
 	golang.org/x/term v0.8.0
 	golang.org/x/text v0.9.0
-	golang.org/x/tools v0.6.0
+	golang.org/x/tools v0.9.3
 	google.golang.org/grpc v1.52.0
 	gopkg.in/square/go-jose.v2 v2.3.1
 	gopkg.in/yaml.v2 v2.4.0
@@ -124,7 +124,7 @@ require (
 	github.com/xdg/stringprep v1.0.0 // indirect
 	go.uber.org/atomic v1.6.0 // indirect
 	go.uber.org/multierr v1.5.0 // indirect
-	golang.org/x/mod v0.8.0 // indirect
+	golang.org/x/mod v0.10.0 // indirect
 	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
 	google.golang.org/api v0.30.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
