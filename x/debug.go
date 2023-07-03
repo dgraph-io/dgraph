81ba0c548b6e8677202bc5dd0e913abdbb1adde7
Harshil Goel <harshil@dgraph.io>
2023-07-03 13:37:25 +0530

    fixed non working log

--- x/debug.go
+++ x/debug.go
@@ -27,21 +27,20 @@ import (
 	"github.com/dgraph-io/badger/v4"
 	bpb "github.com/dgraph-io/badger/v4/pb"
 	"github.com/dgraph-io/dgraph/protos/pb"
-	"github.com/golang/glog"
 )
 
 func PrintRollup(plist *pb.PostingList, parts map[uint64]*pb.PostingList, baseKey []byte, ts uint64) {
 	k, _ := Parse(baseKey)
-	glog.V(1).Infof("[TXNLOG] DOING ROLLUP for key: %+v at timestamp: %v\n", k, ts)
+	log.Printf("[TXNLOG] DOING ROLLUP for key: %+v at timestamp: %v\n", k, ts)
 }
 
 func PrintMutationEdge(plist *pb.DirectedEdge, key ParsedKey, startTs uint64) {
-	glog.V(1).Infof("[TXNLOG] ADDING MUTATION at TS: %v, key: %v, value: %v \n", startTs, key.String(), plist)
+	log.Printf("[TXNLOG] ADDING MUTATION at TS: %v, key: %v, value: %v \n", startTs, key.String(), plist)
 }
 
 func PrintOracleDelta(delta *pb.OracleDelta) {
 	for _, status := range delta.Txns {
-		glog.V(1).Infof("[TXNLOG] COMMITING: startTs: %v, commitTs: %v\n", status.StartTs, status.CommitTs)
+		log.Printf("[TXNLOG] COMMITING: startTs: %v, commitTs: %v\n", status.StartTs, status.CommitTs)
 	}
 }
 
