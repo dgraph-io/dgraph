0cea60dbe9e248d203c82b91ccd8f4d3ecb7bcbc
wmillers <31211860+wmillers@users.noreply.github.com>
2023-07-13 09:04:16 +0800

    chore: fix compilation on 32bit (#8895)

--- worker/queue.go
+++ worker/queue.go
@@ -302,7 +302,7 @@ func (t *tasks) cleanup() {
 func (t *tasks) newId() uint64 {
 	myRaftId := State.WALstore.Uint(raftwal.RaftId)
 	for {
-		id := myRaftId<<32 | uint64(t.rng.Intn(math.MaxUint32))
+		id := myRaftId<<32 | uint64(t.rng.Uint32())
 		// z.Tree cannot store 0 or math.MaxUint64. Check that id is unique.
 		if id != 0 && id != math.MaxUint64 && t.log.Get(id) == 0 {
 			return id
