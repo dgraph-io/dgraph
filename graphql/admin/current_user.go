16209e14ea9ed55eab64fe94feae45aa871be080
Michel Diz <michelconrado@outlook.com>
2023-08-18 00:59:47 -0300

    undo current_user

--- graphql/admin/current_user.go
+++ graphql/admin/current_user.go
@@ -39,15 +39,15 @@ func extractName(ctx context.Context) (string, error) {
 }
 
 func (gsr *currentUserResolver) Rewrite(ctx context.Context,
-	DQLQuery schema.Query) ([]*dql.GraphQuery, error) {
+	gqlQuery schema.Query) ([]*dql.GraphQuery, error) {
 
 	name, err := extractName(ctx)
 	if err != nil {
 		return nil, err
 	}
 
-	DQLQuery.Rename("getUser")
-	DQLQuery.SetArgTo("name", name)
+	gqlQuery.Rename("getUser")
+	gqlQuery.SetArgTo("name", name)
 
-	return gsr.baseRewriter.Rewrite(ctx, DQLQuery)
+	return gsr.baseRewriter.Rewrite(ctx, gqlQuery)
 }
