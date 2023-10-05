681333636b3133e787b6a5a3378506efe9fccb01
rderbier <rderbier@gmail.com>
2023-10-05 11:46:03 -0700

    Update handler.py

--- contrib/embeddings/aws-lambda-transformer/handler.py
+++ contrib/embeddings/aws-lambda-transformer/handler.py
@@ -5,11 +5,11 @@ from sentence_transformers import SentenceTransformer
 # change CACHE else we get following error at runtime in AWS Lambda:
 # There was a problem when trying to write in your cache folder (/home/sbx_user1051/.cache/huggingface/hub).
 os.environ['TRANSFORMERS_CACHE'] = '/var/task'
-
+model = SentenceTransformer('/var/task/model')
 def handler(event, context):
     body = json.loads(event['body'])
     sentences = [body[key] for key in body]
-    model = SentenceTransformer('/var/task/model')
+    
     embeddings = model.encode(sentences)
     keylist = list(body.keys())
     resp = {keylist[i]:json.dumps(embeddings[i].tolist()) for i in range(len(embeddings))}
