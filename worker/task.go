8375b71426ef9d5763a5ae0936b5d0c091c2e1fe
Harshil Goel <harshil@hypermode.com>
2024-04-05 22:49:35 +0530

    fixed lint

--- worker/task.go
+++ worker/task.go
@@ -1791,7 +1791,8 @@ func parseSrcFn(ctx context.Context, q *pb.Query) (*functionContext, error) {
 		case q.UidList != nil && len(fc.tokens) > len(q.UidList.Uids) && fc.fname != eq:
 			fc.tokens = fc.tokens[:0]
 			fc.n = len(q.UidList.Uids)
-		case q.UidList != nil && fc.fname == eq && strings.HasSuffix(attr, "dgraph.type") && int64(len(q.UidList.Uids)) < Config.TypeFilterUidLimit:
+		case q.UidList != nil && fc.fname == eq && strings.HasSuffix(attr, "dgraph.type") &&
+			int64(len(q.UidList.Uids)) < Config.TypeFilterUidLimit:
 			fc.tokens = fc.tokens[:0]
 			fc.n = len(q.UidList.Uids)
 		default:
