4395e5e444e30996f497cd42a6c50fc4a4d4f2e0
Harshil Goel <harshil@hypermode.com>
2024-07-05 18:31:51 +0530

    fixed lint

--- tok/index/helper_test.go
+++ tok/index/helper_test.go
@@ -25,7 +25,7 @@ import (
 	c "github.com/dgraph-io/dgraph/tok/constraints"
 )
 
-func pointerFloatConversion1[T c.Float](encoded []byte, retVal *[]T, floatBits int) {
+func pointerFloatConversion[T c.Float](encoded []byte, retVal *[]T, floatBits int) {
 	floatBytes := floatBits / 8
 
 	// Ensure the byte slice length is a multiple of 8 (size of float64)
@@ -38,47 +38,72 @@ func pointerFloatConversion1[T c.Float](encoded []byte, retVal *[]T, floatBits i
 	*retVal = *(*[]T)(unsafe.Pointer(&encoded))
 }
 
-func pointerFloatConversion[T c.Float](encoded []byte, floatBits int) *[]T {
+func littleEndianBytesAsFloatArray[T c.Float](encoded []byte, retVal *[]T, floatBits int) {
+	// Unfortunately, this is not as simple as casting the result,
+	// and it is also not possible to directly use the
+	// golang "unsafe" library to directly do the conversion.
+	// The machine where this operation gets run might prefer
+	// BigEndian/LittleEndian, but the machine that sent it may have
+	// preferred the other, and there is no way to tell!
+	//
+	// The solution below, unfortunately, requires another memory
+	// allocation.
+	// TODO Potential optimization: If we detect that current machine is
+	// using LittleEndian format, there might be a way of making this
+	// work with the golang "unsafe" library.
 	floatBytes := floatBits / 8
 
 	// Ensure the byte slice length is a multiple of 8 (size of float64)
 	if len(encoded)%floatBytes != 0 {
 		fmt.Println("Invalid byte slice length")
-		return nil
+		return
 	}
 
-	// Create a slice header
-	header := unsafe.Pointer(&encoded)
-	return (*[]T)(header)
+	*retVal = (*retVal)[:0]
+	resultLen := len(encoded) / floatBytes
+	if resultLen == 0 {
+		return
+	}
+	for i := 0; i < resultLen; i++ {
+		// Assume LittleEndian for encoding since this is
+		// the assumption elsewhere when reading from client.
+		// See dgraph-io/dgo/protos/api.pb.go
+		// See also dgraph-io/dgraph/types/conversion.go
+		// This also seems to be the preference from many examples
+		// I have found via Google search. It's unclear why this
+		// should be a preference.
+		if retVal == nil {
+			retVal = &[]T{}
+		}
+		*retVal = append(*retVal, BytesToFloat[T](encoded, floatBits))
+
+		encoded = encoded[(floatBytes):]
+	}
 }
 
 func BenchmarkFloatConverstion(b *testing.B) {
-	num := 1400
+	num := 1500
 	data := make([]byte, 64*num)
-	rand.Read(data)
+	_, err := rand.Read(data)
+	if err != nil {
+		b.Skip()
+	}
 
 	b.Run(fmt.Sprintf("pointerFloat:size=%d", len(data)),
 		func(b *testing.B) {
 
 			temp := make([]float64, num)
 			for k := 0; k < b.N; k++ {
-				pointerFloatConversion1[float64](data, &temp, 64)
+				pointerFloatConversion[float64](data, &temp, 64)
 			}
 
 		})
-	b.Run(fmt.Sprintf("normalFloat:size=%d", len(data)),
+
+	b.Run(fmt.Sprintf("littleEndianFloat:size=%d", len(data)),
 		func(b *testing.B) {
 			temp := make([]float64, num)
-			temp1 := make([]float64, num)
 			for k := 0; k < b.N; k++ {
-				pointerFloatConversion1[float64](data, &temp1, 64)
-				BytesAsFloatArray[float64](data, &temp, 64)
-
-				for i := 0; i < num; i++ {
-					if temp[i] != temp1[i] {
-						fmt.Println("diff", temp, temp1)
-					}
-				}
+				littleEndianBytesAsFloatArray[float64](data, &temp, 64)
 			}
 
 		})
