962efe4308dce4e51a3d80d1226ded86a67db72b
Harshil Goel <harshil@hypermode.com>
2024-12-06 12:29:11 +0530

    removed skull

--- posting/mvcc_test.go
+++ posting/mvcc_test.go
@@ -28,7 +28,7 @@ import (
 
 	"github.com/dgraph-io/dgraph/v24/protos/pb"
 	"github.com/dgraph-io/dgraph/v24/x"
-	"github.com/dgraph-io/ristretto"
+	"github.com/dgraph-io/ristretto/v2"
 	"github.com/dgraph-io/ristretto/v2/z"
 )
 
